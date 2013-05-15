45e29b5aef427f713b19a9d657c963905a6ad2b8
Malte Hübner <maltehuebner@gmx.org>
2013-05-16 00:12:00 +0200

    Controller angepasst, Beispiel entfernt, Objekt wird aus der Datenbank ausgelesen.

--- symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('CalderaCriticalmassBundle:Default:index.html.twig', array('name' => $name));
-    }
+		public function showcityAction($city)
+		{
+			$city = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:City')->findOneByCity($city);
+
+			return $this->render('CalderaCriticalmassBundle:Default:index.html.twig', array('city' => $city));
+		}
 }
