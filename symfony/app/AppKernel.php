d7a53f92fce28c6287a343e7524488425c0963eb
Malte Hübner <maltehuebner@gmx.org>
2013-05-15 22:26:40 +0200

    Zugriffsrechte manipuliert.

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Caldera\CriticalmassBundle\CalderaCriticalmassBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
