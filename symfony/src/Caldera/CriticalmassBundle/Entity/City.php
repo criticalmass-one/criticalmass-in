9697a797802b7c4ae6172088b9a66c722c1aaabf
Malte Hübner <maltehuebner@gmx.org>
2013-05-15 23:29:10 +0200

    Mapping-Annotationen eingefügt.

--- symfony/src/Caldera/CriticalmassBundle/Entity/City.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/City.php
@@ -2,11 +2,43 @@
 
 namespace Caldera\CriticalmassBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="city")
+ */
 class City
 {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+  protected $id;
+
+	/**
+	 * @ORM\Column(type="string", length=50)
+	 */
 	protected $name;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
 	protected $title;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
 	protected $url;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
 	protected $facebook;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
 	protected $twitter;
 }
