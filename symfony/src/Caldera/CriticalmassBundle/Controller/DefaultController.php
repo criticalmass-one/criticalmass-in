89e3e0dafdf0e1f71d86cc0656bad9489ec95ea8
Malte Hübner <maltehuebner@gmx.org>
2013-05-16 14:10:18 +0200

    404-Fehlermeldung beim Versuch, nicht vorhandene Stadt zu öffnen, eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
@@ -6,10 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-		public function showcityAction($city)
-		{
-			$city = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:City')->findOneByCity($city);
+	public function showcityAction($city)
+	{
+		$city = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:City')->findOneByCity($city);
 
+		if (empty($city))
+		{
+			throw $this->createNotFoundException('This city does not exist');
+		}
+		else
+		{
 			return $this->render('CalderaCriticalmassBundle:Default:index.html.twig', array('city' => $city));
 		}
+	}
 }
