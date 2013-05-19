b8132bd5e0c1b975a885e04963813274319595e5
Malte Hübner <maltehuebner@gmx.org>
2013-05-19 13:59:25 +0200

    Klassenname LoadCity zu LoadCityData korrigiert.

--- symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCityData.php
+++ symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCityData.php
@@ -7,7 +7,7 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Caldera\CriticalmassBundle\Entity\City;
 
-class LoadCity extends AbstractFixture implements OrderedFixtureInterface
+class LoadCityData extends AbstractFixture implements OrderedFixtureInterface
 {
 	/**
 	 * {@inheritDoc}
