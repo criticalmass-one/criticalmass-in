a7b23c5cb1315b4cbff2d7f544406af6d0ddb214
Malte Hübner <maltehuebner@gmx.org>
2013-05-21 10:57:59 +0200

    Controller kommentiert.

--- symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
@@ -6,16 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	/**
+	 * Lädt die angegebene Stadt aus der Datenbank und reicht sie an das Template zur Anzeige weiter.
+	 *
+	 * @param $city Name der Stadt
+	 */
 	public function showcityAction($city)
 	{
+		// Stadt anhand des übergebenen Parameters laden
 		$city = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:City')->findOneByCity($city);
 
+		// wurde die Stadt überhaupt gefunden?
 		if (empty($city))
 		{
+			// Fehlermeldung werfen
 			throw $this->createNotFoundException('This city does not exist');
 		}
 		else
 		{
+			// Darstellung an das Template weiterreichen
 			return $this->render('CalderaCriticalmassBundle:Default:index.html.twig', array('city' => $city));
 		}
 	}
