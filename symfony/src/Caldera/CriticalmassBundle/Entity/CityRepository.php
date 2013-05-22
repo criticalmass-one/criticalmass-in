77097ff003f33b43543c04791c0620a9e4f71551
Malte Hübner <maltehuebner@gmx.org>
2013-05-22 15:59:01 +0200

    Methode findNearestedByLocation() in das CityRepository hinzugefügt.

--- symfony/src/Caldera/CriticalmassBundle/Entity/CityRepository.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/CityRepository.php
@@ -6,5 +6,18 @@ use Doctrine\ORM\EntityRepository;
 
 class CityRepository extends EntityRepository
 {
+	/**
+	 * Findet abhängig vom übergebenen Breiten- und Längengrad die nächsten
+	 * Städte in der Reihenfolge ihrer Entfernung.
+	 *
+	 * @param latitude Breitengrad
+	 * @param longitude Längengrad
+	 */
+	public function findNearestedByLocation($latitude, $longitude)
+	{
+		$cities = $this->getEntityManager()->createQuery("SELECT c AS city, SQRT((c.latitude - ".$latitude.") * (c.latitude - ".$latitude.") + (c.longitude - ".$longitude.") * (c.longitude - ".$longitude.")) AS distance FROM CalderaCriticalmassBundle:City c ORDER BY distance ASC")->getResult();
+
+		return $cities;
+	}
 }
 
