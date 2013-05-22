67e97de8dc0320d11252fefcbff25df2af3c80ce
Malte Hübner <maltehuebner@gmx.org>
2013-05-22 14:10:55 +0200

    Action um das Template erweitert.

--- symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
 	public function selectcityAction()
 	{
-
+		return $this->render('CalderaCriticalmassBundle:Default:selectcity.html.twig');
 	}
 
 	/**
