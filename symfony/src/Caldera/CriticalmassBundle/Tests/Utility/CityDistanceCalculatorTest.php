380b090d35a3aa3f7c67f92043bcc55a42454f6b
Malte Hübner <maltehuebner@gmx.org>
2013-05-23 23:26:25 +0200

    Zweiten Testfall mit Berechnung der Entfernung in Kilometern in den Test eingefügt.

--- symfony/src/Caldera/CriticalmassBundle/Tests/Utility/CityDistanceCalculatorTest.php
+++ symfony/src/Caldera/CriticalmassBundle/Tests/Utility/CityDistanceCalculatorTest.php
@@ -24,4 +24,22 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertEquals(1.2774847898, $result);
 	}
+
+	public function testCalculateKilometreDistanceFromCityToCity()
+	{
+		$cdc = new Utility\CityDistanceCalculator();
+
+		$hamburg = new Entity\City();
+		$bremen = new Entity\City();
+
+		$hamburg->setLatitude(53.550556);
+		$hamburg->setLongitude(9.993333);
+
+		$bremen->setLatitude(53.075878);
+		$bremen->setLongitude(8.807311);
+
+		$result = $cdc->calculateKilometreDistanceFromCityToCity($hamburg, $bremen);
+
+		$this->assertEquals(95.8113592349, $result);
+	}
 }
