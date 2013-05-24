302efc1aadd45a2f68d0f86952fbad7f58a82e34
Malte Hübner <maltehuebner@gmx.org>
2013-05-24 14:32:21 +0200

    Schöneren Aufruf als über das Repository für das Laden einer Fahrt verwendet.

--- symfony/src/Caldera/CriticalmassBundle/Entity/RideRepository.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/RideRepository.php
@@ -6,9 +6,4 @@ use Doctrine\ORM\EntityRepository;
 
 class RideRepository extends EntityRepository
 {
-	public function findLatestByCity(City $city)
-	{
-		$ride = $this->getEntityManager()->createQuery("SELECT r FROM CalderaCriticalmassBundle:Ride r WHERE r.city_id = ".$city->getId()." ORDER BY r.date DESC")->getResult();
-		return $ride[0];
-	}
 }
