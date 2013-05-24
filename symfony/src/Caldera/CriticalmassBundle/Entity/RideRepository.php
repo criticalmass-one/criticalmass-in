14cc1aaf446faa8bdb287e85ee544d8c9a0fb2f3
Malte Hübner <maltehuebner@gmx.org>
2013-05-24 14:05:34 +0200

    Semantik des Methodennamens korrigiert.

--- symfony/src/Caldera/CriticalmassBundle/Entity/RideRepository.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/RideRepository.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\EntityRepository;
 
 class RideRepository extends EntityRepository
 {
-	public function findLatestForCity(City $city)
+	public function findLatestByCity(City $city)
 	{
 		$ride = $this->getEntityManager()->createQuery("SELECT r FROM CalderaCriticalmassBundle:Ride r WHERE r.city_id = ".$city->getId()." ORDER BY r.date DESC")->getResult();
 		return $ride[0];
