12c8c265960b69eed2ee58b138e9a1fea9a1643b
Malte Hübner <maltehuebner@gmx.org>
2013-05-24 16:31:57 +0200

    Controller aus dem Beispiel übernommen.

--- symfony/src/Caldera/CriticalmassBundle/Controller/BackendController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/BackendController.php
@@ -3,8 +3,35 @@
 namespace Caldera\CriticalmassBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class BackendController extends Controller
 {
+	public function loginAction()
+	{
+		$request = $this->getRequest();
+		$session = $request->getSession();
 
+		// get the login error if there is one
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+		{
+			$error = $request->attributes->get(
+			SecurityContext::AUTHENTICATION_ERROR
+			);
+		}
+		else
+		{
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
+		return $this->render(
+	            'CalderaCriticalmassBundle:Backend:login.html.twig',
+	            array(
+	                // last username entered by the user
+	                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+	                'error'         => $error,
+	            )
+	        );
+	    }
 }
