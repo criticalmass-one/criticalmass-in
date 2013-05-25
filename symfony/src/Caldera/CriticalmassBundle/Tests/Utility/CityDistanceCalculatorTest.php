0b56a6a1d79891babe67cc4a01065b45c4ffc508
Malte Hübner <maltehuebner@gmx.org>
2013-05-25 23:12:15 +0200

    Aus dem gleichen Grunde überflüssigen Testfall entfernt.

--- symfony/src/Caldera/CriticalmassBundle/Tests/Utility/CityDistanceCalculatorTest.php
+++ symfony/src/Caldera/CriticalmassBundle/Tests/Utility/CityDistanceCalculatorTest.php
@@ -30,26 +30,4 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertEquals(1.2774847898, $result);
 	}
-
-	/**
-	 * Dieser Testfall arbeitet wie testCalculateDistanceFromCityToCity(), lässt
-	 * sich allerdings das Ergebnis in Kilometern zurückgeben.
-	 */
-	public function testCalculateKilometreDistanceFromCityToCity()
-	{
-		$cdc = new Utility\CityDistanceCalculator();
-
-		$hamburg = new Entity\City();
-		$bremen = new Entity\City();
-
-		$hamburg->setLatitude(53.550556);
-		$hamburg->setLongitude(9.993333);
-
-		$bremen->setLatitude(53.075878);
-		$bremen->setLongitude(8.807311);
-
-		$result = $cdc->calculateKilometreDistanceFromCityToCity($hamburg, $bremen);
-
-		$this->assertEquals(95.8113592349, $result);
-	}
 }
