a951b0b1ae80be7d54be10debba31b02df918389
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 19:25:39 +0200

    Quellcode eingerückt und set- und getEmail()-Methoden hinzugefügt.

--- symfony/src/Caldera/CriticalmassBundle/Entity/User.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/User.php
@@ -11,100 +11,116 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class User implements UserInterface, \Serializable
 {
-		/**
-		 * @ORM\Column(type="integer")
-		 * @ORM\Id
-		 * @ORM\GeneratedValue(strategy="AUTO")
-		 */
-		private $id;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-		/**
-		 * @ORM\Column(type="string", length=25, unique=true)
-		 */
-		private $username;
+	/**
+	 * @ORM\Column(type="string", length=25, unique=true)
+	 */
+	private $username;
 
-		/**
-		 * @ORM\Column(type="string", length=32)
-		 */
-		private $salt;
+	/**
+	 * @ORM\Column(type="string", length=32)
+	 */
+	private $salt;
 
-		/**
-		 * @ORM\Column(type="string", length=40)
-		 */
-		private $password;
+	/**
+	 * @ORM\Column(type="string", length=40)
+	 */
+	private $password;
 
-		/**
-		 * @ORM\Column(type="string", length=60, unique=true)
-		 */
-		private $email;
+	/**
+	 * @ORM\Column(type="string", length=60, unique=true)
+	 */
+	private $email;
 
-		/**
-		 * @ORM\Column(name="is_active", type="boolean")
-		 */
-		private $isActive;
+	/**
+	 * @ORM\Column(name="is_active", type="boolean")
+	 */
+	private $isActive;
 
-		public function __construct()
-		{
-				$this->isActive = true;
-				$this->salt = md5(uniqid(null, true));
-		}
+	public function __construct()
+	{
+		$this->isActive = true;
+		$this->salt = md5(uniqid(null, true));
+	}
 
-		/**
-		 * @inheritDoc
-		 */
-		public function getUsername()
-		{
-				return $this->username;
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function getUsername()
+	{
+		return $this->username;
+	}
 
-		/**
-		 * @inheritDoc
-		 */
-		public function getSalt()
-		{
-				return $this->salt;
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function getSalt()
+	{
+		return $this->salt;
+	}
 
-		/**
-		 * @inheritDoc
-		 */
-		public function getPassword()
-		{
-				return $this->password;
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function getPassword()
+	{
+		return $this->password;
+	}
 
-		/**
-		 * @inheritDoc
-		 */
-		public function getRoles()
-		{
-				return array('ROLE_USER');
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
 
-		/**
-		 * @inheritDoc
-		 */
-		public function eraseCredentials()
-		{
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
 
-		/**
-		 * @see \Serializable::serialize()
-		 */
-		public function serialize()
-		{
-				return serialize(array(
-						$this->id,
-				));
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function getRoles()
+	{
+		return array('ROLE_USER');
+	}
 
-		/**
-		 * @see \Serializable::unserialize()
-		 */
-		public function unserialize($serialized)
-		{
-				list (
-						$this->id,
-				) = unserialize($serialized);
-		}
+	/**
+	 * @inheritDoc
+	 */
+	public function eraseCredentials()
+	{
+	}
+
+	/**
+	 * @see \Serializable::serialize()
+	 */
+	public function serialize()
+	{
+		return serialize(array(
+			$this->id,
+		));
+	}
+
+	/**
+	 * @see \Serializable::unserialize()
+	 */
+	public function unserialize($serialized)
+	{
+		list (
+			$this->id,
+		) = unserialize($serialized);
+	}
 }
