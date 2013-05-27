2b0154e2384e3851fc1c29cb6e5043745e64371d
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 18:57:35 +0200

    Klasse an den richtigen Ort verschoben.

---
+++ symfony/src/Caldera/CriticalmassBundle/Entity/Registration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme\AccountBundle\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Acme\AccountBundle\Entity\User;
+
+class Registration
+{
+    /**
+     * @Assert\Type(type="Acme\AccountBundle\Entity\User")
+     * @Assert\Valid()
+     */
+    protected $user;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\True()
+     */
+    protected $termsAccepted;
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function getTermsAccepted()
+    {
+        return $this->termsAccepted;
+    }
+
+    public function setTermsAccepted($termsAccepted)
+    {
+        $this->termsAccepted = (Boolean) $termsAccepted;
+    }
+}
