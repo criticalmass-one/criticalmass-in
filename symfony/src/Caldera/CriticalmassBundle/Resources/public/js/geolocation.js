1234aa398bcda537afc5d16437378f8bd9c59732
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 21:25:45 +0200

    Erster Versuch einer regelmäßigen Aktualisierung mit Geolocation.

--- symfony/src/Caldera/CriticalmassBundle/Resources/public/js/geolocation.js
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/geolocation.js
@@ -19,3 +19,28 @@ function getLocation()
 		alert("Geolocation nicht möglich.");
 	}
 }
+
+function sendPosition(position)
+{
+  $.ajax({
+      type: 'POST',
+      url: 'http://localhost/criticalmass/symfony/web/app_dev.php/trackposition',
+      data: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        altitude: position.coords.altitude
+      },
+      cache: false,
+      success: function(result) {
+          if (result == "true"){
+              alert("true");
+          }else{
+              alert("false");
+          }
+      }
+  });
+}
+window.onload = setInterval(function()
+{
+	navigator.geolocation.getCurrentPosition(sendPosition, error);
+}, 5000);
