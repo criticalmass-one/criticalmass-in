ef1cb06353dbf0b7efc8d7f9db95a2137528fc7d
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 18:15:25 +0200

    User-Entität aus dem Beispiel übernommen.

---
+++ symfony/src/Caldera/CriticalmassBundle/Entity/User.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Caldera\CriticalmassBundle\Entity\UserRepository")
+ */
+class User implements UserInterface, \Serializable
+{
+		/**
+		 * @ORM\Column(type="integer")
+		 * @ORM\Id
+		 * @ORM\GeneratedValue(strategy="AUTO")
+		 */
+		private $id;
+
+		/**
+		 * @ORM\Column(type="string", length=25, unique=true)
+		 */
+		private $username;
+
+		/**
+		 * @ORM\Column(type="string", length=32)
+		 */
+		private $salt;
+
+		/**
+		 * @ORM\Column(type="string", length=40)
+		 */
+		private $password;
+
+		/**
+		 * @ORM\Column(type="string", length=60, unique=true)
+		 */
+		private $email;
+
+		/**
+		 * @ORM\Column(name="is_active", type="boolean")
+		 */
+		private $isActive;
+
+		public function __construct()
+		{
+				$this->isActive = true;
+				$this->salt = md5(uniqid(null, true));
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		public function getUsername()
+		{
+				return $this->username;
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		public function getSalt()
+		{
+				return $this->salt;
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		public function getPassword()
+		{
+				return $this->password;
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		public function getRoles()
+		{
+				return array('ROLE_USER');
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		public function eraseCredentials()
+		{
+		}
+
+		/**
+		 * @see \Serializable::serialize()
+		 */
+		public function serialize()
+		{
+				return serialize(array(
+						$this->id,
+				));
+		}
+
+		/**
+		 * @see \Serializable::unserialize()
+		 */
+		public function unserialize($serialized)
+		{
+				list (
+						$this->id,
+				) = unserialize($serialized);
+		}
+}
