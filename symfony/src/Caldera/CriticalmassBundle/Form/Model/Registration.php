0491c743e8914f791499a08f161116d5d9b01fd2
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 18:59:34 +0200

    Registration-Model aus dem Beispiel übernommen.

---
+++ symfony/src/Caldera/CriticalmassBundle/Form/Model/Registration.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Caldera\CriticalmassBundle\Entity\User;
+
+class Registration
+{
+	/**
+	 * @Assert\Type(type="Acme\AccountBundle\Entity\User")
+	 * @Assert\Valid()
+	 */
+	protected $user;
+
+	/**
+	 * @Assert\NotBlank()
+	 * @Assert\True()
+	 */
+	protected $termsAccepted;
+
+	public function setUser(User $user)
+	{
+		$this->user = $user;
+	}
+
+	public function getUser()
+	{
+		return $this->user;
+	}
+
+	public function getTermsAccepted()
+	{
+		return $this->termsAccepted;
+	}
+
+	public function setTermsAccepted($termsAccepted)
+	{
+		$this->termsAccepted = (Boolean) $termsAccepted;
+	}
+}
