68e465088a2ebb66ac4df7dcb1440b13206a24e1
Malte Hübner <maltehuebner@gmx.org>
2013-05-27 18:52:54 +0200

    Formular aus dem Beispiel übernommen.

---
+++ symfony/src/Caldera/CriticalmassBundle/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder->add('email', 'email');
+		$builder->add('plainPassword', 'repeated', array(
+			'first_name'  => 'password',
+			'second_name' => 'confirm',
+			'type'        => 'password',
+		));
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => 'Caldera\CriticalmassBundle\Entity\User'
+		));
+	}
+
+	public function getName()
+	{
+		return 'user';
+	}
+}
