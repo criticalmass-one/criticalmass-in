a48839c432c89a01540afbf17f8e6c023bc403f1
Malte Hübner <maltehuebner@gmx.org>
2013-05-28 23:07:58 +0200

    JSON-Rückgabewert eingefügt.

--- symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
@@ -9,5 +9,14 @@ use Caldera\CriticalmassBundle\Entity as Entity;
 
 class MapController extends Controller
 {
+	public function mapcenterAction()
+	{
+		$response = new Response();
+		$response->setContent(json_encode(array(
+			'latitude' => 53.57033623530256,
+			'longitude' => 9.719623122674422
+		)));
 
+		$response->headers->set('Content-Type', 'application/json');
+	}
 }
