f6b44451372306c7d94171134f35a872f7992e5e
Malte Hübner <maltehuebner@gmx.org>
2013-05-28 23:04:26 +0200

    Leeren MapController angelegt.

---
+++ symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Caldera\CriticalmassBundle\Entity as Entity;
+
+class MapController extends Controller
+{
+
+}
