7c58352c111a7653c7dfe89041e9738b1ed32bcb
Malte Hübner <maltehuebner@gmx.org>
2013-05-29 01:22:49 +0200

    Namensräume im Controller gefixt.

--- symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
@@ -5,12 +5,15 @@ namespace Caldera\CriticalmassBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use Caldera\CriticalmassBundle\Utility as Utility;
 use Caldera\CriticalmassBundle\Entity as Entity;
 
 class MapController extends Controller
 {
 	public function mapcenterAction()
 	{
+		$mph = new Utility\MapPositionHandler(new Entity\Ride());
+
 		$response = new Response();
 		$response->setContent(json_encode(array(
 			'mapcenter' => array(
