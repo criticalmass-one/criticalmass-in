825a362d7650926010da787d960e99a5024293be
Malte Hübner <maltehuebner@gmx.org>
2013-05-29 01:14:32 +0200

    MapPositionHandler hinzugefügt.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/MapPositionHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility;
+
+use Caldera\CriticalmassBundle\Entity as Entity;
+
+class MapPositionHandler
+{
+	protected Entity\Ride $ride;
+
+	public function __construct(Entity\Ride $ride)
+	{
+		$this->setRide($ride);
+	}
+
+	public function setRide(Entiry\Ride $ride)
+	{
+		$this->ride = $ride;
+	}
+}
