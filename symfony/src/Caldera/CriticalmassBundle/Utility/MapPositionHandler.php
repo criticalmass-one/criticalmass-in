57d747f8741d52a2c6f3f3626dbc10c585c0b99b
Malte Hübner <maltehuebner@gmx.org>
2013-05-29 02:40:22 +0200

    Methode zur Übergabe der Positionen eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Utility/MapPositionHandler.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/MapPositionHandler.php
@@ -8,9 +8,12 @@ class MapPositionHandler
 {
 	protected $ride;
 
-	public function __construct(Entity\Ride $ride)
+	protected $positions = array();
+
+	public function __construct(Entity\Ride $ride, $positions)
 	{
 		$this->setRide($ride);
+		$this->setPositions($positions);
 	}
 
 	public function setRide(Entity\Ride $ride)
@@ -18,17 +21,14 @@ class MapPositionHandler
 		$this->ride = $ride;
 	}
 
+	public function setPositions($positions)
+	{
+		$this->positions = $positions;
+	}
+
 	public function getZoomFactor()
 	{
 		return 10;
-	}
-
-	public function getMapCenter()
-	{
-		return array(
-			'latitude' => $this->getMapCenterLatitude(),
-			'longitude' => $this->getMapCenterLongitude()
-		);
 	}
 
 	public function getMapCenterLatitude()
