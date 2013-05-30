cdd48c02e0a8bcab45893e5c759f3756b8fc326f
Malte Hübner <maltehuebner@gmx.org>
2013-05-30 17:01:12 +0200

    Reihenfolge und Anzahl der Ausgabeergebnisse gesetzt.

--- symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/MapController.php
@@ -14,7 +14,7 @@ class MapController extends Controller
 	{
 		$mph = new Utility\MapPositionHandler(
 			new Entity\Ride(),
-			$this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Position')->findBy(array(), array(), 10)
+			$this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Position')->findBy(array(), array("id" => "DESC"), 100)
 	);
 
 		$response = new Response();
