4a90a8444e6a933432aa9084162e86306b8d0612
Malte Hübner <maltehuebner@gmx.org>
2013-05-31 01:36:27 +0200

    Kleinere Schönheitsoperationen am JavaScript.

--- symfony/src/Caldera/CriticalmassBundle/Resources/public/js/map.js
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/map.js
@@ -1,11 +1,3 @@
-// Create an object containing LatLng, population.
-var citymap = {};
-citymap['wedel'] = {
-  center: new google.maps.LatLng(53.57033623530256, 9.719623122674422),
-  population: 30000
-};
-var cityCircle;
-
 function initialize() {
 	function setMapOptions(result)
 	{
@@ -15,23 +7,24 @@ function initialize() {
 			mapTypeId: google.maps.MapTypeId.TERRAIN
 		}
 
-	  var map = new google.maps.Map(document.getElementById('map-canvas'),
+		var map = new google.maps.Map(document.getElementById('map-canvas'),
 	      mapOptions);
 
-	  for (var pos in result.positions) {
-	    // Construct the circle for each value in citymap. We scale population by 20.
-	    var populationOptions = {
-	      strokeColor: '#FF0000',
-	      strokeOpacity: 0.8,
-	      strokeWeight: 2,
-	      fillColor: '#FF0000',
-	      fillOpacity: 0.35,
-	      map: map,
-	      center: new google.maps.LatLng(result.positions[pos].latitude, result.positions[pos].longitude),
-	      radius: 1000
-	    };
-	    cityCircle = new google.maps.Circle(populationOptions);
-	  }
+		for (var pos in result.positions)
+		{
+			var populationOptions = {
+				strokeColor: '#FF0000',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#FF0000',
+				fillOpacity: 0.35,
+				map: map,
+				center: new google.maps.LatLng(result.positions[pos].latitude, result.positions[pos].longitude),
+				radius: 1000
+			};
+
+			cityCircle = new google.maps.Circle(populationOptions);
+		}
 	}
 
 	$.ajax({
@@ -42,9 +35,6 @@ function initialize() {
 		cache: false,
 		success: setMapOptions
 	});
-
-
-
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
