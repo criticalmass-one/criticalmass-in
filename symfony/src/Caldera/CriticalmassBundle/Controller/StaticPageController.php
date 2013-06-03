3bafea109fa2612a3a525f062c26668a18afc709
Malte Hübner <maltehuebner@gmx.org>
2013-06-04 00:03:43 +0200

    showAction für den StaticPageController eingefügt.

--- symfony/src/Caldera/CriticalmassBundle/Controller/StaticPageController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/StaticPageController.php
@@ -6,4 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class StaticPageController extends Controller
 {
+	public function showAction($page)
+	{
+		return $this->render('CalderaCriticalmassBundle:Default:static:'.$page.'.html.twig');
+	}
 }
