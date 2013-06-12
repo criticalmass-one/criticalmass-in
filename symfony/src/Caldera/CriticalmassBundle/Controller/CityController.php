a1a640174930610d5a6470f8dfefb300b5cfd0ea
Malte Hübner <maltehuebner@gmx.org>
2013-06-13 00:04:36 +0200

    Lade-Methode in den Controller eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CityController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CityController.php
@@ -6,4 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CityController extends Controller
 {
+	public function loadcitiesAction($latitude, $longitude)
+	{
+		$cityResults = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:City')->findNearestedByLocation($latitude, $longitude);
+
+		foreach ($cityResults as $key => $result)
+		{
+			$cityResults[$key]['ride'] = $this->get('caldera_criticalmass_ride_repository')->findOneBy(array('city_id' => $cityResults[$key]['city']->getId()));
+			$cityResults[$key]['distance'] = $this->get('caldera_criticalmass_citydistancecalculator')->calculateDistanceFromCoordToCoord($cityResults[$key]['city']->getLatitude(), $latitude, $cityResults[$key]['city']->getLongitude(), $longitude);
+		}
+
+		return $this->render('CalderaCriticalmassBundle:Rightsidebar:choosecity.html.twig', array('cityResults' => $cityResults));
+	}
 }
