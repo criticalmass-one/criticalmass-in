5df5161624facc2d61d5e250c0c423ba63a6f6e2
Malte Hübner <maltehuebner@gmx.org>
2013-06-13 23:29:45 +0200

    Eigenschaften der Position teilweise in DefaultFixturePosition verschoben.

--- symfony/src/Caldera/CriticalmassBundle/Entity/DefaultFixturePosition.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/DefaultFixturePosition.php
@@ -2,6 +2,21 @@
 
 namespace Caldera\CriticalmassBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="position")
+ */
 class DefaultFixturePosition extends Position
 {
+	public function __construct()
+	{
+		$this->setAccuracy(0.0);
+		$this->setAltitude(0.0);
+		$this->setAltitudeAccuracy(0.0);
+		$this->setHeading(0.0);
+		$this->setSpeed(0.0);
+		$this->setTimestamp(0.0);
+	}
 }
