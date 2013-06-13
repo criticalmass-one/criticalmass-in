d0c468d49d6aa7fbc2668c4b2ce00a660fa9e1b5
Malte Hübner <maltehuebner@gmx.org>
2013-06-13 23:36:28 +0200

    Insgesamt 21 Positionen vorbereitet.

--- symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadPositionData.php
+++ symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadPositionData.php
@@ -23,6 +23,206 @@ class LoadPositionData extends AbstractFixture implements OrderedFixtureInterfac
 
 		$manager->persist($position);
 		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:03:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:06:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:09:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:12:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:15:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:18:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:21:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:24:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:27:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:30:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:33:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:36:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:39:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:42:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:45:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:48:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:51:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:54:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 19:57:00"));
+
+		$manager->persist($position);
+		$manager->flush();
+
+		$position = new DefaultFixturePosition();
+		$position->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$position->setUser($this->getReference("user-maltehuebner"));
+		$position->setLatitude(9.72877);
+		$position->setLongitude(53.57805);
+		$position->setCreationDateTime(new \DateTime("2013-05-31 20:00:00"));
+
+		$manager->persist($position);
+		$manager->flush();
 	}
 
 	/**
