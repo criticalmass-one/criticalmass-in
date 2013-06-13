70dc083505a839de3e46659b9eb026387601ce03
Malte Hübner <maltehuebner@gmx.org>
2013-06-14 01:29:49 +0200

    Dazugehörige Fixture-Klasse definiert.

---
+++ symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCitySlugData.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Caldera\CriticalmassBundle\Entity\City;
+use Caldera\CriticalmassBundle\Entity\CitySlug;
+
+class LoadCitySlugData extends AbstractFixture implements OrderedFixtureInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(ObjectManager $manager)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOrder()
+	{
+		return 5;
+	}
+}
