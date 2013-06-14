ff4c08bc85ac3b6e4c76b4ec74927e4b399849e1
Malte Hübner <maltehuebner@gmx.org>
2013-06-14 04:41:30 +0200

    Überflüssige Zeilenumbrüche entfernt.

--- symfony/src/Caldera/CriticalmassBundle/Resources/public/js/scripts.js
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/scripts.js
@@ -6,13 +6,11 @@ $(window).load(function()
 		{
 			navigator.geolocation.getCurrentPosition(function(position)
 			{
-				$('#rightpanel').load('http://localhost/criticalmass/symfony/web/app_dev.php/loadcities/', { 'latitude': position.coords.latitude, 'longitude': position.coords.longitude }, function()
+				$('#rightpanel').load('/criticalmass/symfony/web/app_dev.php/loadcities/', { 'latitude': position.coords.latitude, 'longitude': position.coords.longitude }, function()
 				{
 					$('#rightpanel').trigger('create');
 				});
 			});
 		}
-
-		
 	});
 });
