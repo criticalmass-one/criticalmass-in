e1a60ce1c5917adb95bd4615dcf0ac5b82f94ba8
Malte Hübner <maltehuebner@gmx.org>
2013-06-15 19:25:02 +0200

    Fixture-Klasse für die Kommentare angelegt.

---
+++ symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCommentData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Caldera\CriticalmassBundle\Entity\Comment;
+
+class LoadCommentData extends AbstractFixture implements OrderedFixtureInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(ObjectManager $manager)
+	{
+		$comment = new Comment();
+		$comment->setUser($this->getReference("user-maltehuebner"));
+		$comment->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$comment->setCreationDateTime("2013-05-31 18:50:12");
+		$comment->setText("Ich freu mich schon mega!");
+
+		$manager->persist($comment);
+		$manager->flush();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOrder()
+	{
+		return 5;
+	}
+}
