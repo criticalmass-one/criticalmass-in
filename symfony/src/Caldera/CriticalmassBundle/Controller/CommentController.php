9fc710c0f5b447b554e8fb21a8be0757476fed06
Malte Hübner <maltehuebner@gmx.org>
2013-06-15 19:37:16 +0200

    Controller-Methode ausgebaut.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
@@ -8,6 +8,12 @@ class CommentController extends Controller
 {
 	public function listcommentsAction($citySlug)
 	{
+		$citySlug = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:CitySlug')->findOneBySlug($citySlug);
+
+		$ride = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Ride')->findOneBy(array('city_id' => $citySlug->getCity()->getId()), array('date' => 'desc'));
 		
+		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findByRide($ride);
+
+		return $this->render('CalderaCriticalmassBundle:RideComments:list.html.twig', array('comments' => $comments));
 	}
 }
