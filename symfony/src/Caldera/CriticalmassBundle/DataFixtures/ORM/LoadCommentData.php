137f455243752e35d01e1e1e67b3386e926dd545
Malte Hübner <maltehuebner@gmx.org>
2013-06-15 19:29:16 +0200

    Fixtures erweitert.

--- symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCommentData.php
+++ symfony/src/Caldera/CriticalmassBundle/DataFixtures/ORM/LoadCommentData.php
@@ -22,6 +22,24 @@ class LoadCommentData extends AbstractFixture implements OrderedFixtureInterface
 
 		$manager->persist($comment);
 		$manager->flush();
+
+		$comment = new Comment();
+		$comment->setUser($this->getReference("user-testuser1"));
+		$comment->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$comment->setCreationDateTime(new \DateTime("2013-05-31 18:59:23"));
+		$comment->setText("Es sind schon fünftausend Leute hier!");
+
+		$manager->persist($comment);
+		$manager->flush();
+
+		$comment = new Comment();
+		$comment->setUser($this->getReference("user-maltehuebner"));
+		$comment->setRide($this->getReference("city-hamburg-ride-2013-05-31"));
+		$comment->setCreationDateTime(new \DateTime("2013-05-31 23:13:01"));
+		$comment->setText("2.222 Teilnehmer? Es war wieder total großartig!");
+
+		$manager->persist($comment);
+		$manager->flush();
 	}
 
 	/**
