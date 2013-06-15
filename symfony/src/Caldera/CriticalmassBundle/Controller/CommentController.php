e083608a5a3e8bc0ffe40feafd9d564f281bfa65
Malte Hübner <maltehuebner@gmx.org>
2013-06-15 21:34:06 +0200

    Controller erzeugt nun ein Eingabeformular für neue Kommentare.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
@@ -3,6 +3,7 @@
 namespace Caldera\CriticalmassBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Caldera\CriticalmassBundle\Entity\Comment;
 
 class CommentController extends Controller
 {
@@ -12,8 +13,10 @@ class CommentController extends Controller
 
 		$ride = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Ride')->findOneBy(array('city_id' => $citySlug->getCity()->getId()), array('date' => 'desc'));
 		
-		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findByRide($ride);
+		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findAll();
 
-		return $this->render('CalderaCriticalmassBundle:RideComments:list.html.twig', array('comments' => $comments));
+	  $form = $this->createFormBuilder(new Comment())->add('text', 'text')->getForm();
+
+		return $this->render('CalderaCriticalmassBundle:RideComments:list.html.twig', array('comments' => $comments, 'form' => $form->createView()));
 	}
 }
