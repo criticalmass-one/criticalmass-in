f565a9d1f0a073537290428f618acd0aa110b5cf
Malte Hübner <maltehuebner@gmx.org>
2013-06-18 01:17:43 +0200

    Kommentare werden nach der angewählten Tour gefiltert.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
@@ -11,11 +11,12 @@ class CommentController extends Controller
 	{
 		$citySlug = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:CitySlug')->findOneBySlug($citySlug);
 
-		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findBy(array(), array('creationDateTime' => 'DESC'));
+		$ride = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Ride')->findOneBy(array('city_id' => $citySlug->getCity()->getId()), array('date' => 'DESC'));
+
+		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findBy(array('ride' => $ride->getId()), array('creationDateTime' => 'DESC'));
 
 		$form = $this->createFormBuilder(new Comment())->add('text', 'text')->getForm();
 
-		$ride = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Ride')->findOneBy(array('city_id' => $citySlug->getCity()->getId()), array('date' => 'desc'));
 
 		return $this->render('CalderaCriticalmassBundle:RideComments:list.html.twig', array('comments' => $comments, 'form' => $form->createView()));
 	}
