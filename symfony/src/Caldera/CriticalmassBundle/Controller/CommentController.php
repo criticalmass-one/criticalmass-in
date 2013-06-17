f05e0edd0cd20ac6bcc2f2bcd3924b0ba091d09d
Malte Hübner <maltehuebner@gmx.org>
2013-06-18 01:01:05 +0200

    Aha, die Kommentare funktionieren ja doch, wenn man sie vernünftig benutzt.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
@@ -34,8 +34,15 @@ class CommentController extends Controller
 
 		if ($form->isValid())
 		{
-			$em = $this->getDoctrine()->getManager()->persist($comment);
+
 		}
+		$comment->setUser($this->getDoctrine()->getRepository('CalderaCriticalmassBundle:User')->findOneByUsername("maltehuebner"));
+		$comment->setRide($ride);
+		$comment->setCreationDateTime(new \DateTime("2013-06-17 23:51:55"));
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($comment);
+		$em->flush();
 
 		return $this->redirect($this->generateUrl('caldera_criticalmass_listcomments', array('citySlug' => 'hamburg')));
 	}
