a464c151b7888be420bb1fd430a677de3af0e4ed
Malte Hübner <maltehuebner@gmx.org>
2013-06-22 17:02:51 +0200

    Defekte Überreste korrigiert.

--- symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
+++ symfony/src/Caldera/CriticalmassBundle/Controller/CommentController.php
@@ -9,14 +9,11 @@ class CommentController extends Controller
 	public function listcommentsAction($citySlug)
 	{
 		
-<<<<<<< Updated upstream
-=======
 		$comments = $this->getDoctrine()->getRepository('CalderaCriticalmassBundle:Comment')->findAll();
 
 		$form = $this->createFormBuilder(new Comment())->add('text', 'text')->getForm();
 
 		return $this->render('CalderaCriticalmassBundle:RideComments:list.html.twig', array('comments' => $comments, 'form' => $form->createView()));
->>>>>>> Stashed changes
 	}
 
 	public function addcommentAction($citySlug)
