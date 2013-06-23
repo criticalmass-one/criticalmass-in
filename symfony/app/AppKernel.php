119e38cbcb3f707eb5fc78e6cfee3264dd7a110c
Malte Hübner <maltehuebner@gmx.org>
2013-06-23 23:29:37 +0200

    Fixtures wieder aktiviert.

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Caldera\CriticalmassBundle\CalderaCriticalmassBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
