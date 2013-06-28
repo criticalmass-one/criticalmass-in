d8303b1dcb9beb33e7cbaf661a52a3ec0c845a9f
Malte Huebner <maltehuebner@gmx.org>
2013-06-28 12:59:55 +0000

    Zoomfaktor wird nun mathematisch berechnet.

--- symfony/src/Caldera/CriticalmassBundle/Utility/MapPositionHandler.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/MapPositionHandler.php
@@ -28,7 +28,52 @@ class MapPositionHandler
 
 	public function getZoomFactor()
 	{
-		return 10;
+		$minX = null;
+                $maxX = null;
+		$minY = null;
+		$maxY = null;
+
+                foreach ($this->getPositions() as $position)
+                {
+                        if (!isset($minX) && !isset($maxX) && !isset($minY) && !isset($maxY))
+                        {
+                                $minX = $position->getLatitude();
+				$maxX = $position->getLatitude();
+				$minY = $position->getLongitude();
+				$maxY = $position->getLongitude();
+                        }
+                        else
+                        {
+                                if ($minX > $position->getLatitude())
+                                {
+                                        $minX = $position->getLatitude();
+                                }
+
+                                if ($maxX < $position->getLatitude())
+                                {
+                                        $maxX = $position->getLatitude();
+                                }
+
+                                if ($minY > $position->getLongitude())
+                                {
+                                        $minY = $position->getLongitude();
+                                }
+
+                                if ($maxY < $position->getLongitude())
+                                {
+                                        $maxY = $position->getLongitude();
+                                }
+                        }
+                }
+
+		$distanceX = $maxX - $minX;
+		$distanceY = $maxY - $minY;
+
+		$distance = ($distanceX > $distanceY ? $distanceX : $distanceY);
+
+		$zoomFactor = floor(log(960 * 360 / $distance / 256)) + 1;
+
+		return $zoomFactor;
 	}
 
 	public function getMapCenterLatitude()
