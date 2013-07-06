c8e2edec4098205757df5586a3ef03e8aabafaa5
Malte Hübner <maltehuebner@gmx.org>
2013-07-06 10:44:22 +0200

    Erster Versuch eines Geocoding-Skriptes.

---
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/geocoding.js
@@ -0,0 +1,33 @@
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById('geocoding-map'), mapOptions);
+
+  $('#caldera_criticalmassbundle_ridetype_location').change(function() {
+    codeAddress();
+  });
+}
+
+function codeAddress() {
+  var address = $('#caldera_criticalmassbundle_ridetype_location').val();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
