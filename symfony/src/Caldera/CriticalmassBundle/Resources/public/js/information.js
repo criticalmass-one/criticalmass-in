334e716f567350fc902002afe6b4c33fef30b33e
Malte Hübner <maltehuebner@gmx.org>
2013-07-06 17:59:51 +0200

    Zentriere die Informationskarte jetzt auf den Treffpunkt.

--- symfony/src/Caldera/CriticalmassBundle/Resources/public/js/information.js
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/information.js
@@ -1,34 +1,23 @@
-var geocoder;
 var map;
 var marker;
 
 function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
-  var mapOptions = {
-    zoom: 12,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  map = new google.maps.Map(document.getElementById('information-map'), mapOptions);
-
+	$.ajax({
+		type: 'GET',
+		url: '/mapapi/getridelocation/hamburg',
+		data: {
+		},
+		success: function(result) {
+		  var latlng = new google.maps.LatLng(result.latitude, result.longitude);
+		  var mapOptions = {
+				zoom: 12,
+				center: latlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+		  }
+		  map = new google.maps.Map(document.getElementById('information-map'), mapOptions);
+			
+		}
+	});
 }
-
-function placeNewMarker(location)
-{
-  if (marker)
-  {
-    marker.setMap(null);
-  }
-
-  map.setCenter(location);
-
-  marker = new google.maps.Marker({
-      map: map,
-      position: location
-  });
-}
-
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
