4f29f27821743256d4eb6e78d3084e1f00cd3d99
Malte Hübner <maltehuebner@gmx.org>
2013-07-13 03:47:36 +0200

    LocationPublishedNotification angelegt.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/Notifications/LocationPublishedNotification.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility\Notifications;
+
+use Caldera\CriticalmassBundle\Entity\Ride as Ride;
+
+class LocationPublishedNotification extends BaseNotification
+{
+	public function __construct(Ride $ride)
+	{
+		$this->setMessage('Der Treffpunkt wurde veröffentlicht: '.$ride->getLocation());
+		$this->setTitle($ride->getCity()->getTitle());
+		$this->setUrl('http://www.criticalmass.in/'.$ride->getCity()->getMainSlug()->getSlug());
+		$this->setUrlTitle('criticalmass.in/'.$ride->getCity()->getMainSlug()->getSlug());
+		$this->setPriority(0);
+		$this->setSound('bike');
+	}
+}
