c0d8b17258ed074e3278b3f62543679e66c819e9
Malte Hübner <maltehuebner@gmx.org>
2013-07-13 03:58:08 +0200

    Ein paar Erweiterungen eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Utility/NotificationPusher.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/NotificationPusher.php
@@ -6,19 +6,29 @@ use \Caldera\CriticalmassBundle\Entity as Entity;
 
 class NotificationPusher
 {
-	public function __construct()
-	{
-		
-	}
+	private $notification;
 
+	public function __construct(Notifications\BaseNotification $notification)
+	{
+		$this->notification = $notification;
+	}
+/*	"token" => "wP7MBPTf5TFvazDCtWf2mL1eH9m1fK",
+	"user" => "Gb1whEAd6G1mUxPccfgHRJWYYAnrxh",*/
 	public function sendNotification()
 	{
 		curl_setopt_array($ch = curl_init(), array(
-			CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+			CURLOPT_URL => 'https://api.pushover.net/1/messages.json',
 			CURLOPT_POSTFIELDS => array(
-				"token" => "wP7MBPTf5TFvazDCtWf2mL1eH9m1fK",
-				"user" => "Gb1whEAd6G1mUxPccfgHRJWYYAnrxh",
-				"message" => "hello world",
+				'token' => $this->notification->getToken(),
+				'user' => $this->notification->getUser(),
+				'message' => $this->notification->getMessage(),
+				'device' => $this->notification->getDevice(),
+				'title' => $this->notification->getTitle(),
+				'url' => $this->notification->getUrl(),
+				'url_title' => $this->notification->getUrlTitle(),
+				'priority' => $this->notification->getPriority(),
+				'timestamp' => $this->notification->getTimeStamp(),
+				'sound' => $this->notification->getSound()
 		)));
 
 		curl_exec($ch);
