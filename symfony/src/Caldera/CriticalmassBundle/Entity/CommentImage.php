07c2cf9c992276b2b4e74ebc2d90d31911bf48c6
Malte Hübner <maltehuebner@gmx.org>
2013-07-16 01:40:34 +0200

    Beispiel-Quelltext eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Entity/CommentImage.php
+++ symfony/src/Caldera/CriticalmassBundle/Entity/CommentImage.php
@@ -139,4 +139,32 @@ class CommentImage
     {
         return $this->path;
     }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
 }
