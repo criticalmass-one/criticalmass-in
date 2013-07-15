5f21abb41ed724b2bd52d47612b9bc4757d4aff7
Malte Hübner <maltehuebner@gmx.org>
2013-07-15 09:07:55 +0200

    Testweise ein bisschen JavaScript-Code eingebaut.

--- symfony/src/Caldera/CriticalmassBundle/Resources/public/js/notification.js
+++ symfony/src/Caldera/CriticalmassBundle/Resources/public/js/notification.js
@@ -1,6 +1,19 @@
 function initialize()
 {
+  $('#notify_ride').change(function()
+  {
+    alert('foo');
+  });
 
+  $('#notify_time').click(function()
+  {
+    alert('bar');
+  });
+
+  $('#notify_location').click(function()
+  {
+    alert('baz');
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
