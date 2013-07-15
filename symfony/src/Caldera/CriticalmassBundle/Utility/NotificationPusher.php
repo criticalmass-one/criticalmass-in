7ffc7bf4d3673d75a0c5bf965685047c930fa9e4
Malte Hübner <maltehuebner@gmx.org>
2013-07-16 00:56:39 +0200

    Schleife eingebaut, die für jeden Benutzer eine Notification erstellt.

--- symfony/src/Caldera/CriticalmassBundle/Utility/NotificationPusher.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/NotificationPusher.php
@@ -19,22 +19,25 @@ class NotificationPusher
 	"user" => "Gb1whEAd6G1mUxPccfgHRJWYYAnrxh",*/
 	public function sendNotification()
 	{
-		curl_setopt_array($ch = curl_init(), array(
-			CURLOPT_URL => 'https://api.pushover.net/1/messages.json',
-			CURLOPT_POSTFIELDS => array(
-				'token' => $this->notification->getToken(),
-				'user' => $this->notification->getUser(),
-				'message' => $this->notification->getMessage(),
-				'device' => $this->notification->getDevice(),
-				'title' => $this->notification->getTitle(),
-				'url' => $this->notification->getUrl(),
-				'url_title' => $this->notification->getUrlTitle(),
-				'priority' => $this->notification->getPriority(),
-				'timestamp' => $this->notification->getTimeStamp(),
-				'sound' => $this->notification->getSound()
-		)));
+		foreach ($this->users as $user)
+		{
+			curl_setopt_array($ch = curl_init(), array(
+				CURLOPT_URL => 'https://api.pushover.net/1/messages.json',
+				CURLOPT_POSTFIELDS => array(
+					'token' => $this->notification->getToken(),
+					'user' => $user->getPushoverKey(),
+					'message' => $this->notification->getMessage(),
+					'device' => $this->notification->getDevice(),
+					'title' => $this->notification->getTitle(),
+					'url' => $this->notification->getUrl(),
+					'url_title' => $this->notification->getUrlTitle(),
+					'priority' => $this->notification->getPriority(),
+					'timestamp' => $this->notification->getTimeStamp(),
+					'sound' => $this->notification->getSound()
+				)));
 
-		curl_exec($ch);
-		curl_close($ch);
+			curl_exec($ch);
+			curl_close($ch);
+		}
 	}
 }
