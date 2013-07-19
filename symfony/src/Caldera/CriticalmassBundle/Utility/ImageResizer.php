c4550a3a68310b2d7998c7c5044b52759a15095e
Malte Hübner <maltehuebner@gmx.org>
2013-07-19 13:08:09 +0200

    Basis-Funktionen eingefügt.

--- symfony/src/Caldera/CriticalmassBundle/Utility/ImageResizer.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/ImageResizer.php
@@ -6,5 +6,20 @@ use \Caldera\CriticalmassBundle\Entity as Entity;
 
 class ImageResizer
 {
+	private $commentImage;
 
+	public function __construct(Entity\CommentImage $commentImage)
+	{
+		$this->commentImage = $commentImage;
+	}
+
+	public function resizeLongSideToLength($length)
+	{
+		
+	}
+
+	public function resizeTo($width, $height)
+	{
+		
+	}
 }
