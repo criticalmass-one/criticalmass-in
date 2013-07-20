443f2a2d5f2f7b0f98a8ee5520c1cd436a68235d
Malte Hübner <maltehuebner@gmx.org>
2013-07-20 02:54:33 +0200

    Konstruktor der Klasse empfängt den ServiceContainer.

--- symfony/src/Caldera/CriticalmassBundle/Utility/CommentImageUploader.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/CommentImageUploader.php
@@ -6,4 +6,11 @@ use \Caldera\CriticalmassBundle\Entity as Entity;
 
 class CommentImageUploader
 {
+	private $container;
+
+	public function __construct(\appDevDebugProjectContainer $container)
+	{
+		$this->container = $container;
+	}
+
 }
