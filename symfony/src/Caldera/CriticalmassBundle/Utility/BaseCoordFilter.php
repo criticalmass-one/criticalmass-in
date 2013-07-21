34bd6cbfb6d6d8ae308bb9ca8cf5bab80eef38f9
Malte Hübner <maltehuebner@gmx.org>
2013-07-21 14:48:10 +0200

    Ein paar Eigenschaften und Methoden hinzugefügt.

--- symfony/src/Caldera/CriticalmassBundle/Utility/BaseCoordFilter.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/BaseCoordFilter.php
@@ -4,5 +4,14 @@ namespace Caldera\CriticalmassBundle\Utility;
 
 abstract class BaseCoordFilter
 {
+	private $ride;
+	private $positions = array();
 
+	public function __construct(Entity\Ride $ride, $positions)
+	{
+		$this->ride = $ride;
+		$this->positions = $positions;
+	}
+
+	public abstract function getCalculatedPositions();
 }
