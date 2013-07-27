c7eebc3b03c4b2e9e37620c14e83585d87292012
Malte Hübner <maltehuebner@gmx.org>
2013-07-28 00:35:22 +0200

    PositionArray als Aufbewahrungsklasse für das Positions-Array angelegt.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/PositionFilter/PositionArray.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility\PositionFilter;
+
+use Caldera\CriticalmassBundle\Entity as Entity;
+
+class PositionArray
+{
+	protected $positions = array();
+
+	public function __construct($positions)
+	{
+		$this->positions = $positions;
+	}
+
+	public function getPositions()
+	{
+		return $this->positions;
+	}
+
+	public function setPositions($positions)
+	{
+		$this->positions = $positions;
+	}
+
+	public function getPosition($key)
+	{
+		return $this->positions[$key];
+	}
+
+	public function setPosition($key, Entity\Position $position)
+	{
+		$this->positions[$key] = $position;
+	}
+
+	public function addPosition(Entity\Position $position)
+	{
+		$this->positions[] = $position;
+	}
+}
