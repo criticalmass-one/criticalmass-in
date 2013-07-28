286f8c8d6fc940e99f6e8abcb69a8add29fe93f3
Malte Hübner <maltehuebner@gmx.org>
2013-07-28 03:35:39 +0200

    CircleMapElement implementiert.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/MapElement/CircleMapElement.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility\MapElement;
+
+use \Caldera\CriticalmassBundle\Entity\Position;
+
+class CircleMapElement
+{
+	protected $centerPosition;
+	protected $radius;
+
+	public function __construct(Position $centerPosition, Position $radius)
+	{
+		$this->centerPosition = $centerPosition;
+		$this->radius = $radius;
+	}
+
+	public function draw()
+	{
+		return array(
+			'id' => 'circle-'.$this->centerPosition.'-'.$this->radius,
+			'type' => 'circle',
+			'latitude' => $this->centerPosition->getLatitude(),
+			'longitude' => $this->centerPosition->getLongitude(),
+			'radius' => $this->toPosition,
+			'strokeColor' => '#ff0000',
+			'fillColor' => '#ff0000',
+			'strokeOpacity' => 0.8,
+			'fillOpacity' => 0.35,
+			'strokeWeight' => 2.0
+			);
+		)
+	}
+}
