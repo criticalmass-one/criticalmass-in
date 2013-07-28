442f32591edd0a2bfa8c60f72bb1ae0543088a04
Malte Hübner <maltehuebner@gmx.org>
2013-07-28 04:27:31 +0200

    BaseMapBuilder als abstrakte Klasse definiert.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/MapBuilder/BaseMapBuilder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility\MapBuilder;
+
+use \Caldera\CriticalmassBundle\Entity as Entity;
+
+abstract class BaseMapBuilder
+{
+	protected $mainPositions;
+
+	protected $additionalPositions;
+
+	protected $ride;
+
+	public function __construct(Entity\Ride $ride)
+	{
+		$this->ride = $ride;
+	}
+
+	public abstract function getUserCounter();
+
+	public abstract function getAverageSpeed();
+
+	public abstract function getZoomFactor();
+
+	public abstract function getMapCenterLatitude();
+
+	public abstract function getMapCenterLongitude();
+
+	public abstract function calculateMainPositions();
+
+	public abstract function calculateAdditionalPositions();
+}
