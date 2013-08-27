251d46c05803b84ae41e4a70834bffed31d365f8
Malte Hübner <maltehuebner@gmx.org>
2013-08-27 22:33:27 +0200

    Neuen Filter angelegt, der alle Positionsdaten aus dem Array entfernt.

---
+++ symfony/src/Caldera/CriticalmassBundle/Utility/PositionFilter/DeletePositionFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Caldera\CriticalmassBundle\Utility\PositionFilter;
+
+use Caldera\CriticalmassBundle\Entity as Entity;
+
+/**
+ * Dieser Filter entfernt alle Positionen aus dem Array und soll natuerlich nur
+ * zu Testzwecken eingesetzt werden.
+ */
+class DeletePositionFilter extends BasePositionFilter
+{
+	/**
+	 * Entfernung aller Positionen.
+	 */
+	public function process()
+	{
+		// alle Positionen durchlaufen
+		foreach ($this->positionArray->getPositions() as $key => $position)
+		{
+			// und loeschen
+			$this->positionArray->deletePosition($key);
+		}
+	}
+}
