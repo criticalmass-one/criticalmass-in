0b65300fdd497aa7943b5790b1a04aa439e67e17
Malte Hübner <maltehuebner@gmx.org>
2013-08-30 11:01:49 +0200

    ImageResizer kommentiert.

--- symfony/src/Caldera/CriticalmassBundle/Utility/ImageResizer.php
+++ symfony/src/Caldera/CriticalmassBundle/Utility/ImageResizer.php
@@ -4,32 +4,72 @@ namespace Caldera\CriticalmassBundle\Utility;
 
 use \Caldera\CriticalmassBundle\Entity as Entity;
 
+/**
+ * Diese Hilfsklasse befasst sich mit der Verkleinerung von Bildern, um sie
+ * beispielsweise als Vorschaubild anzeigen zu koennen.
+ */
 class ImageResizer
 {
+	/**
+	 * Variable fuer den Container, der ueber den Service injiziert wird.
+	 */
 	private $container;
 
+	/**
+	 * Instanz einer CommentImage-Entitaet.
+	 */
 	private $commentImage;
+
+	/**
+	 * Instanz eines GD-Bildes-
+	 */
 	private $image;
 
+	/**
+	 * Der Service-Container wird automatisch mit in den Konstruktor eingebaut.
+	 *
+	 * @param \appDevDebugProjectContainer $container
+	 */
 	public function __construct(\appDevDebugProjectContainer $container)
 	{
 		$this->container = $container;
 	}
 
+	/**
+	 * Legt das korrespondierende CommentImage fest und erzeugt daraus ein GD-
+	 * Bild, das im weiteren Verlauf bearbeitet wird.
+	 *
+	 * @param Entity\CommentImage $commentImage: CommentImage-Entitaet
+	 *
+	 * @return ImageResizer: $this
+	 */
 	public function setCommentImage(Entity\CommentImage $commentImage)
 	{
 		$this->commentImage = $commentImage;
 
+		// Bild aus JPEG-Datei erzeugen
 		$this->image = imagecreatefromjpeg($this->container->getParameter('commentimage.upload_filepath').$this->commentImage->getPath());
 
 		return $this;
 	}
 
+	/**
+	 * Gibt das abgespeicherte CommentImage zurueck.
+	 *
+	 * @return CommentImage: Dazugehoerige CommentImage-Instanz
+	 */
 	public function getCommentImage()
 	{
 		return $this->commentImage;
 	}
 
+	/**
+	 * Stoesst die Verkleinerung des Bildes an. Dazu wird die laengste Seite des
+	 * Bildes auf die entsprechend konfigurierte Laenge aus den Einstellungen ver-
+	 * kleinert.
+	 *
+	 * @return ImageResizer: $this
+	 */
 	public function resize()
 	{
 		$this->resizeLongSideToLength($this->container->getParameter('commentimage.upload_longside'));
@@ -37,12 +77,22 @@ class ImageResizer
 		return $this;
 	}
 
+	/**
+	 * Diese Methode verkleinert die laengste Seite des Bildes auf die im Parame-
+	 * ter uebergebene Laenge. Zunaechst werden anhand des Parameters die neuen
+	 * Dimensionen des Bildes berechnet und anschliessend an die Methode
+	 * resizeTo() delegiert.
+	 * 
+	 * @param Integer $length: Neue Laenge der laengsten Seite
+	 */
 	public function resizeLongSideToLength($length)
 	{
+		// ist das Bild breiter als hoch?
 		if (imagesx($this->image) > imagesy($this->image))
 		{
 			$longSide = imagesx($this->image);
 		}
+		// oder hoeher als breit?
 		else
 		{
 			$longSide = imagesy($this->image);
