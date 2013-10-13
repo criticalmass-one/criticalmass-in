311bd515c231d58d862b00ce967b9e329599d9d4
Malte Hübner <maltehuebner@gmx.org>
2013-10-13 03:06:24 +0200

    Neue showcityAction für die neue Darstellung im MobileBundle angelegt.

--- symfony/src/Caldera/CriticalmassMobileBundle/Controller/MapController.php
+++ symfony/src/Caldera/CriticalmassMobileBundle/Controller/MapController.php
@@ -6,4 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MapController extends Controller
 {
+    public function showcityAction($citySlug)
+    {
+        $city = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($citySlug)->getCity();
+
+        return $this->render('CalderaCriticalmassMobileBundle:Map:showcity.html.twig', array('city' => $city));
+    }
 }
