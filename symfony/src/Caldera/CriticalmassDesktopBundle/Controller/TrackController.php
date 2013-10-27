9f04af831622c3bf82a49b64f113b95ddb7745f6
Malte Hübner <maltehuebner@gmx.org>
2013-10-27 22:29:37 +0100

    Kurzes Update für den TrackController.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/TrackController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/TrackController.php
@@ -6,4 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TrackController extends Controller
 {
+    public function showrideAction($rideId)
+    {
+        $ride = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findOneById($rideId);
+
+        if (!$ride)
+        {
+            throw $this->createNotFoundException('There is no ride with the ID '.$rideId.'.');
+        }
+
+        return $this->render('CalderaCriticalmassDesktopBundle:Live:show.html.twig');
+    }
 }
