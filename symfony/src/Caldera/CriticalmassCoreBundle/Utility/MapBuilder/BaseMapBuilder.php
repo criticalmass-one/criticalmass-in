f51f849ca3f6c7232b38994cbcfb46a4901ed921
Malte Hübner <maltehuebner@gmx.org>
2013-10-29 20:58:46 +0100

    MapBuilder umgebaut auf die Verwendung von Modulen anstatt der Helper.

--- symfony/src/Caldera/CriticalmassCoreBundle/Utility/MapBuilder/BaseMapBuilder.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/MapBuilder/BaseMapBuilder.php
@@ -5,39 +5,22 @@ namespace Caldera\CriticalmassCoreBundle\Utility\MapBuilder;
 use \Caldera\CriticalmassCoreBundle\Utility\MapElement as MapElement;
 use \Caldera\CriticalmassCoreBundle\Entity as Entity;
 use \Caldera\CriticalmassCoreBundle\Utility\PositionArray as PositionArray;
+use \Caldera\CriticalmassCoreBundle\Utility\MapBuilderModule as MapBuilderModule;
 
-/**
- * Zur Anzeige der Daten und eingeblendeten grafischen Elemente auf der Live-
- * Seite wird ein so genannter MapBuilder verwendet, der von dieser Klasse erbt
- * und verschiedene Methoden implementieren muss.
- */
 abstract class BaseMapBuilder
 {
-	/**
-	 * Speicher der Positions-Daten vom Typ PositionArray.
-	 */
-	protected $positionArray;
+	public $positionArray;
 
-	/**
-	 * Schnittstelle zum Zugriff auf die Datenbank.
-	 */
-	protected $doctrine;
+    public $doctrine;
 
-	protected $elements = array();
+    public $elements = array();
 
-	/**
-	 * Speicher fuer die dazugehoerige Ride-Entitaet.
-	 */
-	protected $ride;
+    public $ride;
 
-	/**
-	 * Erzeugt eine Instanz eines MapBuilders. Erwartet wird die dazugehoerige Ri-
-	 * de-Entitaet sowie eine Doctrine-Verbindung.
-	 *
-	 * @param Entity\Ride $ride: Ride-Entitaet
-	 * @param \Doctrine\Bundle\DoctrineBundle\Registry $doctrine: Zugriff auf eine
-	 * Doctrine-Instanz
-	 */
+    public $modules = array();
+
+    public $response;
+
 	public function __construct(Entity\Ride $ride, \Doctrine\Bundle\DoctrineBundle\Registry $doctrine)
 	{
 		$this->ride = $ride;
@@ -47,86 +30,24 @@ abstract class BaseMapBuilder
         $this->positionArray = new PositionArray();
 	}
 
-	/**
-	 * Muss zur Anzeige des Benutzerzaehlers implementiert werden.
-	 *
-	 * @return Integer: Anzahl der Benutzer, deren Positionsdaten in die Berech-
-	 * nung eingeflossen sind
-	 */
-	public abstract function getUserCounter();
+    public function registerModule($moduleName)
+    {
+        $moduleName = "\\Caldera\\CriticalmassCoreBundle\\Utility\\MapBuilderModule\\".$moduleName;
 
-	/**
-	 * Muss zur Anzeige der Durchschnittsgeschwindigkeit implementiert werden.
-	 *
-	 * @return Float: Berechnete Durchschnittsgeschwindigkeit de sTeilnehmerfeldes
-	 */
-	public abstract function getAverageSpeed();
+        $this->modules[$moduleName] = new $moduleName($this);
+    }
 
-	/**
-	 * Muss zur Anzeige einer Karte implementiert werden und den zu verwendenden
-	 * Zoom-Faktor zurueckgeben. Gueltig sind Werte zwischen Eins und Zwoelf.
-	 *
-	 * @return Integer: Zoom-Faktor der eingebetteten Karte
-	 */
-	public abstract function getZoomFactor();
-
-	/**
-	 * Muss zur Angabe des Breitengrades des Mittelpunktes der eingebetteten Kar-
-	 * te implementiert werden.
-	 *
-	 * @return Float: Breitengrad des Mittelpunktes der eingebetteten Karte
-	 */
-	public abstract function getMapCenterLatitude();
-
-	/**
-	 * Muss zur Angabe des Laengengrades des Mittelpunktes der eingebetteten Kar-
-	 * te implementiert werden.
-	 *
-	 * @return Float: Laengengrad des Mittelpunktes der eingebetteten Karte
-	 */
-	public abstract function getMapCenterLongitude();
-
-	/**
-	 * Berechnet die Hauptpositionen zur Berechnung des Mittelpunktes des Teil-
-	 * nehmerfeldes.
-	 */
-	public abstract function calculatePositions();
-
-	/**
-	 * Gibt die JSON-Antwort zurueck, die zur Darstellung der Karteninhalte an den
-	 * Client geschickt wird. Dazu werden die verschiedenen Informationen in einem
-	 * mehrdimensionalen Array abgelegt und in das JSON-Format uebertragen.
-	 *
-	 * @return String: JSON-Daten zur Darstellung der Karteninhalte
-	 */
-	public function draw()
-	{/*
-		 = array();
-
-        $marker = new MapElement\MarkerMapElement($this->ride);
-        $this->elements[] = $marker->draw();
-
-        $this->elements = array_merge($this->elements, $this->getMainPositions());
-		
-		$main = $this->mainPositions->getPositions();
-
-        if (count($main) > 1)
+    public function execute()
+    {
+        foreach ($this->modules as $module)
         {
-      		$arrow = new MapElement\ArrowMapElement($main[0], $main[1]);
-            $elements[] = $arrow->draw();
+            $module->execute();
         }
+    }
+	public function draw()
+	{
+        $this->response['elements'] = $this->elements;
 
-		$elements = array_merge($elements, $this->getAdditionalPositions());*/
-
-		return array(
-			'mapCenter' => array(
-				'latitude' => $this->getMapCenterLatitude(),
-				'longitude' => $this->getMapCenterLongitude()
-				),
-				'zoom' => $this->getZoomFactor(),
-				'elements' => $this->elements,
-				'usercounter' => $this->getUserCounter(),
-				'averagespeed' => $this->getAverageSpeed()
-			);
+		return $this->response;
 	}
 }
--- symfony/src/Caldera/CriticalmassCoreBundle/Utility/MapBuilder/LiveMapBuilder.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/MapBuilder/LiveMapBuilder.php
@@ -7,100 +7,24 @@ use Caldera\CriticalmassCoreBundle\Utility\MapElement as MapElement;
 use Caldera\CriticalmassCoreBundle\Utility\MapBuilder\MapBuilderHelper as MapBuilderHelper;
 use Caldera\CriticalmassCoreBundle\Utility as Utility;
 
-/**
- * Implementierung eines MapBuilders, der allen Anspruechen der Aufgabenstel-
- * lung genuegt.
- */
 class LiveMapBuilder extends BaseMapBuilder
 {
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getUserCounter()
-	{
-		$ucc = new MapBuilderHelper\UserCounterCalculator($this->positionArray, $this->ride);
+    public function registerModules()
+    {
+        $this->registerModule("MapCenterMapBuilderModule");
+        $this->registerModule("StandardPositionMapBuilderModule");
+        $this->registerModule("PermanentPositionMapBuilderModule");
+        $this->registerModule("AverageSpeedMapBuilderModule");
+        $this->registerModule("ZoomFactorMapBuilderModule");
+        $this->registerModule("UserOnlineMapBuilderModule");
+    }
 
-		return $ucc->getUserCounter();
-	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getAverageSpeed()
-	{
-		$asc = new MapBuilderHelper\AverageSpeedCalculator($this->positionArray, $this->ride);
 
-		return $asc->getAverageSpeed();
-	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getZoomFactor()
-	{
-		$zfc = new MapBuilderHelper\ZoomFactorCalculator($this->positionArray, $this->ride);
 
-		return $zfc->getZoomFactor();
-	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getMapCenterLatitude()
-	{
-        if ($this->ride->isRideRolling())
-        {
-    		$mcc = new MapBuilderHelper\MapCenterCalculator($this->positionArray, $this->ride);
-
-    		return $mcc->getMapCenterLatitude();
-        }
-        else
-        {
-            return $this->ride->getPublicLatitude();
-        }
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getMapCenterLongitude()
-	{
-        if ($this->ride->isRideRolling())
-        {
-    		$mcc = new MapBuilderHelper\MapCenterCalculator($this->positionArray, $this->ride);
-
-    		return $mcc->getMapCenterLongitude();
-        }
-        else
-        {
-            return $this->ride->getPublicLongitude();
-        }
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function calculatePositions()
-	{
-		$psf = new PositionFilterChain\PositionFilterChain();
-
-		$psf->setDoctrine($this->doctrine);
-		$psf->setRide($this->ride);
-		$psf->execute();
-
-        $this->positionArray->merge($psf->getPositionArray());
-
-        $counter = 0;
-
-        foreach ($psf->getPositionArray()->getPositions() as $position)
-        {
-            $circle = new MapElement\CircleMapElement($position, 100);
-
-            $this->elements['position-'.$counter] = $circle->draw();
-            ++$counter;
-        }
-	}
-
+/**
     public function calculatePermanentPositions()
     {
         $psf = new PositionFilterChain\PermanentPositionFilterChain();
@@ -125,10 +49,10 @@ class LiveMapBuilder extends BaseMapBuilder
         {
             $arrow = new MapElement\ArrowMapElement($positionArray[0], $positionArray[1]);
             $elements[] = $arrow->draw();
-        }*/
+        }
 
         $this->calculatePermanentPositions();
         $marker = new MapElement\RideMarkerMapElement($this->ride);
         $this->elements[] = $marker->draw();
-    }
+    }*/
 }
