d8137ec3759c02e5109d31c0126d90fa9392663e
Malte Hübner <maltehuebner@gmx.org>
2013-10-30 20:19:38 +0100

    RideCompiler implementiert.

---
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/PositionFilterChain/RideCompiler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Caldera\CriticalmassCoreBundle\Utility\PositionFilterChain;
+
+use Caldera\CriticalmassCoreBundle\Entity\Ride;
+use \Caldera\CriticalmassCoreBundle\Utility\DistanceCalculator;
+use \Caldera\CriticalmassCoreBundle\Entity\Position;
+use \Caldera\CriticalmassCoreBundle\Entity\RideTrack;
+
+
+class RideCompiler
+{
+    protected $ride;
+    protected $doctrine;
+
+    public function __construct(Ride $ride, $doctrine)
+    {
+        $this->ride = $ride;
+        $this->doctrine = $doctrine;
+    }
+
+    public function execute()
+    {
+        $positions = $this->doctrine->getRepository('CalderaCriticalmassCoreBundle:Position')->findByRide($this->ride->getId());
+
+        $latestPosition = null;
+
+        $dc = new DistanceCalculator();
+
+        foreach ($positions as $position)
+        {
+
+            if (!$latestPosition)
+            {
+                $latestPosition = $position;
+                $this->convertPositionToRideTrack($position);
+            }
+            else
+            {
+                $distance = $dc->calculateDistanceFromPositionToPosition($latestPosition, $position);
+
+                if ($distance > 0.05)
+                {
+                    $this->convertPositionToRideTrack($position);
+                    $latestPosition = $position;
+                }
+            }
+        }
+    }
+
+    protected function convertPositionToRideTrack(Position $position)
+    {
+        $rt = new RideTrack();
+        $rt->setRide($this->ride);
+        $rt->setLatitude($position->getLatitude());
+        $rt->setLongitude($position->getLongitude());
+        $rt->setDateTime($position->getCreationDateTime());
+
+        $manager = $this->doctrine->getManager();
+        $manager->persist($rt);
+        $manager->flush();
+    }
+} 
