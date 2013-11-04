f2246aca365560761f286679fbfaef44f681f550
Malte Hübner <maltehuebner@gmx.org>
2013-11-04 12:18:56 +0100

    DefaultController angelegt, hauptsächlich zum Testen verwendet.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace Caldera\CriticalmassGalleryBundle\Controller;
 
+use Caldera\CriticalmassGalleryBundle\Utility\ExifReader;
+use Caldera\CriticalmassStatisticBundle\Utility\Trackable;
+use Caldera\CriticalmassGalleryBundle\Utility\ExifGpsReader;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class DefaultController extends Controller implements Trackable
 {
     public function indexAction($name)
     {
-        return $this->render('CalderaCriticalmassGalleryBundle:Default:index.html.twig', array('name' => $name));
+        $filename = "/Applications/XAMPP/htdocs/criticalmass/symfony/web/IMG_9707.jpg";
+
+        $exif = exif_read_data($filename);
+
+        $er = new ExifReader();
+
+        print_r($er->getLens());
+
+        return new Response();
     }
 }
