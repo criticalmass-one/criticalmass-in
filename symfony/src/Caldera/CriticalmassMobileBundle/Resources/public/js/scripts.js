4504667e1826098676a1fc6e7e1d1b37c1c9c405
Malte Hübner <maltehuebner@gmx.org>
2014-05-04 03:39:00 +0200

    Jetzt wird auch die Stadt gewechselt, wenn man diesen Knopf drückt.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/scripts.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/scripts.js
@@ -23,7 +23,11 @@ function loadAllCities()
             for (index in data.cities)
             {
                 var city = data.cities[index];
+                var cityId = 'city-' + city.id + '-' + city.slug;
                 var listItem = document.createElement('li');
+
+                $(listItem).attr('data-cityslug', city.slug);
+                $(listItem).attr('id', cityId);
 
                 $(listItem).append('<h3>' + city.title + '</h3>');
 
@@ -33,6 +37,8 @@ function loadAllCities()
                 }
 
                 $('#cityList').append(listItem);
+
+                $('#' + cityId).click(function(data) { citySlugString = $(this).attr('data-cityslug'); });
             }
 
             $('#cityList').listview('refresh');
