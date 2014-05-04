e1a6d73e42cc56ee6a4739310b2bb36d82aaec6b
Malte Hübner <maltehuebner@gmx.org>
2014-05-04 03:13:47 +0200

    Städteauswahl aus dem rechten Panel in eine ListView umgebaut.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/scripts.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/scripts.js
@@ -12,65 +12,30 @@ function logout()
     });
 }
 
-/**
- * Ruft die Liste der Staedte mit den Koordinaten der Position des Benutzers
- * auf, so dass die Liste nach der Entfernung der Staedte zu der angegebenen
- * Position sortiert wird. Anschliessend wird der Create-Trigger aufgerufen, um
- * die jQuery-mobile-Darstellung aufzurufen.
- *
- * @param position: Ergebnis der Geolocation-Abfrage
- */
-function sidebarLocationSuccess(position)
+function loadAllCities()
 {
-	$('#rightpanel').load('/loadcities/', { 'latitude': position.coords.latitude, 'longitude': position.coords.longitude }, function()
-	{
-        sessionStorage.setItem('rightsidebarContent', $('#rightpanel').html());
-		$('#rightpanel').trigger('create');
-	});
-}
-
-/**
- * Wenn keine Geolocation-Daten bestimmt werden koennen, wird die Staedteliste
- * ohne Koordinaten aufgerufen, so dass die Resultate lediglich nach dem Alpha-
- * bet sortiert werden. Anschliessend wird der Create-Trigger aufgerufen, um
- * die jQuery-mobile-Darstellung aufzurufen.
- */
-function sidebarLocationNoPosition()
-{
-	$('#rightpanel').load('/loadcities/', function()
-	{
-        sessionStorage.setItem('rightsidebarContent', $('#rightpanel').html());
-		$('#rightpanel').trigger('create');
-	});
-}
-
-/**
- * Beim Oeffnen einer Seite wird ein Event-Listener installiert, der das Oeff-
- * nen des rechten Panels abfaengt. Bevor das Panel geoeffnet wird, erfolgt ei-
- * ne Geolocation-Abfrage, um die einzublendende Staedte-Liste nach der Ent-
- * fernung der Staedte zum Standort des Benutzers zu sortieren.
- */
-$(window).load(function()
-{
-	// Event-Listener eintragen
-	$('#rightpanel').on('panelbeforeopen', function(event, ui)
-	{
-        if (sessionStorage.getItem('rightsidebarContent') != null)
+    $.ajax({
+        type: 'GET',
+        url: '/app_dev.php/api/cities/listall',
+        cache: false,
+        success: function(data)
         {
-            $('#rightpanel').html(sessionStorage.getItem('rightsidebarContent'));
-            $('#rightpanel').trigger('create');
+            for (index in data.cities)
+            {
+                var city = data.cities[index];
+                var listItem = document.createElement('li');
+
+                $(listItem).append('<h3>' + city.title + '</h3>');
+
+                if (city.description != null)
+                {
+                    $(listItem).append('<p>' + city.description + '</p>');
+                }
+
+                $('#cityList').append(listItem);
+            }
+
+            $('#cityList').listview('refresh');
         }
-        else
-		// Ist der Geolocation-Service verfuegbar?
-		if (navigator.geolocation)
-		{
-			// dann die Staedte nach ihrer Entfernung zum Benutzer sortieren
-			navigator.geolocation.getCurrentPosition(sidebarLocationSuccess, sidebarLocationNoPosition);
-		}
-		else
-		{
-			// ansonsten die nach dem Alphabet sortierte Liste ausgeben
-			sidebarLocationNoPosition();
-		}
-	});
-});
+    });
+}
