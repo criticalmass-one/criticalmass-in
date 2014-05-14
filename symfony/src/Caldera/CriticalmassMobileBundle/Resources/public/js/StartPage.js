af9a5c6eaa6ff109d13cb2a471dcb34187136e3a
Malte Hübner <maltehuebner@gmx.org>
2014-05-14 22:27:56 +0200

    Daten der Städteliste werden jetzt im Sitzungsspeicher gehalten.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/StartPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/StartPage.js
@@ -14,47 +14,60 @@ StartPage.prototype.cityList;
 
 StartPage.prototype.loadAllCities = function()
 {
-    $.ajax({
-        type: 'GET',
-        url: this.getApiPrefix() + 'cities/listall',
-        cache: false,
-        context: this,
-        success: function(data)
-        {
-            for (index in data.cities)
+    if (sessionStorage.cityListData)
+    {
+        this.createCityList(JSON.parse(sessionStorage.cityListData));
+    }
+    else
+    {
+        $.ajax({
+            type: 'GET',
+            url: this.getApiPrefix() + 'cities/listall',
+            cache: false,
+            context: this,
+            success: function(data)
             {
-                var city = data.cities[index];
-                var cityId = 'city-' + city.id + '-' + city.slug;
-                var listItem = document.createElement('li');
-
-                $(listItem).attr('data-cityslug', city.slug);
-                $(listItem).attr('id', cityId);
-
-                var linkItem = document.createElement('a');
-
-                $(linkItem).append('<img src="http://www.criticalmass.local/bundles/calderacriticalmassmobile/images/criticalmass-bikefist-100.png" />')
-                $(linkItem).append('<h3>' + city.title + '</h3>');
-
-                if (city.description != null)
-                {
-                    $(linkItem).append('<p>' + city.description + '</p>');
-                }
-
-                $(listItem).append(linkItem);
-
-                $('#cityList').append(listItem);
-
-                // TODO Mit echtem Binding wird das hier schöner
-                var this2 = this;
-
-                $('#' + cityId).click(function()
-                {
-                    var newCitySlug = $(this).attr('data-cityslug');
-                    this2.switchCity(newCitySlug);
-                });
+                sessionStorage.cityListData = JSON.stringify(data);
+                this.createCityList(data);
             }
+        });
+    }
+}
 
-            $('#cityList').listview('refresh');
+StartPage.prototype.createCityList = function(data)
+{
+    for (index in data.cities)
+    {
+        var city = data.cities[index];
+        var cityId = 'city-' + city.id + '-' + city.slug;
+        var listItem = document.createElement('li');
+
+        $(listItem).attr('data-cityslug', city.slug);
+        $(listItem).attr('id', cityId);
+
+        var linkItem = document.createElement('a');
+
+        $(linkItem).append('<img src="http://www.criticalmass.local/bundles/calderacriticalmassmobile/images/criticalmass-bikefist-100.png" />')
+        $(linkItem).append('<h3>' + city.title + '</h3>');
+
+        if (city.description != null)
+        {
+            $(linkItem).append('<p>' + city.description + '</p>');
         }
-    });
+
+        $(listItem).append(linkItem);
+
+        $('#cityList').append(listItem);
+
+        // TODO Mit echtem Binding wird das hier schöner
+        var this2 = this;
+
+        $('#' + cityId).click(function()
+        {
+            var newCitySlug = $(this).attr('data-cityslug');
+            this2.switchCity(newCitySlug);
+        });
+    }
+
+    $('#cityList').listview('refresh');
 }
