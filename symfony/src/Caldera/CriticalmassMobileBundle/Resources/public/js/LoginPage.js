27160f1dfcc8dce47c0b07eaa557a41da56b71b3
Malte Hübner <maltehuebner@gmx.org>
2014-05-14 19:43:02 +0200

    LoginPage erstellt.

---
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/LoginPage.js
@@ -0,0 +1,34 @@
+$(function ()
+{
+    $('#form-login').submit(function (e) {
+        e.preventDefault();
+
+        var $this = $(e.currentTarget),
+            inputs = {};
+
+        // Send all form's inputs
+        $.each($this.find('input'), function (i, item) {
+            var $item = $(item);
+            inputs[$item.attr('name')] = $item.val();
+        });
+
+        // Send form into ajax
+        $.ajax({
+            url: $this.attr('action'),
+            type: 'POST',
+            dataType: 'json',
+            data: inputs,
+            success: function (data) {
+                if (data.has_error) {
+                    // Insert your error process
+                    alert(data.error);
+                }
+                else {
+                    // Insert your success process
+                    alert('Welcome ' + data.username + ' !');
+                    window.location = data.target_path;
+                }
+            }
+        });
+    });
+});
