f3800e62d3c60e0fcd3b669b15b091ba5147e2fe
Malte Hübner <maltehuebner@gmx.org>
2014-05-20 18:40:21 +0200

    Sonst ist das kaputt :(

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/MapPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/MapPage.js
@@ -16,7 +16,7 @@ MapPage.prototype.initMap = function()
 {
     if (this.map == null)
     {
-        this.map = new Map('map', new City('hamburg'), this);
+        this.map = new Map('map', CityFactory.getCityBySlug('hamburg'), this);
     }
     else
     {
