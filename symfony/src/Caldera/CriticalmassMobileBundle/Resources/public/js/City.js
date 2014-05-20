e539930832fe63850d9878111dbc3c769142c4c7
Malte Hübner <maltehuebner@gmx.org>
2014-05-20 18:40:32 +0200

    City bildet jetzt das Datenbank-Schema ab.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/City.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/City.js
@@ -1,12 +1,22 @@
-City = function(citySlug)
+City = function()
 {
-    if (citySlug)
-    {
-        this.citySlug = citySlug;
-    }
 };
 
+City.prototype.id = null;
 City.prototype.citySlug = null;
+City.prototype.city = null;
+City.prototype.title = null;
+City.prototype.description = null;
+
+City.prototype.getId = function()
+{
+    return this.id;
+};
+
+City.prototype.setId = function(id)
+{
+    this.id = id;
+};
 
 City.prototype.getCitySlug = function()
 {
@@ -17,3 +27,42 @@ City.prototype.setCitySlug = function(citySlug)
 {
     this.citySlug = citySlug;
 };
+
+City.prototype.getCity = function()
+{
+    return this.city;
+};
+
+City.prototype.setCity = function(city)
+{
+    this.city = city;
+};
+
+City.prototype.getTitle = function()
+{
+    return this.title;
+};
+
+City.prototype.setTitle = function(title)
+{
+    this.title = title;
+};
+
+City.prototype.getDescripton = function()
+{
+    return this.description;
+};
+
+City.prototype.setDescription = function(description)
+{
+    this.description = description;
+};
+
+City.prototype.parseAjaxResultData = function(ajaxResultData)
+{
+    this.setId(ajaxResultData.id);
+    this.setCitySlug(ajaxResultData.slug);
+    this.setCity(ajaxResultData.city);
+    this.setTitle(ajaxResultData.title);
+    this.setDescription(ajaxResultData.description);
+}
