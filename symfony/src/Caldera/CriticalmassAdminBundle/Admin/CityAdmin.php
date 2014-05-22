58beccdc814666fb1f9b1978641708255e066da5
Malte Hübner <maltehuebner@gmx.org>
2014-05-23 01:00:36 +0200

    CityAdmin hinzugefügt und verlinkt.

---
+++ symfony/src/Caldera/CriticalmassAdminBundle/Admin/CityAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Caldera\CriticalmassAdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CityAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('city', 'text', array('label' => 'Stadt'))
+            ->add('title', 'text', array('label' => 'Bezeichnung'))
+            ->add('description', 'text', array('label' => 'Beschreibung'))
+            ->add('url', 'text', array('label' => 'Webseite'))
+            ->add('facebook', 'text', array('label' => 'facebook-Seite'))
+            ->add('twitter', 'text', array('label' => 'Twitter-Konto'))
+            ->add('latitude', 'text', array('label' => 'Breitengrad'))
+            ->add('longitude', 'text', array('label' => 'Längengrad'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('city')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('city')
+        ;
+    }
+}
