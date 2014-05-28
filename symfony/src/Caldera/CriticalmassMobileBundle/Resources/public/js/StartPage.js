2d833f8df56fa5f19e970fcd522c788234834535
Malte Hübner <maltehuebner@gmx.org>
2014-05-28 19:33:04 +0200

    Anzeige der Städte-Liste auf der Startseite aufgebohrt.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/StartPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/StartPage.js
@@ -16,39 +16,51 @@ StartPage.prototype.initCityList = function()
     {
         CityFactory.storeAllCities();
     }
-
-    var cities = JSON.parse(localStorage.cityListData).cities;
-
-    for (index in cities)
+    else
     {
-        var city = cities[index];
-        var cityId = 'city-' + city.id + '-' + city.slug;
-        var listItem = document.createElement('li');
+        var cities = JSON.parse(localStorage.cityListData).cities;
 
-        $(listItem).attr('data-cityslug', city.slug);
-        $(listItem).attr('id', cityId);
-
-        var linkItem = document.createElement('a');
-
-        $(linkItem).append('<img src="http://www.criticalmass.local/bundles/calderacriticalmassmobile/images/criticalmass-bikefist-100.png" />')
-        $(linkItem).append('<h3>' + city.title + '</h3>');
-
-        if (city.description != null)
+        for (index in cities)
         {
-            $(linkItem).append('<p>' + city.description + '</p>');
+            var city = cities[index];
+            var cityId = 'city-' + city.id + '-' + city.slug;
+            var listItem = document.createElement('li');
+
+            $(listItem).attr('data-cityslug', city.slug);
+            $(listItem).attr('id', cityId);
+
+            var linkItem = document.createElement('a');
+
+            $(linkItem).append('<img src="http://www.criticalmass.local/bundles/calderacriticalmassmobile/images/criticalmass-bikefist-100.png" />')
+            $(linkItem).append('<h3>' + city.title + '</h3>');
+
+            if (city.description != null)
+            {
+                $(linkItem).append('<p>' + city.description + '</p>');
+            }
+
+            var ride = RideFactory.getRideFromStorageBySlug(city.slug);
+
+            if (ride)
+            {
+                var dateTime = ride.getDateTime();
+
+                $(linkItem).append('<p class="ui-li-aside">' + dateTime.getDate() + '.' + (dateTime.getMonth() + 1) + '.' + dateTime.getFullYear() + ' ' + dateTime.getHours() + '.' + (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()) + ' Uhr</p>');
+            }
+
+            $(listItem).append(linkItem);
+
+            $('#cityList').append(listItem);
+
+            var this2 = this;
+
+            $('#' + cityId).click(function()
+            {
+                var newCitySlug = $(this).attr('data-cityslug');
+                this2.switchCityBySlug(newCitySlug);
+                window.location = '#mapPage';
+            });
         }
-
-        $(listItem).append(linkItem);
-
-        $('#cityList').append(listItem);
-
-        var this2 = this;
-
-        $('#' + cityId).click(function()
-        {
-            var newCitySlug = $(this).attr('data-cityslug');
-            this2.switchCityBySlug(newCitySlug);
-        });
     }
 
     $('#cityList').listview('refresh');
