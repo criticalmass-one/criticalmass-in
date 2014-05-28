7f93cfd80eb3deec2b44f755f01267d48d1cc1b7
Malte Hübner <maltehuebner@gmx.org>
2014-05-28 20:30:50 +0200

    Und noch einmal.

--- symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
@@ -8,7 +8,7 @@ class RideRepository extends EntityRepository
 {
 	public function findCurrentRides()
 	{
-		$query = $this->getEntityManager()->createQuery("SELECT r AS ride FROM CalderaCriticalmassCoreBundle:Ride r GROUP BY r.city ORDER BY r.date DESC");
+		$query = $this->getEntityManager()->createQuery("SELECT r AS ride FROM CalderaCriticalmassCoreBundle:Ride r GROUP BY r.city ORDER BY r.dateTime DESC");
 
 		return $query->getResult();
 	}
