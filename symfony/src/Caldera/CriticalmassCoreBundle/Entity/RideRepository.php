100550c87500f93e5e38baa2f98855e8a562c2fa
Malte Hübner <maltehuebner@gmx.org>
2014-06-05 23:35:48 +0200

    Touren werden jetzt nur innerhalb des jeweiligen Sichtbarkeitszeitraumes auf der Karte ausgegeben.

--- symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
@@ -8,7 +8,7 @@ class RideRepository extends EntityRepository
 {
 	public function findCurrentRides()
 	{
-		$query = $this->getEntityManager()->createQuery("SELECT r AS ride FROM CalderaCriticalmassCoreBundle:Ride r GROUP BY r.city ORDER BY r.dateTime DESC");
+		$query = $this->getEntityManager()->createQuery('SELECT r AS ride FROM CalderaCriticalmassCoreBundle:Ride r WHERE r.visibleSince <= CURRENT_TIMESTAMP() AND r.visibleUntil >= CURRENT_TIMESTAMP() GROUP BY r.city ORDER BY r.dateTime DESC');
 
 		return $query->getResult();
 	}
