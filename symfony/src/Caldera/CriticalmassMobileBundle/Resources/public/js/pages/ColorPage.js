ffe393601dc98126595999f046938c6b0ee3bfd0
Malte Hübner <maltehuebner@gmx.org>
2014-06-05 23:46:23 +0200

    ColorPage angelegt.

---
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/ColorPage.js
@@ -0,0 +1,13 @@
+ColorPage = function(pageIdentifier)
+{
+    this.pageIdentifier = pageIdentifier;
+};
+
+ColorPage.prototype = new AppPage();
+
+ColorPage.prototype.constructor = ColorPage;
+
+ColorPage.prototype.initPage = function()
+{
+
+};
