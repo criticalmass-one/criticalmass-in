b8e709eec6d4212bedb10fb8fce6fee1ee0c4552
Malte Hübner <maltehuebner@gmx.org>
2014-06-06 16:46:51 +0200

    Kleine Spielerei.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/ColorPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/ColorPage.js
@@ -9,5 +9,34 @@ ColorPage.prototype.constructor = ColorPage;
 
 ColorPage.prototype.initPage = function()
 {
+    var this2 = this;
 
+    $('#' + this.pageIdentifier + ' input').on('change', function()
+    {
+        this2.updateUserColor();
+    });
+
+    $('#randomColorButton').on('click', function()
+    {
+        this2.setRandomColor();
+    });
+};
+
+ColorPage.prototype.setRandomColor = function()
+{
+    var red = Math.floor(Math.random() * 255);
+    var green = Math.floor(Math.random() * 255);
+    var blue = Math.floor(Math.random() * 255);
+
+    $('#slider-usercolor-red').slider('value', red);
+    $('#colorExample').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
+};
+
+ColorPage.prototype.updateUserColor = function()
+{
+    var red = $('#slider-usercolor-red').val();
+    var green = $('#slider-usercolor-green').val();
+    var blue = $('#slider-usercolor-blue').val();
+
+    $('#colorExample').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
 };
