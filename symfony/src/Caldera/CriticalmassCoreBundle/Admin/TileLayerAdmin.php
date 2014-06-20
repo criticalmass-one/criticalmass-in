0edb7fbc02e346f1425232654ba5bfc524938fb9
Malte Hübner <maltehuebner@gmx.org>
2014-06-20 22:04:33 +0200

    Administrationsbereich für TileLayer hinzugefügt.

---
+++ symfony/src/Caldera/CriticalmassCoreBundle/Admin/TileLayerAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Caldera\CriticalmassCoreBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class TileLayerAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Beschreibung', array('class' => 'col-md-6'))
+                ->add('title', 'text', array('label' => 'Titel'))
+                ->add('description', 'textarea', array('label' => 'Beschreibung'))
+            ->end()
+            ->with('Einstellungen', array('class' => 'col-md-6'))
+                ->add('address', 'text', array('label' => 'Adresse'))
+                ->add('attributation', 'text', array('label' => 'Attributation'))
+            ->end();
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+        ;
+    }
+}
