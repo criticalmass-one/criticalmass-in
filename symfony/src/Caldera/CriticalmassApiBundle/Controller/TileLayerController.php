64bef0126903e62c07c18cede81fa005197cafa6
Malte Hübner <maltehuebner@gmx.org>
2014-06-22 01:43:53 +0200

    TileLayerController zum Ausgeben vorhandener TileLayer hinzugefügt.

---
+++ symfony/src/Caldera/CriticalmassApiBundle/Controller/TileLayerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Caldera\CriticalmassApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Caldera\CriticalmassCoreBundle\Utility as Utility;
+use Caldera\CriticalmassCoreBundle\Entity as Entity;
+
+class TileLayerController extends Controller
+{
+    public function listtilelayersAction()
+    {
+        $user = $this->getUser();
+
+        $resultArray = array();
+
+        $tileLayers = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:TileLayer')->findBy(array('active' => true), array('title' => 'ASC'));
+
+        foreach ($tileLayers as $tileLayer)
+        {
+            $resultArray[$tileLayer->getId()] = array('title' => $tileLayer->getTitle(),
+                                   'address' => ($tileLayer->getPlusOnly() && !$user ? '' : $tileLayer->getAddress()),
+                                   'attributation' => $tileLayer->getAttributation());
+        }
+
+        // neue Antwort zusammensetzen und als JSON klassifizieren
+        $response = new Response();
+        $response->setContent(json_encode($resultArray));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
