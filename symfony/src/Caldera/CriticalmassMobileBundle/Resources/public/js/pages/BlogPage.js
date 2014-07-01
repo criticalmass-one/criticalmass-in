5bb7a4f0225f2f815b48e3787d9b2b2f47857a3c
Malte Hübner <maltehuebner@gmx.org>
2014-07-02 00:22:40 +0200

    BlogPage implementiert.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/BlogPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/BlogPage.js
@@ -8,5 +8,26 @@ BlogPage.prototype.constructor = BlogPage;
 
 BlogPage.prototype.initPage = function()
 {
-    alert('foobarbaz');
+    function callback()
+    {
+        var articleList = ArticleFactory.getArticles();
+
+        for (var index in articleList)
+        {
+            var article = articleList[index];
+
+            var html = '<li id="blogArticle' + article.getId() + '" class="blogArticle ui-corner-all custom-corners">';
+            html += '<div class="ui-bar ui-bar-a"><h3>' + article.getTitle() + '</h3></div>';
+            html += '<div class="ui-body ui-body-a">';
+            html += '<img class="gravatarIcon" src="https://www.gravatar.com/avatar/' + article.getGravatar() + '?s=42" />';
+            html += '<span class="author">von <strong class="username">' + article.getUsername() + '</strong><br /><time>' + article.getFormattedDateTime() + '</time></span>';
+            html += '<p>' + article.getFormattedText() + '</p></div>';
+            html += '</li>';
+
+            $('ul#blogArticleList').append(html);
+        }
+    }
+
+    CallbackHell.registerEventListener('articleListRefreshed', callback);
+    ArticleFactory.refreshAllStoredArticles();
 };
