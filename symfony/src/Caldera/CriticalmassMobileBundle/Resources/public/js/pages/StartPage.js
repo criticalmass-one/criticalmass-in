86c7cedd5a968664ef3561017e5abd58c0a85d99
Malte Hübner <maltehuebner@gmx.org>
2014-07-02 09:15:45 +0200

    Einrückungen und so korrigiert.

--- symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/StartPage.js
+++ symfony/src/Caldera/CriticalmassMobileBundle/Resources/public/js/pages/StartPage.js
@@ -3,7 +3,7 @@ StartPage = function(pageIdentifier)
     this.pageIdentifier = pageIdentifier;
 
     this.initStartPageEventListeners();
-}
+};
 
 StartPage.prototype = new AppPage();
 StartPage.prototype.constructor = StartPage;
@@ -11,54 +11,54 @@ StartPage.prototype.constructor = StartPage;
 StartPage.prototype.initStartPageEventListeners = function()
 {
 
-}
+};
 
 StartPage.prototype.initPage = function()
 {
-        var cities = CityFactory.getAllCities();
+    var cities = CityFactory.getAllCities();
 
-        for (index in cities)
+    for (index in cities)
+    {
+        var city = cities[index];
+        var cityId = 'city-' + city.id + '-' + city.slug;
+        var listItem = document.createElement('li');
+
+        $(listItem).attr('data-cityslug', city.slug);
+        $(listItem).attr('id', cityId);
+
+        var linkItem = document.createElement('a');
+
+        $(linkItem).append('<h3>' + city.title + '</h3>');
+
+        if (city.description != null)
         {
-            var city = cities[index];
-            var cityId = 'city-' + city.id + '-' + city.slug;
-            var listItem = document.createElement('li');
-
-            $(listItem).attr('data-cityslug', city.slug);
-            $(listItem).attr('id', cityId);
-
-            var linkItem = document.createElement('a');
-
-            $(linkItem).append('<h3>' + city.title + '</h3>');
-
-            if (city.description != null)
-            {
-                $(linkItem).append('<p>' + city.description + '</p>');
-            }
-
-            var ride = RideFactory.getRideFromStorageBySlug(city.slug);
-
-            if (ride)
-            {
-                var dateTime = ride.getDateTime();
-
-                $(linkItem).append('<p class="ui-li-aside">' + dateTime.getDate() + '.' + (dateTime.getMonth() + 1) + '.' + dateTime.getFullYear() + ' ' + dateTime.getHours() + '.' + (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()) + ' Uhr</p>');
-            }
-
-            $(listItem).append(linkItem);
-
-            $('#cityList').append(listItem);
-
-            var this2 = this;
-
-            $('#' + cityId).click(function()
-            {
-                var newCitySlug = $(this).attr('data-cityslug');
-                this2.switchCityBySlug(newCitySlug);
-
-                $(":mobile-pagecontainer").pagecontainer("change", "#mapPage");
-            });
-
-
-            $('#cityList').listview('refresh');
+            $(linkItem).append('<p>' + city.description + '</p>');
         }
-}
+
+        var ride = RideFactory.getRideFromStorageBySlug(city.slug);
+
+        if (ride)
+        {
+            var dateTime = ride.getDateTime();
+
+            $(linkItem).append('<p class="ui-li-aside">' + dateTime.getDate() + '.' + (dateTime.getMonth() + 1) + '.' + dateTime.getFullYear() + ' ' + dateTime.getHours() + '.' + (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()) + ' Uhr</p>');
+        }
+
+        $(listItem).append(linkItem);
+
+        $('#cityList').append(listItem);
+
+        var this2 = this;
+
+        $('#' + cityId).click(function()
+        {
+            var newCitySlug = $(this).attr('data-cityslug');
+            this2.switchCityBySlug(newCitySlug);
+
+            $(":mobile-pagecontainer").pagecontainer("change", "#mapPage");
+        });
+
+
+        $('#cityList').listview('refresh');
+    }
+};
