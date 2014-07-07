b14ed69c8fa872ab9fc5fdeb0d8b1d3743f5e1a6
Malte Hübner <maltehuebner@gmx.org>
2014-07-07 20:18:45 +0200

    Noch etwas mehr an der Statistik-Seite gefeilt.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/StatisticController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/StatisticController.php
@@ -8,6 +8,23 @@ class StatisticController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CalderaCriticalmassDesktopBundle:Statistic:index.html.twig');
+        $cities = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:City')->findBy(array(), array('city' => 'ASC'));
+
+        $ridesResult = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findLatestRidesOrderByParticipants(new \DateTime('2014-06-01 00:00:00'), new \DateTime('2014-06-30 23:59:59'));
+
+        $rides = array();
+
+        foreach ($cities as $city)
+        {
+            $rides[$city->getMainSlugString()] = null;
+        }
+
+        foreach ($ridesResult as $ride)
+        {
+            $rides[$ride->getCity()->getMainSlugString()] = $ride;
+        }
+
+
+        return $this->render('CalderaCriticalmassDesktopBundle:Statistic:index.html.twig', array('cities' => $cities, 'rides' => $rides));
     }
 }
