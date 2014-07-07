eb8a3b5170735cf9babbe286c1970100600d3531
Malte Hübner <maltehuebner@gmx.org>
2014-07-07 20:41:14 +0200

    Zähle im Controller nun auch die Gesamtzahl der Teilnehmer und die maximale Anzahl aller Städte.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/StatisticController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/StatisticController.php
@@ -12,6 +12,19 @@ class StatisticController extends Controller
 
         $rides = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findLatestRidesOrderByParticipants(new \DateTime('2014-06-01 00:00:00'), new \DateTime('2014-06-30 23:59:59'));
 
-        return $this->render('CalderaCriticalmassDesktopBundle:Statistic:index.html.twig', array('cities' => $cities, 'rides' => $rides));
+        $maxParticipants = 0;
+        $totalParticipants = 0;
+
+        foreach ($rides as $ride)
+        {
+            if ($ride->getEstimatedParticipants() > $maxParticipants)
+            {
+                $maxParticipants = $ride->getEstimatedParticipants();
+            }
+
+            $totalParticipants += $ride->getEstimatedParticipants();
+        }
+
+        return $this->render('CalderaCriticalmassDesktopBundle:Statistic:index.html.twig', array('cities' => $cities, 'rides' => $rides, 'maxParticipants' => $maxParticipants, 'totalParticipants' => $totalParticipants));
     }
 }
