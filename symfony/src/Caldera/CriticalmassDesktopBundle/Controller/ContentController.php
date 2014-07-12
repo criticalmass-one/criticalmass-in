27185b9434ed57953ba2579b23ff83cbcfa652fb
Malte Hübner <maltehuebner@gmx.org>
2014-07-12 21:35:15 +0200

    ContentController zeigt nun auch den richtigen Inhalt an >.<

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/ContentController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/ContentController.php
@@ -3,14 +3,22 @@
 namespace Caldera\CriticalmassDesktopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ContentController extends Controller
 {
     public function showAction($classSlug, $itemSlug)
     {
         $contentClass = $this->getDoctrine()->getRepository('CalderaCriticalmassContentBundle:ContentClass')->findOneBy(array('slug' => $classSlug));
-        $contentItem = $this->getDoctrine()->getRepository('CalderaCriticalmassContentBundle:ContentItem')->findOneBy(array('contentClass' => $contentClass->getId()));
+        $contentItem = $this->getDoctrine()->getRepository('CalderaCriticalmassContentBundle:ContentItem')->findOneBy(array('contentClass' => $contentClass->getId(), 'slug' => $itemSlug));
 
-        return $this->render('CalderaCriticalmassDesktopBundle:Content:show.html.twig', array('contentItem' => $contentItem));
+        if ($contentItem)
+        {
+            return $this->render('CalderaCriticalmassDesktopBundle:Content:show.html.twig', array('contentItem' => $contentItem));
+        }
+        else
+        {
+            throw new NotFoundHttpException('Diesen Inhalt gibt es nicht. Mehr wissen wir auch nicht :(');
+        }
     }
 }
