383f0e40cf89cbb24720419e19f3b5123c3dfc8d
Malte Hübner <maltehuebner@gmx.org>
2014-07-15 23:17:47 +0200

    Rufe nun E-Mails über IMAP ab und zeige die sogar an.

--- symfony/src/Caldera/CriticalmassGlympseBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassGlympseBundle/Controller/DefaultController.php
@@ -3,11 +3,48 @@
 namespace Caldera\CriticalmassGlympseBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CalderaCriticalmassGlympseBundle:Default:index.html.twig', array('name' => $name));
+        $mbox = imap_open("{mail.caldera.cc:993/imap/ssl/novalidate-cert}", "malte@maltehuebner.com", "");
+
+        echo "<h1>Postfächer</h1>\n";
+        $folders = imap_listmailbox($mbox, "{mail.caldera.cc:993}", "*");
+
+        if ($folders == false) {
+            echo "Abruf fehlgeschlagen<br />\n";
+        } else {
+            foreach ($folders as $val) {
+                echo $val . "<br />\n";
+            }
+        }
+
+        echo "<h1>Nachrichten in INBOX</h1>\n";
+        $headers = imap_headers($mbox);
+
+        if ($headers == false) {
+            echo "Abruf fehlgeschlagen<br />\n";
+        } else {
+            $counter = 0;
+
+            foreach ($headers as $val) {
+                ++$counter;
+                $status = $val[1];
+
+                if ($status == "D")
+                {
+                    $body = imap_body($mbox, $counter);
+
+                    echo imap_qprint($body);
+                }
+            }
+        }
+
+        imap_close($mbox);
+
+        return new Response();
     }
 }
