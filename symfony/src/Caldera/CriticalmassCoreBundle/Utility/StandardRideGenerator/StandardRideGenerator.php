dd3734f98a96609e3345ecfd47039691b783199b
Malte Hübner <maltehuebner@gmx.org>
2014-07-19 13:01:17 +0200

    Poah, endlich auch noch diese blöden Visibility-DateTime-Werte berechnet.

--- symfony/src/Caldera/CriticalmassCoreBundle/Utility/StandardRideGenerator/StandardRideGenerator.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/StandardRideGenerator/StandardRideGenerator.php
@@ -27,6 +27,7 @@ class StandardRideGenerator {
         $this->calculateDate();
         $this->calculateTime();
         $this->calculateLocation();
+        $this->calculateVisibility();
 
         return $this->ride;
     }
@@ -90,5 +91,25 @@ class StandardRideGenerator {
         {
             $this->ride->setHasLocation(false);
         }
+    }
+
+    protected function calculateVisibility()
+    {
+        $intervalOneWeek = new \DateInterval('P1W');
+        $intervalThreeWeeks = new \DateInterval('P3W');
+
+        $timestamp = $this->ride->getDateTime()->format('U');
+
+        $visibleSince = new \DateTime();
+        $visibleSince->setTimestamp($timestamp);
+
+        $visibleUntil = new \DateTime();
+        $visibleUntil->setTimestamp($timestamp);
+
+        $visibleSince->sub($intervalThreeWeeks);
+        $visibleUntil->add($intervalOneWeek);
+
+        $this->ride->setVisibleSince($visibleSince);
+        $this->ride->setVisibleUntil($visibleUntil);
     }
 } 
