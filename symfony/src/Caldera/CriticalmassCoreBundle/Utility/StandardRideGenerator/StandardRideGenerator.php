ae7204833cbac8401c325cedd8ac0530b95b608c
Malte Hübner <maltehuebner@gmx.org>
2014-07-19 11:58:03 +0200

    Konstruktor und einige Eigenschaften angelegt.

--- symfony/src/Caldera/CriticalmassCoreBundle/Utility/StandardRideGenerator/StandardRideGenerator.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/StandardRideGenerator/StandardRideGenerator.php
@@ -2,6 +2,19 @@
 
 namespace Caldera\CriticalmassCoreBundle\Utility\StandardRideGenerator;
 
-class StandardRideGenerator {
+use Caldera\CriticalmassCoreBundle\Entity\Ride;
 
+class StandardRideGenerator {
+    protected $year;
+    protected $month;
+    protected $city;
+    protected $ride;
+
+    public function __construct($city, $year, $month)
+    {
+        $this->year = $year;
+        $this->month = $month;
+        $this->city = $city;
+        $this->ride = new Ride();
+    }
 } 
