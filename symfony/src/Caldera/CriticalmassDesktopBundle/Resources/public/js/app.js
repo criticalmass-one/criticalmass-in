bfca33e5e03a8955a425477d40115b83ea556c5e
Malte Hübner <maltehuebner@gmx.org>
2014-07-26 22:15:59 +0200

    En bisschen klappt das ja schon mit den Layern.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/app.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/app.js
@@ -152,3 +152,47 @@ function showCityInfo(citySlug)
     map.panTo([city.getLatitude(), city.getLongitude()]);
     map.setZoom(15);
 }
+
+if (document.body.clientWidth <= 767) {
+    var isCollapsed = true;
+} else {
+    var isCollapsed = false;
+    sidebar.show();
+}
+
+var mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    subdomains: ["otile1", "otile2", "otile3", "otile4"],
+    attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+});
+var mapquestOAM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+    maxZoom: 18,
+    subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+    attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+});
+var mapquestHYB = L.layerGroup([L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+    maxZoom: 18,
+    subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"]
+}), L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/hyb/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+    attribution: 'Labels courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+})]);
+
+CallbackHell.registerEventListener('tileLayerListRefreshed', function()
+{
+    var tileLayerObjects = TileLayerFactory.getTileLayers();
+    var tileLayers = new Array();
+
+    for (var index in tileLayerObjects)
+    {
+        var tileLayerObject = tileLayerObjects[index];
+        var tileLayer = L.tileLayer(tileLayerObject.getAddress(), {foo: 'bar'});
+
+        tileLayers[tileLayerObject.getTitle()] = tileLayer;
+    }
+
+    var layerControl = L.control.groupedLayers(tileLayers, {
+        collapsed: true
+    }).addTo(map);
+});
