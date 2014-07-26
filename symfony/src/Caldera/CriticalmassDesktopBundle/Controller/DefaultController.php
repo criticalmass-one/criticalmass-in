598c8002851e1c58820f74f5ca77a483f48d5570
Malte Hübner <maltehuebner@gmx.org>
2014-07-26 18:40:52 +0200

    Zeige nun nur eingeschaltete Städte an.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $cities = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:City')->findBy(array(), array('city' => 'ASC'));
+        $cities = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:City')->findBy(array('enabled' => true), array('city' => 'ASC'));
 
         return $this->render('CalderaCriticalmassDesktopBundle:City:list.html.twig', array('cities' => $cities));
     }
