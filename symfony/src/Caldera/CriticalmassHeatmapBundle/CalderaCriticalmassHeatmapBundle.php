2391898b5c9150a1aa253a595236ea276513f1aa
Malte Hübner <maltehuebner@gmx.org>
2014-07-28 18:30:47 +0200

    Heatmap-Kram ins StatisticBundle verschoben und HeatmapBundle gelöscht.

--- symfony/src/Caldera/CriticalmassHeatmapBundle/CalderaCriticalmassHeatmapBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Caldera\CriticalmassHeatmapBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CalderaCriticalmassHeatmapBundle extends Bundle
-{
-}
--- symfony/src/Caldera/CriticalmassStatisticBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassStatisticBundle/Controller/DefaultController.php
@@ -10,4 +10,43 @@ class DefaultController extends Controller
     {
         return $this->render('CalderaCriticalmassStatisticBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function generateAction($heatmapId)
+    {
+        $heatmap = $this->getDoctrine()->getRepository('CalderaCriticalmassHeatmapBundle:Heatmap')->findOneById($heatmapId);
+
+        foreach ($heatmap->getRides() as $ride)
+        {
+            echo $ride->getId()."<br />";
+            $gpxc = new GPXConverter();
+            $gpxc->loadContentFromString($ride->getOptimizedGpxContent());
+            $gpxc->parseContent();
+
+            $pathArray = $gpxc->getPathArray();
+
+            for ($zoom = 0; $zoom < 16; ++$zoom)
+            {
+                $osmmdc = new OSMMapDimensionCalculator($pathArray, $zoom);
+
+                for ($tileX = $osmmdc->getLeftTile(); $tileX <= $osmmdc->getRightTile(); ++$tileX)
+                {
+                    for ($tileY = $osmmdc->getTopTile(); $tileY >= $osmmdc->getBottomTile(); --$tileY)
+                    {
+                        $tile = new Tile();
+                        $tile->generatePlaceByTileXTileYZoom($tileX, $tileY, $zoom);
+                        $tile->dropPathArray($pathArray);
+
+                        $tp = new PNGTilePrinter($tile, $heatmap);
+                        $tp->printTile();
+                        $tp->saveTile();
+                    }
+                }
+            }
+        }
+
+        $response = new Response();
+        //$response->setContent($tp->getImageFileContent());
+        //$response->headers->set('Content-Type', 'image/png');
+        return $response;
+    }
 }
