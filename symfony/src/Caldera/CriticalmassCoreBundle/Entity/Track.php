846535a3c1869e5b1d5dad4909a33195f4c4ca71
Malte Hübner <maltehuebner@gmx.org>
2014-07-28 13:00:18 +0200

    Track-Entität für gespeicherte GPX-Dateien angelegt.

---
+++ symfony/src/Caldera/CriticalmassCoreBundle/Entity/Track.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Caldera\CriticalmassCoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="track")
+ * @ORM\Entity()
+ */
+class Track
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $username;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Ride", inversedBy="tracks")
+     * @ORM\JoinColumn(name="ride_id", referencedColumnName="id")
+     */
+    protected $ride;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Application\Sonata\UserBundle\Entity\User", inversedBy="tracks")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Caldera\CriticalmassGlympseBundle\Entity\Ticket", inversedBy="tracks")
+     * @ORM\JoinColumn(name="ticket_id", referencedColumnName="id")
+     */
+    protected $ticket;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $creationDateTime;
+
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $gpx;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Track
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set gpx
+     *
+     * @param string $gpx
+     * @return Track
+     */
+    public function setGpx($gpx)
+    {
+        $this->gpx = $gpx;
+
+        return $this;
+    }
+
+    /**
+     * Get gpx
+     *
+     * @return string 
+     */
+    public function getGpx()
+    {
+        return $this->gpx;
+    }
+
+    /**
+     * Set ride
+     *
+     * @param \Caldera\CriticalmassCoreBundle\Entity\Ride $ride
+     * @return Track
+     */
+    public function setRide(\Caldera\CriticalmassCoreBundle\Entity\Ride $ride = null)
+    {
+        $this->ride = $ride;
+
+        return $this;
+    }
+
+    /**
+     * Get ride
+     *
+     * @return \Caldera\CriticalmassCoreBundle\Entity\Ride 
+     */
+    public function getRide()
+    {
+        return $this->ride;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Application\Sonata\UserBundle\Entity\User $user
+     * @return Track
+     */
+    public function setUser(\Application\Sonata\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Application\Sonata\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set ticket
+     *
+     * @param \Caldera\CriticalmassGlympseBundle\Entity\Ticket $ticket
+     * @return Track
+     */
+    public function setTicket(\Caldera\CriticalmassGlympseBundle\Entity\Ticket $ticket = null)
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+
+    /**
+     * Get ticket
+     *
+     * @return \Caldera\CriticalmassGlympseBundle\Entity\Ticket 
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
+    /**
+     * Set creationDateTime
+     *
+     * @param \DateTime $creationDateTime
+     * @return Track
+     */
+    public function setCreationDateTime($creationDateTime)
+    {
+        $this->creationDateTime = $creationDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDateTime
+     *
+     * @return \DateTime 
+     */
+    public function getCreationDateTime()
+    {
+        return $this->creationDateTime;
+    }
+}
