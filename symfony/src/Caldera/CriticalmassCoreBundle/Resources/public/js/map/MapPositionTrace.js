0a8804b05516f47aa036dcf4b353e25e2b43faa9
Malte Hübner <maltehuebner@gmx.org>
2014-07-30 00:37:36 +0200

    Weitere Sachen implementiert.

--- symfony/src/Caldera/CriticalmassCoreBundle/Resources/public/js/map/MapPositionTrace.js
+++ symfony/src/Caldera/CriticalmassCoreBundle/Resources/public/js/map/MapPositionTrace.js
@@ -1,7 +1,6 @@
-MapPositionTrace = function(map, mapPositons)
+MapPositionTrace = function(map)
 {
     this.map = map;
-    this.mapPositions = mapPositions;
 };
 
 MapPositionTrace.prototype.map = null;
@@ -9,3 +8,32 @@ MapPositionTrace.prototype.map = null;
 MapPositionTrace.prototype.mapPositions = null;
 
 MapPositionTrace.prototype.positionsArray = new Array();
+
+MapPositionTrace.prototype.addPosition(position)
+{
+    this.positionsArray[position.username].push(position);
+
+};
+
+MapPositionTrace.prototype.drawPositionTrace()
+{
+    for (var username in this.positionArray)
+    {
+        var latLngs = new Array();
+
+        for (var index in this.positionArray[username])
+        {
+            var position = this.positionArray[username][index];
+
+            var latLng = L.latLng(position.latitude, position.longitude);
+
+            latLngs.push(latLng);
+        }
+
+        options = {
+            color: "#f00"
+        };
+
+        L.multiPolyline(latlngs, options);
+    }
+};
