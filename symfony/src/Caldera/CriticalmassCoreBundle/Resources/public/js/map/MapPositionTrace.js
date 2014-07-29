6cf8b5cd8d2b5753981b7d7223b42f9a0d3a0ce8
Malte Hübner <maltehuebner@gmx.org>
2014-07-30 00:22:13 +0200

    Erste Version eines MapPositionTrace angelegt.

---
+++ symfony/src/Caldera/CriticalmassCoreBundle/Resources/public/js/map/MapPositionTrace.js
@@ -0,0 +1,11 @@
+MapPositionTrace = function(map, mapPositons)
+{
+    this.map = map;
+    this.mapPositions = mapPositions;
+};
+
+MapPositionTrace.prototype.map = null;
+
+MapPositionTrace.prototype.mapPositions = null;
+
+MapPositionTrace.prototype.positionsArray = new Array();
