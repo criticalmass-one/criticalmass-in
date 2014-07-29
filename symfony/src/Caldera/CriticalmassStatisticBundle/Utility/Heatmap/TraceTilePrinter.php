9bdb2f2c0740f10e7662fc0a7bff397f9890d6c8
Malte Hübner <maltehuebner@gmx.org>
2014-07-29 11:27:38 +0200

    Wenn keine Pixel in einem File liegen, brauchen wir uns auch nicht weiter darum kümmern.

--- symfony/src/Caldera/CriticalmassStatisticBundle/Utility/Heatmap/TraceTilePrinter.php
+++ symfony/src/Caldera/CriticalmassStatisticBundle/Utility/Heatmap/TraceTilePrinter.php
@@ -7,6 +7,7 @@ use Caldera\CriticalmassStatisticBundle\Entity\Heatmap;
 
 class TraceTilePrinter extends AbstractTilePrinter {
     protected $ticket;
+    protected $changed = false;
 
     public function __construct(Tile $tile, Heatmap $heatmap, Ticket $ticket)
     {
@@ -17,50 +18,57 @@ class TraceTilePrinter extends AbstractTilePrinter {
 
     public function printTile()
     {
-        //$image = imagecreatetruecolor($this->tile->getSize(), $this->tile->getSize());
-        //$image = imagecreatefrompng('/Applications/XAMPP/htdocs/kachel.png');
-
-        if (file_exists($this->getPath().$this->getFilename()))
+        if ($this->tile->hasPixel())
         {
-            $oldImage = imagecreatefrompng($this->getPath().$this->getFilename());
-            $image = imagecreatefrompng($this->getPath().$this->getFilename());
-        }
-        else
-        {
-            $image = imagecreatetruecolor($this->tile->getSize(), $this->tile->getSize());
-            $transparent = imagecolorallocatealpha($image, 0, 0, 0, 127);
-            imagefill($image, 0, 0, $transparent);
-        }
+            if (file_exists($this->getPath().$this->getFilename()))
+            {
+                $image = imagecreatefrompng($this->getPath().$this->getFilename());
+            }
+            else
+            {
+                $image = imagecreatetruecolor($this->tile->getSize(), $this->tile->getSize());
+                $transparent = imagecolorallocatealpha($image, 0, 0, 0, 127);
+                imagefill($image, 0, 0, $transparent);
+            }
 
-        $pixel = $this->tile->popPixel();
-
-        while ($pixel != null)
-        {
-            $color = imagecolorallocate($image, $this->ticket->getColorRed(), $this->ticket->getColorGreen(), $this->ticket->getColorBlue());
-
-            imagefilledellipse($image, $pixel->getX(), $pixel->getY(), 1, 1, $color);
             $pixel = $this->tile->popPixel();
+
+            while ($pixel != null)
+            {
+                $color = imagecolorallocate($image, $this->ticket->getColorRed(), $this->ticket->getColorGreen(), $this->ticket->getColorBlue());
+
+                imagefilledellipse($image, $pixel->getX(), $pixel->getY(), 1, 1, $color);
+                $pixel = $this->tile->popPixel();
+
+                $this->changed = true;
+            }
+
+            if ($this->changed)
+            {
+                ob_start();
+                imagealphablending($image, false);
+                imagesavealpha($image, true);
+                imagepng($image);
+                $this->imageFileContent = ob_get_contents();
+                ob_end_clean();
+            }
+
+            imagedestroy($image);
         }
-
-        ob_start();
-        imagealphablending($image, false);
-        imagesavealpha($image, true);
-        imagepng($image);
-        $this->imageFileContent = ob_get_contents();
-        ob_end_clean();
-
-        imagedestroy($image);
     }
 
     public function saveTile()
     {
-        $pathname = $this->getPath();
-        $filename = $this->getFilename();
+        if ($this->changed)
+        {
+            $pathname = $this->getPath();
+            $filename = $this->getFilename();
 
-        @mkdir($pathname, 0777, true);
+            @mkdir($pathname, 0777, true);
 
-        $handle = fopen($pathname.$filename, "w");
-        fwrite($handle, $this->imageFileContent);
-        fclose($handle);
+            $handle = fopen($pathname.$filename, "w");
+            fwrite($handle, $this->imageFileContent);
+            fclose($handle);
+        }
     }
 }
