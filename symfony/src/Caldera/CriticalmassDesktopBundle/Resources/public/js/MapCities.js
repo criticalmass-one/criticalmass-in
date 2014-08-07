67e4b111f4fb33ce1d72c6c13daada7d46c99f08
Malte Hübner <maltehuebner@gmx.org>
2014-08-07 18:36:22 +0200

    So klappt’s schon viel besser.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/MapCities.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/MapCities.js
@@ -24,59 +24,22 @@ MapCities.prototype.drawCityMarkers = function()
     {
         var city = cities[slug];
         var ride = RideFactory.getRideFromStorageBySlug(slug);
-        var popupHTML = '';
         var latLng = null;
 
         if (ride != null && ride.getHasLocation())
         {
             latLng = [ride.getLatitude(), ride.getLongitude()];
-
-            popupHTML += '<h3>' + (ride.getTitle() ? ride.getTitle() : city.getTitle()) + '</h3>';
-            popupHTML += '<p>Nächste Tour: ' + ride.getFormattedDateTime() + '<br />Treffpunkt: ' + ride.getLocation() + '</p>';
-
-            if (ride.getDescription())
-            {
-                popupHTML += '<p>' + ride.getDescription() + '</p>';
-            }
-        }
-        else
-        if (ride != null)
-        {
-            latLng = [city.getLatitude(), city.getLongitude()];
-
-            popupHTML += '<h3>' + (ride.getTitle() ? ride.getTitle() : city.getTitle()) + '</h3>';
-            popupHTML += '<p>Nächste Tour: ' + ride.getFormattedDateTime() + '<br /><em>Der Treffpunkt ist noch nicht bekannt.</em></p>';
-
-            if (ride.getDescription())
-            {
-                popupHTML += '<p>' + ride.getDescription() + '</p>';
-            }
         }
         else
         {
             latLng = [city.getLatitude(), city.getLongitude()];
-            popupHTML += '<h3>' + city.getTitle() + '</h3>';
-
-            if (city.getDescription())
-            {
-                popupHTML += '<p>' + city.getDescription() + '</p>';
-            }
-            else
-            {
-                popupHTML += '<p>Für diese Stadt liegen momentan keine weiteren Informationen vor :(</p>';
-            }
         }
 
         var marker = L.marker(latLng, { riseOnHover: true, icon: criticalmassIcon, citySlug: slug });
-        marker.bindPopup(popupHTML);
 
-        var this2 = this;
-        marker.on('click', function(e)
+        marker.on('click', function()
         {
-            _paq.push(['trackEvent', 'markerCityRide', 'click']);
-
-            this2.map.positions.stopAutoFollowing();
-            this2.map.switchCity(this.options.citySlug);
+            showCityInfo(this.options.citySlug);
         });
 
         marker.addTo(this.map);
