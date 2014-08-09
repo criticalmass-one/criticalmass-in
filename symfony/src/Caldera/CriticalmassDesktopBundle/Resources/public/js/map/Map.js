2546ec1b8e8f80846be7b5170006788be073352c
Malte Hübner <maltehuebner@gmx.org>
2014-08-09 02:40:56 +0200

    Anordnung noch einmal gestrafft.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/map/Map.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/map/Map.js
@@ -16,14 +16,14 @@ Map.prototype.initMap = function()
     this.mapCities = new MapCities(this.map);
     this.mapCities.drawCityMarkers();
 
-    this.initMapView = new InitMapView(this.map);
-    this.initMapView.initView();
-
     this.mapTileLayer = new MapTileLayer(this.map);
     this.mapTileLayer.init();
 
     this.mapView = new MapView(this.map);
     this.mapView.initEventListeners();
+
+    this.initMapView = new InitMapView(this.map, this);
+    this.initMapView.initView();
 
     this.mapSidebar = new MapSidebar(this.map, 'sidebar');
     this.mapSidebar.init();
