04b90f427e08b15e5561861861203df260faabb8
Malte Hübner <maltehuebner@gmx.org>
2014-08-09 02:41:12 +0200

    InitMapView kann jetzt auf andere Module der Karte zugreifen.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/map/InitMapView.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/map/InitMapView.js
@@ -1,24 +1,23 @@
-InitMapView = function(map, citySlug)
+InitMapView = function(map, mapContainer)
 {
     this.map = map;
-    this.citySlug = citySlug;
-
-    this.mapView = new MapView(this.map);
+    this.mapContainer = mapContainer;
 };
 
 InitMapView.prototype.map = null;
+InitMapView.mapContainer = null;
 
 InitMapView.prototype.initView = function()
 {
     var city = CityFactory.getCityFromStorageBySlug(this.citySlug);
     var ride = RideFactory.getRideFromStorageBySlug(this.citySlug);
 
-    if (this.mapView.hasOverridingMapPosition())
+    if (this.mapContainer.mapView.hasOverridingMapPosition())
     {
         this.initWithOverride();
     }
     else
-    if (ride != null && ride.isRolling() && this.map.positions.getLatestPosition())
+    if (ride != null && ride.isRolling() && this.mapContainer.mapPositions.getLatestPosition())
     {
         this.initWithPosition();
     }
@@ -44,7 +43,7 @@ InitMapView.prototype.initView = function()
 
 InitMapView.prototype.initWithOverride = function()
 {
-    var mapPositon = this.mapView.getOverridingMapPosition();
+    var mapPositon = this.mapContainer.mapView.getOverridingMapPosition();
     this.map.setView([mapPositon.latitude, mapPositon.longitude], mapPositon.zoomFactor);
 
     //_paq.push(['trackEvent', 'initView', 'override']);
@@ -68,7 +67,7 @@ InitMapView.prototype.initWithCity = function(city)
 
 InitMapView.prototype.initWithPosition = function()
 {
-    var latestPosition = this.map.positions.getLatestPosition();
+    var latestPosition = this.mapContainer.mapPositions.getLatestPosition();
 
     this.map.setView(latestPosition.getLatLng(), 14);
 };
