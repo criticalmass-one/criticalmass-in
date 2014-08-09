d2e6848803a537e15723dc04fbaafdd1f0642436
Malte Hübner <maltehuebner@gmx.org>
2014-08-09 02:12:35 +0200

    Map weiter ausgebaut.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/Map.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/Map.js
@@ -23,4 +23,29 @@ Map.prototype.initMap = function()
 
     this.mapView = new MapView(this.map);
     this.mapView.initEventListeners();
+
+    this.mapTileLayer = new MapTileLayer(this.map);
+    this.mapTileLayer.init();
+
+    var zoomControl = L.control.zoom({
+        position: "topright"
+    }).addTo(this.map);
+
+    var heatmapGroup = L.layerGroup([L.tileLayer("https://www.criticalmass.cm/images/heatmap/ae5e6d7e21c2936051a06a0f2f40661e/{z}/{x}/{y}.png", {
+        maxZoom: 18
+    })]);
+
+    var layerControl = L.control.groupedLayers(this.mapTileLayer.tileLayers, {
+        "Critical Mass": {
+            "Städte": this.mapCities.layerGroup,
+            "Teilnehmer": this.mapPositions.layerGroup,
+            "Heatmaps": heatmapGroup
+        }
+    }, {
+        collapsed: false
+    });
+
+    layerControl.addTo(this.map);
+
+
 };
