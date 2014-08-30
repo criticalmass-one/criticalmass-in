61efbf513e35c445480bd673248175f3044988f0
Malte Hübner <maltehuebner@gmx.org>
2014-08-30 18:30:40 +0200

    Das war mal der Versuch, den Login-Vorgang mitten auf der Seite stattfinden zu lassen. Raus mit dem Slice.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/DefaultController.php
@@ -18,32 +18,7 @@ class DefaultController extends Controller
     {
         $city = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($slug)->getCity();
 
-        $request = $this->container->get('request');
-        /* @var $request \Symfony\Component\HttpFoundation\Request */
-        $session = $request->getSession();
-        /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        return $this->render('CalderaCriticalmassDesktopBundle:Default:index.html.twig', array('citySlug' => $city->getMainSlugString()));
 
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = '';
-        }
-
-        if ($error) {
-            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
-            $error = $error->getMessage();
-        }
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
-
-        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
-
-        return $this->render('CalderaCriticalmassDesktopBundle:Default:index.html.twig', array('last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,'citySlug' => $city->getMainSlugString()));
     }
 }
