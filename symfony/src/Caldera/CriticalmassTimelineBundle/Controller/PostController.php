d5ed87a0d2bedda09b32b4450443c97adaaae5bc
Malte Hübner <maltehuebner@gmx.org>
2014-09-23 17:59:25 +0200

    Neuen PostController angelegt, der sich global um die Darstellung von Kommentaren kümmern soll.

---
+++ symfony/src/Caldera/CriticalmassTimelineBundle/Controller/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Caldera\CriticalmassTimelineBundle\Controller;
+
+use Caldera\CriticalmassTimelineBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostController extends Controller
+{
+    public function listAction(Request $request)
+    {
+        $posts = $this->getDoctrine()->getRepository('CalderaCriticalmassTimelineBundle:Post')->findBy(array('enabled' => true), array('dateTime' => 'DESC'));
+
+        return $this->render('CalderaCriticalmassTimelineBundle:Timeline:list.html.twig', array('posts' => $posts));
+    }
+}
