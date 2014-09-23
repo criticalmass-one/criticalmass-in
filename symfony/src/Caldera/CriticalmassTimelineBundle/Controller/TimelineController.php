83f89784a43bb97dc398ebad9dfe81204f815345
Malte Hübner <maltehuebner@gmx.org>
2014-09-23 13:57:03 +0200

    Man kann nun auf Beiträge absenden.

--- symfony/src/Caldera/CriticalmassTimelineBundle/Controller/TimelineController.php
+++ symfony/src/Caldera/CriticalmassTimelineBundle/Controller/TimelineController.php
@@ -2,14 +2,33 @@
 
 namespace Caldera\CriticalmassTimelineBundle\Controller;
 
+use Caldera\CriticalmassTimelineBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class TimelineController extends Controller
 {
-    public function listAction()
+    public function listAction(Request $request)
     {
         $posts = $this->getDoctrine()->getRepository('CalderaCriticalmassTimelineBundle:Post')->findBy(array('enabled' => true), array('dateTime' => 'DESC'));
 
-        return $this->render('CalderaCriticalmassTimelineBundle:Timeline:list.html.twig', array('posts' => $posts));
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('message', 'text')
+            ->add('LOS', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $post->setUser($this->getUser());
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return $this->render('CalderaCriticalmassTimelineBundle:Timeline:list.html.twig', array('posts' => $posts, 'form' => $form->createView()));
     }
 }
