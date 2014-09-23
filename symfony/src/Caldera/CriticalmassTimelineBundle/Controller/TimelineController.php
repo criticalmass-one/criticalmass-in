e82528bc368ed28ab007f89f4706c5a956fa184a
Malte Hübner <maltehuebner@gmx.org>
2014-09-24 00:37:39 +0200

    Dafür kann der ganze Kram ja nun weg, der ist ja ausgelagert worden.

--- symfony/src/Caldera/CriticalmassTimelineBundle/Controller/TimelineController.php
+++ symfony/src/Caldera/CriticalmassTimelineBundle/Controller/TimelineController.php
@@ -12,23 +12,6 @@ class TimelineController extends Controller
     {
         $posts = $this->getDoctrine()->getRepository('CalderaCriticalmassTimelineBundle:Post')->findBy(array('enabled' => true), array('dateTime' => 'DESC'));
 
-        $post = new Post();
-        $form = $this->createFormBuilder($post)
-            ->add('message', 'text')
-            ->add('LOS', 'submit')
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-
-            $post->setUser($this->getUser());
-            $em->persist($post);
-            $em->flush();
-        }
-
-        return $this->render('CalderaCriticalmassTimelineBundle:Timeline:list.html.twig', array('posts' => $posts, 'form' => $form->createView()));
+        return $this->render('CalderaCriticalmassTimelineBundle:Timeline:list.html.twig', array('posts' => $posts));
     }
 }
