6984ea342f12808332bf58d3b2e2a4cfe1a26184
Malte Hübner <maltehuebner@gmx.org>
2014-09-23 13:24:53 +0200

    Post-Entität weiter ausgerüstet.

--- symfony/src/Caldera/CriticalmassTimelineBundle/Entity/Post.php
+++ symfony/src/Caldera/CriticalmassTimelineBundle/Entity/Post.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Caldera\CriticalmassTiemlineBundle\Entity;
+namespace Caldera\CriticalmassTimelineBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * @ORM\Entity()
@@ -24,13 +25,13 @@ class Post
     protected $user;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Ride", inversedBy="posts")
+     * @ORM\ManyToOne(targetEntity="Caldera\CriticalmassCoreBundle\Entity\Ride", inversedBy="posts")
      * @ORM\JoinColumn(name="ride_id", referencedColumnName="id")
      */
     protected $ride;
 
     /**
-     * @ORM\ManyToOne(targetEntity="City", inversedBy="posts")
+     * @ORM\ManyToOne(targetEntity="Caldera\CriticalmassCoreBundle\Entity\City", inversedBy="posts")
      * @ORM\JoinColumn(name="city_id", referencedColumnName="id")
      */
     protected $city;
@@ -65,4 +66,221 @@ class Post
      */
     protected $enabled = true;
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hasCoords
+     *
+     * @param boolean $hasCoords
+     * @return Post
+     */
+    public function setHasCoords($hasCoords)
+    {
+        $this->hasCoords = $hasCoords;
+
+        return $this;
+    }
+
+    /**
+     * Get hasCoords
+     *
+     * @return boolean 
+     */
+    public function getHasCoords()
+    {
+        return $this->hasCoords;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     * @return Post
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float 
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     * @return Post
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float 
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set dateTime
+     *
+     * @param \DateTime $dateTime
+     * @return Post
+     */
+    public function setDateTime($dateTime)
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get dateTime
+     *
+     * @return \DateTime 
+     */
+    public function getDateTime()
+    {
+        return $this->dateTime;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Post
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Post
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Application\Sonata\UserBundle\Entity\User $user
+     * @return Post
+     */
+    public function setUser(\Application\Sonata\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Application\Sonata\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set ride
+     *
+     * @param \Caldera\CriticalmassTimelineBundle\Entity\Ride $ride
+     * @return Post
+     */
+    public function setRide(\Caldera\CriticalmassTimelineBundle\Entity\Ride $ride = null)
+    {
+        $this->ride = $ride;
+
+        return $this;
+    }
+
+    /**
+     * Get ride
+     *
+     * @return \Caldera\CriticalmassTimelineBundle\Entity\Ride 
+     */
+    public function getRide()
+    {
+        return $this->ride;
+    }
+
+    /**
+     * Set city
+     *
+     * @param \Caldera\CriticalmassTimelineBundle\Entity\City $city
+     * @return Post
+     */
+    public function setCity(\Caldera\CriticalmassTimelineBundle\Entity\City $city = null)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return \Caldera\CriticalmassTimelineBundle\Entity\City 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
 }
