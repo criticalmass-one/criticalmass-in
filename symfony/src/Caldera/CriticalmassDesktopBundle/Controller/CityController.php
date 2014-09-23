efe736f0baceafd4af44a4a522c13ba253ee5fd3
Malte Hübner <maltehuebner@gmx.org>
2014-09-23 17:56:52 +0200

    Formular eingebaut, um neue Kommentare entgegenzunehmen.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/CityController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/CityController.php
@@ -3,7 +3,9 @@
 namespace Caldera\CriticalmassDesktopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Caldera\CriticalmassTimelineBundle\Entity\Post;
 
 class CityController extends Controller
 {
@@ -14,7 +16,7 @@ class CityController extends Controller
         return $this->render('CalderaCriticalmassDesktopBundle:City:list.html.twig', array('cities' => $cities));
     }
 
-    public function showAction($citySlug)
+    public function showAction(Request $request, $citySlug)
     {
         $city = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($citySlug)->getCity();
 
@@ -23,12 +25,30 @@ class CityController extends Controller
             throw new NotFoundHttpException('Wir konnten keine Stadt unter der Bezeichnung "'.$citySlug.'" finden :(');
         }
 
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('message', 'text')
+            ->add('LOS', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $post->setUser($this->getUser());
+            $post->setCity($city);
+            $em->persist($post);
+            $em->flush();
+        }
+
         $currentRide = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findOneBy(array('city' => $city->getId()), array('dateTime' => 'DESC'));
         $rides = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findBy(array('city' => $city->getId()), array('dateTime' => 'DESC'));
 
         // shift the first ride from the array as the first one is the current and should not be displayed at the recent rides list
         array_shift($rides);
 
-        return $this->render('CalderaCriticalmassDesktopBundle:City:show.html.twig', array('city' => $city, 'currentRide' => $currentRide, 'rides' => $rides));
+        return $this->render('CalderaCriticalmassDesktopBundle:City:show.html.twig', array('city' => $city, 'currentRide' => $currentRide, 'rides' => $rides, 'form' => $form->createView()));
     }
 }
