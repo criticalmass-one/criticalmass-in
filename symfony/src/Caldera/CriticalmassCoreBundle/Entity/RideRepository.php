72ef09ab1623e57f4c8b9cc877bfc044bbb0ee10
Malte Hübner <maltehuebner@gmx.org>
2014-10-03 00:07:46 +0200

    Ride-Repository um Methode zum Zugriff auf einen Monat im Jahr erweitert.

--- symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Entity/RideRepository.php
@@ -76,6 +76,14 @@ class RideRepository extends EntityRepository
         return $result;
     }
 
+    public function findRidesByYearMonth($year, $month)
+    {
+        $startDateTime = new \DateTime($year.'-'.$month.'-01');
+        $endDateTime = new \DateTime($year.'-'.$month.'-'.$startDateTime->format('t'));
+
+        return $this->findRidesInInterval($startDateTime, $endDateTime);
+    }
+
     public function findLatestForCitySlug($citySlug)
     {
         $query = $this->getEntityManager()->createQuery('SELECT r AS ride FROM CalderaCriticalmassCoreBundle:Ride r JOIN CalderaCriticalmassCoreBundle:City c WITH c.id = r.city JOIN CalderaCriticalmassCoreBundle:CitySlug cs WITH cs.city = c.id WHERE cs.slug = \''.$citySlug.'\' GROUP BY r.city ORDER BY r.dateTime DESC');
