1131efa2aceee17ed153373fa4e6d80525cbf60d
Malte Hübner <maltehuebner@gmx.org>
2014-10-02 15:02:52 +0200

    Zeige nun die zehn Touren mit den meisten Teilnehmern an.

--- symfony/src/Caldera/CriticalmassStatisticBundle/Controller/StatisticController.php
+++ symfony/src/Caldera/CriticalmassStatisticBundle/Controller/StatisticController.php
@@ -11,6 +11,15 @@ class StatisticController extends Controller
     {
         $cities = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:City')->findCities();
 
-        return $this->render('CalderaCriticalmassStatisticBundle:Statistic:index.html.twig');
+        $topParticipantsRides = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findBy(array(), array('estimatedParticipants' => 'DESC'), 10);
+
+        return $this->render('CalderaCriticalmassStatisticBundle:Statistic:index.html.twig', array('cities' => $cities, 'topParticipantRides' => $topParticipantsRides));
+    }
+
+    public function cityparticipantsAction(Request $request, $citySlug)
+    {
+        $city = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($citySlug)->getCity();
+
+        return $this->render('CalderaCriticalmassStatisticBundle:Statistic:cityparticipants.html.twig', array('city' => $city));
     }
 }
