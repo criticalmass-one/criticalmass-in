fe6115d142bbb32afebf87062bd931ca324301fe
Malte Hübner <maltehuebner@gmx.org>
2014-10-09 14:26:08 +0200

    StandardCityType ergänzt.

--- symfony/src/Caldera/CriticalmassCoreBundle/Type/StandardCityType.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Type/StandardCityType.php
@@ -21,13 +21,13 @@ class StandardCityType extends AbstractType
             ->add('cityPopulation', 'integer', array('required' => false))
             ->add('punchLine', 'text', array('required' => false))
             ->add('longDescription', 'textarea', array('required' => false))
-            ->add('isStandardable', 'textarea', array('required' => false))
-            ->add('standardDayOfWeek', 'textarea', array('required' => false))
-            ->add('standardWeekOfMonth', 'textarea', array('required' => false))
+            ->add('isStandardable', 'checkbox', array('required' => false))
+            ->add('standardDayOfWeek', 'choice', array('label' => 'Wochentag', 'choices' => array(1 => 'Montag', 2 => 'Dienstag', 3 => 'Mittwoch', 4 => 'Donnerstag', 5 => 'Freitag', 6 => 'Sonnabend', 0 => 'Sonntag'), 'required' => true))
+            ->add('standardWeekOfMonth', 'choice', array('label' => 'Woche im Monat', 'choices' => array(1 => 'Erste Woche im Monat', 2 => 'Zweite Woche im Monat', 3 => 'Dritte Woche im Monat', 4 => 'Vierte Woche im Monat', 0 => 'Letzte Woche im Monat'), 'required' => true))
             ->add('standardTime', 'time', array('required' => false))
-            ->add('standardLocation', 'textarea', array('required' => false))
-            ->add('standardLatitude', 'textarea', array('required' => false))
-            ->add('standardLongitude', 'textarea', array('required' => false));
+            ->add('standardLocation', 'text', array('required' => false))
+            ->add('standardLatitude', 'hidden', array('required' => false))
+            ->add('standardLongitude', 'hidden', array('required' => false));
     }
 
     public function getName()
