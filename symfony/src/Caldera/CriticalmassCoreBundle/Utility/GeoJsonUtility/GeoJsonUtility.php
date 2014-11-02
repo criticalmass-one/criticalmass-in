5ecfc44bf9c57e9739b585959dd0801fb8072df1
Malte Hübner <maltehuebner@gmx.org>
2014-11-02 18:35:41 +0100

    This is shit.

--- symfony/src/Caldera/CriticalmassCoreBundle/Utility/GeoJsonUtility/GeoJsonUtility.php
+++ symfony/src/Caldera/CriticalmassCoreBundle/Utility/GeoJsonUtility/GeoJsonUtility.php
@@ -4,6 +4,7 @@ namespace Caldera\CriticalmassCoreBundle\Utility\GeoJsonUtility;
 
 
 use Caldera\CriticalmassCoreBundle\Entity\Track;
+use Caldera\CriticalmassCoreBundle\Utility\GpxReader\GpxReader;
 
 class GeoJsonUtility
 {
@@ -16,14 +17,31 @@ class GeoJsonUtility
 
     public function saveFile($filename)
     {
-        $handle = fopen($filename, "a");
-        fwrite($handle, $this->json);
+        $handle = fopen($filename, "w+");
+        fwrite($handle, 'var line = '.$this->json.';');
         fclose($handle);
     }
 
     public function addTrackAsPolyline(Track $track)
     {
+        if (!$this->json)
+        {
+            $this->createTemplate();
+        }
 
+        $gr = new GpxReader();
+        $gr->loadTrack($track);
+
+        $json = json_decode($this->json, true);
+
+        $feature = array();
+        $feature['type'] = 'Feature';
+        $feature['geometry']['type'] = 'LineString';
+        $feature['geometry']['coordinates'] = $gr->generateJsonArray();
+
+        array_push($json['features'], $feature);
+
+        $this->json = json_encode($json, JSON_FORCE_OBJECT);
     }
 
     public function createTemplate()
