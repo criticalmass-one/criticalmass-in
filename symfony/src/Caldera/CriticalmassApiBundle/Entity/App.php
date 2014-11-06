f3b73909a67ec0f787f8e2ffb176176d704601b5
Malte Hübner <maltehuebner@gmx.org>
2014-11-06 01:33:38 +0100

    Added entity for apps.

---
+++ symfony/src/Caldera/CriticalmassApiBundle/Entity/App.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Caldera\CriticalmassApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Table(name="app")
+ * @ORM\Entity
+ */
+class App
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Application\Sonata\UserBundle\Entity\User", inversedBy="posts")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $user;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    protected $token;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $apiCalls;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $creationDateTime;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     * @return App
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string 
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set apiCalls
+     *
+     * @param integer $apiCalls
+     * @return App
+     */
+    public function setApiCalls($apiCalls)
+    {
+        $this->apiCalls = $apiCalls;
+
+        return $this;
+    }
+
+    /**
+     * Get apiCalls
+     *
+     * @return integer 
+     */
+    public function getApiCalls()
+    {
+        return $this->apiCalls;
+    }
+
+    /**
+     * Set creationDateTime
+     *
+     * @param \DateTime $creationDateTime
+     * @return App
+     */
+    public function setCreationDateTime($creationDateTime)
+    {
+        $this->creationDateTime = $creationDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDateTime
+     *
+     * @return \DateTime 
+     */
+    public function getCreationDateTime()
+    {
+        return $this->creationDateTime;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Application\Sonata\UserBundle\Entity\User $user
+     * @return App
+     */
+    public function setUser(\Application\Sonata\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Application\Sonata\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
