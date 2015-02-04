b71a2b7447b971330f5ef25c9e123a572aa4e934
Malte Hübner <maltehuebner@gmx.org>
2015-02-04 17:48:23 +0100

    Moved some stuff to GpsConverter.

---
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Utility/Gps/GpsConverter.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: malte
+ * Date: 04.02.15
+ * Time: 17:13
+ */
+
+namespace Caldera\CriticalmassGalleryBundle\Utility\Gps;
+
+class GpsConverter {
+
+    protected function coordinateToDec($coordinate) {
+        list($dividend, $divisor) = split('/', $coordinate);
+
+        if ($divisor == 0) {
+            return 0;
+        } else {
+            return $dividend / $divisor;
+        }
+    }
+    
+    public function convert($value)
+    {
+        $deg = $this->coordinateToDec($value[0]);
+        $min = $this->coordinateToDec($value[1]);
+        $sec = $this->coordinateToDec($value[2]);
+
+        return $deg + ((($min * 60) + ($sec)) / 3600);
+    }
+}
--- symfony/src/Caldera/CriticalmassGalleryBundle/Utility/Gps/GpsReader.php
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Utility/Gps/GpsReader.php
@@ -22,16 +22,6 @@ class GpsReader {
     {
         $this->filename = $filename;
     }
-
-    protected function coordinateToDec($coordinate) {
-        list($dividend, $divisor) = split('/', $coordinate);
-        
-        if ($divisor == 0) {
-            return 0;
-        } else {
-            return $dividend / $divisor;
-        }
-    }
     
     public function execute()
     {
@@ -39,19 +29,12 @@ class GpsReader {
         
         if (isset($exif['GPS']['GPSLatitude']) && isset($exif['GPS']['GPSLongitude']))
         {
+            $gc = new GpsConverter();
+            
             $result = array();
-            
-            $deg = $this->coordinateToDec($exif['GPS']['GPSLatitude'][0]);
-            $min = $this->coordinateToDec($exif['GPS']['GPSLatitude'][1]);
-            $sec = $this->coordinateToDec($exif['GPS']['GPSLatitude'][2]);
 
-            $result['latitude'] = $deg + ((($min * 60) + ($sec)) / 3600);
-            
-            $deg = $this->coordinateToDec($exif['GPS']['GPSLongitude'][0]);
-            $min = $this->coordinateToDec($exif['GPS']['GPSLongitude'][1]);
-            $sec = $this->coordinateToDec($exif['GPS']['GPSLongitude'][2]);
-
-            $result['longitude'] = $deg + ((($min * 60) + ($sec)) / 3600);
+            $result['latitude'] = $gc->convert($exif['GPS']['GPSLatitude']);
+            $result['longitude'] = $gc->convert($exif['GPS']['GPSLongitude']);
             
             return $result;
         }
