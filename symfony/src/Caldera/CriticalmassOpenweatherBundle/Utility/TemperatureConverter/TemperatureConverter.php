635337367f2f73f1995b8b2e5b7785fa0be4e510
Malte Hübner <maltehuebner@gmx.org>
2015-02-04 15:04:50 +0100

    Moved stuff to AbstractTemperatureConverter.

--- symfony/src/Caldera/CriticalmassOpenweatherBundle/Utility/TemperatureConverter/TemperatureConverter.php
+++ symfony/src/Caldera/CriticalmassOpenweatherBundle/Utility/TemperatureConverter/TemperatureConverter.php
@@ -8,33 +8,7 @@
 
 namespace Caldera\CriticalmassOpenweatherBundle\Utility\TemperatureConverter;
 
-class TemperatureConverter {
-    protected $celsius;
-    protected $fahrenheit;
-    
-    public function getFahrenheit()
-    {
-        return $this->fahrenheit;
-    }
-
-    public function setFahrenheit($fahrenheit)
-    {
-        $this->fahrenheit = $fahrenheit;
-        
-        return $this;
-    }
-
-    public function getCelsius()
-    {
-        return $this->celsius;
-    }
-
-    public function setCelsius($celsius)
-    {
-        $this->celsius = $celsius;
-        
-        return $this;
-    }
+class TemperatureConverter extends AbstractTemperatureConverter {
 
     public function convert()
     {
