9b8cfa3247d34d927e02dc6e2a09b4368815642e
Malte Hübner <maltehuebner@gmx.org>
2015-02-04 17:37:00 +0100

    Remove these old useless tests.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Tests/Controller/GalleryControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Caldera\CriticalmassGalleryBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class GalleryControllerTest extends WebTestCase
-{
-}
--- symfony/src/Caldera/CriticalmassGalleryBundle/Tests/Controller/PhotosControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Caldera\CriticalmassGalleryBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class PhotosControllerTest extends WebTestCase
-{
-}
