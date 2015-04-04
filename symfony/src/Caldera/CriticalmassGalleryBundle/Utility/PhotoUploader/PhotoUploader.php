99392610754f0469fdb13fd10025295e077848db
Malte Hübner <maltehuebner@gmx.org>
2015-04-04 22:10:37 +0200

    Reordered PhotoUploader and make also thumbnails.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Utility/PhotoUploader/PhotoUploader.php
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Utility/PhotoUploader/PhotoUploader.php
@@ -18,20 +18,23 @@ class PhotoUploader {
     {
         $this->photo->getFile()->move($this->photo->getUploadRootDir(), $this->photo->getId().'.jpg');
 
-        if ($this->photo->getFile()->getClientOriginalExtension() == "jpg" ||
-            $this->photo->getFile()->getClientOriginalExtension() == "JPG" ||
-            $this->photo->getFile()->getClientOriginalExtension() == "jpeg" ||
-            $this->photo->getFile()->getClientOriginalExtension() == "JPEG")
-        {
-            $pr = new PhotoResizer();
-            $pr->setPhoto($this->photo);
-            $pr->resizeFactor(0.5);
-            $pr->saveJpeg($this->photo->getUploadRootDir().$this->photo->getId().'.small.jpg');
-            //$smallSize = $utility->reduceSize($this, 0.5);
-            //$utility->makeSmallPhotoJPG($this, $smallSize['width'], $smallSize['height'], "_klein");
-            //$utility->makeSmallPhotoJPG($this, 200, 200, "_thumbnail");
-            //$utility->getMetaInfos($this);
-
-        }
+        $this->makeSmallPhoto();
+        $this->makeThumbnail();
+    }
+    
+    protected function makeSmallPhoto()
+    {
+        $pr = new PhotoResizer();
+        $pr->setPhoto($this->photo);
+        $pr->resizeFactor(0.5);
+        $pr->saveJpeg($this->photo->getUploadRootDir().$this->photo->getId().'.small.jpg');
+    }
+    
+    protected function makeThumbnail()
+    {
+        $pr = new PhotoResizer();
+        $pr->setPhoto($this->photo);
+        $pr->resizeFactor(0.2);
+        $pr->saveJpeg($this->photo->getUploadRootDir().$this->photo->getId().'.thumbnail.jpg');
     }
 }
