55c3cf5b6b4091d4b7602c82d8050bd74f95d9aa
Malte Hübner <maltehuebner@gmx.org>
2015-04-06 00:52:56 +0200

    Exported more stuff to external files.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery.js
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery.js
@@ -2,13 +2,13 @@ Gallery = function()
 {
 };
 
-Gallery.prototype.init = function(photoArray)
+Gallery.prototype.init = function(map, photoArray)
 {
     var photo;
     
     while (photo = photoArray.pop())
     {
-        alert(photo.getId());
+        photo.addTo(map);
     }
 };
 
@@ -22,7 +22,7 @@ Photo = function(id, latitude, longitude)
     this.id = id;
     this.latitude = latitude;
     this.longitude = longitude;
-}
+};
 
 Photo.prototype.address = null;
 Photo.prototype.latitude = null;
@@ -31,14 +31,37 @@ Photo.prototype.longitude = null;
 Photo.prototype.getId = function()
 {
     return this.id;
-}
+};
 
 Photo.prototype.getLatitude = function()
 {
     return this.latitude;
-}
+};
 
 Photo.prototype.getLongitude = function()
 {
     return this.longitude;
-}
+};
+
+Photo.prototype.addTo = function(map)
+{
+    var locationIcon = L.icon({
+        iconUrl: 'https://www.criticalmass.in/images/marker/marker-red.png',
+        iconRetinaUrl: '/images/marker/marker-red-2x.png',
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        popupAnchor: [0, -36],
+        shadowUrl: '/images/marker/defaultshadow.png',
+        shadowSize: [41, 41],
+        shadowAnchor: [13, 41]
+    });
+    
+    var photoMarker = L.marker([this.getLatitude(), this.getLongitude()], { icon:locationIcon, draggable: true });
+    photoMarker.addTo(map);
+
+    photoMarker.on('click', function()
+    {
+        var photoPath = '/photos/' + this.getId() + '.jpg';
+        $.fancybox( {href : photoPath, title : 'Lorem lipsum'} );
+    });
+};
