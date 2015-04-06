2caf82ab4bdc95ef60518d7c96d9f599953cbdb9
Malte Hübner <maltehuebner@gmx.org>
2015-04-06 15:08:48 +0200

    New kind of Gallery creation applied.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery/Gallery.js
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery/Gallery.js
@@ -2,12 +2,28 @@ Gallery = function()
 {
 };
 
-Gallery.prototype.init = function(map, photoArray)
+Gallery.prototype.photoArray = new Array();
+
+Gallery.prototype.addPhotos = function(photoArray)
+{
+    this.photoArray.merge(photoArray);
+};
+
+Gallery.prototype.map = null;
+
+Gallery.prototype.addTo = function(map)
+{
+    this.map = map;
+    
+    this.init();
+};
+
+Gallery.prototype.init = function()
 {
     var photo;
 
     while (photo = photoArray.pop())
     {
-        photo.addTo(map);
+        photo.addTo(this.map);
     }
 };
