c8eb619cbfcab29575e9f6286b54888a8d01004a
Malte Hübner <maltehuebner@gmx.org>
2015-04-06 15:10:37 +0200

    This is a better approach.

--- symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery/Gallery.js
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Resources/public/js/gallery/Gallery.js
@@ -6,7 +6,12 @@ Gallery.prototype.photoArray = new Array();
 
 Gallery.prototype.addPhotos = function(photoArray)
 {
-    this.photoArray.merge(photoArray);
+    this.photoArray = $.merge(this.photoArray, photoArray);
+};
+
+Gallery.prototype.addPhoto = function(photo)
+{
+    this.photoArray[photo.getId()] = photo;
 };
 
 Gallery.prototype.map = null;
@@ -22,7 +27,7 @@ Gallery.prototype.init = function()
 {
     var photo;
 
-    while (photo = photoArray.pop())
+    while (photo = this.photoArray.pop())
     {
         photo.addTo(this.map);
     }
