791af2da308527ef4db7810622b934977b82d48d
Malte Hübner <maltehuebner@gmx.org>
2015-04-22 15:05:17 +0200

    Fixed typo in SubrideContainer.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/Subride/SubrideContainer.js
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Resources/public/js/Subride/SubrideContainer.js
@@ -1,7 +1,6 @@
 SubrideContainer = function()
 {
     
-    
 };
 
 SubrideContainer.prototype.list = new Array();
@@ -17,7 +16,7 @@ SubrideContainer.prototype.addTo = function(map)
     this.markerGroup = L.featureGroup();
     
     for (index = 0; index < this.list.length; ++index) {
-        this.list[index].addTo(markerGroup);
+        this.list[index].addTo(this.markerGroup);
     }
 
     this.markerGroup.addTo(map);
