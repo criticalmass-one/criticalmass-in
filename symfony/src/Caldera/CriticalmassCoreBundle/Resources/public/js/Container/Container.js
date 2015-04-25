5fab1e89c2f08b6122b20747a98adb071a8b3676
Malte Hübner <maltehuebner@gmx.org>
2015-04-25 21:59:58 +0200

    Added addLayer() and removeLayer() to Container.

--- symfony/src/Caldera/CriticalmassCoreBundle/Resources/public/js/Container/Container.js
+++ symfony/src/Caldera/CriticalmassCoreBundle/Resources/public/js/Container/Container.js
@@ -48,3 +48,13 @@ Container.prototype.getEntity = function(index)
 {
     return this.list[index];
 };
+
+Container.prototype.addLayer = function(index)
+{
+    this.list[index].addTo(this.layer);
+};
+
+Container.prototype.removeLayer = function(index)
+{
+    return this.list[index].removeLayer(this.layer);
+};
