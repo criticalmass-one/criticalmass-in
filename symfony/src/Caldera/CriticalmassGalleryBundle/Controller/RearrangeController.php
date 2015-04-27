5dbaaba29f45fd1cab208582771f7e54847162de
Malte Hübner <maltehuebner@gmx.org>
2015-04-27 13:29:19 +0200

    Copypasta of some basic stuff.

---
+++ symfony/src/Caldera/CriticalmassGalleryBundle/Controller/RearrangeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Caldera\CriticalmassGalleryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RearrangeController extends Controller
+{
+    public function rearrangeAction(Request $request, $citySlug, $rideDate)
+    {
+        $citySlugObj = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($citySlug);
+
+        if (!$citySlugObj)
+        {
+            throw new NotFoundHttpException('Wir haben leider keine Stadt in der Datenbank, die sich mit '.$citySlug.' identifiziert.');
+        }
+
+        $city = $citySlugObj->getCity();
+
+        try {
+            $rideDateTime = new \DateTime($rideDate);
+        }
+        catch (\Exception $e)
+        {
+            throw new NotFoundHttpException('Mit diesem Datum können wir leider nichts anfange. Bitte gib ein Datum im Format YYYY-MM-DD an.');
+        }
+
+        $ride = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findCityRideByDate($city, $rideDateTime);
+
+        if (!$ride)
+        {
+            throw new NotFoundHttpException('Wir haben leider keine Tour in '.$city->getCity().' am '.$rideDateTime->format('d. m. Y').' gefunden.');
+        }
+
+        return $this->render('CalderaCriticalmassGalleryBundle:Rearrange:rearrange.html.twig', array('city' => $city, 'ride' => $ride, 'dateTime' => new \DateTime()));
+    }
+}
