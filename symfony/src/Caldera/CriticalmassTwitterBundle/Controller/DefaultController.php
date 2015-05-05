8cbfa81484984a817de62d0c157251114b2613e1
Malte Hübner <maltehuebner@gmx.org>
2015-05-05 15:08:21 +0200

    Just testing.

--- symfony/src/Caldera/CriticalmassTwitterBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/CriticalmassTwitterBundle/Controller/DefaultController.php
@@ -2,12 +2,20 @@
 
 namespace Caldera\CriticalmassTwitterBundle\Controller;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Caldera\CriticalmassTwitterBundle\Utility\TwitterGateway\TwitterGateway;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('CalderaCriticalmassTwitterBundle:Default:index.html.twig', array('name' => $name));
+        $ride = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->find(1118);
+
+        $tg = new TwitterGateway($this->container->getParameter('twitter.consumer_key'), $this->container->getParameter('twitter.consumer_secret'), $this->container->getParameter('twitter.access_token'), $this->container->getParameter('twitter.access_token_secret'));
+        $tg->fetchTweetsForRide($ride);
+
+        return new Response('');
     }
 }
