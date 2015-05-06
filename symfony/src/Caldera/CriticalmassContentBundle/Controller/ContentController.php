ff033cf16be0f9f8dfa2aacd862aed2a0cf33109
Malte Hübner <maltehuebner@gmx.org>
2015-05-06 22:25:08 +0200

    Added edit action - first try :(

--- symfony/src/Caldera/CriticalmassContentBundle/Controller/ContentController.php
+++ symfony/src/Caldera/CriticalmassContentBundle/Controller/ContentController.php
@@ -2,6 +2,7 @@
 
 namespace Caldera\CriticalmassContentBundle\Controller;
 
+use Caldera\CriticalmassContentBundle\Type\ContentType;
 use Michelf\Markdown;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,5 +19,41 @@ class ContentController extends Controller
         $parsedText = $markdown->transform($content->getText());
         
         return $this->render('CalderaCriticalmassContentBundle:Content:show.html.twig', array('content' => $content, 'parsedText' => $parsedText));
+    }
+    
+    public function editAction(Request $request, $slug)
+    {
+        $content = $this->getDoctrine()->getRepository('CalderaCriticalmassContentBundle:Content')->findBy(array('slug' => $slug, 'enabled' => true));
+
+        $content = array_pop($content);
+        
+        $archiveContent = clone $content;
+        $archiveContent->setArchiveUser($this->getUser());
+        $archiveContent->setArchiveParent($content);
+
+        $form = $this->createForm(new ContentType(), $content, array('action' => $this->generateUrl('caldera_criticalmass_content_edit', array('slug' => $content->getSlug()))));
+
+        $form->handleRequest($request);
+
+        // TODO: remove this shit and test the validation in the template
+        $hasErrors = null;
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($form->getData());
+            $em->persist($archiveContent);
+            $em->flush();
+
+            // TODO: remove also this
+            $hasErrors = false;
+        }
+        elseif ($form->isSubmitted())
+        {
+            // TODO: remove even more shit
+            $hasErrors = true;
+        }
+
+        return $this->render('CalderaCriticalmassContentBundle:Content:edit.html.twig', array('content' => $content, 'form' => $form->createView(), 'hasErrors' => $hasErrors));
     }
 }
