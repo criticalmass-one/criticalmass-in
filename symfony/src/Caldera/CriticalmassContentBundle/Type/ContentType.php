3d2a8355c1e16937db5817f7c3447340848ab2fd
Malte Hübner <maltehuebner@gmx.org>
2015-05-06 22:18:56 +0200

    First version of ContentType.

---
+++ symfony/src/Caldera/CriticalmassContentBundle/Type/ContentType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Caldera\CriticalmassCoreBundle\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text', array('required' => true))
+            ->add('text', 'textarea', array('required' => true))
+            ->add('slug', 'text', array('required' => false));
+    }
+
+    public function getName()
+    {
+        return 'content';
+    }
+}
