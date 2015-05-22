762810b4f1fbb289b073e5393141dfac2750739a
Malte Hübner <maltehuebner@gmx.org>
2015-05-22 13:16:20 +0200

    Added FrontpageController.

---
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/FrontpageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Caldera\CriticalmassDesktopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class FrontpageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('CalderaCriticalmassDesktopBundle:Default:index.html.twig');
+    }
+
+    public function slugindexAction($slug)
+    {
+        $city = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:CitySlug')->findOneBySlug($slug)->getCity();
+
+        return $this->render('CalderaCriticalmassDesktopBundle:Default:index.html.twig', array('citySlug' => $city->getMainSlugString()));
+    }
+}
