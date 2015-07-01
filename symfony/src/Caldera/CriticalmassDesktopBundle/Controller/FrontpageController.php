d86127b0a946a4c6593ed25c1d7dca9916ad7f9e
Malte Hübner <maltehuebner@gmx.org>
2015-07-01 20:58:09 +0200

    Added cool carousel to front page.

--- symfony/src/Caldera/CriticalmassDesktopBundle/Controller/FrontpageController.php
+++ symfony/src/Caldera/CriticalmassDesktopBundle/Controller/FrontpageController.php
@@ -9,8 +9,15 @@ class FrontpageController extends Controller
 {
     public function indexAction()
     {
-        $articles = $this->getDoctrine()->getRepository('CalderaCriticalmassBlogBundle:Article')->findBy(array());
-            
-        return $this->render('CalderaCriticalmassDesktopBundle:Frontpage:index.html.twig', array('blogArticles' => $articles));
+        $dateTime = new \DateTime();
+        
+        $blogArticles = $this->getDoctrine()->getRepository('CalderaCriticalmassBlogBundle:Article')->findBy(array());
+        $carouselRides = $this->getDoctrine()->getRepository('CalderaCriticalmassCoreBundle:Ride')->findRecentRides(null, null, 5, 1000);
+        
+        return $this->render('CalderaCriticalmassDesktopBundle:Frontpage:index.html.twig',
+            [
+                'blogArticles' => $blogArticles,
+                'carouselRides' => $carouselRides
+            ]);
     }
 }
