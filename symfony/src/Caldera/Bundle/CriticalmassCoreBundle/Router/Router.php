2bf963953b3603cf103fa3456346b98696f97f4b
Malte Hübner <maltehuebner@gmx.org>
2016-01-12 00:02:17 +0100

    Added more stuff to own router.

--- symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Router/Router.php
+++ symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Router/Router.php
@@ -2,6 +2,7 @@
 
 namespace Caldera\Bundle\CriticalmassCoreBundle\Router;
 
+use Caldera\Bundle\CriticalmassModelBundle\Entity\City;
 use Caldera\Bundle\CriticalmassModelBundle\Entity\Ride;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Routing\Router as sfRouter;
@@ -9,22 +10,37 @@ use Symfony\Bundle\FrameworkBundle\Routing\Router as sfRouter;
 class Router extends sfRouter
 {
 
-    public function generate($name, $parameters = [], $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    public function generate($object, $parameters = [], $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
     {
-        if ($name instanceof Ride) {
-            return $this->generateRideUrl($name, $referenceType);
+        if ($object instanceof Ride) {
+            return $this->generateRideUrl($object, $referenceType);
         }
 
-        return parent::generate($name, $parameters, $referenceType);
+        if ($object instanceof City) {
+            return $this->generateCityUrl($object, $referenceType);
+        }
+
+        return parent::generate($object, $parameters, $referenceType);
     }
 
-    private function generateRideUrl(Ride $ride, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    protected function generateRideUrl(Ride $ride, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
     {
         $route = 'caldera_criticalmass_ride_show';
 
         $parameters = [
             'citySlug' => $ride->getCity()->getMainSlugString(),
             'rideDate' => $ride->getFormattedDate()
+        ];
+
+        return parent::generate($route, $parameters, $referenceType);
+    }
+
+    protected function generateCityUrl(City $city, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    {
+        $route = 'caldera_criticalmass_desktop_city_show';
+
+        $parameters = [
+            'citySlug' => $city->getMainSlugString()
         ];
 
         return parent::generate($route, $parameters, $referenceType);
