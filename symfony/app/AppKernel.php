f1270d34a8b044acfa3aabcc6d6d52ccefd7aa66
Malte Hübner <maltehuebner@gmx.org>
2016-01-26 01:38:14 +0100

    Added elastica bundle.

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
