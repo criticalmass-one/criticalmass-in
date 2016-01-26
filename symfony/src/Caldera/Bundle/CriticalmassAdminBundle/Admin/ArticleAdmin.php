2a39a9d8bb82ff7d34a34edeb8d72d40d9ee33bf
Malte Hübner <maltehuebner@gmx.org>
2016-01-26 01:38:34 +0100

    Fixed ArticleAdmin.

--- symfony/src/Caldera/Bundle/CriticalmassAdminBundle/Admin/ArticleAdmin.php
+++ symfony/src/Caldera/Bundle/CriticalmassAdminBundle/Admin/ArticleAdmin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Caldera\CriticalmassCoreBundle\Admin;
+namespace Caldera\Bundle\CriticalmassAdminBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
