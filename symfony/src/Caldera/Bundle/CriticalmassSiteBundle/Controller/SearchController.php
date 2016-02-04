52561274bea88db33dba9b062c8e511d0f5d4713
Malte Hübner <maltehuebner@gmx.org>
2016-02-04 14:44:55 +0100

    WIP at search results.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/SearchController.php
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/SearchController.php
@@ -26,19 +26,61 @@ class SearchController extends AbstractController
         return $finder->find($query);
     }
 
+
+    protected function searchCity($queryPhrase)
+    {
+        $finder = $this->container->get('fos_elastica.finder.criticalmass.city');
+
+        $simpleQueryString = new \Elastica\Query\SimpleQueryString($queryPhrase, ['city', 'title', 'description', 'longDescription', 'punchLine', 'standardLocation']);
+
+        $archivedFilter= new \Elastica\Filter\Term(['isArchived' => false]);
+        $enabledFilter = new \Elastica\Filter\Term(['isEnabled' => true]);
+
+        $filter = new \Elastica\Filter\BoolAnd([$archivedFilter, $enabledFilter]);
+
+        $filteredQuery = new \Elastica\Query\Filtered($simpleQueryString, $filter);
+
+        $query = new \Elastica\Query($filteredQuery);
+
+        $query->setSize(50);
+        $query->setMinScore(0.5);
+
+        return $finder->find($query);
+    }
+
+    protected function searchRide($queryPhrase)
+    {
+        $finder = $this->container->get('fos_elastica.finder.criticalmass.ride');
+
+        $simpleQueryString = new \Elastica\Query\SimpleQueryString($queryPhrase, ['title', 'description', 'location']);
+
+        $archivedFilter= new \Elastica\Filter\Term(['isArchived' => false]);
+
+        $filteredQuery = new \Elastica\Query\Filtered($simpleQueryString, $archivedFilter);
+
+        $query = new \Elastica\Query($filteredQuery);
+
+        $query->setSize(50);
+        $query->setMinScore(0.5);
+
+        return $finder->find($query);
+    }
+
     public function queryAction(Request $request)
     {
         $queryPhrase = $request->get('query');
 
-        $results = [];
-
-        $results = $this->searchContent($queryPhrase);
+        $contentResults = $this->searchContent($queryPhrase);
+        $cityResults = $this->searchCity($queryPhrase);
+        $rideResults = $this->searchRide($queryPhrase);
 
         return $this->render(
             'CalderaCriticalmassSiteBundle:Search:result.html.twig',
             [
                 'query' => $queryPhrase,
-                'results' => $results
+                'contentResults' => $contentResults,
+                'cityResults' => $cityResults,
+                'rideResults' => $rideResults,
             ]
         );
     }
