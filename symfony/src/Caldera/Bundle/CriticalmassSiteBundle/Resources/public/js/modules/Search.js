63d77a76911dda3f67c60b816847f1091e899099
Malte Hübner <maltehuebner@gmx.org>
2016-02-04 20:44:20 +0100

    Nicer view for typeahead stuff.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/Search.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/Search.js
@@ -17,34 +17,39 @@ define(['typeahead', 'bloodhound'], function() {
 
         $('#search-input').typeahead(
             {
-                hint: true,
+                hint: false,
                 highlight: true,
-                minLength: 1
+                minLength: 1,
+                classNames: {
+                    dataset: 'tt-dataset tt-dataset-results container'
+                }
             },
             {
                 name: 'results',
                 source: bh.ttAdapter(),
                 displayKey: 'value',
                 templates: {
-                    header: function() {
-                        return '<ul class="typeahead dropdown-menu" role="menu">';
-                    },
-                    footer: function() {
-                        return '</ul>';
-                    },
                     suggestion: function(data) {
+                        var html = '';
+                        html += '<div class="row padding-top-small padding-bottom-small">';
+                        html += '<div class="col-md-12">';
+
                         if (data.type == 'city') {
-                            return '<li><a class="dropdown-item" href="' + data.url + '" role="option"><i class="fa fa-university"></i> ' + data.value + '</a></li>';
+                            html += '<a href="' + data.url + '"><i class="fa fa-university"></i> ' + data.value + '</a>';
                         }
 
                         if (data.type == 'ride') {
-                            return '<li><a class="dropdown-item" href="' + data.url + '" role="option"><i class="fa fa-bicycle"></i> ' + data.value + '</a></li>';
+                            html += '<a href="' + data.url + '"><i class="fa fa-bicycle"></i> ' + data.value + '</a>';
                         }
 
                         if (data.type == 'content') {
-                            return '<li><a class="dropdown-item" href="' + data.url + '" role="option"><i class="fa fa-file-text-o"></i> ' + data.value + '</a></li>';
+                            html += '<a href="' + data.url + '"><i class="fa fa-file-text-o"></i> ' + data.value + '</a>';
                         }
 
+                        html += '</div>';
+                        html += '</div>';
+
+                        return html;
                     }
                 }
             }
