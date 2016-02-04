f71e8e5c8971224e802f52827fe147dae48a5754
Malte Hübner <maltehuebner@gmx.org>
2016-02-04 17:50:29 +0100

    Renew dropzone stuff.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/UploadPhotoPage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/UploadPhotoPage.js
@@ -1,5 +1,17 @@
 define(['dropzone'], function(Dropzone) {
     UploadPhotoPage = function(context, options) {
+        this._loadStyles();
+    };
+
+
+    UploadPhotoPage.prototype._loadStyles = function() {
+        var $link = $('<link>', {
+            rel: 'stylesheet',
+            type: 'text/css',
+            href: '/bundles/calderacriticalmasssite/css/external/dropzone.basic.css'
+        });
+
+        $link.appendTo('head');
     };
 
     return UploadPhotoPage;
