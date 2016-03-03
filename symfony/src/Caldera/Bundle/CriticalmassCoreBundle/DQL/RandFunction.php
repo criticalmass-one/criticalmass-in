5a0fe457a2059a647d2c384789d779812f056b7e
Malte Hübner <maltehuebner@gmx.org>
2016-03-03 18:27:16 +0100

    Add RAND() function to doctrine.

---
+++ symfony/src/Caldera/Bundle/CriticalmassCoreBundle/DQL/RandFunction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Caldera\Bundle\CriticalmassCoreBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Parser;
+
+class RandFunction extends FunctionNode
+{
+    private $arg;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+       return 'RAND()';
+    }
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
