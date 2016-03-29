b81a1dc0fc47ef63f0adb91fbe72c5363b2359ea
Malte Hübner <maltehuebner@gmx.org>
2016-03-29 14:39:29 +0200

    Whoops, forgot to enable this again.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/RidePage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/RidePage.js
@@ -151,15 +151,15 @@ define(['Map', 'Container', 'CityEntity', 'RideEntity', 'TrackEntity', 'SubrideE
     };
 
     RidePage.prototype.focus = function() {
-        //if (!this._trackContainer.isEmpty()) {
-        //    var bounds = this._trackContainer.getBounds();
+        if (!this._trackContainer.isEmpty()) {
+            var bounds = this._trackContainer.getBounds();
 
-        //    this._map.fitBounds(bounds);
-        //} else if (this._ride && this._ride.hasLocation()) {
+            this._map.fitBounds(bounds);
+        } else if (this._ride && this._ride.hasLocation()) {
             this._map.setView([this._ride.getLatitude(), this._ride.getLongitude()], 10);
-        //} else {
-        //    this._map.setView([this._city.getLatitude(), this._city.getLongitude()], 10);
-        //}
+        } else {
+            this._map.setView([this._city.getLatitude(), this._city.getLongitude()], 10);
+        }
     };
 
     return RidePage;
