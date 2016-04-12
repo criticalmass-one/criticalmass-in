c2e5b2e05ea90cbde4f5e17c2eeb8234aa985f32
Malte Hübner <maltehuebner@gmx.org>
2016-04-13 00:16:13 +0200

    Optimized Frontpage.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/FrontpageController.php
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/FrontpageController.php
@@ -9,26 +9,52 @@ class FrontpageController extends AbstractController
 {
     public function indexAction(Request $request)
     {
-        $currentRides = $this->getRideRepository()->findFrontpageRides();
-
         $this->getMetadata()
             ->setDescription('criticalmass.in sammelt Fotos, Tracks und Informationen über weltweite Critical-Mass-Touren')
             ->setKeywords('Critical Mass, Tracks, Live-Tracking, Tracking');
+
+        $rideList = $this->getFrontpageRideList();
+
+        $endDateTime = new \DateTime();
+        $startDateTime = new \DateTime();
+        $monthInterval = new \DateInterval('P1M');
+        $startDateTime->sub($monthInterval);
 
         /**
          * @var Timeline $timeline
          */
         $timelineContent = $this
             ->get('caldera.criticalmass.timeline.cached')
+            ->setDateRange($startDateTime, $endDateTime)
             ->execute()
             ->getTimelineContent();
 
         return $this->render(
             'CalderaCriticalmassSiteBundle:Frontpage:index.html.twig',
             [
-                'currentRides' => $currentRides,
-                'timelineContent' => $timelineContent
+                'timelineContent' => $timelineContent,
+                'rideList' => $rideList
             ]
         );
+    }
+
+    public function getFrontpageRideList()
+    {
+        $rides = $this->getRideRepository()->findFrontpageRides();
+
+        $rideList = [];
+
+        foreach ($rides as $ride) {
+            $rideDate = $ride->getFormattedDate();
+            $citySlug = $ride->getCity()->getSlug();
+
+            if (!array_key_exists($rideDate, $rideList)) {
+                $rideList[$rideDate] = [];
+            }
+
+            $rideList[$rideDate][$citySlug] = $ride;
+        }
+
+        return $rideList;
     }
 }
