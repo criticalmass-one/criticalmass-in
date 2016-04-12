06dc5c50f4c99fba5b2898e4f77159db3c7b6645
Malte Hübner <maltehuebner@gmx.org>
2016-04-12 23:37:08 +0200

    Added pager.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/TimelineController.php
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Controller/TimelineController.php
@@ -21,7 +21,14 @@ class TimelineController extends AbstractController
 {
     public function yearmonthAction(Request $request, $year, $month)
     {
+        $lowerLimitDateTime = new \DateTime('2010-01-01');
+
         $startDateTime = new \DateTime($year.'-'.$month.'-01');
+
+        if ($startDateTime < $lowerLimitDateTime) {
+            $startDateTime = new \DateTime();
+        }
+
         $endDateTime = new \DateTime($year.'-'.$month.'-'.$startDateTime->format('t'));
 
         /**
@@ -33,12 +40,25 @@ class TimelineController extends AbstractController
             ->execute()
             ->getTimelineContent();
 
+        $nextDateTime = $this->getNextDateTime($startDateTime);
+        $previousDateTime = $this->getPreviousDateTime($startDateTime);
+
+        if ($nextDateTime > new \DateTime()) {
+            $nextDateTime = null;
+        }
+
+        if ($previousDateTime < $lowerLimitDateTime) {
+            $previousDateTime = null;
+        }
+
         return $this->render(
             'CalderaCriticalmassSiteBundle:Timeline:yearmonth.html.twig',
             [
                 'timelineContent' => $timelineContent,
                 'startDateTime' => $startDateTime,
-                'endDateTime' => $endDateTime
+                'endDateTime' => $endDateTime,
+                'nextDateTime' => $nextDateTime,
+                'previousDateTime' => $previousDateTime
             ]
         );
     }
@@ -48,5 +68,23 @@ class TimelineController extends AbstractController
         $dateTime = new \DateTime();
 
         return $this->yearmonthAction($request, $dateTime->format('Y'), $dateTime->format('M'));
+    }
+
+    protected function getNextDateTime(\DateTime $dateTime)
+    {
+        $nextDateTime = clone $dateTime;
+
+        $dateInterval = new \DateInterval('P1M');
+
+        return $nextDateTime->add($dateInterval);
+    }
+
+    protected function getPreviousDateTime(\DateTime $dateTime)
+    {
+        $previousDateTime = clone $dateTime;
+
+        $dateInterval = new \DateInterval('P1M');
+
+        return $previousDateTime->sub($dateInterval);
     }
 }
