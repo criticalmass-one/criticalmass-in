f3f0d0585e9af0b853067892d86b8baf7d163eb6
Malte Hübner <maltehuebner@gmx.org>
2016-04-13 23:19:00 +0200

    WIP.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
@@ -1,4 +1,4 @@
-define(['socketio'], function(io) {
+define(['socketio', 'dateformat'], function(io) {
     ChatPage = function() {
 
     };
@@ -24,10 +24,31 @@ define(['socketio'], function(io) {
             return false;
         });
 
-        socket.on('message', function(msg){
-            $('#messages').prepend($('<li>').text(msg.message));
-        });
+        socket.on('message', this._printMessage);
     };
+
+    ChatPage.prototype._printMessage = function(message) {
+        var date = new Date(message.timestamp);
+
+        var html = '';
+        html += '<li>';
+        html += '<div class="media">';
+        html += '<div class="media-left"><a href="#"><img class="media-object" src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=64" alt="..."></a></div>';
+        html += '<div class="media-body">';
+        html += '<h4 class="media-heading" style="color: ' + message.userColor + ';">' + message.username + '</h4>';
+        html += message.message;
+        html += '<small style="display: block;">' + date.format('dd.mm.yyyy HH:MM:ss')  + '&nbsp;Uhr</small>';
+        html += '</div>';
+        html += '</div>';
+        html += '</div>';
+        html += '</li>';
+
+        $('#messages').prepend(html);
+    };
+
+
+
+
 
     ChatPage.prototype._buildMessage = function() {
         return {
