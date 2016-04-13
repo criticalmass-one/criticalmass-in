e972cd9cb3493c4427374c8764f957ca8387bd22
Malte Hübner <maltehuebner@gmx.org>
2016-04-13 22:52:59 +0200

    WIP.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
@@ -1,15 +1,39 @@
 define(['socketio'], function(io) {
     ChatPage = function() {
+
+    };
+
+    ChatPage.prototype._userToken = null;
+
+    ChatPage.prototype.setUserToken = function(userToken) {
+        this._userToken = userToken;
+    };
+
+    ChatPage.prototype.startChat = function() {
         var socket = io('http://criticalmass.cm:3000');
-        $('form').submit(function(){
-            socket.emit('chat message', $('#m').val());
+
+        var that = this;
+
+        $('form').submit(function() {
+            var message = that._buildMessage();
+
+            socket.emit('message', message);
+
             $('#m').val('');
+
             return false;
         });
 
-        socket.on('chat message', function(msg){
-            $('#messages').prepend($('<li>').text(msg));
+        socket.on('message', function(msg){
+            $('#messages').prepend($('<li>').text(msg.message));
         });
+    };
+
+    ChatPage.prototype._buildMessage = function() {
+        return {
+            userToken: this._userToken || '',
+            message: $('#m').val()
+        };
     };
 
     return ChatPage;
