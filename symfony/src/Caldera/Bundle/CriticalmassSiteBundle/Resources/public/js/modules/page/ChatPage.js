e899d0cb88d53b8e9b5797c3a283d2cd427dc084
Malte Hübner <maltehuebner@gmx.org>
2016-04-18 00:55:44 +0200

    Have to remove some redundant source code, but it works.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/ChatPage.js
@@ -22,6 +22,7 @@ define(['socketio', 'dateformat'], function(io) {
         this._initEvents();
 
         this._socket.on('message', this._printMessage.bind(this));
+        this._socket.on('memberlist', this._memberlist.bind(this));
         this._socket.on('joined', this._memberJoined.bind(this));
     };
 
@@ -34,6 +35,8 @@ define(['socketio', 'dateformat'], function(io) {
             var gender = $(this).data('gender');
 
             that._chooseGender(gender);
+
+            $('#gender-selector').hide();
         });
     };
     
@@ -52,9 +55,15 @@ define(['socketio', 'dateformat'], function(io) {
     ChatPage.prototype._initSocket = function() {
         this._socket = io('http://criticalmass.cm:3000');
 
+        this._getMemberlist();
+
         if (this._userToken || this._anonymousNameId) {
             this._join();
         }
+    };
+
+    ChatPage.prototype._getMemberlist = function() {
+        this._socket.emit('memberlist');
     };
 
     ChatPage.prototype._join = function() {
@@ -97,14 +106,46 @@ define(['socketio', 'dateformat'], function(io) {
         $('#messages').prepend(html);
     };
 
-    ChatPage.prototype._memberJoined = function(message) {
-        $('#members').prepend('<li>' + message.username + '</li>');
+    ChatPage.prototype._memberJoined = function(joinMessage) {
+        var html = '';
+        html += '<li class="margin-bottom-medium">';
+        html += '<div class="media">';
+        html += '<div class="media-left"><img class="media-object img-circle" src="http://www.gravatar.com/avatar/' + joinMessage.gravatarHash + '?s=16" alt="..."></div>';
+        html += '<div class="media-body">';
+        html += '<h4 class="media-heading" style="color: ' + joinMessage.userColor + ';">' + joinMessage.username + '</h4>';
+        html += '</div>';
+        html += '</div>';
+        html += '</div>';
+        html += '</li>';
+
+        $('#members').prepend(html);
     };
 
     ChatPage.prototype._memberLeft = function(message) {
 
     };
-    
+
+    ChatPage.prototype._memberlist = function(memberlist) {
+        $('#members').html('');
+
+        for (var index in memberlist) {
+            var member = memberlist[index];
+
+            var html = '';
+            html += '<li class="margin-bottom-medium">';
+            html += '<div class="media">';
+            html += '<div class="media-left"><img class="media-object img-circle" src="http://www.gravatar.com/avatar/' + member.gravatarHash + '?s=16" alt="..."></div>';
+            html += '<div class="media-body">';
+            html += '<h4 class="media-heading" style="color: ' + member.userColor + ';">' + member.username + '</h4>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</li>';
+
+            $('#members').prepend(html);
+        }
+    };
+
     ChatPage.prototype._buildMessage = function() {
         return {
 
