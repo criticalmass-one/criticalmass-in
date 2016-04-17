7e6fbab7935d92cf17be80143db4e5329ad80f6c
Malte Hübner <maltehuebner@gmx.org>
2016-04-17 21:12:07 +0200

    Yeah, pick unused names first.

--- symfony/src/Caldera/Bundle/CriticalmassModelBundle/Repository/AnonymousNameRepository.php
+++ symfony/src/Caldera/Bundle/CriticalmassModelBundle/Repository/AnonymousNameRepository.php
@@ -6,21 +6,26 @@ use Doctrine\ORM\EntityRepository;
 
 class AnonymousNameRepository extends EntityRepository
 {
-    public function findOneRandomUnusedName()
+    public function findOneRandomUnusedName($sex = 'male', $locale = 'de')
     {
         $builder = $this->createQueryBuilder('name');
 
         $builder->select('name');
 
+        $builder->leftJoin('name.posts', 'posts');
+
         $builder->where($builder->expr()->eq('name.enabled', 1));
-        $builder->andWhere($builder->expr()->eq('name.locale', '\'de\''));
+        $builder->andWhere($builder->expr()->eq('name.locale', '\''.$locale.'\''));
+        $builder->andWhere($builder->expr()->eq('name.sex', '\''.$sex.'\''));
+
+        $builder->orderBy('posts.dateTime', 'ASC');
 
         $query = $builder->getQuery();
 
         $result = $query->getResult();
 
         $randomKey = array_rand($result);
-
+        
         return $result[$randomKey];
     }
 }
