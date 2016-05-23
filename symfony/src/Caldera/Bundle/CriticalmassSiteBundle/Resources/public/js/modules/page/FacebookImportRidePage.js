8b1347ce98a44763c78d93800c094199f9d4edb1
Malte Hübner <maltehuebner@gmx.org>
2016-05-23 23:55:30 +0200

    Get all the stuff done for updating ride from facebook event values.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/FacebookImportRidePage.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/page/FacebookImportRidePage.js
@@ -15,6 +15,8 @@ define(['AutoMap'], function() {
     };
 
     FacebookImportRidePage.prototype._initCopyButtons = function() {
+        var that = this;
+
         $('button.copy-button').on('click', function() {
             $input = $(this).parent().find('.value');
 
@@ -22,11 +24,51 @@ define(['AutoMap'], function() {
 
             $col = $row.find('.col-md-4:nth-child(2)');
 
-            $dst = $col.find('.value');
+            $stringInput = $col.find('.string-value');
+            $dateSelects = $col.find('.date-value');
+            $timeSelects = $col.find('.time-value');
 
-            $dst.val($input.val());
+            if ($stringInput) {
+                $stringInput.val($input.val());
+            }
+
+            if ($dateSelects.length > 0) {
+                that._copyDateValue($dateSelects, $input.val());
+            }
+
+            if ($timeSelects.length > 0) {
+                that._copyTimeValue($timeSelects, $input.val());
+            }
         });
     };
+
+    FacebookImportRidePage.prototype._copyValue = function() {
+
+    };
+
+    FacebookImportRidePage.prototype._copyDateValue = function($timeSelects, value) {
+        var date = value.split('.');
+
+        var $day = $timeSelects.find('select:nth-child(1)');
+        var $month = $timeSelects.find('select:nth-child(2)');
+        var $year = $timeSelects.find('select:nth-child(3)');
+
+        $day.val(parseInt(date[0]));
+        $month.val(parseInt(date[1]));
+        $year.val(parseInt(date[2]));
+    };
+
+    FacebookImportRidePage.prototype._copyTimeValue = function($timeSelects, value) {
+        var time = value.split(':');
+
+        var $hour = $timeSelects.find('select:nth-child(1)');
+        var $minute = $timeSelects.find('select:nth-child(2)');
+
+        $hour.val(parseInt(time[0]));
+        $minute.val(parseInt(time[1]));
+    };
+
+
 
     return FacebookImportRidePage;
 });
