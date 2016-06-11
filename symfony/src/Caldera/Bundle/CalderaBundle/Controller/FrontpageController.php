10e2e2b7da8dea8c17d5a717b9ebb18af6a61070
Malte Hübner <maltehuebner@gmx.org>
2016-06-11 02:04:21 +0200

    WIP for CalderaBundle.

--- symfony/src/Caldera/Bundle/CalderaBundle/Controller/FrontpageController.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/Controller/FrontpageController.php
@@ -4,10 +4,10 @@ namespace Caldera\Bundle\CalderaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class FrontpageController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CalderaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('CalderaBundle:Frontpage:index.html.twig');
     }
 }
