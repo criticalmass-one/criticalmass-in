18fb5613a1ad839057391311c062bc9cc9224ffa
Malte Hübner <maltehuebner@gmx.org>
2016-08-16 23:08:54 +0200

    Added second view for single posts.

--- symfony/src/Caldera/Bundle/CriticalmassBlogBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/Bundle/CriticalmassBlogBundle/Controller/DefaultController.php
@@ -4,17 +4,34 @@ namespace Caldera\Bundle\CriticalmassBlogBundle\Controller;
 
 use Caldera\Bundle\CriticalmassSiteBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends AbstractController
 {
     public function indexAction(Request $request)
     {
-        $posts = $this->getBlogPostRepository()->findAll();
+        $posts = $this->getBlogPostRepository()->findBy([], ['dateTime' => 'DESC']);
 
         return $this->render(
             'CalderaCriticalmassBlogBundle:Default:index.html.twig',
             [
                 'posts' => $posts
+            ]
+        );
+    }
+
+    public function viewAction(Request $request, $slug)
+    {
+        $post = $this->getBlogPostRepository()->findOneBySlug($slug);
+
+        if (!$post) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render(
+            'CalderaCriticalmassBlogBundle:Default:view.html.twig',
+            [
+                'post' => $post
             ]
         );
     }
