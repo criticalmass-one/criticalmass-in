0ec59905881266092ed71a352f71a0921c28465d
Malte Hübner <maltehuebner@gmx.org>
2016-08-17 22:25:13 +0200

    Fixing this migration with WTF.

--- symfony/app/DoctrineMigrations/Version20160817170240.php
+++ symfony/app/DoctrineMigrations/Version20160817170240.php
@@ -18,7 +18,6 @@ class Version20160817170240 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE article');
         $this->addSql('ALTER TABLE post ADD blog_post_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA77FBEAF FOREIGN KEY (blog_post_id) REFERENCES blog_post (id)');
         $this->addSql('CREATE INDEX IDX_5A8A6C8DA77FBEAF ON post (blog_post_id)');
@@ -32,8 +31,6 @@ class Version20160817170240 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, abstract LONGTEXT DEFAULT NULL, text LONGTEXT NOT NULL, dateTime DATETIME NOT NULL, slug VARCHAR(255) NOT NULL, INDEX IDX_23A0E66A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user_user (id)');
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DA77FBEAF');
         $this->addSql('DROP INDEX IDX_5A8A6C8DA77FBEAF ON post');
         $this->addSql('ALTER TABLE post DROP blog_post_id');
