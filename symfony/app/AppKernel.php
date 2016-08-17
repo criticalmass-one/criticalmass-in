382f15e2710668d166876d93d5e161f81c6a1d94
Malte Hübner <maltehuebner@gmx.org>
2016-08-17 21:26:24 +0200

    Forgot to add this bundle.

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Craue\FormFlowBundle\CraueFormFlowBundle(),
             new Caldera\Bundle\CriticalmassRocksBundle\CalderaCriticalmassRocksBundle(),
             new Caldera\Bundle\CriticalmassPhotoBundle\CalderaCriticalmassPhotoBundle(),
+            new Caldera\Bundle\CriticalmassBlogBundle\CalderaCriticalmassBlogBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
         );
 
