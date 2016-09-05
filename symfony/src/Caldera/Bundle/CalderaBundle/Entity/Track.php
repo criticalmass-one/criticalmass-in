2fe9888cf4d8fed762ef1faad878629e216a4103
Malte Hübner <maltehuebner@gmx.org>
2016-09-05 09:37:36 +0200

    Use unknown track source by default.

--- symfony/src/Caldera/Bundle/CalderaBundle/Entity/Track.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/Entity/Track.php
@@ -170,7 +170,8 @@ class Track
     /**
      * @ORM\Column(type="string", columnDefinition="ENUM('TRACK_SOURCE_GPX', 'TRACK_SOURCE_STRAVA', 'TRACK_SOURCE_RUNKEEPER', 'TRACK_SOURCE_RUNTASTIC', 'TRACK_SOURCE_DRAW', 'TRACK_SOURCE_GLYMPSE', 'TRACK_SOURCE_CRITICALMAPS', 'TRACK_SOURCE_UNKNOWN')")
      */
-    protected $source;
+    protected $source = self::TRACK_SOURCE_UNKNOWN;
+
     /**
      * @ORM\Column(type="integer")
      *
