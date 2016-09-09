c5baadc8337bd596ace20476dfe996d304d37c38
Malte Hübner <maltehuebner@gmx.org>
2016-09-09 20:39:29 +0200

    Foo.

--- symfony/web/app_dev.php
+++ symfony/web/app_dev.php
@@ -13,13 +13,13 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
-    header('HTTP/1.0 403 Forbidden');
+    //header('HTTP/1.0 403 Forbidden');
 //    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
 
+Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
@@ -28,4 +28,5 @@ Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
+
 $kernel->terminate($request, $response);
