e310a0855d4311486eb0c0ddec466eaf3d2c3b19
Malte Hübner <maltehuebner@gmx.org>
2016-09-17 00:50:30 +0200

    Some refactoring.

--- symfony/src/Caldera/Bundle/CriticalmassCoreBundle/BaseTrait/ViewStorageTrait.php
+++ symfony/src/Caldera/Bundle/CriticalmassCoreBundle/BaseTrait/ViewStorageTrait.php
@@ -14,8 +14,22 @@ use Lsw\MemcacheBundle\Cache\LoggingMemcache;
 
 trait ViewStorageTrait
 {
-    protected function countView(ViewableInterface $viewable, string $identifier)
+    protected function getIdentifier(ViewableInterface $viewable): string
     {
+        $namespaceClass = get_class($viewable);
+        $namespaceParts = explode('\\', $namespaceClass);
+
+        $classname = array_pop($namespaceParts);
+
+        $identifier = lcfirst($classname);
+
+        return $identifier;
+    }
+
+    protected function countView(ViewableInterface $viewable)
+    {
+        $identifier = $this->getIdentifier($viewable);
+
         /** @var LoggingMemcache $memcache */
         $memcache = $this->get('memcache.criticalmass');
 
@@ -41,38 +55,66 @@ trait ViewStorageTrait
         $memcache->set($identifier.$viewable->getId().'_view'.$additionalViews, $viewArray);
     }
 
+    /**
+     * @param Thread $thread
+     * @deprecated
+     */
     protected function countThreadView(Thread $thread)
     {
-        $this->countView($thread, 'thread');
+        $this->countView($thread);
     }
 
+    /**
+     * @param Photo $photo
+     * @deprecated
+     */
     protected function countPhotoView(Photo $photo)
     {
-        $this->countView($photo, 'photo');
+        $this->countView($photo);
     }
 
+    /**
+     * @param Ride $ride
+     * @deprecated
+     */
     protected function countRideView(Ride $ride)
     {
-        $this->countView($ride, 'ride');
+        $this->countView($ride);
     }
 
+    /**
+     * @param Event $event
+     * @deprecated
+     */
     protected function countEventView(Event $event)
     {
-        $this->countView($event, 'event');
+        $this->countView($event);
     }
 
+    /**
+     * @param City $city
+     * @deprecated
+     */
     protected function countCityView(City $city)
     {
-        $this->countView($city, 'city');
+        $this->countView($city);
     }
 
+    /**
+     * @param BlogPost $blogPost
+     * @deprecated
+     */
     protected function countBlogPostView(BlogPost $blogPost)
     {
-        $this->countView($blogPost, 'blogPost');
+        $this->countView($blogPost);
     }
 
+    /**
+     * @param Content $content
+     * @deprecated
+     */
     protected function countContentView(Content $content)
     {
-        $this->countView($content, 'content');
+        $this->countView($content);
     }
 }
