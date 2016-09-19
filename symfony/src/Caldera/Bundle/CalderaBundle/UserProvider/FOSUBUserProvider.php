e24df4af1fc300927d9b59b3996a39e6332792aa
Malte Hübner <maltehuebner@gmx.org>
2016-09-20 01:02:39 +0200

    WIP.

--- symfony/src/Caldera/Bundle/CalderaBundle/UserProvider/FOSUBUserProvider.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/UserProvider/FOSUBUserProvider.php
@@ -15,18 +15,19 @@ class FOSUBUserProvider extends BaseClass
     {
         $property = $this->getProperty($response);
         $username = $response->getUsername();
-        //on connect - get the access token and the user ID
+
         $service = $response->getResourceOwner()->getName();
         $setter = 'set'.ucfirst($service);
         $setter_id = $setter.'Id';
         $setter_token = $setter.'AccessToken';
+
         //we "disconnect" previously connected users
         if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
             $previousUser->$setter_id(null);
             $previousUser->$setter_token(null);
             $this->userManager->updateUser($previousUser);
         }
-        //we connect current user
+
         $user->$setter_id($username);
         $user->$setter_token($response->getAccessToken());
         $this->userManager->updateUser($user);
@@ -38,31 +39,42 @@ class FOSUBUserProvider extends BaseClass
     {
         $username = $response->getUsername();
         $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
-        //when the user is registrating
+
         if (null === $user) {
             $service = $response->getResourceOwner()->getName();
+
             $setter = 'set'.ucfirst($service);
             $setter_id = $setter.'Id';
             $setter_token = $setter.'AccessToken';
-            // create new user here
+
             $user = $this->userManager->createUser();
             $user->$setter_id($username);
             $user->$setter_token($response->getAccessToken());
-            //I have set all requested data with the user's username
-            //modify here with relevant data
-            $user->setUsername($username);
-            $user->setEmail($username);
-            $user->setPassword($username);
-            $user->setEnabled(true);
+
+            $this->setUserData($user, $response);
+
             $this->userManager->updateUser($user);
             return $user;
         }
-        //if user exists - go with the HWIOAuth way
+
         $user = parent::loadUserByOAuthUserResponse($response);
         $serviceName = $response->getResourceOwner()->getName();
         $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
-        //update access token
+
         $user->$setter($response->getAccessToken());
         return $user;
+    }
+
+    protected function setUserData(UserInterface $user, UserResponseInterface $response)
+    {
+        $username = $response->getNickname() ? $response->getNickname() : $response->getUsername();
+        $email = $response->getEmail() ? $response->getEmail() : $response->getUsername();
+
+        $user
+            ->setUsername($username)
+            ->setEmail($email)
+            ->setPassword('')
+            ->setEnabled(true)
+        ;
     }
 }
