0fd6ae533145b74c561819b396d2e4dd90f3523a
Malte Hübner <maltehuebner@gmx.org>
2016-09-29 15:51:17 +0200

    Add new parser step to link to subheadlines.

---
+++ symfony/src/Caldera/Bundle/CalderaBundle/Parser/MultiStepParser/Step/AnchorParserStep.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Caldera\Bundle\CalderaBundle\Parser\MultiStepParser\Step;
+
+use Caldera\Bundle\CalderaBundle\Parser\MultiStepParser\StepInterface;
+
+class AnchorParserStep implements StepInterface
+{
+    public function __construct()
+    {
+
+    }
+
+    protected static function callback(array $matches): string
+    {
+        $caption = $matches[1];
+
+        $anchor = preg_replace("/[^A-Za-z0-9 ]/", '', $caption);
+        $anchor = str_replace(' ', '-', $anchor);
+        $anchor = urlencode($anchor);
+
+        $result = '<a name="'.$anchor.'" href="#'.$anchor.'"><h3>'.$caption.'</h3></a>';
+
+        return $result;
+    }
+
+    public function parse(string $text): string
+    {
+        $text = preg_replace_callback('#\<h3\>(.*)\<\/h3\>#', 'self::callback', $text);
+
+        return $text;
+    }
+}
