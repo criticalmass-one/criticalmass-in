fae2243786a497edb9351c233f528c7ebce4dc09
Malte Hübner <maltehuebner@gmx.org>
2016-10-15 14:19:46 +0200

    Fix for RideEstimateType.

--- symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Form/Type/RideEstimateType.php
+++ symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Form/Type/RideEstimateType.php
@@ -3,6 +3,7 @@
 namespace Caldera\Bundle\CriticalmassCoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RideEstimateType extends AbstractType
@@ -10,7 +11,7 @@ class RideEstimateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('estimatedParticipants', 'text', array('required' => true));
+            ->add('estimatedParticipants', TextType::class, array('required' => true));
     }
 
     public function getName()
