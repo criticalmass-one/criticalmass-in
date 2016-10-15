dd0cfb04830ad97b1279c31e8e8bb32d72f545b4
Malte Hübner <maltehuebner@gmx.org>
2016-10-15 17:32:19 +0200

    Fixing SubrideType.

--- symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Form/Type/SubrideType.php
+++ symfony/src/Caldera/Bundle/CriticalmassCoreBundle/Form/Type/SubrideType.php
@@ -3,6 +3,10 @@
 namespace Caldera\Bundle\CriticalmassCoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class SubrideType extends AbstractType
@@ -10,15 +14,15 @@ class SubrideType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', 'text', array('required' => false))
-            ->add('description', 'textarea', array('required' => false))
-            ->add('time', 'time')
-            ->add('location', 'text', array('required' => false))
-            ->add('latitude', 'hidden')
-            ->add('longitude', 'hidden')
-            ->add('facebook', 'text', array('required' => false))
-            ->add('twitter', 'text', array('required' => false))
-            ->add('url', 'text', array('required' => false));
+            ->add('title', TextType::class, array('required' => false))
+            ->add('description', TextareaType::class, array('required' => false))
+            ->add('time', TimeType::class)
+            ->add('location', TextType::class, array('required' => false))
+            ->add('latitude', HiddenType::class)
+            ->add('longitude', HiddenType::class)
+            ->add('facebook', TextType::class, array('required' => false))
+            ->add('twitter', TextType::class, array('required' => false))
+            ->add('url', TextType::class, array('required' => false));
     }
 
     public function getName()
