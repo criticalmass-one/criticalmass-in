06007e41bf4715f33c11fe2265215e58930bf424
Malte Hübner <maltehuebner@gmx.org>
2016-10-22 21:13:57 +0200

    Fix routing for city requests.

--- symfony/src/Caldera/Bundle/CriticalmassTipsBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/Bundle/CriticalmassTipsBundle/Controller/DefaultController.php
@@ -5,10 +5,11 @@ namespace Caldera\Bundle\CriticalmassTipsBundle\Controller;
 use Caldera\Bundle\CalderaBundle\Manager\ContentManager\ContentManager;
 use Caldera\Bundle\CriticalmassSiteBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, string $citySlug): Response
     {
         /** @var ContentManager $contentManager */
         $contentManager = $this->get('caldera.manager.content_manager');
