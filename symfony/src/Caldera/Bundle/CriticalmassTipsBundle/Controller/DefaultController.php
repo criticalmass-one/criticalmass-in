ac729571a1ac77b88fe8681a43e9f2e69a2fdc45
Malte Hübner <maltehuebner@gmx.org>
2016-10-26 11:10:06 +0200

    Count views on content.

--- symfony/src/Caldera/Bundle/CriticalmassTipsBundle/Controller/DefaultController.php
+++ symfony/src/Caldera/Bundle/CriticalmassTipsBundle/Controller/DefaultController.php
@@ -3,18 +3,23 @@
 namespace Caldera\Bundle\CriticalmassTipsBundle\Controller;
 
 use Caldera\Bundle\CalderaBundle\Manager\ContentManager\ContentManager;
+use Caldera\Bundle\CriticalmassCoreBundle\BaseTrait\ViewStorageTrait;
 use Caldera\Bundle\CriticalmassSiteBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
+    use ViewStorageTrait;
+
     public function indexAction(Request $request, string $citySlug): Response
     {
         /** @var ContentManager $contentManager */
         $contentManager = $this->get('caldera.manager.content_manager');
 
         $content = $contentManager->getBySlug('tipps-fuer-die-teilnahme-in-hamburg');
+
+        $this->countView($content);
         
         return $this->render(
             'CalderaCriticalmassTipsBundle:Default:index.html.twig',
