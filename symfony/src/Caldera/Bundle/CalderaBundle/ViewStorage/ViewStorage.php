b81023212bd8bd7bbcd9084d66dbd171f8a76662
Malte Hübner <maltehuebner@gmx.org>
2016-10-26 20:47:43 +0200

    WIP for new storage.

---
+++ symfony/src/Caldera/Bundle/CalderaBundle/ViewStorage/ViewStorage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maltehuebner
+ * Date: 26.10.16
+ * Time: 15:26
+ */
+
+namespace Caldera\Bundle\CalderaBundle\ViewStorage;
+
+
+class ViewStorage
+{
+
+}
---
+++ symfony/src/Caldera/Bundle/CalderaBundle/ViewStorage/ViewStorageInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: maltehuebner
+ * Date: 26.10.16
+ * Time: 15:26
+ */
+interface ViewStorageInterface
+{
+
+}
