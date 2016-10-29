e5d82448eb957631230f7fa73bd615208f9d8ad9
Malte Hübner <maltehuebner@gmx.org>
2016-10-29 22:29:55 +0200

    Add more lines of code to OsmTile.

--- symfony/src/Caldera/Bundle/CalderaBundle/MapPrinter/Tile/OsmTile.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/MapPrinter/Tile/OsmTile.php
@@ -2,8 +2,42 @@
 
 namespace Caldera\Bundle\CalderaBundle\MapPrinter\Tile;
 
-class OsmTile extends AbstractTile
+class OsmTile implements TileInterface
 {
+    /** @var int $pixelWidth */
     protected $pixelWidth = 256;
+
+    /** @var int $pixelHeight */
     protected $pixelHeight = 256;
+
+    /** @var int $x */
+    protected $x;
+
+    /** @var int $y */
+    protected $y;
+
+    /** @var int $zoomLevel */
+    protected $zoomLevel;
+
+    public function __construct(int $x, int $y, int $zoomLevel)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->zoomLevel = $zoomLevel;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    public function getZoomLevel(): int
+    {
+        return $this->zoomLevel;
+    }
 }
