3b9cda15bf703b551704e73085e275ab5aedb1e0
Malte Hübner <maltehuebner@gmx.org>
2016-10-29 15:50:52 +0200

    Add some more access methods.

--- symfony/src/Caldera/Bundle/CalderaBundle/MapPrinter/Coord/Coord.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/MapPrinter/Coord/Coord.php
@@ -15,4 +15,44 @@ class Coord
         $this->latitude = $latitude;
         $this->longitude = $longitude;
     }
+
+    public function toArray(): array
+    {
+        return [
+            $this->latitude,
+            $this->longitude
+        ];
+    }
+
+    public function toInversedArray(): array
+    {
+        return [
+            $this->longitude,
+            $this->latitude
+        ];
+    }
+
+    public function setLatitude(float $latitude): Coord
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function setLongitude(float $longitude): Coord
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
 }
