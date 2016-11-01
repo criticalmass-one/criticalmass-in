667c6cb862a09ca11b9c96afa2027471583d310e
Malte Hübner <maltehuebner@gmx.org>
2016-11-01 18:24:00 +0100

    Okay, fix everything regarding routing.

--- symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
@@ -4,10 +4,10 @@ namespace Caldera\Bundle\CyclewaysBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class MapController extends Controller
+class IncidentController extends Controller
 {
     public function mapAction()
     {
-        return $this->render('CalderaCyclewaysBundle:Frontpage:index.html.twig');
+        return $this->render('CalderaCyclewaysBundle:Incident:map.html.twig');
     }
 }
