f687205ed908d67558b7eaeb18866a87358005db
Malte Hübner <maltehuebner@gmx.org>
2016-11-02 12:08:21 +0100

    WIP in adding new incidents.

--- symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
@@ -2,7 +2,11 @@
 
 namespace Caldera\Bundle\CyclewaysBundle\Controller;
 
+use Caldera\Bundle\CalderaBundle\Entity\City;
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
+use Caldera\Bundle\CriticalmassCoreBundle\Form\Type\IncidentType;
 use Caldera\Bundle\CriticalmassSiteBundle\Controller\AbstractController;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -48,5 +52,78 @@ class IncidentController extends AbstractController
         $serializedData = $serializer->serialize($results, 'json');
 
         return new Response($serializedData);
+    }
+
+    public function addAction(Request $request, $citySlug)
+    {
+        $city = $this->getCheckedCity($citySlug);
+
+        $incident = new Incident();
+        $incident->setUser($this->getUser());
+        $incident->setCity($city);
+
+        $form = $this->createForm(
+            IncidentType::class,
+            $incident,
+            [
+                'action' => $this->generateUrl(
+                    'caldera_cycleways_incident_add',
+                    [
+                        'citySlug' => $city->getSlug()
+                    ]
+                )
+            ]
+        );
+
+        if ('POST' == $request->getMethod()) {
+            return $this->addPostAction($request, $incident, $city, $form);
+        } else {
+            return $this->addGetAction($request, $incident, $city, $form);
+        }
+    }
+
+    public function addGetAction(Request $request, Incident $incident, City $city, Form $form)
+    {
+        return $this->render(
+            'CalderaCyclewaysBundle:Incident:edit.html.twig',
+            [
+                'incident' => null,
+                'city' => $city,
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    public function addPostAction(Request $request, Incident $incident, City $city, Form $form)
+    {
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($form->getData());
+            $em->flush();
+
+            $form = $this->createForm(
+                IncidentType::class,
+                $incident,
+                [
+                    'action' => $this->generateUrl(
+                        'caldera_cycleways_incident_add',
+                        [
+                            'citySlug' => $city->getSlug()
+                        ]
+                    )
+                ]
+            );
+        }
+
+        return $this->render(
+            'CalderaCriticalmassSiteBundle:Incident:edit.html.twig',
+            array(
+                'incident' => $incident,
+                'form' => $form->createView(),
+                'city' => $city
+            )
+        );
     }
 }
---
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Form/Type/IncidentType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Caldera\Bundle\CyclewaysBundle\Form\Type;
+
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class IncidentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, array('required' => false))
+            ->add('description', TextareaType::class, array('required' => false))
+            ->add('polyline', HiddenType::class)
+            ->add('geometryType', HiddenType::class)
+            ->add('incidentType', ChoiceType::class,
+                [
+                    'choices' => [
+                        0 => Incident::INCIDENT_RAGE,
+                        1 => Incident::INCIDENT_DANGER,
+                        2 => Incident::INCIDENT_ROADWORKS
+                    ]
+                ]
+            )
+            ->add('visibleFrom', DateType::class)
+            ->add('visibleTo', DateType::class)
+            ->add('expires', CheckboxType::class);
+    }
+
+    public function getName()
+    {
+        return 'incident';
+    }
+}
