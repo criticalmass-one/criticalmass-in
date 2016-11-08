9807001b8c1209602cf70eef7c59a0ebb8df7f90
Malte Hübner <maltehuebner@gmx.org>
2016-11-08 23:15:35 +0100

    Added type hinting.

--- symfony/src/Caldera/Bundle/CalderaBundle/Manager/AbstractManager.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/Manager/AbstractManager.php
@@ -2,11 +2,14 @@
 
 namespace Caldera\Bundle\CalderaBundle\Manager;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
 abstract class AbstractManager
 {
+    /** @var Registry $doctrine */
     protected $doctrine;
 
-    public function __construct($doctrine)
+    public function __construct(Registry $doctrine)
     {
         $this->doctrine = $doctrine;
     }
