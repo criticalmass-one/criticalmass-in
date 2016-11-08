073abb12c5c893d10f6946295cd33262bff41aac
Malte Hübner <maltehuebner@gmx.org>
2016-11-08 20:15:13 +0100

    Starting with IncidentManager.

---
+++ symfony/src/Caldera/Bundle/CalderaBundle/Manager/IncidentManager/IncidentManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Caldera\Bundle\CalderaBundle\Manager\ContentManager;
+
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
+use Caldera\Bundle\CalderaBundle\Manager\AbstractElasticManager;
+use Caldera\Bundle\CalderaBundle\Manager\AbstractManager;
+use Caldera\Bundle\CalderaBundle\Repository\IncidentRepository;
+use Caldera\Bundle\CalderaBundle\Repository\PostRepository;
+
+class IncidentManager extends AbstractElasticManager
+{
+    /** @var IncidentRepository $incidentRepository */
+    protected $incidentRepository = null;
+
+    public function __construct($doctrine, $elasticIndex)
+    {
+        parent::__construct($doctrine, $elasticIndex);
+
+        $this->incidentRepository = $this->doctrine->getRepository('CalderaBundle:Incident');
+    }
+
+    public function getIncidentsInBounds(Incident $incident): array
+    {
+        return $this->postRepository->getPostsForIncident($incident);
+    }
+}
