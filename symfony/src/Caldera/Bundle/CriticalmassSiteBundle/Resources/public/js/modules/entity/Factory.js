11d34941c8e7ff99150484f78c503f90d9d4e961
Malte Hübner <maltehuebner@gmx.org>
2016-11-10 23:13:56 +0100

    Do not add another underscore. Happens if entities are serialized and parsed again.

--- symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/entity/Factory.js
+++ symfony/src/Caldera/Bundle/CriticalmassSiteBundle/Resources/public/js/modules/entity/Factory.js
@@ -89,14 +89,20 @@ define([], function () {
             if (object.hasOwnProperty(property)) {
                 entityProperty = property.charAt(0).toLowerCase() + property.slice(1);
 
+                var prefix = '';
+
+                if (entityProperty.charAt(0) != '_') {
+                    prefix = '_';
+                }
+
                 if (entityProperty == 'timestamp') {
-                    entity['_' + entityProperty] = new Date(object[property] * 1000);
+                    entity[prefix + entityProperty] = new Date(object[property] * 1000);
                 } else if (entityProperty == 'city') {
-                    entity['_' + entityProperty] = this.createCity(object[property]);
+                    entity[prefix + entityProperty] = this.createCity(object[property]);
                 } else if (entityProperty == 'user') {
-                    entity['_' + entityProperty] = this.createUser(object[property]);
+                    entity[prefix + entityProperty] = this.createUser(object[property]);
                 } else {
-                    entity['_' + entityProperty] = object[property];
+                    entity[prefix + entityProperty] = object[property];
                 }
             }
         }
