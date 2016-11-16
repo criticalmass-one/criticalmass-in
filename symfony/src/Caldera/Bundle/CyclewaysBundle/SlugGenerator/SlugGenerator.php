2a65a9426c371c1493ffcce8648d2daf8ff8e0fc
Malte Hübner <maltehuebner@gmx.org>
2016-11-17 00:35:20 +0100

    Implement some more stuff.

--- symfony/src/Caldera/Bundle/CyclewaysBundle/SlugGenerator/SlugGenerator.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/SlugGenerator/SlugGenerator.php
@@ -2,7 +2,20 @@
 
 namespace Caldera\Bundle\CyclewaysBundle\SlugGenerator;
 
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
+use Malenki\Slug;
+
 class SlugGenerator
 {
+    public function __construct()
+    {
+    }
 
+    public function generateSlug(Incident $incident): string
+    {
+        $slug = new Slug($incident->getTitle().' '.$incident->getStreet().' '.$incident->getDistrict().' '.$incident->getCity()->getCity().' '.$incident->getId());
+        $incident->setSlug($slug);
+
+        return $slug;
+    }
 }
