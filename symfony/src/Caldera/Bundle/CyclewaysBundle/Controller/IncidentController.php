08b3a93f2741a484f4b0f1d6aea1b6be6fbfe7a2
Malte Hübner <maltehuebner@gmx.org>
2016-11-16 21:56:35 +0100

    Copy create permalink method.

--- symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/IncidentController.php
@@ -208,38 +208,6 @@ class IncidentController extends AbstractController
         return $title;
     }
 
-    protected function createPermalink(Incident $incident)
-    {
-        $url = $this->generateUrl(
-            'caldera_cycleways_incident_show',
-            [
-                'slug' => $incident->getSlug()
-            ],
-            UrlGeneratorInterface::ABSOLUTE_URL
-        );
-
-        $apiUrl =  $this->getParameter('sqibe.api_url');
-
-        $data = [
-            'url' => $url,
-            'title' => $incident->getTitle(),
-            'format'   => 'json',
-            'action'   => 'shorturl',
-            'username' => $this->getParameter('sqibe.api_username'),
-            'password' => $this->getParameter('sqibe.api_password')
-        ];
-
-        $curl = new Curl();
-        $curl->post(
-            $apiUrl,
-            $data
-        );
-
-        $permalink = $curl->response->shorturl;
-
-        $incident->setPermalink($permalink);
-    }
-
     protected function storeView(Incident $incident)
     {
         $viewStorage = $this->get('caldera.view_storage.cache');
--- symfony/src/Caldera/Bundle/CyclewaysBundle/PermalinkManager/SqibePermalinkManager.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/PermalinkManager/SqibePermalinkManager.php
@@ -2,10 +2,46 @@
 
 namespace Caldera\Bundle\CyclewaysBundle\Permalinkmanager;
 
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
+use Curl\Curl;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class SqibePermalinkManager
 {
     public function __construct()
     {
-        
+
+    }
+
+    protected function createPermalink(Incident $incident)
+    {
+        $url = $this->generateUrl(
+            'caldera_cycleways_incident_show',
+            [
+                'slug' => $incident->getSlug()
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $apiUrl =  $this->getParameter('sqibe.api_url');
+
+        $data = [
+            'url' => $url,
+            'title' => $incident->getTitle(),
+            'format'   => 'json',
+            'action'   => 'shorturl',
+            'username' => $this->getParameter('sqibe.api_username'),
+            'password' => $this->getParameter('sqibe.api_password')
+        ];
+
+        $curl = new Curl();
+        $curl->post(
+            $apiUrl,
+            $data
+        );
+
+        $permalink = $curl->response->shorturl;
+
+        $incident->setPermalink($permalink);
     }
 }
