6fc34fccbdab02006abf28215ae4e316409f15f8
Malte Hübner <maltehuebner@gmx.org>
2016-11-28 20:17:15 +0100

    Fix name mismatch.

--- symfony/app/DoctrineMigrations/Version20161128190211.php
+++ symfony/app/DoctrineMigrations/Version20161128190211.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20161128120211 extends AbstractMigration
+class Version20161128190211 extends AbstractMigration
 {
     /**
      * @param Schema $schema
