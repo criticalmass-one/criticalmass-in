de9b9819d8067ad044d1badf545ee9eaaa2e5242
Malte Hübner <maltehuebner@gmx.org>
2016-12-06 23:38:17 +0100

    Get PhotoController like a bit of WIP.

--- symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/PhotoController.php
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/PhotoController.php
@@ -2,46 +2,40 @@
 
 namespace Caldera\Bundle\CyclewaysBundle\Controller;
 
-use Caldera\Bundle\CalderaBundle\Entity\Event;
+use Caldera\Bundle\CalderaBundle\Entity\Incident;
 use Caldera\Bundle\CalderaBundle\Entity\Photo;
-use Caldera\Bundle\CalderaBundle\Entity\Ride;
-use Caldera\Bundle\CriticalmassCoreBundle\Image\ExifReader\DateTimeExifReader;
-use Caldera\Bundle\CriticalmassCoreBundle\Image\PhotoGps\PhotoGps;
+use Caldera\Bundle\CriticalmassSiteBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class PhotoUploadController extends AbstractController
+class PhotoController extends AbstractController
 {
-    public function uploadAction(Request $request, $citySlug, $rideDate = null, $eventSlug = null)
+    public function uploadAction(Request $request, string $slug): Response
     {
-        $ride = null;
-        $event = null;
+        $incident = $this->getIncidentRepository()->findOneBySlug($slug);
 
-        if ($rideDate) {
-            $ride = $this->getCheckedCitySlugRideDateRide($citySlug, $rideDate);
-        } else {
-            $event = $this->getEventRepository()->findOneBySlug($eventSlug);
+        if (!$incident) {
+            throw $this->createNotFoundException();
         }
 
         if ($request->getMethod() == 'POST') {
-            return $this->uploadPostAction($request, $ride, $event);
+            return $this->uploadPostAction($request, $incident);
         } else {
-            return $this->uploadGetAction($request, $ride, $event);
+            return $this->uploadGetAction($request, $incident);
         }
     }
 
-    protected function uploadGetAction(Request $request, Ride $ride = null, Event $event = null)
+    protected function uploadGetAction(Request $request, Incident $incident): Response
     {
         return $this->render(
-            'CalderaCriticalmassSiteBundle:PhotoUpload:upload.html.twig',
+            'CalderaCyclewaysBundle:Photo:upload.html.twig',
             [
-                'ride' => $ride,
-                'event' => $event
+                'incident' => $incident
             ]
         );
     }
 
-    protected function uploadPostAction(Request $request, Ride $ride = null, Event $event = null)
+    protected function uploadPostAction(Request $request, Incident $incident): Response
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -50,54 +44,11 @@ class PhotoUploadController extends AbstractController
         $photo->setImageFile($request->files->get('file'));
         $photo->setUser($this->getUser());
 
-        if ($ride) {
-            $photo->setRide($ride);
-            $photo->setCity($ride->getCity());
-        }
-
-        if ($event) {
-            $photo->setEvent($event);
-            $photo->setCity($event->getCity());
-        }
+        $photo->setIncident($incident);
+        $photo->setCity($incident->getCity());
 
         $em->persist($photo);
         $em->flush();
-
-        /**
-         * @var DateTimeExifReader $dter
-         */
-        $dter = $this->get('caldera.criticalmass.image.exifreader.datetime');
-
-        $dateTime = $dter
-            ->setPhoto($photo)
-            ->execute()
-            ->getDateTime();
-
-        $photo->setDateTime($dateTime);
-
-        $em->persist($photo);
-        $em->flush();
-
-        $track = null;
-
-        if ($ride) {
-            $track = $this->getTrackRepository()->findByUserAndRide($ride, $this->getUser());
-        }
-
-        if ($ride && $track) {
-            /**
-             * @var PhotoGps $pgps
-             */
-            $pgps = $this->get('caldera.criticalmass.image.photogps');
-
-            $pgps
-                ->setPhoto($photo)
-                ->setTrack($track)
-                ->execute();
-
-            $em->merge($photo);
-            $em->flush();
-        }
 
         return new Response('foo');
     }
