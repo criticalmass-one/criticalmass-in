87258af181cdbc313155bb3e2667e087752a59fe
Malte Hübner <maltehuebner@gmx.org>
2016-12-06 22:48:27 +0100

    Stupid copy of PhotoUploadController.

---
+++ symfony/src/Caldera/Bundle/CyclewaysBundle/Controller/PhotoController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Caldera\Bundle\CyclewaysBundle\Controller;
+
+use Caldera\Bundle\CalderaBundle\Entity\Event;
+use Caldera\Bundle\CalderaBundle\Entity\Photo;
+use Caldera\Bundle\CalderaBundle\Entity\Ride;
+use Caldera\Bundle\CriticalmassCoreBundle\Image\ExifReader\DateTimeExifReader;
+use Caldera\Bundle\CriticalmassCoreBundle\Image\PhotoGps\PhotoGps;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PhotoUploadController extends AbstractController
+{
+    public function uploadAction(Request $request, $citySlug, $rideDate = null, $eventSlug = null)
+    {
+        $ride = null;
+        $event = null;
+
+        if ($rideDate) {
+            $ride = $this->getCheckedCitySlugRideDateRide($citySlug, $rideDate);
+        } else {
+            $event = $this->getEventRepository()->findOneBySlug($eventSlug);
+        }
+
+        if ($request->getMethod() == 'POST') {
+            return $this->uploadPostAction($request, $ride, $event);
+        } else {
+            return $this->uploadGetAction($request, $ride, $event);
+        }
+    }
+
+    protected function uploadGetAction(Request $request, Ride $ride = null, Event $event = null)
+    {
+        return $this->render(
+            'CalderaCriticalmassSiteBundle:PhotoUpload:upload.html.twig',
+            [
+                'ride' => $ride,
+                'event' => $event
+            ]
+        );
+    }
+
+    protected function uploadPostAction(Request $request, Ride $ride = null, Event $event = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $photo = new Photo();
+
+        $photo->setImageFile($request->files->get('file'));
+        $photo->setUser($this->getUser());
+
+        if ($ride) {
+            $photo->setRide($ride);
+            $photo->setCity($ride->getCity());
+        }
+
+        if ($event) {
+            $photo->setEvent($event);
+            $photo->setCity($event->getCity());
+        }
+
+        $em->persist($photo);
+        $em->flush();
+
+        /**
+         * @var DateTimeExifReader $dter
+         */
+        $dter = $this->get('caldera.criticalmass.image.exifreader.datetime');
+
+        $dateTime = $dter
+            ->setPhoto($photo)
+            ->execute()
+            ->getDateTime();
+
+        $photo->setDateTime($dateTime);
+
+        $em->persist($photo);
+        $em->flush();
+
+        $track = null;
+
+        if ($ride) {
+            $track = $this->getTrackRepository()->findByUserAndRide($ride, $this->getUser());
+        }
+
+        if ($ride && $track) {
+            /**
+             * @var PhotoGps $pgps
+             */
+            $pgps = $this->get('caldera.criticalmass.image.photogps');
+
+            $pgps
+                ->setPhoto($photo)
+                ->setTrack($track)
+                ->execute();
+
+            $em->merge($photo);
+            $em->flush();
+        }
+
+        return new Response('foo');
+    }
+}
