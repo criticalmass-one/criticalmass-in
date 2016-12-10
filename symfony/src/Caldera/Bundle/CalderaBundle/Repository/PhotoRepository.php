5f6eb7a0a7c213c50ab851d1536e99dcfd8be7c4
Malte Hübner <maltehuebner@gmx.org>
2016-12-10 21:56:03 +0100

    Add incident order to photo repository.

--- symfony/src/Caldera/Bundle/CalderaBundle/Repository/PhotoRepository.php
+++ symfony/src/Caldera/Bundle/CalderaBundle/Repository/PhotoRepository.php
@@ -26,8 +26,10 @@ class PhotoRepository extends EntityRepository
 
         if ($photo->getRide()) {
             $builder->where($builder->expr()->eq('photo.ride', $photo->getRide()->getId()));
-        } else {
+        } elseif ($photo->getEvent()) {
             $builder->where($builder->expr()->eq('photo.event', $photo->getEvent()->getId()));
+        } elseif ($photo->getIncident()) {
+            $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
         }
 
         $builder->andWhere($builder->expr()->lt('photo.dateTime', '\'' . $photo->getDateTime()->format('Y-m-d H:i:s') . '\''));
@@ -59,8 +61,10 @@ class PhotoRepository extends EntityRepository
 
         if ($photo->getRide()) {
             $builder->where($builder->expr()->eq('photo.ride', $photo->getRide()->getId()));
-        } else {
+        } elseif ($photo->getEvent()) {
             $builder->where($builder->expr()->eq('photo.event', $photo->getEvent()->getId()));
+        } elseif ($photo->getIncident()) {
+            $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
         }
 
         $builder->andWhere($builder->expr()->gt('photo.dateTime', '\'' . $photo->getDateTime()->format('Y-m-d H:i:s') . '\''));
