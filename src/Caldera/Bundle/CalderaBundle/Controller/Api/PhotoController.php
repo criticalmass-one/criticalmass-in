a93d9e22fc965616d536f16d1179b84706635072
Malte Hübner <maltehuebner@gmx.org>
2017-02-23 15:14:10 +0100

    Add apidoc.

--- src/Caldera/Bundle/CalderaBundle/Controller/Api/PhotoController.php
+++ src/Caldera/Bundle/CalderaBundle/Controller/Api/PhotoController.php
@@ -4,11 +4,12 @@ namespace Caldera\Bundle\CalderaBundle\Controller\Api;
 
 use Caldera\Bundle\CalderaBundle\Traits\RepositoryTrait;
 use Caldera\Bundle\CalderaBundle\Traits\UtilTrait;
+use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-class CityController extends BaseController
+class PhotoController extends BaseController
 {
     use RepositoryTrait;
     use UtilTrait;
@@ -19,32 +20,13 @@ class CityController extends BaseController
      *  description="This is a description of your API method"
      * )
      */
-    public function listAction(): Response
+    public function galleryAction(): Response
     {
-        $cityList = $this->getCityRepository()->findEnabledCities();
+        $gallery = $this->getPhotoRepository()->findRidesForGallery();
 
         $view = View::create();
         $view
-            ->setData($cityList)
-            ->setFormat('json')
-            ->setStatusCode(200);
-
-        return $this->handleView($view);
-    }
-
-    /**
-     * @ApiDoc(
-     *  resource=true,
-     *  description="This is a description of your API method"
-     * )
-     */
-    public function showAction(string $citySlug): Response
-    {
-        $city = $this->getCheckedCity($citySlug);
-
-        $view = View::create();
-        $view
-            ->setData($city)
+            ->setData($gallery)
             ->setFormat('json')
             ->setStatusCode(200);
 
--- src/Caldera/Bundle/CalderaBundle/Controller/Api/RideController.php
+++ src/Caldera/Bundle/CalderaBundle/Controller/Api/RideController.php
@@ -7,12 +7,19 @@ use Caldera\Bundle\CalderaBundle\Traits\UtilTrait;
 use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class RideController extends BaseController
 {
     use RepositoryTrait;
     use UtilTrait;
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method"
+     * )
+     */
     public function showAction(string $citySlug, string $rideDate): Response
     {
         $ride = $this->getCheckedCitySlugRideDateRide($citySlug, $rideDate);
@@ -26,6 +33,12 @@ class RideController extends BaseController
         return $this->handleView($view);
     }
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method"
+     * )
+     */
     public function showCurrentAction(string $citySlug): Response
     {
         $city = $this->getCheckedCity($citySlug);
@@ -41,6 +54,12 @@ class RideController extends BaseController
         return $this->handleView($view);
     }
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method"
+     * )
+     */
     public function listAction(): Response
     {
         $rideList = $this->getRideRepository()->findCurrentRides();
