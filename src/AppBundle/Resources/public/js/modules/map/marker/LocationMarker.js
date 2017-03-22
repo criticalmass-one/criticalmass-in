ae42aec39ed0d08279c39efbc0c242af9d2e5b89
Malte Hübner <maltehuebner@gmx.org>
2017-03-22 21:45:42 +0100

    Fix LocationMarker.

--- src/AppBundle/Resources/public/js/modules/map/marker/LocationMarker.js
+++ src/AppBundle/Resources/public/js/modules/map/marker/LocationMarker.js
@@ -8,15 +8,11 @@ define(['Marker'], function () {
     LocationMarker.prototype.constructor = LocationMarker;
 
     LocationMarker.prototype._initIcon = function () {
-        this._icon = L.icon({
-            iconUrl: this._baseIconUrl + 'marker-yellow.png',
-            iconRetinaUrl: this._baseIconUrl + 'marker-yellow-2x.png',
-            iconSize: [25, 41],
-            iconAnchor: [13, 41],
-            popupAnchor: [0, -36],
-            shadowUrl: this._baseIconUrl + 'defaultshadow.png',
-            shadowSize: [41, 41],
-            shadowAnchor: [13, 41]
+        this._icon = L.ExtraMarkers.icon({
+            icon: 'fa-bicycle',
+            markerColor: 'yellow',
+            shape: 'circle',
+            prefix: 'fa'
         });
     };
 
