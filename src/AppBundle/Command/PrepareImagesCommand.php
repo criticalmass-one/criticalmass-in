5090c5626676e3d3dfaaf7e2e4371faa2e8d6d7e
Malte Hübner <maltehuebner@gmx.org>
2017-05-28 00:56:49 +0200

    WIP.

--- src/AppBundle/Command/PrepareImagesCommand.php
+++ src/AppBundle/Command/PrepareImagesCommand.php
@@ -4,7 +4,6 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Photo;
 use AppBundle\Entity\Ride;
-use AppBundle\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Liip\ImagineBundle\Controller\ImagineController;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -13,11 +12,18 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class PrepareImagesCommand extends ContainerAwareCommand
 {
     /** @var Registry $doctrine */
-    protected $doctrine;
+    protected $doctrine = null;
+
+    /** @var UploaderHelper $uploaderHelper*/
+    protected $uploaderHelper = null;
+
+    /** @var ImagineController $imagineController */
+    protected $imagineController = null;
 
     protected function configure()
     {
@@ -40,6 +46,8 @@ class PrepareImagesCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->doctrine = $this->getContainer()->get('doctrine');
+        $this->uploaderHelper = $this->getContainer()->get('vich_uploader.templating.helper.uploader_helper');
+        $this->imagineController = $this->getContainer()->get('liip_imagine.controller');
 
         /** @var Ride $ride */
         $ride = $this->doctrine->getRepository('AppBundle:Ride')->findByCitySlugAndRideDate($input->getArgument('citySlug'), $input->getArgument('rideDate'));
@@ -55,31 +63,17 @@ class PrepareImagesCommand extends ContainerAwareCommand
                 $photo->getId()
             ));
         }
-
-
     }
 
     protected function filter(Photo $photo)
     {
-        $uploaderHelper = $this->getContainer()->get('vich_uploader.templating.helper.uploader_helper');
+        $filename = $this->uploaderHelper->asset($photo, 'imageFile');
 
-        $filename = $uploaderHelper->asset($photo, 'imageFile');
-
-        $request = new Request();
-
-        /** @var ImagineController */
-        $imagine = $this
-            ->getContainer()
-            ->get('liip_imagine.controller');
-
-        /** @var RedirectResponse */
-        $imagemanagerResponse = $imagine
+        $this->imagineController
             ->filterAction(
-                $request,
+                new Request(),
                 $filename,
                 'gallery_photo_thumb'
             );
-
-        var_dump($imagemanagerResponse);
     }
 }
