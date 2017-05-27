27a111c652e5caee203dab7a5beb87b30c225cdc
Malte Hübner <maltehuebner@gmx.org>
2017-05-28 01:04:06 +0200

    Add ProgressBar.

--- src/AppBundle/Command/PrepareImagesCommand.php
+++ src/AppBundle/Command/PrepareImagesCommand.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Ride;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Liip\ImagineBundle\Controller\ImagineController;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -52,20 +53,30 @@ class PrepareImagesCommand extends ContainerAwareCommand
         /** @var Ride $ride */
         $ride = $this->doctrine->getRepository('AppBundle:Ride')->findByCitySlugAndRideDate($input->getArgument('citySlug'), $input->getArgument('rideDate'));
 
-        $photos = $this->doctrine->getRepository('AppBundle:Photo')->findPhotosByRide($ride);
+        $photoList = $this->doctrine->getRepository('AppBundle:Photo')->findPhotosByRide($ride);
+
+        $filterList = $this->getFilterList();
+
+        $progress = new ProgressBar($output, count($filterList) * count($photoList));
 
         /** @var Photo $photo */
-        foreach ($photos as $photo) {
-            foreach ($this->getFilterList() as $filter) {
+        foreach ($photoList as $photo) {
+            foreach ($filterList as $filter) {
                 $this->applyFilter($photo, $filter);
 
-                $output->writeln(sprintf(
-                    'Applied filter <comment>%s</comment> to photo <info>#%d</info>',
-                    $filter,
+                if ($output->isDebug()) {
+                    $output->writeln(sprintf(
+                        'Applied filter <comment>%s</comment> to photo <info>#%d</info>',
+                        $filter,
                         $photo->getId()
-                ));
+                    ));
+                }
+
+                $progress->advance();
             }
         }
+
+        $progress->finish();
     }
 
     protected function applyFilter(Photo $photo, string $filter): void
