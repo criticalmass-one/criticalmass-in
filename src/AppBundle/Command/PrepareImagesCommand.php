56031b1183de4592274921e7ecb023c074439310
Malte Hübner <maltehuebner@gmx.org>
2017-05-28 00:49:38 +0200

    WIP.

--- src/AppBundle/Command/PrepareImagesCommand.php
+++ src/AppBundle/Command/PrepareImagesCommand.php
@@ -4,27 +4,20 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Photo;
 use AppBundle\Entity\Ride;
-use AppBundle\Entity\Track;
 use AppBundle\Entity\User;
-use AppBundle\Image\PhotoGps\PhotoGps;
 use Doctrine\Bundle\DoctrineBundle\Registry;
-use Doctrine\ORM\EntityManager;
+use Liip\ImagineBundle\Controller\ImagineController;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class PrepareImagesCommand extends ContainerAwareCommand
 {
     /** @var Registry $doctrine */
     protected $doctrine;
-
-    /** @var EntityManager $manager */
-    protected $manager;
-
-    /** @var \DateTimeZone $dateTimeZone */
-    protected $dateTimeZone = null;
 
     protected function configure()
     {
@@ -41,54 +34,52 @@ class PrepareImagesCommand extends ContainerAwareCommand
                 InputArgument::REQUIRED,
                 'Date of the ride'
             )
-            ->addArgument(
-                'username',
-                InputArgument::REQUIRED,
-                'Username'
-            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->doctrine = $this->getContainer()->get('doctrine');
-        $this->photoGps = $this->getContainer()->get('caldera.criticalmass.image.photogps');
-        $this->manager = $this->doctrine->getManager();
-
-        if ($input->hasArgument('photoDateTimeZone') && $input->getArgument('photoDateTimeZone')) {
-            $this->dateTimeZone = new \DateTimeZone($input->getArgument('photoDateTimeZone'));
-
-            $this->photoGps->setDateTimeZone($this->dateTimeZone);
-        }
 
         /** @var Ride $ride */
         $ride = $this->doctrine->getRepository('AppBundle:Ride')->findByCitySlugAndRideDate($input->getArgument('citySlug'), $input->getArgument('rideDate'));
-
-        /** @var User $user */
-        $user = $this->doctrine->getRepository('AppBundle:User')->findOneByUsername($input->getArgument('username'));
-
-        /** @var Track $track */
-        $track = $this->doctrine->getRepository('AppBundle:Track')->findByUserAndRide($ride, $user);
 
         $photos = $this->doctrine->getRepository('AppBundle:Photo')->findPhotosByRide($ride);
 
         /** @var Photo $photo */
         foreach ($photos as $photo) {
-            $this->photoGps->setPhoto($photo);
-            $this->photoGps->setTrack($track);
-
-            $this->photoGps->execute();
+            $this->filter($photo);
 
             $output->writeln(sprintf(
-                'Updated location of photo <comment>#%d</comment> to <info>%f,%f</info>',
-                $photo->getId(),
-                $photo->getLatitude(),
-                $photo->getLongitude()
+                'Filtered photo <comment>#%d</comment>',
+                $photo->getId()
             ));
-
-            $this->manager->merge($photo);
         }
 
-        $this->manager->flush();
+
+    }
+
+    protected function filter(Photo $photo)
+    {
+        $uploaderHelper = $this->getContainer()->get('vich_uploader.templating.helper.uploader_helper');
+
+        $filename = $uploaderHelper->asset($photo, 'imageFile');
+
+        $request = new Request();
+
+        /** @var ImagineController */
+        $imagine = $this
+            ->getContainer()
+            ->get('liip_imagine.controller');
+
+        /** @var RedirectResponse */
+        $imagemanagerResponse = $imagine
+            ->filterAction(
+                $request,
+                $filename,
+                'gallery_photo_thumb'
+            );
+
+        var_dump($imagemanagerResponse);
     }
 }
