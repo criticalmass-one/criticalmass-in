60ce6b6ce51f445deb8b45823153a873dcf44560
Malte Hübner <maltehuebner@gmx.org>
2017-05-29 22:22:47 +0200

    WIP.

--- src/AppBundle/Resources/public/js/modules/page/PhotoCensorPage.js
+++ src/AppBundle/Resources/public/js/modules/page/PhotoCensorPage.js
@@ -1,10 +1,24 @@
 define(['CriticalService', 'jquery-areaselect'], function (CriticalService) {
     PhotoCensorPage = function () {
-        alert('foo');
+        this._initPhoto();
+
         this._CriticalService = CriticalService;
     };
 
     PhotoCensorPage.prototype._CriticalService = null;
+
+    PhotoCensorPage.prototype._initPhoto = function () {
+        $('#photo').selectAreas({
+            minSize: [30, 30],    // Minimum size of a selection
+            maxSize: [400, 300],  // Maximum size of a selection
+            onChanged: $.noop,    // fired when a selection is released
+            onChanging: $.noop    // fired during the modification of a selection
+        });
+
+        $('#save').click(function() {
+            console.log($('#photo').selectAreas('areas'));
+        });
+    };
 
     return PhotoCensorPage;
 });
