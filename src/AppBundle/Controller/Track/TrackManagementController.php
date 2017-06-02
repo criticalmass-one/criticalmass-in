9d25f608ec7c74f8433728badea916af3a6ba2d7
Malte Hübner <maltehuebner@gmx.org>
2017-06-02 18:59:14 +0200

    Optimize track download.

--- src/AppBundle/Controller/Track/TrackManagementController.php
+++ src/AppBundle/Controller/Track/TrackManagementController.php
@@ -16,6 +16,7 @@ use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class TrackManagementController extends AbstractController
@@ -44,30 +45,22 @@ class TrackManagementController extends AbstractController
         );
     }
 
-    public function downloadAction(Request $request, $trackId)
+    public function downloadAction(Request $request, UserInterface $user, int $trackId): Response
     {
-        $track = $this->getTrackRepository()->find($trackId);
+        $track = $this->getCredentialsCheckedTrack($user, $trackId);
 
-        if ($track && $track->getUser()->equals($this->getUser())) {
-            $path = $this->getParameter('kernel.root_dir');
-            $helper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
-            $filename = $helper->asset($track, 'trackFile');
+        $trackContent = file_get_contents($this->getTrackFilename($track));
 
-            $trackContent = file_get_contents($path . '/../web' . $filename);
+        $response = new Response();
 
-            $response = new Response();
+        $response->headers->add([
+            'Content-disposition' => 'attachment; filename=track.gpx',
+            'Content-type', 'text/plain'
+        ]);
 
-            $response->headers->add([
-                'Content-disposition' => 'attachment; filename=track.gpx',
-                'Content-type', 'text/plain'
-            ]);
+        $response->setContent($trackContent);
 
-            $response->setContent($trackContent);
-
-            return $response;
-        }
-
-        return $this->redirect($this->generateUrl('caldera_criticalmass_track_track_list'));
+        return $response;
     }
 
     /**
@@ -318,5 +311,32 @@ class TrackManagementController extends AbstractController
         $em->flush();
 
         return $this->redirectToRoute('caldera_criticalmass_track_list');
+    }
+
+    protected function getCredentialsCheckedTrack(UserInterface $user, int $trackId): Track
+    {
+        /**
+         * @var Track $track
+         */
+        $track = $this->getTrackRepository()->find($trackId);
+
+        if (!$track) {
+            throw $this->createNotFoundException();
+        }
+
+        if ($track->getUser() !== $user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        return $track;
+    }
+
+    protected function getTrackFilename(Track $track): string
+    {
+        $rootDirectory = $this->getParameter('kernel.root_dir');
+        $helper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
+        $filename = $helper->asset($track, 'trackFile');
+
+        return $rootDirectory . '/../web' . $filename;
     }
 }
