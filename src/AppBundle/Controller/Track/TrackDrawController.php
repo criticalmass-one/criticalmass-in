ffc34ddd81f03a209f31a78cea1985495fefeee4
Malte Hübner <maltehuebner@gmx.org>
2017-06-02 18:35:20 +0200

    Splitting up controllers.

---
+++ src/AppBundle/Controller/Track/TrackDrawController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AppBundle\Controller\Track;
+
+use AppBundle\Controller\AbstractController;
+use AppBundle\Entity\Ride;
+use AppBundle\Entity\Track;
+use AppBundle\Traits\TrackHandlingTrait;
+use AppBundle\Gps\TrackTimeShift\TrackTimeShift;
+use AppBundle\UploadValidator\TrackValidator;
+use AppBundle\UploadValidator\UploadValidatorException\TrackValidatorException\TrackValidatorException;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+class TrackDrawController extends AbstractController
+{
+    use TrackHandlingTrait;
+
+    public function drawAction(Request $request, $citySlug, $rideDate)
+    {
+        $ride = $this->getCheckedCitySlugRideDateRide($citySlug, $rideDate);
+
+        if ('POST' == $request->getMethod()) {
+            return $this->drawPostAction($request, $ride);
+        } else {
+            return $this->drawGetAction($request, $ride);
+        }
+    }
+
+    protected function drawGetAction(Request $request, Ride $ride)
+    {
+        return $this->render(
+            'AppBundle:Track:draw.html.twig',
+            [
+                'ride' => $ride
+            ]
+        );
+    }
+
+    protected function drawPostAction(Request $request, Ride $ride)
+    {
+        $polyline = $request->request->get('polyline');
+        $geojson = $request->request->get('geojson');
+
+        $track = new Track();
+
+        $track->setCreationDateTime(new \DateTime());
+        $track->setPolyline($polyline);
+        $track->setGeoJson($geojson);
+        $track->setRide($ride);
+        $track->setSource(Track::TRACK_SOURCE_DRAW);
+        $track->setUser($this->getUser());
+        $track->setUsername($this->getUser()->getUsername());
+        $track->setTrackFilename('foo');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($track);
+        $em->flush();
+
+        return $this->redirectToRoute('caldera_criticalmass_track_list');
+    }
+}
