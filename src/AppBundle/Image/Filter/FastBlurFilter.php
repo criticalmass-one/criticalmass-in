7556621a3b3a5551c4273de7d6fe461c896eb62a
Malte Hübner <maltehuebner@gmx.org>
2017-06-04 15:54:24 +0200

    Introduce new FastBlurFilter.

---
+++ src/AppBundle/Image/Filter/FastBlurFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Image\Filter;
+
+use Imagine\Image\ImageInterface;
+use Liip\ImagineBundle\Imagine\Filter\Loader\LoaderInterface;
+
+class FastBlurFilter implements LoaderInterface
+{
+    public function load(ImageInterface $image, array $options = array()): ImageInterface
+    {
+        $dimension = $image->getSize();
+        $pixelateDimension = $dimension->scale(0.15);
+
+        $image
+            ->resize($pixelateDimension, ImageInterface::FILTER_CUBIC)
+            ->resize($dimension, ImageInterface::FILTER_CUBIC)
+        ;
+
+        return $image;
+    }
+}
