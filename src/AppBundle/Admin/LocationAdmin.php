4c21e88372f753e69070663afb5af99012e9a848
Malte Hübner <maltehuebner@gmx.org>
2017-06-08 16:55:29 +0200

    Enhanced LocationAdmin.

--- src/AppBundle/Admin/LocationAdmin.php
+++ src/AppBundle/Admin/LocationAdmin.php
@@ -11,16 +11,39 @@ class LocationAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('name', 'text');
+        $formMapper
+            ->with('Location', ['class' => 'col-md-6'])
+            ->add('title')
+            ->add('description')
+            ->end()
+
+            ->with('Settings', ['class' => 'col-md-6'])
+            ->add('city')
+            ->add('slug')
+            ->end()
+
+            ->with('Coords', ['class' => 'col-md-6'])
+            ->add('latitude')
+            ->add('longitude')
+            ->end()
+        ;
+
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('name');
+        $datagridMapper
+            ->add('title')
+            ->add('city')
+            ->add('description')
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('name');
+        $listMapper
+            ->addIdentifier('title')
+            ->add('city')
+        ;
     }
 }
