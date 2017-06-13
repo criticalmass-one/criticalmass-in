ba30bd610978838e2edc90aa726c1ac2de7cf530
Malte Hübner <maltehuebner@gmx.org>
2017-06-13 12:17:59 +0200

    WIP for more usermanagement.

--- src/UserBundle/Controller/ProfileManagementController.php
+++ src/UserBundle/Controller/ProfileManagementController.php
@@ -3,12 +3,35 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use UserBundle\Form\Type\UsernameEmailType;
 
 class ProfileManagementController extends Controller
 {
     public function manageAction(): Response
     {
         return $this->render('UserBundle:ProfileManagement:manage.html.twig');
+    }
+
+    public function editAction(Request $request, UserInterface $user): Response
+    {
+        $userForm = $this->createForm(
+            UsernameEmailType::class,
+            $user,
+            [
+                'action' => $this->generateUrl(
+                    'criticalmass_user_usermanagement_edit'
+                )
+            ]
+        );
+
+        return $this->render(
+            'UserBundle:ProfileManagement:edit.html.twig',
+            [
+                'userForm' => $userForm->createView()
+            ]
+        );
     }
 }
--- src/UserBundle/Form/Type/UsernameEmailType.php
+++ src/UserBundle/Form/Type/UsernameEmailType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class UsernameEmailType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username', TextType::class)
@@ -17,9 +17,8 @@ class UsernameEmailType extends AbstractType
         ;
     }
 
-    public function getName()
+    public function getName(): string
     {
         return 'user_username';
     }
-
 }
