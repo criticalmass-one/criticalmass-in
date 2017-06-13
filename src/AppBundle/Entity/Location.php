028e83c4da160e8b0e7051a6e17a39fa508d459b
Malte Hübner <maltehuebner@gmx.org>
2017-06-13 21:35:36 +0200

    Activate audit for Location entity.

--- src/AppBundle/Entity/Location.php
+++ src/AppBundle/Entity/Location.php
@@ -5,7 +5,6 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
-
 /**
  * @ORM\Table(name="location")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\LocationRepository")
