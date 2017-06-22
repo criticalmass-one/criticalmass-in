ef7f3025b6d8d3d83810e612def810684af19c8c
Malte Hübner <maltehuebner@gmx.org>
2017-06-22 16:56:11 +0200

    Fix css.

--- src/AppBundle/Resources/public/js/modules/FahrradstadtHamburg.js
+++ src/AppBundle/Resources/public/js/modules/FahrradstadtHamburg.js
@@ -21,9 +21,8 @@ define(['jquery'], function ($) {
                 var url = $item.find('link').text();
                 var title = $item.find('title').text();
                 var imageUrl = $item.find('enclosure').attr('url');
-                imageUrl = 'https://fahrradstadt.hamburg/media/cache/standard/photos/594b722e4a510.JPG';
 
-                $panel = $('<div class="panel panel-default"><div class="panel-heading">Fahrradstadt.Hamburg</div><div class="panel-body"><div class="row"><div class="col-md-12"><a href="' + url + '"><img class="img-responsive" src="' + imageUrl + '" /></a></div></div><div class="row"><div class="col-md-12 text-center"><a href="' + url + '"><h3 class="h4">' + title +'</h3></a></div></div><div class="row"><div class="col-md-12 text-center"><a href="' + url + '">zur Fahrradstadt</a></div></div></div></div>');
+                $panel = $('<div class="panel panel-fahrradstadt"><div class="panel-heading">Fahrradstadt.Hamburg</div><div class="panel-body"><div class="row"><div class="col-md-12"><a href="' + url + '"><img class="img-responsive" src="' + imageUrl + '" /></a></div></div><div class="row"><div class="col-md-12 text-center"><a href="' + url + '"><h3 class="h4">' + title +'</h3></a></div></div><div class="row"><div class="col-md-12 text-center"><a class="btn btn-fahrradstadt" href="' + url + '">zur Fahrradstadt</a></div></div></div></div>');
 
                 this._$container.append($panel);
 
