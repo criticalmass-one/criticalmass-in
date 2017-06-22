a7e1fe75ed5ec4be17fe2fd911851078e3a425cd
Malte Hübner <maltehuebner@gmx.org>
2017-06-22 15:08:52 +0200

    Add BikerightVoucherAdmin.

---
+++ src/AppBundle/Admin/BikerightVoucherAdmin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class BikerightVoucherAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Voucher', ['class' => 'col-md-6'])
+            ->add('code')
+            ->add('user')
+            ->end()
+
+            ->with('Date', ['class' => 'col-md-6'])
+            ->add('createdAt')
+            ->add('assignedAt')
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('code')
+            ->add('user')
+            ->add('createdAt')
+            ->add('assignedAt')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('code')
+            ->add('user')
+            ->add('assignedAt')
+        ;
+    }
+}
