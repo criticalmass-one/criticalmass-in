f79deb35271135440a1676187073907deb69ef12
Malte Hübner <maltehuebner@gmx.org>
2017-06-28 15:21:39 +0200

    Compose own Markdown flavor.

--- src/AppBundle/Markdown/CriticalMarkdown.php
+++ src/AppBundle/Markdown/CriticalMarkdown.php
@@ -9,26 +9,38 @@ use cebe\markdown\inline as inline;
 
 class CriticalMarkdown extends Parser
 {
-// include block element parsing using traits
-    use block\CodeTrait;
-    use block\HeadlineTrait;
     use block\HtmlTrait {
         parseInlineHtml as private;
     }
+
     use block\ListTrait {
-        // Check Ul List before headline
         identifyUl as protected identifyBUl;
         consumeUl as protected consumeBUl;
     }
+
     use block\QuoteTrait;
+
     use block\RuleTrait {
-        // Check Hr before checking lists
         identifyHr as protected identifyAHr;
         consumeHr as protected consumeAHr;
     }
 
-    // include inline element parsing using traits
     use inline\CodeTrait;
     use inline\EmphStrongTrait;
     use inline\LinkTrait;
+
+    use inline\StrikeoutTrait;
+    use inline\UrlLinkTrait;
+
+    protected $html5 = true;
+
+    /**
+     * @param string $text
+     */
+    protected function renderText($text): string
+    {
+         $br = $this->html5 ? "<br>\n" : "<br />\n";
+
+        return strtr($text[1], ["  \n" => $br, "\n" => $br]);
+    }
 }
