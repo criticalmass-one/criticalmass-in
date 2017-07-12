e0a25b22228d3747384c2d907bc0ee470457fcbd
Malte Hübner <maltehuebner@gmx.org>
2017-07-12 23:58:50 +0200

    Remove unused entities from ObjectRouter.

--- src/AppBundle/Router/ObjectRouter.php
+++ src/AppBundle/Router/ObjectRouter.php
@@ -32,20 +32,12 @@ class ObjectRouter
             return $this->generateRideUrl($object, $referenceType);
         }
 
-        if ($object instanceof Event) {
-            return $this->generateEventUrl($object, $referenceType);
-        }
-
         if ($object instanceof City) {
             return $this->generateCityUrl($object, $referenceType);
         }
 
         if ($object instanceof Photo) {
             return $this->generatePhotoUrl($object, $referenceType);
-        }
-
-        if ($object instanceof Content) {
-            return $this->generateContentUrl($object, $referenceType);
         }
 
         if ($object instanceof Board) {
@@ -68,10 +60,6 @@ class ObjectRouter
             return $this->generateRegionUrl($object, $referenceType);
         }
 
-        if ($object instanceof BlogPost) {
-            return $this->generateBlogPostUrl($object, $referenceType);
-        }
-
         return $this->router->generate($object, [], $referenceType);
     }
 
@@ -82,18 +70,6 @@ class ObjectRouter
         $parameters = [
             'citySlug' => $ride->getCity()->getMainSlugString(),
             'rideDate' => $ride->getFormattedDate()
-        ];
-
-        return $this->router->generate($route, $parameters, $referenceType);
-    }
-
-    protected function generateEventUrl(Event $event, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
-    {
-        $route = 'caldera_criticalmass_event_show';
-
-        $parameters = [
-            'citySlug' => $event->getCity()->getMainSlugString(),
-            'eventSlug' => $event->getSlug()
         ];
 
         return $this->router->generate($route, $parameters, $referenceType);
@@ -112,33 +88,14 @@ class ObjectRouter
 
     protected function generatePhotoUrl(Photo $photo, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
     {
-        $route = '';
+        $route = 'caldera_criticalmass_photo_show_ride';
 
         $parameters = [
             'citySlug' => $photo->getCity()->getMainSlugString(),
             'photoId' => $photo->getId()
         ];
-
-        if ($photo->getRide()) {
-            $route = 'caldera_criticalmass_photo_show_ride';
-
-            $parameters['rideDate'] = $photo->getRide()->getFormattedDate();
-        } else {
-            $route = 'caldera_criticalmass_photo_show_event';
-
-            $parameters['eventSlug'] = $photo->getEvent()->getSlug();
-        }
-
-        return $this->router->generate($route, $parameters, $referenceType);
-    }
-
-    protected function generateContentUrl(Content $content, $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
-    {
-        $route = 'caldera_criticalmass_content_display';
-
-        $parameters = [
-            'slug' => $content->getSlug()
-        ];
+        
+        $parameters['rideDate'] = $photo->getRide()->getFormattedDate();
 
         return $this->router->generate($route, $parameters, $referenceType);
     }
@@ -172,17 +129,6 @@ class ObjectRouter
 
         $parameters = [
             'trackId' => $track->getId()
-        ];
-
-        return $this->router->generate($route, $parameters, $referenceType);
-    }
-
-    private function generateBlogPostUrl(BlogPost $blogPost, $referenceType)
-    {
-        $route = 'caldera_criticalmass_blog_post';
-
-        $parameters = [
-            'slug' => $blogPost->getSlug()
         ];
 
         return $this->router->generate($route, $parameters, $referenceType);
