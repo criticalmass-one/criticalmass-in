177a3f9d44af5587537bac206210dabf5614dfd8
Malte Hübner <maltehuebner@gmx.org>
2017-09-30 22:13:06 +0200

    Or show city map.

--- src/AppBundle/Twig/Extension/StaticmapsTwigExtension.php
+++ src/AppBundle/Twig/Extension/StaticmapsTwigExtension.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Twig\Extension;
 
+use AppBundle\Entity\City;
 use AppBundle\Entity\Ride;
 
 class StaticmapsTwigExtension extends \Twig_Extension
 {
+    /** @var string $staticmapsHost */
     protected $staticmapsHost = '';
 
     public function __construct(string $staticmapsHost)
@@ -24,6 +26,8 @@ class StaticmapsTwigExtension extends \Twig_Extension
     {
         if ($object instanceof Ride) {
             return $this->staticmapsRide($object);
+        } elseif ($object instanceof City) {
+            return $this->staticmapsCity($object);
         }
 
         return '';
@@ -38,12 +42,30 @@ class StaticmapsTwigExtension extends \Twig_Extension
     {
         $parameters = [
             sprintf('center=%f,%f', $ride->getLatitude(), $ride->getLongitude()),
-            sprintf('markers=%f,%f', $ride->getLatitude(), $ride->getLongitude()),
+            sprintf('markers=%f,%f,%s,%s', $ride->getLatitude(), $ride->getLongitude(), 'red', 'bicycle'),
             'zoom=14',
             'size=865x512',
             'maptype=wikimedia-intl',
         ];
 
+        return $this->generateMapUrl($parameters);
+    }
+
+    public function staticmapsCity(City $city): string
+    {
+        $parameters = [
+            sprintf('center=%f,%f', $city->getLatitude(), $city->getLongitude()),
+            sprintf('markers=%f,%f,%s,%s', $city->getLatitude(), $city->getLongitude(), 'blue', 'university'),
+            'zoom=14',
+            'size=865x512',
+            'maptype=wikimedia-intl',
+        ];
+
+        return $this->generateMapUrl($parameters);
+    }
+
+    protected function generateMapUrl(array $parameters = []): string
+    {
         return sprintf('%sstaticmap.php?%s', $this->staticmapsHost, implode('&',$parameters));
     }
 }
