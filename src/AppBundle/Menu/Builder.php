4a7ccab73563c7fc5fa4d0ba7fbb6aee0b0095b5
Malte Hübner <maltehuebner@gmx.org>
2017-10-02 13:11:02 +0200

    WIP.

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -36,8 +36,52 @@ class Builder implements ContainerAwareInterface
         $menu['Community']->addChild('Diskussion');
         $menu['Community']->addChild('Fotos');
 
+        $menu->addChild('Anmelden', ['route' => 'fos_user_security_login', 'attributes' => ['foo' => 'bar']]);
+
         $menu->addChild('Benutzerkonto', ['route' => 'caldera_criticalmass_frontpage']);
+        $menu['Benutzerkonto']->addChild('Dein Profil');
+        $menu['Benutzerkonto']->addChild('Abmelden', ['route' => 'fos_user_security_logout']);
 
         return $menu;
     }
 }
+
+/**
+ * {% if app.getUser() %}
+
+<li class="dropdown">
+<a href="{{ path('fos_user_security_login') }}" class="dropdown-toggle"
+data-toggle="dropdown">
+<i class="fa fa-user"></i>
+{% trans %}criticalmass.navigation.account.account{% endtrans %}
+<span class="caret"></span>
+</a>
+
+<ul class="dropdown-menu" role="menu">
+<li>
+<a href="{{ path('caldera_criticalmass_track_list') }}">
+{% trans %}criticalmass.navigation.account.tracks{% endtrans %}
+</a>
+</li>
+<li>
+<a href="{{ path('caldera_criticalmass_photo_user_list') }}">
+{% trans %}criticalmass.navigation.account.photos{% endtrans %}
+</a>
+</li>
+<li class="divider"></li>
+<li>
+<a href="{{ path('fos_user_security_logout') }}">
+{% trans %}criticalmass.navigation.account.logout{% endtrans %}
+</a>
+</li>
+</ul>
+</li>
+{% else %}
+<li>
+<a href="#" data-toggle="modal" data-target="#loginModal">
+<i class="fa fa-user"></i>
+{% trans %}criticalmass.navigation.account.login{% endtrans %}
+</a>
+</li>
+{% endif %}
+ */
