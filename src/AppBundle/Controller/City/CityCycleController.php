f5b5019ecf03bef90547709e9de09604f7cd52be
Malte Hübner <maltehuebner@gmx.org>
2017-10-05 17:45:04 +0200

    Add first cycle action.

---
+++ src/AppBundle/Controller/City/CityCycleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller\City;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use AppBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CityCycleController extends AbstractController
+{
+    /**
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function listAction(Request $request, UserInterface $user, string $citySlug): Response
+    {
+        return $this->render('AppBundle:CityCycle:list.html.twig');
+    }
+}
