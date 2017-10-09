27957fdccf34939967c99df1a8412c55b09cc551
Malte Hübner <maltehuebner@gmx.org>
2017-10-09 16:20:54 +0200

    WIP.

--- src/AppBundle/Command/InitAuditCommand.php
+++ src/AppBundle/Command/InitAuditCommand.php
@@ -11,6 +11,7 @@ use AppBundle\EntityInterface\AuditableInterface;
 use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -26,25 +27,32 @@ class InitAuditCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $doctrine = $this->getContainer()->get('doctrine');
-        $repo = $doctrine->getRepository('AppBundle:City');
         $manager = $doctrine->getManager();
 
         $classnames = [City::class, Ride::class, Subride::class, Region::class, Location::class];
 
         foreach ($classnames as $classname) {
+
             $entities = $this->findEntities($doctrine, $classname);
+
+            $output->writeln(sprintf('Init <info>%d</info> entities of <comment>%s</comment> now', count($entities), $classname));
+            $progress = new ProgressBar($output, count($entities));
 
             foreach ($entities as $entity) {
                 $this->initEntity($manager, $entity);
-            }
-        }
 
-        $manager->flush();
+                $progress->advance();
+            }
+
+            $manager->flush();
+
+            $progress->finish();
+        }
     }
 
     protected function findEntities(Doctrine $doctrine, string $classname): array
     {
-        return $doctrine->getRepository($classname)->findBy(['isArchived' => false]);
+        return $doctrine->getRepository($classname)->findAll();
     }
 
     protected function initEntity(ObjectManager $manager, AuditableInterface $auditable): void
