b0ad4788e94d0309e2bc82046d2f8ce2e4e7397c
Malte Hübner <maltehuebner@gmx.org>
2017-10-09 22:58:07 +0200

    WIP.

--- src/AppBundle/Repository/RideEstimateRepository.php
+++ src/AppBundle/Repository/RideEstimateRepository.php
@@ -8,26 +8,26 @@ class RideEstimateRepository extends EntityRepository
 {
     public function findForTimelineRideParticipantsEstimateCollector(\DateTime $startDateTime = null, \DateTime $endDateTime = null, $limit = null)
     {
-        $builder = $this->createQueryBuilder('estimate');
+        $builder = $this->createQueryBuilder('e');
 
-        $builder->select('estimate');
-        $builder->where($builder->expr()->isNull('estimate.track'));
-        $builder->andWhere($builder->expr()->isNull('estimate.estimatedDistance'));
-        $builder->andWhere($builder->expr()->isNull('estimate.estimatedDuration'));
+        $builder->select('e');
+        $builder->where($builder->expr()->isNull('e.track'));
+        $builder->andWhere($builder->expr()->isNull('e.estimatedDistance'));
+        $builder->andWhere($builder->expr()->isNull('e.estimatedDuration'));
 
         if ($startDateTime) {
-            $builder->andWhere($builder->expr()->gte('estimate.dateTime', '\'' . $startDateTime->format('Y-m-d H:i:s') . '\''));
+            $builder->andWhere($builder->expr()->gte('e.dateTime', '\'' . $startDateTime->format('Y-m-d H:i:s') . '\''));
         }
 
         if ($endDateTime) {
-            $builder->andWhere($builder->expr()->lte('estimate.dateTime', '\'' . $endDateTime->format('Y-m-d H:i:s') . '\''));
+            $builder->andWhere($builder->expr()->lte('e.dateTime', '\'' . $endDateTime->format('Y-m-d H:i:s') . '\''));
         }
 
         if ($limit) {
             $builder->setMaxResults($limit);
         }
 
-        $builder->addOrderBy('estimate.dateTime', 'DESC');
+        $builder->addOrderBy('e.dateTime', 'DESC');
 
         $query = $builder->getQuery();
 
