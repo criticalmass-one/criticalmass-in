06a31fd9036ed7debe52bd02a4f3b35781dbeb45
Malte Hübner <maltehuebner@gmx.org>
2017-10-09 22:57:45 +0200

    Rename $dateTime in repository.

--- src/AppBundle/Repository/RideEstimateRepository.php
+++ src/AppBundle/Repository/RideEstimateRepository.php
@@ -16,18 +16,18 @@ class RideEstimateRepository extends EntityRepository
         $builder->andWhere($builder->expr()->isNull('estimate.estimatedDuration'));
 
         if ($startDateTime) {
-            $builder->andWhere($builder->expr()->gte('estimate.creationDateTime', '\'' . $startDateTime->format('Y-m-d H:i:s') . '\''));
+            $builder->andWhere($builder->expr()->gte('estimate.dateTime', '\'' . $startDateTime->format('Y-m-d H:i:s') . '\''));
         }
 
         if ($endDateTime) {
-            $builder->andWhere($builder->expr()->lte('estimate.creationDateTime', '\'' . $endDateTime->format('Y-m-d H:i:s') . '\''));
+            $builder->andWhere($builder->expr()->lte('estimate.dateTime', '\'' . $endDateTime->format('Y-m-d H:i:s') . '\''));
         }
 
         if ($limit) {
             $builder->setMaxResults($limit);
         }
 
-        $builder->addOrderBy('estimate.creationDateTime', 'DESC');
+        $builder->addOrderBy('estimate.dateTime', 'DESC');
 
         $query = $builder->getQuery();
 
