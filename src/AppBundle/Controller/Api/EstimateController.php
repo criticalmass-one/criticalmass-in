b754742c60473bf8d3821970e65aaaeedfa798f9
Malte Hübner <maltehuebner@gmx.org>
2017-10-09 23:36:45 +0200

    WIP for ride fetching.

--- src/AppBundle/Controller/Api/EstimateController.php
+++ src/AppBundle/Controller/Api/EstimateController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Entity\Ride;
 use AppBundle\Entity\RideEstimate;
 use AppBundle\Model\CreateEstimateModel;
 use AppBundle\Traits\RepositoryTrait;
 use AppBundle\Traits\UtilTrait;
+use FOS\ElasticaBundle\Finder\FinderInterface;
 use JMS\Serializer\Context;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,9 +40,66 @@ class EstimateController extends BaseController
 
         $estimate
             ->setEstimatedParticipants($model->getEstimation())
-            ->setCreationDateTime($model->getDateTime())
+            ->setLatitude($model->getLatitude())
+            ->setLongitude($model->getLongitude())
         ;
 
+        if ($model->getDateTime()) {
+            $estimate
+                ->setDateTime($model->getDateTime())
+            ;
+        }
+
+        $ride = $this->findNearestRide($model);
+
+        if ($ride) {
+            echo "WEFWEFEWFEWF";
+            echo $ride->getFancyTitle();
+        }
+
         return $estimate;
+    }
+
+    protected function findNearestRide(CreateEstimateModel $model): ?Ride
+    {
+        /** @var FinderInterface $finder */
+        $finder = $this->container->get('fos_elastica.finder.criticalmass.ride');
+
+        $geoFilter = new \Elastica\Filter\GeoDistance(
+            'pin',
+            [
+                'lat' => $model->getLatitude(),
+                'lon' => $model->getLongitude()
+            ],
+            '25km'
+        );
+
+        $filteredQuery = new \Elastica\Query\Filtered(new \Elastica\Query\MatchAll(), $geoFilter);
+
+        $query = new \Elastica\Query($filteredQuery);
+
+        $query->setSize(1);
+        $query->setSort(
+            [
+                '_geo_distance' =>
+                    [
+                        'pin' =>
+                            [
+                                $model->getLatitude(),
+                                $model->getLongitude()
+                            ],
+                        'order' => 'asc',
+                        'unit' => 'km'
+                    ]
+            ]
+        );
+
+        $results = $finder->find($query, 1);
+
+        if (is_array($results)) {
+            return array_pop($results);
+        }
+
+        return null;
     }
 }
