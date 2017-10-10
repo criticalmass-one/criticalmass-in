43e1edfca6ed1142062ff0fcd55a490d3f125232
Malte Hübner <maltehuebner@gmx.org>
2017-10-10 19:41:34 +0200

    Add description for adding estimates.

--- src/AppBundle/Controller/Api/EstimateController.php
+++ src/AppBundle/Controller/Api/EstimateController.php
@@ -20,6 +20,17 @@ class EstimateController extends BaseController
     use UtilTrait;
 
     /**
+     * You can add an estimation of ride participants like this:
+     *
+     * <pre>{
+     *   "latitude": 53.549280,
+     *   "longitude": 9.979589,
+     *   "estimation": 6554,
+     *   "dateTime": 1506710306
+     * }</pre>
+     *
+     * If you do not provide <code>dateTime</code> it will use the current time.
+     * 
      * @ApiDoc(
      *  resource=true,
      *  description="Adds an estimation to statistic"
