50ca31b35829355884edab9dd506eba567a4fa71
Malte Hübner <maltehuebner@gmx.org>
2017-10-31 12:06:37 +0100

    Fix admin.

--- src/AppBundle/Admin/HelpCategoryAdmin.php
+++ src/AppBundle/Admin/HelpCategoryAdmin.php
@@ -7,6 +7,7 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class HelpCategoryAdmin extends AbstractAdmin
@@ -14,11 +15,15 @@ class HelpCategoryAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('language', TextType::class)
+            ->with('Content', ['class' => 'col-md-6'])
             ->add('title', TextType::class)
-            ->add('intro', TextType::class)
+            ->add('intro', TextAreaType::class)
+            ->end()
+            ->with('Settings', ['class' => 'col-md-6'])
+            ->add('language', TextType::class)
             ->add('side', TextType::class)
             ->add('position', NumberType::class, ['required' => false])
+            ->end()
         ;
     }
 
