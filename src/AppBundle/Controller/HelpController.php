a6745f33c212d0c0247a240379e580efc1432ca8
Malte Hübner <maltehuebner@gmx.org>
2017-10-31 17:51:53 +0100

    Also show faqs.

--- src/AppBundle/Controller/HelpController.php
+++ src/AppBundle/Controller/HelpController.php
@@ -9,13 +9,23 @@ use Symfony\Component\HttpFoundation\Response;
 class HelpController extends AbstractController
 {
     const HELP_CATEGORY_ID = 7;
+    const FAQ_CATEGORY_ID = 8;
 
     public function helpAction(Request $request): Response
     {
         $mainCategory = $this->getHelpCategoryRepository()->find(self::HELP_CATEGORY_ID);
 
         return $this->render('AppBundle:Help:help.html.twig', [
-                'mainCategory' => $mainCategory,
-            ]);
+            'mainCategory' => $mainCategory,
+        ]);
+    }
+
+    public function faqAction(Request $request): Response
+    {
+        $mainCategory = $this->getHelpCategoryRepository()->find(self::FAQ_CATEGORY_ID);
+
+        return $this->render('AppBundle:Help:faq.html.twig', [
+            'mainCategory' => $mainCategory,
+        ]);
     }
 }
