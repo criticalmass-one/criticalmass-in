d5cf0713a7de8467d29fd4ba0c28cc24f94361c5
Malte Hübner <maltehuebner@gmx.org>
2017-10-31 17:33:50 +0100

    WIP for HelpCategoryAdmin.

--- src/AppBundle/Admin/HelpCategoryAdmin.php
+++ src/AppBundle/Admin/HelpCategoryAdmin.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Admin;
 
+use AppBundle\Entity\HelpCategory;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,8 +22,11 @@ class HelpCategoryAdmin extends AbstractAdmin
             ->add('intro', TextAreaType::class)
             ->end()
             ->with('Settings', ['class' => 'col-md-6'])
+            ->add('parent', EntityType::class, [
+                'class' => HelpCategory::class
+            ])
             ->add('language', TextType::class)
-            ->add('side', TextType::class)
+            ->add('side', TextType::class, ['required' => false])
             ->add('position', NumberType::class, ['required' => false])
             ->end()
         ;
