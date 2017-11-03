7303ed44ae7170b67fd15ccbb2965287d6bbe0e3
Malte Hübner <maltehuebner@gmx.org>
2017-11-03 17:06:51 +0100

    Remove unused entites.

--- src/Criticalmass/Bundle/AppBundle/Traits/ViewStorageTrait.php
+++ src/Criticalmass/Bundle/AppBundle/Traits/ViewStorageTrait.php
@@ -47,38 +47,11 @@ trait ViewStorageTrait
     }
 
     /**
-     * @param Event $event
-     * @deprecated
-     */
-    protected function countEventView(Event $event)
-    {
-        $this->countView($event);
-    }
-
-    /**
      * @param City $city
      * @deprecated
      */
     protected function countCityView(City $city)
     {
         $this->countView($city);
-    }
-
-    /**
-     * @param BlogPost $blogPost
-     * @deprecated
-     */
-    protected function countBlogPostView(BlogPost $blogPost)
-    {
-        $this->countView($blogPost);
-    }
-
-    /**
-     * @param Content $content
-     * @deprecated
-     */
-    protected function countContentView(Content $content)
-    {
-        $this->countView($content);
     }
 }
