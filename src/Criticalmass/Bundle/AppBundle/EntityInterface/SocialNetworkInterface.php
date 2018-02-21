5ee2ce9f6b5f8114fc6c326fca7319185e20ae76
Malte Hübner <maltehuebner@gmx.org>
2018-02-21 19:58:14 +0100

    Begin with an entity.

---
+++ src/Criticalmass/Bundle/AppBundle/EntityInterface/SocialNetworkInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Criticalmass\Bundle\AppBundle\EntityInterface;
+
+
+interface SocialNetworkInterface
+{
+    const FACEBOOK = 'facebook';
+    const TWITTER = 'twitter';
+    const HOMEPAGE = 'homepage';
+    const INSTAGRAM = 'instagram';
+}
