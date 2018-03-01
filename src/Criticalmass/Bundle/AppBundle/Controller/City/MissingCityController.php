f6f3d549a0158a8aa0d1337521df5b65f4a61508
Malte Hübner <maltehuebner@gmx.org>
2018-03-01 18:13:38 +0100

    Add kind of 404 page for non existing cities.

--- src/Criticalmass/Bundle/AppBundle/Controller/City/MissingCityController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/City/MissingCityController.php
@@ -3,13 +3,67 @@
 namespace Criticalmass\Bundle\AppBundle\Controller\City;
 
 use Criticalmass\Bundle\AppBundle\Controller\AbstractController;
+use Criticalmass\Bundle\AppBundle\Entity\City;
+use maxh\Nominatim\Nominatim;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class MissingCityController extends AbstractController
 {
-    public function missingAction(Request $request): Response
+    public function missingAction(Request $request, string $citySlug): Response
     {
-        return new Response('foo');
+        $url = 'https://nominatim.openstreetmap.org/';
+        $nominatim = new Nominatim($url);
+
+        $search = $nominatim->newSearch()
+            ->city($citySlug)
+            ->addressDetails();
+
+        $result = $nominatim->find($search);
+        $firstResult = array_shift($result);
+
+        if ($firstResult) {
+            $city = $this->lookupCity($citySlug);
+
+            return $this->render('AppBundle:CityManagement:missing.html.twig', [
+                'city' => $city,
+            ]);
+        }
+
+        throw $this->createNotFoundException();
+    }
+
+    protected function lookupCity(string $citySlug): ?City
+    {
+        $url = 'https://nominatim.openstreetmap.org/';
+        $nominatim = new Nominatim($url);
+
+        $search = $nominatim->newSearch()
+            ->city($citySlug)
+            ->addressDetails();
+
+        $result = $nominatim->find($search);
+        $firstResult = array_shift($result);
+
+        if ($firstResult) {
+            $city = $this->createCity($firstResult);
+
+            return $city;
+        }
+
+        return null;
+    }
+
+    protected function createCity(array $result): City
+    {
+        $city = new City();
+
+        $city
+            ->setLatitude($result['lat'])
+            ->setLongitude($result['lon'])
+            ->setCity($result['address']['city'])
+        ;
+
+        return $city;
     }
 }
