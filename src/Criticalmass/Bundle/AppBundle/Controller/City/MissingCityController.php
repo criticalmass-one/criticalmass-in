a7ff6becc12be5059e89964fe25367d0492baa22
Malte Hübner <maltehuebner@gmx.org>
2018-03-02 02:33:05 +0100

    Introduce NominatimCityBridge.

--- src/Criticalmass/Bundle/AppBundle/Controller/City/MissingCityController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/City/MissingCityController.php
@@ -4,6 +4,7 @@ namespace Criticalmass\Bundle\AppBundle\Controller\City;
 
 use Criticalmass\Bundle\AppBundle\Controller\AbstractController;
 use Criticalmass\Bundle\AppBundle\Entity\City;
+use Criticalmass\Component\OpenStreetMap\NominatimCityBridge\NominatimCityBridge;
 use maxh\Nominatim\Nominatim;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,60 +13,11 @@ class MissingCityController extends AbstractController
 {
     public function missingAction(Request $request, string $citySlug): Response
     {
-        $url = 'https://nominatim.openstreetmap.org/';
-        $nominatim = new Nominatim($url);
-
-        $search = $nominatim->newSearch()
-            ->city($citySlug)
-            ->addressDetails();
-
-        $result = $nominatim->find($search);
-        $firstResult = array_shift($result);
-
-        if ($firstResult) {
-            $city = $this->lookupCity($citySlug);
-
-            return $this->render('AppBundle:CityManagement:missing.html.twig', [
-                'city' => $city,
-            ]);
-        }
+        $city = $this->get(NominatimCityBridge::class)->lookupCity($citySlug);
 
         return $this->render('AppBundle:CityManagement:missing.html.twig', [
+            'city' => $city,
             'citySlug' => $citySlug,
         ]);
-    }
-
-    protected function lookupCity(string $citySlug): ?City
-    {
-        $url = 'https://nominatim.openstreetmap.org/';
-        $nominatim = new Nominatim($url);
-
-        $search = $nominatim->newSearch()
-            ->city($citySlug)
-            ->addressDetails();
-
-        $result = $nominatim->find($search);
-        $firstResult = array_shift($result);
-
-        if ($firstResult) {
-            $city = $this->createCity($firstResult);
-
-            return $city;
-        }
-
-        return null;
-    }
-
-    protected function createCity(array $result): City
-    {
-        $city = new City();
-
-        $city
-            ->setLatitude($result['lat'])
-            ->setLongitude($result['lon'])
-            ->setCity($result['address']['city'])
-        ;
-
-        return $city;
     }
 }
