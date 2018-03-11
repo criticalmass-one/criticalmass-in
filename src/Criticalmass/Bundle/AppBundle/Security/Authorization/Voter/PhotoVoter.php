f0a3ce06c2df52bd45b5c6110a94df6f28f2056c
Malte Hübner <maltehuebner@gmx.org>
2018-03-11 02:18:56 +0100

    Introduce PhotoVoter.

---
+++ src/Criticalmass/Bundle/AppBundle/Security/Authorization/Voter/PhotoVoter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Criticalmass\Bundle\AppBundle\Security\Authorization\Voter;
+
+use Criticalmass\Bundle\AppBundle\Entity\Photo;
+use Criticalmass\Bundle\AppBundle\Entity\User;
+
+class PhotoVoter extends AbstractVoter
+{
+    protected function canView(Photo $photo, User $user): bool
+    {
+        return false;
+    }
+
+    protected function canEdit(Photo $photo, User $user): bool
+    {
+        if ($user->hasRole('ROLE_ADMIN')) {
+            return true;
+        }
+
+        if ($user === $photo->getUser()) {
+            return true;
+        }
+
+        return false;
+    }
+}
