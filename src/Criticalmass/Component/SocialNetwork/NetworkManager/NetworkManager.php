121b1f2ea3781f2add8b6dd9e3e2ec4a105f60fd
Malte Hübner <maltehuebner@gmx.org>
2018-03-23 10:31:34 +0100

    More list styling.

--- src/Criticalmass/Component/SocialNetwork/NetworkManager/NetworkManager.php
+++ src/Criticalmass/Component/SocialNetwork/NetworkManager/NetworkManager.php
@@ -4,13 +4,13 @@ namespace Criticalmass\Component\SocialNetwork\NetworkManager;
 
 use Criticalmass\Component\SocialNetwork\Network\NetworkInterface;
 
-class NetworkManager
+class NetworkManager implements NetworkManagerInterface
 {
     protected $networkList = [];
 
     public function addNetwork(NetworkInterface $network): NetworkManager
     {
-        $this->networkList[get_class($network)] = $network;
+        $this->networkList[$network->getIdentifier()] = $network;
 
         return $this;
     }
@@ -18,5 +18,15 @@ class NetworkManager
     public function getNetworkList(): array
     {
         return $this->networkList;
+    }
+
+    public function hasNetwork(string $identifier): bool
+    {
+        return array_key_exists($identifier, $this->networkList);
+    }
+
+    public function getNetwork(string $identifier): NetworkInterface
+    {
+        return $this->networkList[$identifier];
     }
 }
