cef53ffdd303b0617ce6fe3652d33960afc278f1
Malte Hübner <maltehuebner@gmx.org>
2018-03-31 19:16:57 +0200

    Also test track mock.

--- src/Criticalmass/Component/Image/Tests/PhotoGpsTest/Mocks/MockTrack.php
+++ src/Criticalmass/Component/Image/Tests/PhotoGpsTest/Mocks/MockTrack.php
@@ -8,6 +8,6 @@ class MockTrack extends Track
 {
     public function getTrackFilename(): ?string
     {
-
+        return '../../src/Criticalmass/Component/Image/Tests/PhotoGpsTest/Files/braunschweig.gpx';
     }
 }
--- src/Criticalmass/Component/Image/Tests/PhotoGpsTest/PhotoGpsTest.php
+++ src/Criticalmass/Component/Image/Tests/PhotoGpsTest/PhotoGpsTest.php
@@ -5,6 +5,7 @@ namespace Criticalmass\Component\Util\Tests;
 use Criticalmass\Component\Image\PhotoGps\PhotoGps;
 use Criticalmass\Component\Image\PhotoGps\PhotoGpsInterface;
 use Criticalmass\Component\Image\Tests\PhotoGpsTest\Mocks\GpsPhoto;
+use Criticalmass\Component\Image\Tests\PhotoGpsTest\Mocks\MockTrack;
 use Criticalmass\Component\Image\Tests\PhotoGpsTest\Mocks\NoGpsPhoto;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -27,6 +28,8 @@ class PhotoGpsTest extends KernelTestCase
         $this->getPhotoGps()->setPhoto($photo)->execute();
 
         $this->assertFalse($photo->hasCoordinates());
+        $this->assertNull($photo->getLatitude());
+        $this->assertNull($photo->getLongitude());
     }
 
     public function testPhotoWithCoords(): void
@@ -36,17 +39,19 @@ class PhotoGpsTest extends KernelTestCase
         $this->getPhotoGps()->setPhoto($photo)->execute();
 
         $this->assertTrue($photo->hasCoordinates());
+        $this->assertEquals(52.266666666667, $photo->getLatitude());
+        $this->assertEquals(10.5, $photo->getLongitude());
     }
-
 
     public function testPhotoTrackCoords(): void
     {
         $photo = new NoGpsPhoto();
         $track = new MockTrack();
 
-
-        $this->getPhotoGps()->setPhoto($photo)->setTrack()->execute();
+        $this->getPhotoGps()->setPhoto($photo)->setTrack($track)->execute();
 
         $this->assertTrue($photo->hasCoordinates());
+        $this->assertEquals(52.26329, $photo->getLatitude());
+        $this->assertEquals(10.527089, $photo->getLongitude());
     }
 }
