2ad5856ef98692dc983e9674b41b346ae348c753
Malte Hübner <maltehuebner@gmx.org>
2018-04-07 01:08:14 +0200

    Rename caldera_criticalmass_desktop_city_edit to caldera_criticalmass_city_edit.

--- src/Criticalmass/Bundle/AppBundle/Controller/City/CityManagementController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/City/CityManagementController.php
@@ -84,7 +84,7 @@ class CityManagementController extends AbstractController
             $hasErrors = false;
 
             $form = $this->createForm(StandardCityType::class, $city, [
-                'action' => $this->generateUrl('caldera_criticalmass_desktop_city_edit', [
+                'action' => $this->generateUrl('caldera_criticalmass_city_edit', [
                     'citySlug' => $citySlug->getSlug(),
                 ])
             ]);
@@ -118,7 +118,7 @@ class CityManagementController extends AbstractController
     public function editAction(Request $request, UserInterface $user, City $city): Response
     {
         $form = $this->createForm(StandardCityType::class, $city, [
-            'action' => $this->generateUrl('caldera_criticalmass_desktop_city_edit', [
+            'action' => $this->generateUrl('caldera_criticalmass_city_edit', [
                 'citySlug' => $city->getMainSlugString()
             ])
         ]);
