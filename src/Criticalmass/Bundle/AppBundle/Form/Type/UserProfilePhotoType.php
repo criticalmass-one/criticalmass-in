d8b6c47869161267a310c50abeef31721336286e
Malte Hübner <maltehuebner@gmx.org>
2018-04-27 10:04:24 +0200

    Form styling.

--- src/Criticalmass/Bundle/AppBundle/Form/Type/UserProfilePhotoType.php
+++ src/Criticalmass/Bundle/AppBundle/Form/Type/UserProfilePhotoType.php
@@ -1,32 +1,26 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Criticalmass\Bundle\AppBundle\Form\Type;
 
-use Criticalmass\Bundle\AppBundle\Repository\RegionRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class UserProfilePhotoType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('imageFile',
                 VichFileType::class, [
-                    'required' => false
+                    'required' => false,
+                    'allow_delete' => false,
+                    'download_link' => false,
                 ]
             );
     }
 
-    public function getName()
+    public function getName(): string
     {
         return 'user_photo';
     }
