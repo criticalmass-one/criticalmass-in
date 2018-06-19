396adb31f6b28c260ac3c1cbfb90989c9b53c09f
Malte Hübner <maltehuebner@gmx.org>
2018-06-19 21:12:57 +0200

    Add pagination.

--- src/Criticalmass/Bundle/AppBundle/Controller/Statistic/MonthlyStatsController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/Statistic/MonthlyStatsController.php
@@ -10,20 +10,24 @@ class MonthlyStatsController extends AbstractController
     public function listRidesAction(int $year = null, int $month = null): Response
     {
         if (!$year || $month) {
-            $dateTime = new \DateTime();
+            $dateTime = new \DateTimeImmutable();
 
             $year = (int) $dateTime->format('Y');
             $month = (int) $dateTime->format('m');
         } else {
             $dateTimeSpec = sprintf('%d-%d-01', $year, $month);
-            $dateTime = new \DateTime($dateTimeSpec);
+            $dateTime = new \DateTimeImmutable($dateTimeSpec);
         }
 
         $rides = $this->getRideRepository()->findEstimatedRides($year, $month);
 
+        $month = new \DateInterval('P1M');
+
         return $this->render('AppBundle:Statistic:list_rides.html.twig', [
             'rides' => $rides,
             'dateTime' => $dateTime,
+            'previousDateTime' => $dateTime->sub($month),
+            'nextDateTime' => $dateTime->add($month),
         ]);
     }
 }
