4689449ddcd228cd17aff6ad5e4076cba00c238b
Malte Hübner <maltehuebner@gmx.org>
2018-06-20 19:54:25 +0200

    Refactor prefetched data.

--- src/Criticalmass/Bundle/AppBundle/Controller/Search/PrefetchController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/Search/PrefetchController.php
@@ -5,16 +5,40 @@ namespace Criticalmass\Bundle\AppBundle\Controller\Search;
 use Criticalmass\Bundle\AppBundle\Controller\AbstractController;
 use Criticalmass\Bundle\AppBundle\Entity\City;
 use Criticalmass\Bundle\AppBundle\Entity\Ride;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PrefetchController extends AbstractController
 {
-    public function prefetchAction(Request $request)
+    public function prefetchAction(): Response
     {
+        $prefetchedData = array_merge($this->getCitiesResult(), $this->getRidesResult());
+
+        return new Response(json_encode($prefetchedData), 200, [
+            'Content-Type' => 'text/json'
+        ]);
+    }
+
+    protected function getCitiesResult(): array
+    {
+        $cities = $this->getCityRepository()->findEnabledCities();
         $result = [];
 
+        /** @var City $city */
+        foreach ($cities as $city) {
+            $result[] = [
+                'type' => 'city',
+                'url' => $this->generateObjectUrl($city),
+                'value' => $city->getCity()
+            ];
+        }
+
+        return $result;
+    }
+
+    protected function getRidesResult(): array
+    {
         $rides = $this->getRideRepository()->findCurrentRides();
+        $result = [];
 
         /** @var Ride $ride */
         foreach ($rides as $ride) {
@@ -29,23 +53,6 @@ class PrefetchController extends AbstractController
             ];
         }
 
-        $cities = $this->getCityRepository()->findEnabledCities();
-
-        /** @var City $city */
-        foreach ($cities as $city) {
-            $result[] = [
-                'type' => 'city',
-                'url' => $this->generateObjectUrl($city),
-                'value' => $city->getCity()
-            ];
-        }
-
-        return new Response(
-            json_encode($result),
-            200,
-            [
-                'Content-Type' => 'text/json'
-            ]
-        );
+        return $result;
     }
 }
