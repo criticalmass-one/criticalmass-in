54efc76c476fce0bd22198e551a107bf143d5eba
Malte Hübner <maltehuebner@gmx.org>
2018-06-21 10:08:00 +0200

    Fix search for rides.

--- src/Criticalmass/Bundle/AppBundle/Controller/Search/SearchController.php
+++ src/Criticalmass/Bundle/AppBundle/Controller/Search/SearchController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends AbstractController
 {
-    protected function createQuery(string $queryPhrase, AbstractQuery $cityFilter, AbstractQuery $countryFilter): Query {
+    protected function createQuery(string $queryPhrase): Query {
         if ($queryPhrase) {
             $simpleQueryString = new \Elastica\Query\SimpleQueryString($queryPhrase,
                 ['title', 'description', 'location']);
@@ -22,11 +22,8 @@ class SearchController extends AbstractController
 
         $enabledFilter = new \Elastica\Query\Term(['isEnabled' => true]);
 
-        $filter = new \Elastica\Query\BoolQuery();
-        $filter->addFilter($enabledFilter)->addFilter($cityFilter)->addFilter($countryFilter);
-
         $filteredQuery = new \Elastica\Query\BoolQuery();
-        $filteredQuery->addFilter($simpleQueryString)->addFilter($filter);
+        $filteredQuery->addFilter($simpleQueryString)->addFilter($enabledFilter);
 
         $query = new \Elastica\Query($filteredQuery);
 
@@ -38,7 +35,7 @@ class SearchController extends AbstractController
 
     protected function performSearch(Query $query, IndexManager $manager): ResultSet
     {
-        $search = $manager->getIndex('criticalmass')->createSearch();
+        $search = $manager->getIndex('criticalmass_ride')->createSearch();
 
         //$search->addType('ride');
         //$search->addType('city');
@@ -46,79 +43,23 @@ class SearchController extends AbstractController
         return $search->search($query);
     }
 
-    protected function addAggregations(\Elastica\Query $query)
-    {
-        $aggregation = new \Elastica\Aggregation\Terms('city');
-        $aggregation->setField('city');
-        $aggregation->setSize(50);
-        $query->addAggregation($aggregation);
-
-        $aggregation = new \Elastica\Aggregation\Terms('country');
-        $aggregation->setField('country');
-        $aggregation->setSize(50);
-        $query->addAggregation($aggregation);
-
-        return $query;
-    }
-
-    protected function createCityFilter(array $cities = [])
-    {
-        $filters = [];
-
-        foreach ($cities as $city) {
-            $filters[] = new \Elastica\Query\Term(['city' => $city]);
-        }
-
-        return new \Elastica\Query\BoolOr($filters);
-    }
-
-    protected function createCountryFilter(array $countries = [])
-    {
-        $filters = [];
-
-        foreach ($countries as $country) {
-            $filters[] = new \Elastica\Query\Term(['country' => $country]);
-        }
-
-        return new \Elastica\Query\BoolOr($filters);
-    }
-
     public function queryAction(Request $request, IndexManager $manager): Response
     {
         $queryPhrase = $request->get('query');
-        $cities = $request->get('cities');
-        $countries = $request->get('countries');
 
-        if ($cities) {
-            $cityFilter = $this->createCityFilter($cities);
-        } else {
-            $cityFilter = new \Elastica\Query\MatchAll();
-        }
-
-        if ($countries) {
-            $countryFilter = $this->createCountryFilter($countries);
-        } else {
-            $countryFilter = new \Elastica\Query\MatchAll();
-        }
-
-        $query = $this->createQuery($queryPhrase, $cityFilter, $countryFilter);
-
-        $query = $this->addAggregations($query);
-
+        $query = $this->createQuery($queryPhrase);
+        
         /** @var ResultSet $resultSet */
         $resultSet = $this->performSearch($query, $manager);
 
-        $transformer = $this->get('fos_elastica.elastica_to_model_transformer.collection.criticalmass');
+        $transformer = $this->get('fos_elastica.elastica_to_model_transformer.collection.criticalmass_ride');
 
         $results = $transformer->transform($resultSet->getResults());
 
-        return $this->render('AppBundle:Search:result.html.twig',
-            [
-                'results' => $results,
-                'resultSet' => $resultSet,
-                'query' => $queryPhrase
-
-            ]
-        );
+        return $this->render('AppBundle:Search:result.html.twig', [
+            'results' => $results,
+            'resultSet' => $resultSet,
+            'query' => $queryPhrase,
+        ]);
     }
 }
