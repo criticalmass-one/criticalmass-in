7b33bb4f1db662784a900e23658ec8a3d028dec2
Malte Hübner <maltehuebner@gmx.org>
2018-07-25 00:10:17 +0200

    Implement DurationTwigExtension.

---
+++ src/Twig/Extension/DurationTwigExtension.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Twig\Extension;
+
+use Khill\Duration\Duration;
+
+class DurationTwigExtension extends \Twig_Extension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new \Twig_SimpleFunction('human_duration', [$this, 'duration'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function duration(string $duration): string
+    {
+        return (new Duration())->humanize($duration);
+    }
+
+    public function getName(): string
+    {
+        return 'duration_extension';
+    }
+}
+
