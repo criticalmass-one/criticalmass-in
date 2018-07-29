c62c2ae360678c821b72b8747270f16af6b45b5a
Malte Hübner <maltehuebner@gmx.org>
2018-07-29 12:10:07 +0200

    Integrate Track entity.

--- src/Criticalmass/Geo/EntityInterface/TrackInterface.php
+++ src/Criticalmass/Geo/EntityInterface/TrackInterface.php
@@ -6,6 +6,6 @@ use Caldera\GeoBasic\Track\TrackInterface as BaseTrackInterface;
 
 interface TrackInterface extends BaseTrackInterface
 {
-    public function setPreviewPolyline(string $previewPolyline = null): TrackInterface;
-    public function getPreviewPolyline(): ?string;
+    public function setReducedPolyline(string $reducedPolyline = null): TrackInterface;
+    public function getReducedPolyline(): ?string;
 }
