f42e93112b58115586d0d53b4711bd68c537ae2b
Malte Hübner <maltehuebner@gmx.org>
2018-07-31 18:32:34 +0200

    GpxWriter.

--- tests/GpxWriter/GpxWriterTest.php
+++ tests/GpxWriter/GpxWriterTest.php
@@ -1,13 +1,13 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Tests\DistanceCalculator;
 
-use Caldera\GeoBundle\GpxWriter\GpxWriter;
+use App\Criticalmass\Geo\GpxWriter\GpxWriter;
 use PHPUnit\Framework\TestCase;
 
 class GpxWriterTest extends TestCase
 {
-    public function testGpxWriter1()
+    public function testGpxWriter1(): void
     {
         $gpxWriter = new GpxWriter();
 
