4d88950b833a3773696b3abf557c33b943d2deea
Malte Hübner <maltehuebner@gmx.org>
2018-09-18 16:31:58 +0200

    There is no chat anymore.

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -92,28 +92,6 @@ class PostRepository extends EntityRepository
         return $query->getResult();
     }
 
-    public function findRecentChatMessages($limit = 25)
-    {
-        $builder = $this->createQueryBuilder('post');
-
-        $builder->select('post');
-
-        $builder->where($builder->expr()->eq('post.enabled', 1));
-        $builder->andWhere($builder->expr()->eq('post.chat', 1));
-
-        if ($limit) {
-            $builder->setMaxResults($limit);
-        }
-
-        $builder->addOrderBy('post.dateTime', 'DESC');
-
-        $query = $builder->getQuery();
-
-        $result = $query->getResult();
-
-        return $result;
-    }
-
     public function findForTimelineThreadPostCollector(
         \DateTime $startDateTime = null,
         \DateTime $endDateTime = null,
