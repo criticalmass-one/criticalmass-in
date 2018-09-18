bfb13998177199b099a3c7991a602a038b528d51
Malte Hübner <maltehuebner@gmx.org>
2018-09-18 13:59:22 +0200

    Config.

--- config/bundles.php
+++ config/bundles.php
@@ -31,4 +31,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sherlockode\SonataGentelellaBundle\SherlockodeSonataGentelellaBundle::class => ['all' => true],
+
 ];
