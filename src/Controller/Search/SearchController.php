fb33ce417c417b057436a02b2b053fa4d7e4fc6d
Malte Hübner <maltehuebner@gmx.org>
2018-09-25 19:20:40 +0200

    Extract prefetch stuff to PrefetchController.

--- src/Controller/Search/SearchController.php
+++ src/Controller/Search/SearchController.php
@@ -3,13 +3,9 @@
 namespace App\Controller\Search;
 
 use App\Controller\AbstractController;
-use App\Criticalmass\Router\ObjectRouterInterface;
-use App\Entity\City;
-use App\Entity\Ride;
 use Elastica\ResultSet;
 use FOS\ElasticaBundle\Index\IndexManager;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends AbstractController
 {
@@ -120,41 +116,6 @@ class SearchController extends AbstractController
             'results' => $results,
             'resultSet' => $resultSet,
             'query' => $queryPhrase,
-        ]);
-    }
-
-    public function prefetchAction(ObjectRouterInterface $objectRouter): Response
-    {
-        $result = [];
-
-        $rides = $this->getRideRepository()->findCurrentRides();
-
-        /** @var Ride $ride */
-        foreach ($rides as $ride) {
-            $result[] = [
-                'type' => 'ride',
-                'url' => $objectRouter->generate($ride),
-                'value' => $ride->getTitle(),
-                'meta' => [
-                    'dateTime' => $ride->getDateTime()->format('Y-m-d\TH:i:s'),
-                    'location' => ($ride->getHasLocation() ? $ride->getLocation() : '')
-                ]
-            ];
-        }
-
-        $cities = $this->getCityRepository()->findEnabledCities();
-
-        /** @var City $city */
-        foreach ($cities as $city) {
-            $result[] = [
-                'type' => 'city',
-                'url' => $objectRouter->generate($city),
-                'value' => $city->getCity()
-            ];
-        }
-
-        return new Response(json_encode($result), 200, [
-            'Content-Type' => 'text/json'
         ]);
     }
 }
