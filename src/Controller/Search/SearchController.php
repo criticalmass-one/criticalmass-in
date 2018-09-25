50954077cedb2d3f8495d5e3dd963a950b1f3f82
Malte Hübner <maltehuebner@gmx.org>
2018-09-25 20:07:24 +0200

    Simplify everything.

--- src/Controller/Search/SearchController.php
+++ src/Controller/Search/SearchController.php
@@ -3,17 +3,14 @@
 namespace App\Controller\Search;
 
 use App\Controller\AbstractController;
-use Elastica\ResultSet;
-use FOS\ElasticaBundle\Index\IndexManager;
+use Elastica\Query;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends AbstractController
 {
-    protected function createQuery(
-        $queryPhrase//,
-       // \Elastica\Filter\AbstractFilter $cityFilter,
-        //\Elastica\Filter\AbstractFilter $countryFilter
-    ) {
+    protected function createQuery(string $queryPhrase): Query
+    {
         if ($queryPhrase) {
             $simpleQueryString = new \Elastica\Query\SimpleQueryString($queryPhrase,
                 ['title', 'description', 'location']);
@@ -36,85 +33,22 @@ class SearchController extends AbstractController
         return $query;
     }
 
-    protected function performSearch(\Elastica\Query $query, IndexManager $manager)
     {
-        $search = $manager->getIndex('criticalmass_city')->createSearch();
 
-        //$search->addType('ride');
-        //$search->addType('city');
-
-        return $search->search($query);
     }
 
-    protected function addAggregations(\Elastica\Query $query)
-    {
-        $aggregation = new \Elastica\Aggregation\Terms('city');
-        $aggregation->setField('city');
-        $aggregation->setSize(50);
-        $query->addAggregation($aggregation);
-
-        $aggregation = new \Elastica\Aggregation\Terms('country');
-        $aggregation->setField('country');
-        $aggregation->setSize(50);
-        $query->addAggregation($aggregation);
-
-        return $query;
-    }
-
-    protected function createCityFilter(array $cities = [])
-    {
-        $filters = [];
-
-        foreach ($cities as $city) {
-            $filters[] = new \Elastica\Filter\Term(['city' => $city]);
-        }
-
-        return new \Elastica\Filter\BoolOr($filters);
-    }
-
-    protected function createCountryFilter(array $countries = [])
-    {
-        $filters = [];
-
-        foreach ($countries as $country) {
-            $filters[] = new \Elastica\Filter\Term(['country' => $country]);
-        }
-
-        return new \Elastica\Filter\BoolOr($filters);
-    }
-
-    public function queryAction(Request $request, IndexManager $manager)
+    public function queryAction(Request $request): Response
     {
         $queryPhrase = $request->get('query');
-        $cities = $request->get('cities');
-        $countries = $request->get('countries');
-/*
-        if ($cities) {
-            $cityFilter = $this->createCityFilter($cities);
-        } else {
-            $cityFilter = new \Elastica\Filter\MatchAll();
-        }
 
-        if ($countries) {
-            $countryFilter = $this->createCountryFilter($countries);
-        } else {
-            $countryFilter = new \Elastica\Filter\MatchAll();
-        }
-*/
-        $query = $this->createQuery($queryPhrase);//, $cityFilter, $countryFilter);
+        $query = $this->createQuery($queryPhrase);
 
-        $query = $this->addAggregations($query);
+        $finder = $this->get('fos_elastica.finder.criticalmass_city');
 
-        /** @var ResultSet $resultSet */
-        $resultSet = $this->performSearch($query, $manager);
-
-        $transformer = $this->get('fos_elastica.elastica_to_model_transformer.collection.criticalmass_city');
-
-        $cityResults = $transformer->transform($resultSet->getResults());
+        $cityResults = $finder->find($query);
 
         return $this->render('Search/result.html.twig', [
             'cityResults' => $cityResults,
-            'resultSet' => $resultSet,
             'query' => $queryPhrase,
         ]);
     }
