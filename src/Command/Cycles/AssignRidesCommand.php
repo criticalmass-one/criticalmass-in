97d7007616ed9dbaf5b33a567241c814929adec3
Malte Hübner <maltehuebner@gmx.org>
2018-10-05 17:49:40 +0200

    Add non-interactive mode.

--- src/Command/Cycles/AssignRidesCommand.php
+++ src/Command/Cycles/AssignRidesCommand.php
@@ -46,8 +46,8 @@ class AssignRidesCommand extends Command
             ->addOption('from', null, InputOption::VALUE_OPTIONAL, 'DateTime of period to start')
             ->addOption('until', null, InputOption::VALUE_OPTIONAL, 'DateTime of period to end')
             ->addOption('ignore', null, InputOption::VALUE_NONE, 'Ignore rides with cycle')
-            ->addOption('ignoreCycle', null, InputOption::VALUE_REQUIRED, 'Cycle id to ignore')
-            ->addOption('defaultCycle', null, InputOption::VALUE_REQUIRED, 'Default cycle id');
+            ->addOption('ignore-cycle', null, InputOption::VALUE_REQUIRED, 'Cycle id to ignore')
+            ->addOption('default-cycle', null, InputOption::VALUE_REQUIRED, 'Default cycle id');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
@@ -55,43 +55,15 @@ class AssignRidesCommand extends Command
         $city = $this->getCityBySlug($input->getArgument('citySlug'));
         $fromDateTime = $input->getOption('from') ? new \DateTime($input->getOption('from')) : null;
         $untilDateTime = $input->getOption('until') ? new \DateTime($input->getOption('until')) : null;
-        $ignoreCycleId = $input->getOption('ignoreCycle');
 
         $rideList = $this->registry->getRepository(Ride::class)->findRides($fromDateTime, $untilDateTime, $city);
 
         $output->writeln(sprintf('Found <info>%d</info> rides for city <comment>%s</comment>', count($rideList), $city->getCity()));
 
-        $questionHelper = $this->getHelper('question');
-
-        if ($defaultCycleId = $input->getOption('defaultCycle')) {
-            $question = new Question(sprintf('Please submit cycle id or press enter to proceed: <info>[%d]</info> ', $defaultCycleId));
+        if ($input->getOption('no-interaction')) {
+            $this->autoAssignRides($input, $output, $rideList);
         } else {
-            $question = new Question(sprintf('Please submit cycle id or press enter to proceed: '));
-        }
-
-        /** @var Ride $ride */
-        foreach ($rideList as $ride) {
-            if ($ride->getCycle() && $input->hasOption('ignore')) {
-                continue;
-            }
-
-            if ($ride->getCycle() && $ride->getCycle()->getId() === $ignoreCycleId) {
-                continue;
-            }
-
-            if ($ride->getCycle()) {
-                $output->writeln(sprintf('Ride <info>%d</info> at <comment>%s</comment> is assigned to cycle <info>%d</info>.', $ride->getId(), $ride->getDateTime()->format('Y-m-d H:i:s'), $ride->getCycle()->getId()));
-            } else {
-                $output->writeln(sprintf('Ride <info>%d</info> at <comment>%s</comment> is not assigned to a cycle.', $ride->getId(), $ride->getDateTime()->format('Y-m-d H:i:s')));
-            }
-
-            $newCycleId = $questionHelper->ask($input, $output, $question);
-
-            if ($newCycleId = intval($newCycleId)) {
-                $ride->setCycle($this->getCycleById($newCycleId));
-
-                $output->writeln(sprintf('Assigned cycle <info>%d</info> to ride.', $newCycleId));
-            }
+            $this->runInteractiveRideAssignment($input, $output, $rideList);
         }
 
         $this->registry->getManager()->flush();
@@ -112,5 +84,69 @@ class AssignRidesCommand extends Command
         }
 
         return $this->cycleCache[$cycleId];
+    }
+
+    protected function autoAssignRides(InputInterface $input, OutputInterface $output, array $rideList): void
+    {
+        if (!$input->getOption('default-cycle')) {
+            $output->writeln('Please submit a default cycle with <comment>--default-cycle</comment> to auto-assign cycles to rides');
+            return;
+        }
+
+        $defaultCycle = $this->getCycleById(intval($input->getOption('default-cycle')));
+
+        if (!$defaultCycle) {
+            $output->writeln(sprintf('No cycle found with id <info>%d</info>', $input->getOption('defaultCycle')));
+            return;
+        }
+
+        /** @var Ride $ride */
+        foreach ($rideList as $ride) {
+            if ($ride->getCycle() && $input->hasOption('ignore')) {
+                continue;
+            }
+
+            if ($ride->getCycle() && $ride->getCycle()->getId() === intval($input->getOption('ignoreCycle'))) {
+                continue;
+            }
+
+            $ride->setCycle($defaultCycle);
+        }
+    }
+
+    protected function runInteractiveRideAssignment(InputInterface $input, OutputInterface $output, array $rideList): void
+    {
+        $questionHelper = $this->getHelper('question');
+
+        if ($defaultCycleId = $input->getOption('default-cycle')) {
+            $question = new Question(sprintf('Please submit cycle id or press enter to proceed: <info>[%d]</info> ', $defaultCycleId));
+        } else {
+            $question = new Question(sprintf('Please submit cycle id or press enter to proceed: '));
+        }
+
+        /** @var Ride $ride */
+        foreach ($rideList as $ride) {
+            if ($ride->getCycle() && $input->getOption('ignore')) {
+                continue;
+            }
+
+            if ($ride->getCycle() && $ride->getCycle()->getId() === intval($input->getOption('ignore-cycle'))) {
+                continue;
+            }
+
+            if ($ride->getCycle()) {
+                $output->writeln(sprintf('Ride <info>%d</info> at <comment>%s</comment> is assigned to cycle <info>%d</info>.', $ride->getId(), $ride->getDateTime()->format('Y-m-d H:i:s'), $ride->getCycle()->getId()));
+            } else {
+                $output->writeln(sprintf('Ride <info>%d</info> at <comment>%s</comment> is not assigned to a cycle.', $ride->getId(), $ride->getDateTime()->format('Y-m-d H:i:s')));
+            }
+
+            $newCycleId = $questionHelper->ask($input, $output, $question);
+
+            if ($newCycleId = intval($newCycleId)) {
+                $ride->setCycle($this->getCycleById($newCycleId));
+
+                $output->writeln(sprintf('Assigned cycle <info>%d</info> to ride.', $newCycleId));
+            }
+        }
     }
 }
