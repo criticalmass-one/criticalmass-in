cecebd13fd1aced0eabda3d88363079fa5b71acc
Malte Hübner <maltehuebner@gmx.org>
2018-12-08 17:05:20 +0100

    Require presta sitemap bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -31,4 +31,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
 ];
