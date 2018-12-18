cfde40e80d81affd1006abf415e2f63a922f0234
Malte Hübner <maltehuebner@gmx.org>
2018-12-18 13:08:25 +0100

    Cookie-notice.

--- gulpfile.js
+++ gulpfile.js
@@ -124,6 +124,7 @@ gulp.task('copy-js-external', function () {
         //'node_modules/leaflet-extra-markers/dist/js/leaflet.extra-markers.min.js',
         'node_modules/chart.js/dist/Chart.bundle.min.js',
         'node_modules/datatables/media/js/jquery.dataTables.min.js',
+        'node_modules/cookie-notice/dist/cookie.notice.min.js',
     ])
         .pipe(gulp.dest('public/js/'));
 });
