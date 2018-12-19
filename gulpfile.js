74609ecde7968e2c4cf6b5fe000b3d22d551b9c6
Malte Hübner <maltehuebner@gmx.org>
2018-12-19 09:24:45 +0100

    Leaflet-sleep.

--- gulpfile.js
+++ gulpfile.js
@@ -127,6 +127,7 @@ gulp.task('copy-js-external', function () {
         'node_modules/chart.js/dist/Chart.bundle.min.js',
         'node_modules/datatables/media/js/jquery.dataTables.min.js',
         'node_modules/cookie-notice/dist/cookie.notice.min.js',
+        'node_modules/leaflet-sleep/Leaflet.Sleep.js',
         'node_modules/requirejs/require.js',
     ])
         .pipe(gulp.dest('public/js/'));
