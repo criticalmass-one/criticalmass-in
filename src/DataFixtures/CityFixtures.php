ce580a588ad06e32ba9172c283de8cbf4f12c8f2
Malte Hübner <maltehuebner@gmx.org>
2019-01-02 12:57:19 +0100

    Introduce fixtures.

---
+++ src/DataFixtures/CityFixtures.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\City;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $manager->persist($this->createCity('Hamburg'));
+        $manager->persist($this->createCity('Halle'));
+        $manager->persist($this->createCity('Berlin'));
+        $manager->persist($this->createCity('London', 'Europe/London'));
+
+        $manager->flush();
+    }
+
+    protected function createCity(string $cityName, string $timezone = 'Europe/Berlin'): City
+    {
+        $city = new City();
+        $city
+            ->setTitle(sprintf('Critical Mass %s', $cityName))
+            ->setCity($cityName)
+            ->setTimezone($timezone);
+
+        return $city;
+    }
+}
