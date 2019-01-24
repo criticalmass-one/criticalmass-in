3bce629a8eb0e3f3a1e87aeb9df1ab09022f9fec
Malte Hübner <maltehuebner@gmx.org>
2019-01-24 20:50:27 +0100

    WIP for NetworkDetector.

--- src/Criticalmass/SocialNetwork/NetworkDetector/NetworkDetector.php
+++ src/Criticalmass/SocialNetwork/NetworkDetector/NetworkDetector.php
@@ -1,47 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Criticalmass\SocialNetwork\NetworkDetector;
 
 use App\Entity\SocialNetworkProfile;
 use App\Criticalmass\SocialNetwork\Network\NetworkInterface;
-use App\Criticalmass\SocialNetwork\NetworkManager\NetworkManager;
 
-class NetworkDetector
+class NetworkDetector extends AbstractNetworkDetector
 {
-    /** @var NetworkManager $networkManager */
-    protected $networkManager;
-
-    /** @var array $networkList */
-    protected $networkList = [];
-
-    public function __construct(NetworkManager $networkManager)
-    {
-        $this->networkManager = $networkManager;
-        $this->networkList = $networkManager->getNetworkList();
-
-        $this->sortNetworkList();
-    }
-
     public function detect(SocialNetworkProfile $socialNetworkProfile): ?NetworkInterface
     {
         /** @var NetworkInterface $network */
         foreach ($this->networkList as $network) {
-            echo $network->getName();
             if ($network->accepts($socialNetworkProfile)) {
                 return $network;
             }
         }
 
         return null;
-    }
-
-    protected function sortNetworkList(): NetworkDetector
-    {
-        usort($this->networkList, function(NetworkInterface $a, NetworkInterface$b)
-        {
-            return $b->getDetectorPriority() <=> $a->getDetectorPriority();
-        });
-
-        return $this;
     }
 }
