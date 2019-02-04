ec60ce37cd7446f692cdd6c71675750b8a8115c8
Malte Hübner <maltehuebner@gmx.org>
2019-02-04 09:23:50 +0100

    Even more wip.

--- assets/js/modules/ScrollToPost.js
+++ assets/js/modules/ScrollToPost.js
@@ -2,20 +2,40 @@ define([], function () {
 
     ScrollToPost = function (context, options) {
 
+        var hash = this._getHash();
+
+        if (hash.startsWith('post-')) {
+            this._highlightPost(hash);
+        }
+
+        this._addEventListers();
+    };
+
+    ScrollToPost.prototype._getHash = function () {
         var url = window.location.hash;
         var idx = url.indexOf('#');
         var hash = idx != -1 ? url.substring(idx+1) : '';
 
-        if (hash.startsWith('post-')) {
-            var $anchor = $('a[name=' + hash + ']');
-            var $post = $anchor.closest('.post');
-            var $tabContent = $post.closest('.tab-pane');
-            var $tab = $('a[href=\'#' + $tabContent.attr('id') + '\']');
+        return hash;
+    };
 
-            $post.addClass('bg-warning');
+    ScrollToPost.prototype._addEventListers = function () {
+        $('a[href^=\'#post-\']').on('click', function (e) {
+            e.preventDefault();
 
-            $tab.tab('show');
-        }
+            this._highlightPost(this._getHash());
+        });
+    };
+
+    ScrollToPost.prototype._highlightPost = function (hash) {
+        var $anchor = $('a[name=' + hash + ']');
+        var $post = $anchor.closest('.post');
+        var $tabContent = $post.closest('.tab-pane');
+        var $tab = $('a[href=\'#' + $tabContent.attr('id') + '\']');
+
+        $post.addClass('bg-warning');
+
+        $tab.tab('show');
     };
 
     return ScrollToPost;
