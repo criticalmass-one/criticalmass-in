1c054164d51e2dc1e2720ee08af7e869ea8dbea0
Malte Hübner <maltehuebner@gmx.org>
2019-02-11 22:12:33 +0100

    Fetch city data from api.

--- assets/js/modules/page/CityListPage.js
+++ assets/js/modules/page/CityListPage.js
@@ -1,13 +1,49 @@
-define(['jquery', 'jquery.dataTables'], function () {
+define(['jquery', 'jquery.dataTables', 'Map', 'CityMarker'], function ($) {
     var CityListPage = function (context) {
-        this.$cityListTable = context;
+        this.cityListTableSelector = context;
 
         this._init();
     };
 
     CityListPage.prototype._init = function () {
-        $(this.$cityListTable).DataTable({
+        let table = $(this.cityListTableSelector).DataTable({
             'paging': false
+        });
+
+        let that = this;
+
+        $(this.cityListTableSelector + ' tbody').on('click', 'td', function () {
+            var $tr = $(this).closest('tr');
+            var row = table.row($tr);
+
+            if (row.child.isShown()) {
+                row.child.hide();
+                $tr.removeClass('shown');
+            }
+            else {
+                that._buildChildRow($tr, row);
+            }
+        } );
+    };
+
+    CityListPage.prototype._buildChildRow = function($tr, row) {
+        let citySlug = $tr.data('city-slug');
+        let url = Routing.generate('caldera_criticalmass_rest_city_show', { citySlug: citySlug});
+
+        $.ajax(url, {
+            success: function(cityData) {
+                let html = '<div class="row"><div class="col-md-6"><div style="height: 150px;" id="map-' + citySlug + '"></div></div><div class="col-md-6"><h4>' + cityData.title + '</h4></div></div>';
+
+                row.child(html).show();
+                $tr.addClass('shown');
+
+                let map = new Map('map-' + citySlug);
+                let mapCenter = [cityData.latitude, cityData.longitude];
+
+                map.setView(mapCenter, 12);
+                let cityMarker = new CityMarker(mapCenter);
+                cityMarker.addTo(map);
+            }
         });
     };
 
