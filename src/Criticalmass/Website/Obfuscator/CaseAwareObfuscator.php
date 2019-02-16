9d72e64944a0e2678b28967d643c5bc006c68c33
Malte Hübner <maltehuebner@gmx.org>
2019-02-16 21:13:29 +0100

    Even more improvements.

--- src/Criticalmass/Website/Obfuscator/CaseAwareObfuscator.php
+++ src/Criticalmass/Website/Obfuscator/CaseAwareObfuscator.php
@@ -12,16 +12,21 @@ class CaseAwareObfuscator implements ObfuscatorInterface
             $ord = ord($text[$index]);
 
             if ($ord >= 65 && $ord <= 90) {
-                $ord = (($ord - 65 + 1) % 26 + 65);
+                $ord = $this->changeChar(65, $ord);
             }
 
             if ($ord >= 97 && $ord <= 122) {
-                $ord = (($ord - 97 + 1) % 26 + 97);
+                $ord = $this->changeChar(97, $ord);
             }
 
             $text[$index] = chr($ord);
         }
 
         return $text;
+    }
+
+    protected function changeChar(int $range, int $ord, int $step = 1): int
+    {
+        return (($ord - $range + $step) % 26 + $range);
     }
 }
