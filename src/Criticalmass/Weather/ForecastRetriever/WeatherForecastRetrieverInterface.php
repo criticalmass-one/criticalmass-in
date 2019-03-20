755eb6283550d64605ecea8486e2a74d17d424ca
Malte Hübner <maltehuebner@gmx.org>
2019-03-20 17:12:59 +0100

    Remove weather stuff.

--- src/Criticalmass/Weather/ForecastRetriever/WeatherForecastRetrieverInterface.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace App\Criticalmass\Weather\ForecastRetriever;
-
-interface WeatherForecastRetrieverInterface
-{
-    public function retrieve(\DateTime $startDateTime = null, \DateTime $endDateTime = null): array;
-    public function getNewWeatherForecasts(): array;
-}
