1221754139bf0c1ab9ce34c3e44ef76f7c42a103
Malte Hübner <maltehuebner@gmx.org>
2019-05-06 13:57:40 +0200

    WIP.

--- src/Controller/Photo/PhotoExportController.php
+++ src/Controller/Photo/PhotoExportController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Photo;
 
 use App\Controller\AbstractController;
+use App\Entity\Photo;
 use Symfony\Component\HttpFoundation\Response;
 use Flagception\Bundle\FlagceptionBundle\Annotations\Feature;
 
@@ -13,6 +14,24 @@ class PhotoExportController extends AbstractController
 {
     public function listAction(): Response
     {
-        return new Response('foo');
+        $photoBasePath = $this->getParameter('upload_destination.photo');
+
+        $photoList = $this->getPhotoRepository()->findAll();
+
+        $tsvList = [];
+
+        /** @var Photo $photo */
+        foreach ($photoList as $photo) {
+            $filename = sprintf('%s/%s', $photoBasePath, $photo->getImageName());
+
+            $size = @filesize($filename);
+            $hash = @sha1_file($filename);
+
+            $webPath = sprintf('http://criticalmass.cm/photos/%s', $photo->getImageName());
+
+            $tsvList[] = sprintf('%s\t%s\t%s', $webPath, $size, $hash);
+        }
+
+        return new Response(implode('\n', $tsvList), 200, ['Content-type: text/tsv']);
     }
 }
