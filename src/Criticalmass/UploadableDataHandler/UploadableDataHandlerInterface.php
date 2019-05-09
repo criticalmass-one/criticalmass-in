cb2a36b9f337f18b6328703a0b4ce9ac050da7b3
Malte Hübner <maltehuebner@gmx.org>
2019-05-09 08:35:23 +0200

    Allow to overwrite data.

--- src/Criticalmass/UploadableDataHandler/UploadableDataHandlerInterface.php
+++ src/Criticalmass/UploadableDataHandler/UploadableDataHandlerInterface.php
@@ -5,4 +5,5 @@ namespace App\Criticalmass\UploadableDataHandler;
 interface UploadableDataHandlerInterface
 {
     public function calculateForEntity(UploadableEntity $entity): UploadableEntity;
+    public function getFilenameProperty(string $fqcn): string;
 }
