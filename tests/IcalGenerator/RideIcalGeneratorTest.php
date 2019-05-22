ba7920649db03b046b5732f5df3da5791a409203
Malte Hübner <maltehuebner@gmx.org>
2019-05-22 09:09:42 +0200

    Add description property to test.

--- tests/IcalGenerator/RideIcalGeneratorTest.php
+++ tests/IcalGenerator/RideIcalGeneratorTest.php
@@ -238,4 +238,45 @@ END:VCALENDAR\r\n";
 
         $this->assertEquals($expectedContent, $actualContent);
     }
+
+    public function testRideWithCityDateTimeDescription(): void
+    {
+        $timezoneSpec = 'Europe/Berlin';
+        $rideDateTime = new \DateTime('2011-06-24 19:00:00', new \DateTimeZone($timezoneSpec));
+
+        $city = new City();
+        $city
+            ->addSlug(new CitySlug('hamburg'))
+            ->setTimezone($timezoneSpec);
+
+        $ride = new Ride();
+        $ride
+            ->setDateTime($rideDateTime)
+            ->setCity($city)
+            ->setDescription('Testbeschreibung Foo Bar Baz');
+
+        $rideIcalGenerator = new RideIcalGenerator();
+        $rideIcalGenerator->generateForRide($ride);
+
+        $dtStamp = new \DateTime();
+        $dtStampString = $dtStamp->format('Ymd\THis\Z');
+
+        $expectedContent = "BEGIN:VCALENDAR\r
+VERSION:2.0\r
+PRODID:-//Sabre//Sabre VObject 4.2.0//EN\r
+CALSCALE:GREGORIAN\r
+BEGIN:VEVENT\r
+UID:criticalmass-one-hamburg-1308934800\r
+DTSTAMP:".$dtStampString."\r
+SUMMARY:\r
+DESCRIPTION:Testbeschreibung Foo Bar Baz\r
+DTSTART;TZID=Europe/Berlin:20110624T190000\r
+DTEND;TZID=Europe/Berlin:20110624T210000\r
+END:VEVENT\r
+END:VCALENDAR\r\n";
+
+        $actualContent = $rideIcalGenerator->getSerializedContent();
+
+        $this->assertEquals($expectedContent, $actualContent);
+    }
 }
