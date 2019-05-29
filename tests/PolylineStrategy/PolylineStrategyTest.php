d633d5b7a47b0c63fec2c4dea73fd2a2bdd1ba36
Malte Hübner <maltehuebner@gmx.org>
2019-05-29 08:21:19 +0200

    Broken WIP for test.

--- tests/PolylineStrategy/PolylineStrategyTest.php
+++ tests/PolylineStrategy/PolylineStrategyTest.php
@@ -5,37 +5,57 @@ namespace Tests\PolylineStrategy;
 use App\Criticalmass\Geo\Entity\Position;
 use App\Criticalmass\Geo\PolylineGenerator\PolylineGenerator;
 use App\Criticalmass\Geo\PolylineGenerator\PolylineStrategy\FullPolylineStrategy;
+use App\Criticalmass\Geo\PolylineGenerator\PolylineStrategy\ReducedPolylineStrategy;
 use App\Criticalmass\Geo\PositionList\PositionList;
 use PHPUnit\Framework\TestCase;
 
 class PolylineStrategyTest extends TestCase
 {
-    public function testPolylineGenerator(): void
+    protected function createTestPositionList(): PositionList
     {
         $positionList = new PositionList();
 
-        $positionList
-            ->push(new Position(57, 10))
-            ->push(new Position(57, 9))
-            ->push(new Position(56, 9))
-            ->push(new Position(56, 10));
+        $longitude = 10;
 
+        for ($latitude = 10; $latitude < 50; ++$latitude) {
+            $positionList->add(new Position($latitude, $longitude));
+        }
+
+        $latitude = 50;
+
+        for ($longitude = 10; $longitude < 50; ++$longitude) {
+            $positionList->add(new Position($latitude, $longitude));
+        }
+
+        return $positionList;
+    }
+
+    public function testPolylineGenerator(): void
+    {
         $polylineGenerator = new PolylineGenerator();
         $polyline = $polylineGenerator
             ->setStrategy(new FullPolylineStrategy())
-            ->execute($positionList);
+            ->execute($this->createTestPositionList());
 
-        $this->assertEquals('_y{zI_c`|@?~hbE~hbE??_ibE', $polyline);
+        $this->assertEquals('_c`|@_c`|@_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE??_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE', $polyline);
+    }
+
+    public function testPolylineGeneratorWithReducedStrategy(): void
+    {
+        $polylineGenerator = new PolylineGenerator();
+        $polyline = $polylineGenerator
+            ->setStrategy(new ReducedPolylineStrategy())
+            ->execute($this->createTestPositionList());
+
+        $this->assertEquals('_c`|@_c`|@_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE??_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE?_ibE', $polyline);
     }
 
     public function testPolylineGeneratorWithEmptyPositionList(): void
     {
-        $positionList = new PositionList();
-
         $polylineGenerator = new PolylineGenerator();
         $polyline = $polylineGenerator
             ->setStrategy(new FullPolylineStrategy())
-            ->execute($positionList);
+            ->execute(new PositionList());
 
         $this->assertEquals('', $polyline);
     }
