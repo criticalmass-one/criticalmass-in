693efb7790c72338966070f1d9b893e0dc8bbad6
Malte Hübner <maltehuebner@gmx.org>
2019-05-29 08:12:59 +0200

    Add a test!

---
+++ tests/PolylineStrategy/PolylineStrategyTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PolylineStrategy;
+
+use App\Criticalmass\Geo\Entity\Position;
+use App\Criticalmass\Geo\PolylineGenerator\PolylineGenerator;
+use App\Criticalmass\Geo\PolylineGenerator\PolylineStrategy\FullPolylineStrategy;
+use App\Criticalmass\Geo\PositionList\PositionList;
+use PHPUnit\Framework\TestCase;
+
+class PolylineStrategyTest extends TestCase
+{
+    public function testPolylineGenerator(): void
+    {
+        $positionList = new PositionList();
+
+        $positionList
+            ->push(new Position(57, 10))
+            ->push(new Position(57, 9))
+            ->push(new Position(56, 9))
+            ->push(new Position(56, 10));
+
+        $polylineGenerator = new PolylineGenerator();
+        $polyline = $polylineGenerator
+            ->setStrategy(new FullPolylineStrategy())
+            ->execute($positionList);
+
+        $this->assertEquals('_y{zI_c`|@?~hbE~hbE??_ibE', $polyline);
+    }
+}
