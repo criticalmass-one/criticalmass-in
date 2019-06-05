e0addae41d855c636e66803bc3734223df2e2049
Malte Hübner <maltehuebner@gmx.org>
2019-06-05 17:39:37 +0200

    Prepare more position tests.

--- tests/Geo/Converter/GpxPointToPositionConverter/StandardGpxPointsTest.php
+++ tests/Geo/Converter/GpxPointToPositionConverter/StandardGpxPointsTest.php
@@ -1,24 +1,13 @@
 <?php declare(strict_types=1);
 
-namespace Tests\Geo\Converter;
+namespace Tests\Geo\Converter\GpxPointToPositionConverter;
 
 use App\Criticalmass\Geo\Converter\GpxPointToPositionConverter;
 use App\Criticalmass\Geo\Entity\Position;
 use PHPUnit\Framework\TestCase;
 
-class GpxPointToPositionConverterTest extends TestCase
+class StandardGpxPointsTest extends TestCase
 {
-    public function testConverterEmptyGpx(): void
-    {
-        $gpxString = '<?xml version="1.0"?><trkpt />';
-
-        $xmlGpx = new \SimpleXMLElement($gpxString);
-
-        $position = GpxPointToPositionConverter::convert($xmlGpx);
-
-        $this->assertNull($position);
-    }
-
     public function testConverterLatLngGpx(): void
     {
         $gpxString = '<?xml version="1.0"?>
@@ -78,41 +67,5 @@ class GpxPointToPositionConverterTest extends TestCase
             ->setDateTime(new \DateTime('2011-06-24 19:00:00'));
 
         $this->assertEquals($expectedPosition, $actualPosition);
-    }
-
-    public function testConverterAltitudeDateTimeGpx(): void
-    {
-        $gpxString = '<?xml version="1.0"?>
-<trkpt><ele>42.3</ele><time>2011-06-24T19:00:00Z</time></trkpt>';
-
-        $xmlGpx = new \SimpleXMLElement($gpxString);
-
-        $position = GpxPointToPositionConverter::convert($xmlGpx);
-
-        $this->assertNull($position);
-    }
-
-    public function testConverterDateTimeGpx(): void
-    {
-        $gpxString = '<?xml version="1.0"?>
-<trkpt><time>2011-06-24T19:00:00Z</time></trkpt>';
-
-        $xmlGpx = new \SimpleXMLElement($gpxString);
-
-        $position = GpxPointToPositionConverter::convert($xmlGpx);
-
-        $this->assertNull($position);
-    }
-
-    public function testConverterAltitudeGpx(): void
-    {
-        $gpxString = '<?xml version="1.0"?>
-<trkpt><ele>42.3</ele></trkpt>';
-
-        $xmlGpx = new \SimpleXMLElement($gpxString);
-
-        $position = GpxPointToPositionConverter::convert($xmlGpx);
-
-        $this->assertNull($position);
     }
 }
