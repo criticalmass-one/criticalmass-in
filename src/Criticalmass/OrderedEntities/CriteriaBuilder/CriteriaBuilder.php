223a981bd928a2370de628f1fedd549d99454751
Malte Hübner <maltehuebner@gmx.org>
2019-06-06 12:35:01 +0200

    WIP.

--- src/Criticalmass/OrderedEntities/CriteriaBuilder/CriteriaBuilder.php
+++ src/Criticalmass/OrderedEntities/CriteriaBuilder/CriteriaBuilder.php
@@ -2,11 +2,22 @@
 
 namespace App\Criticalmass\OrderedEntities\CriteriaBuilder;
 
+use App\Criticalmass\OrderedEntities\Annotation\AbstractAnnotation;
+use App\Criticalmass\OrderedEntities\Annotation\Order;
 use App\Criticalmass\OrderedEntities\OrderedEntityInterface;
+use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\Collections\Criteria;
 
 class CriteriaBuilder implements CriteriaBuilderInterface
 {
+    /** @var Reader $annotationReader */
+    protected $annotationReader;
+
+    public function __construct(Reader $annotationReader)
+    {
+        $this->annotationReader = $annotationReader;
+    }
+
     public function build(OrderedEntityInterface $orderedEntity, string $direction): Criteria
     {
         $expr = Criteria::expr();
@@ -16,6 +27,28 @@ class CriteriaBuilder implements CriteriaBuilderInterface
 
         $criteria->orderBy(['dateTime' => 'asc']);
 
+        $this->handleAnnotations($orderedEntity);
+
         return $criteria;
     }
+
+    protected function handleAnnotations(OrderedEntityInterface $orderedEntity): void
+    {
+        $reflectionClass = new \ReflectionClass($orderedEntity);
+        $properties = $reflectionClass->getProperties();
+
+        foreach ($properties as $key => $property) {
+            $annotations = $this->annotationReader->getPropertyAnnotations($property);
+            
+            /** @var AbstractAnnotation $parameterAnnotation */
+            foreach ($annotations as $annotation) {
+                if ($annotation instanceof Order) {
+                    dump($annotation);die;
+                }
+            }
+        }
+
+        return;
+    }
+
 }
