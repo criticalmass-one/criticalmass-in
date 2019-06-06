c53915155423e0298984a073d79e4c6963e0958c
Malte Hübner <maltehuebner@gmx.org>
2019-06-06 12:27:36 +0200

    Introduce more annotations.

---
+++ src/Criticalmass/OrderedEntities/Annotation/Boolean.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\OrderedEntities\Annotation;
+
+/**
+ * @Annotation
+ */
+class Boolean extends AbstractAnnotation
+{
+    /** @var bool $value */
+    protected $value;
+
+    public function getValue(): ?bool
+    {
+        return $this->value();
+    }
+}
