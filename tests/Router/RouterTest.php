520358941c1647eb06bb8851d7f5d38b85415205
Malte Hübner <maltehuebner@gmx.org>
2019-06-07 12:50:15 +0200

    Okay, it will not be that easy.

--- tests/Router/RouterTest.php
+++ tests/Router/RouterTest.php
@@ -2,28 +2,37 @@
 
 namespace Tests\Router;
 
-use App\Criticalmass\Router\ObjectRouterInterface;
+use App\Criticalmass\Router\ObjectRouter;
 use App\Entity\City;
 use App\Entity\CitySlug;
 use App\Entity\Ride;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Doctrine\Common\Annotations\AnnotationReader;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Router;
 
-class RouterTest extends KernelTestCase
+class RouterTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::bootKernel();
-    }
-
-    protected function getRouter(): ObjectRouterInterface
-    {
-        $container = self::$container;
-
-        return $container->get(ObjectRouterInterface::class);
-    }
-
     public function test1(): void
     {
+        $route = new Route('caldera_criticalmass_city_show');
+
+        $routeCollection = $this->createMock(RouteCollection::class);
+        $routeCollection->method('get')->willReturn($route);
+
+        $requestContext = $this->createMock(RequestContext::class);
+        $urlGenerator = new UrlGenerator($routeCollection, $requestContext);
+
+        $router = $this->createMock(Router::class);
+        $router->method('getRouteCollection')->willReturn($routeCollection);
+        $router->method('getGenerator')->willReturn($urlGenerator);
+
+        $annotationReader = new AnnotationReader();
+        $objectRouter = new ObjectRouter($router, $annotationReader);
+
         $citySlug = new CitySlug();
         $city = new City();
 
@@ -33,7 +42,7 @@ class RouterTest extends KernelTestCase
 
         $city->setMainSlug($citySlug);
 
-        $route = $this->getRouter()->generate($city, 'caldera_criticalmass_city_show');
+        $route = $objectRouter->generate($city, 'caldera_criticalmass_city_show');
 
         $this->assertEquals('/testcity', $route);
     }
