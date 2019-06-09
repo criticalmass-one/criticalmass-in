4cc861afef29546510f4fcd6883663aaf4a6edad
Malte Hübner <maltehuebner@gmx.org>
2019-06-10 01:00:51 +0200

    Add ViewStoragePersisterTest without user.

--- tests/ViewStorage/ViewStoragePersisterTest.php
+++ tests/ViewStorage/ViewStoragePersisterTest.php
@@ -14,6 +14,63 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ViewStoragePersisterTest extends TestCase
 {
+    public function testWithoutUser(): void
+    {
+        $testClass = new TestClass();
+
+        $expectedPersistedClass = new TestView();
+        $expectedPersistedClass
+            ->setId(1)
+            ->setTest($testClass)
+            ->setDateTime(new \DateTime());
+
+        $viewEntityFactoryRegistry = $this->createMock(RegistryInterface::class);
+        $viewEntityFactoryRegistry
+            ->expects($this->never())
+            ->method('getRepository')
+            ->with($this->equalTo(User::class));
+
+        $viewStoragePersisterRepository = $this->createMock(ObjectRepository::class);
+        $viewStoragePersisterRepository
+            ->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo(1))
+            ->will($this->returnValue($testClass));
+
+        $viewStoragePersisterManager = $this->createMock(EntityManagerInterface::class);
+        $viewStoragePersisterManager
+            ->expects($this->once())
+            ->method('persist')
+            ->with($this->equalTo($expectedPersistedClass, 0.5));
+
+        $viewStoragePersisterRegistry = $this->createMock(RegistryInterface::class);
+        $viewStoragePersisterRegistry
+            ->expects($this->once())
+            ->method('getRepository')
+            ->with($this->equalTo('App:Test'))
+            ->will($this->returnValue($viewStoragePersisterRepository));
+
+        $viewStoragePersisterRegistry
+            ->expects($this->once())
+            ->method('getManager')
+            ->will($this->returnValue($viewStoragePersisterManager));
+
+        $serializer = SerializerBuilder::create()->build();
+        $viewEntityFactory = new ViewEntityFactory($viewEntityFactoryRegistry);
+        $viewEntityFactory->setEntityNamespace('Tests\\ViewStorage\\');
+
+        $viewStoragePersister = new ViewStoragePersister($viewStoragePersisterRegistry, $serializer, $viewEntityFactory);
+        $viewStoragePersister->setEntityNamespace('Tests\\ViewStorage\\');
+
+        $view = new View();
+        $view
+            ->setDateTime(new \DateTime())
+            ->setEntityClassName('Test')
+            ->setEntityId(1);
+
+        $viewStoragePersister->storeView($view);
+    }
+
     public function testWithUser(): void
     {
         $user = new User();
