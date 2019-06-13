1c4ed2bd2bdbc92bfab5986279e8b96903f295b1
Malte Hübner <maltehuebner@gmx.org>
2019-06-13 09:22:22 +0200

    First bunch of creating blog.

---
+++ src/Controller/Blog/BlogController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Blog;
+
+use App\Controller\AbstractController;
+use App\Entity\Blog;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends AbstractController
+{
+    public function overviewAction(RegistryInterface $registry): Response
+    {
+        return $this->render('Blog/overview.html.twig', [
+            'blog_posts' => $registry->getRepository(Blog::class)->findAll(),
+        ]);
+    }
+}
