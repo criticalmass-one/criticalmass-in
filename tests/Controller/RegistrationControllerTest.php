b93f5270461c65248d71cb9276ab7d8d260483a4
Malte Hübner <maltehuebner@gmx.org>
2019-06-15 19:21:51 +0200

    Finally test mails.

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -24,8 +24,8 @@ class RegistrationControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/register/');
 
-        $username = uniqid('test-', true);
-        $email = sprintf('%s@test.criticalmass.in', $username);
+        $username = uniqid('criticalmass-test-', false);
+        $email = sprintf('%s@caldera.cc', $username);
         $password = 'test-123456';
 
         $form = $crawler->filter('.fos_user_registration_register')->form();
@@ -37,13 +37,49 @@ class RegistrationControllerTest extends WebTestCase
             'fos_user_registration_form[plainPassword][second]' => $password,
         ]);
 
+        $client->enableProfiler();
+
         $client->submit($form);
 
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $mailCollector = $client->getProfile()->getCollector('swiftmailer');
 
         $client->followRedirect();
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertSelectorTextContains('html h2', sprintf('Hej %s!', $username));
+
+        // checks that an email was sent
+        $this->assertSame(1, $mailCollector->getMessageCount());
+
+        $collectedMessages = $mailCollector->getMessages();
+        $message = $collectedMessages[0];
+
+        // Asserting email data
+        $this->assertInstanceOf('Swift_Message', $message);
+        $this->assertSame(sprintf('Willkommen %s!', $username), $message->getSubject());
+        $this->assertSame('malte@caldera.cc', key($message->getFrom()));
+        $this->assertSame($email, key($message->getTo()));
+        /*$this->assertSame(
+            'Hallo %username%!
+
+            Besuchen Sie bitte folgende Seite, um Ihr Benutzerkonto zu bestätigen: %confirmationUrl%
+
+            Mit besten Grüßen,
+            das Team.',
+            $message->getBody()
+        );*/
+    }
+
+    /**
+     * @depends testRegistrationForm
+     */
+    public function testConfirmationMail(): void
+    {
+         $foo = $this->mailbox->searchMailbox('NEW');
+
+         dump($foo);
+
     }
 }
