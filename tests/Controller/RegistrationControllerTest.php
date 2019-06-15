c4d5f2156c3d825d7b5113a0d800268c636b5254
Malte Hübner <maltehuebner@gmx.org>
2019-06-15 21:47:00 +0200

    Test mail content.

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -2,10 +2,22 @@
 
 namespace Tests\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationControllerTest extends WebTestCase
 {
+    protected function createTestUser(): User
+    {
+        $user = new User();
+        $user
+            ->setUsername(uniqid('criticalmass-test-', false))
+            ->setEmail($email = sprintf('%s@caldera.cc', $user->getUsername()))
+            ->setPlainPassword('test-123456');
+
+        return $user;
+    }
+
     public function testRegistrationFormVisible(): void
     {
         $client = static::createClient();
@@ -20,21 +32,49 @@ class RegistrationControllerTest extends WebTestCase
 
     public function testRegistrationForm(): void
     {
+        $testUser = $this->createTestUser();
+
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/register/');
-
-        $username = uniqid('criticalmass-test-', false);
-        $email = sprintf('%s@caldera.cc', $username);
-        $password = 'test-123456';
-
         $form = $crawler->filter('.fos_user_registration_register')->form();
 
         $form->setValues([
-            'fos_user_registration_form[username]' => $username,
-            'fos_user_registration_form[email]' => $email,
-            'fos_user_registration_form[plainPassword][first]' => $password,
-            'fos_user_registration_form[plainPassword][second]' => $password,
+            'fos_user_registration_form[username]' => $testUser->getUsername(),
+            'fos_user_registration_form[email]' => $testUser->getEmail(),
+            'fos_user_registration_form[plainPassword][first]' => $testUser->getPlainPassword(),
+            'fos_user_registration_form[plainPassword][second]' => $testUser->getPlainPassword(),
+        ]);
+
+        $client->enableProfiler();
+
+        $client->submit($form);
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $client->followRedirect();
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('html h2', sprintf('Hej %s!', $testUser->getUsername()));
+    }
+
+    /**
+     * @depends testRegistrationForm
+     */
+    public function testConfirmationMail(): void
+    {
+        $testUser = $this->createTestUser();
+
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register/');
+        $form = $crawler->filter('.fos_user_registration_register')->form();
+
+        $form->setValues([
+            'fos_user_registration_form[username]' => $testUser->getUsername(),
+            'fos_user_registration_form[email]' => $testUser->getEmail(),
+            'fos_user_registration_form[plainPassword][first]' => $testUser->getPlainPassword(),
+            'fos_user_registration_form[plainPassword][second]' => $testUser->getPlainPassword(),
         ]);
 
         $client->enableProfiler();
@@ -47,39 +87,25 @@ class RegistrationControllerTest extends WebTestCase
 
         $client->followRedirect();
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSelectorTextContains('html h2', sprintf('Hej %s!', $username));
+        /** @var User $user */
+        $user = self::$container->get('doctrine')->getRepository(User::class)->findOneByEmail($testUser->getEmail());
 
-        // checks that an email was sent
         $this->assertSame(1, $mailCollector->getMessageCount());
 
         $collectedMessages = $mailCollector->getMessages();
         $message = $collectedMessages[0];
 
-        // Asserting email data
         $this->assertInstanceOf('Swift_Message', $message);
-        $this->assertSame(sprintf('Willkommen %s!', $username), $message->getSubject());
+        $this->assertSame(sprintf('Willkommen %s!', $testUser->getUsername()), $message->getSubject());
         $this->assertSame('malte@caldera.cc', key($message->getFrom()));
-        $this->assertSame($email, key($message->getTo()));
-        /*$this->assertSame(
-            'Hallo %username%!
+        $this->assertSame($testUser->getEmail(), key($message->getTo()));
+        $this->assertSame(sprintf('Hallo %s!
 
-            Besuchen Sie bitte folgende Seite, um Ihr Benutzerkonto zu bestätigen: %confirmationUrl%
+Besuchen Sie bitte folgende Seite, um Ihr Benutzerkonto zu bestätigen: http://localhost/register/confirm/%s
 
-            Mit besten Grüßen,
-            das Team.',
+Mit besten Grüßen,
+das Team.', $testUser->getUsername(), $user->getConfirmationToken()),
             $message->getBody()
-        );*/
-    }
-
-    /**
-     * @depends testRegistrationForm
-     */
-    public function testConfirmationMail(): void
-    {
-         $foo = $this->mailbox->searchMailbox('NEW');
-
-         dump($foo);
-
+        );
     }
 }
