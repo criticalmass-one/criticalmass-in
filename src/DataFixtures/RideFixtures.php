1360dcee3c169e9846944d9ecd3521d51cb22a7a
Malte Hübner <maltehuebner@gmx.org>
2019-06-25 13:06:32 +0200

    More fixes.

--- src/DataFixtures/RideFixtures.php
+++ src/DataFixtures/RideFixtures.php
@@ -2,8 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Criticalmass\CitySlug\Handler\CitySlugHandler;
-use App\Entity\City;
 use App\Entity\Ride;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -13,20 +11,41 @@ class RideFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-24 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-25 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-26 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-27 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-28 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-29 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-30 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2050-09-24 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2035-06-24 19:00:00'), 'kidical-mass-hamburg-2035', 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-03-25 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-24 19:00:00'), null, 53.5, 10.5));
-        $manager->persist($this->createRide('hamburg', new \DateTime('2011-07-29 19:00:00'), null, 53.5, 10.5));
+        $this->createOtherRides($manager);
+        $this->createSpecialHamburgRides($manager);
 
         $manager->flush();
+    }
+
+    protected function createSpecialHamburgRides(ObjectManager $manager): void
+    {
+        $manager->persist($this->createRide('hamburg', new \DateTime('2011-03-25 19:00:00'),null, 53.5, 10.5));
+        $manager->persist($this->createRide('hamburg', new \DateTime('2011-06-24 19:00:00'),null, 53.5, 10.5));
+        $manager->persist($this->createRide('hamburg', new \DateTime('2011-07-29 19:00:00'),null, 53.5, 10.5));
+
+        $manager->persist($this->createRide('hamburg', new \DateTime('2050-09-24 19:00:00'),null, 53.5, 10.5));
+        $manager->persist($this->createRide('hamburg', new \DateTime('2035-06-24 19:00:00'),'kidical-mass-hamburg-2035', 53.5, 10.5));
+    }
+
+    protected function createOtherRides(ObjectManager $manager): void
+    {
+        $citySlugs = ['hamburg', 'halle', 'berlin', 'mainz', 'london', 'esslingen'];
+
+        $endDateTime = new \DateTime('2029-12-31');
+        $interval = new \DateInterval('P1M');
+
+        /** @var string $citySlug */
+        foreach ($citySlugs as $citySlug) {
+            $dateTime = new \DateTime('2015-01-01 19:00:00', new \DateTimeZone('UTC'));
+
+            while ($dateTime < $endDateTime) {
+                $ride = $this->createRide($citySlug, $dateTime, null,53.5, 10.5);
+
+                $manager->persist($ride);
+
+                $dateTime->add($interval);
+            }
+        }
     }
 
     protected function createRide(string $citySlug, \DateTime $dateTime, string $rideSlug = null, float $latitude, float $longitude): Ride
