91329ea02b7f8228f60cbb7a98b52802ebd5119c
Malte Hübner <maltehuebner@gmx.org>
2019-07-09 00:06:32 +0200

    Add more ride param converter tests.

--- tests/ParamConverter/RideParamConverterTest.php
+++ tests/ParamConverter/RideParamConverterTest.php
@@ -134,4 +134,102 @@ class RideParamConverterTest extends TestCase
         $this->assertCount(1, $request->attributes);
         $this->assertEquals($ride, $request->attributes->get('ride'));
     }
+
+    public function testRideParamConverterWithRideYearMonthDay(): void
+    {
+        $city = new City();
+        $ride = new Ride();
+        $ride->setCity($city);
+
+        $citySlug = new CitySlug();
+        $citySlug->setCity($city);
+
+        $citySlugRepository = $this->createMock(CitySlugRepository::class);
+        $citySlugRepository
+            ->expects($this->once())
+            ->method('__call')
+            ->with($this->equalTo('findOneBySlug'), $this->equalTo(['test-city-slug']))
+            ->will($this->returnValue($citySlug));
+
+        $rideRepository = $this->createMock(RideRepository::class);
+        $rideRepository
+            ->expects($this->once())
+            ->method('findCityRideByDate')
+            ->with($this->equalTo($city), $this->equalTo(new \DateTime('2011-06-24 00:00:00')))
+            ->will($this->returnValue($ride));
+
+        $registry = $this->createMock(RegistryInterface::class);
+
+        $registry
+            ->expects($this->any())
+            ->method('getRepository')
+            ->withConsecutive($this->equalTo(CitySlug::class), $this->equalTo(Ride::class))
+            ->willReturnOnConsecutiveCalls($citySlugRepository, $rideRepository);
+
+        $paramConverter = new RideParamConverter($registry);
+
+        $config = new ParamConverterConfig([
+            'class' => 'App:Ride',
+            'name' => 'ride',
+        ]);
+
+        $request = new Request([
+            'citySlug' => 'test-city-slug',
+            'rideIdentifier' => '2011-06-24'
+        ]);
+
+        $paramConverter->apply($request, $config);
+
+        $this->assertCount(1, $request->attributes);
+        $this->assertEquals($ride, $request->attributes->get('ride'));
+    }
+
+    public function testRideParamConverterWithRideYearMonth(): void
+    {
+        $city = new City();
+        $ride = new Ride();
+        $ride->setCity($city);
+
+        $citySlug = new CitySlug();
+        $citySlug->setCity($city);
+
+        $citySlugRepository = $this->createMock(CitySlugRepository::class);
+        $citySlugRepository
+            ->expects($this->once())
+            ->method('__call')
+            ->with($this->equalTo('findOneBySlug'), $this->equalTo(['test-city-slug']))
+            ->will($this->returnValue($citySlug));
+
+        $rideRepository = $this->createMock(RideRepository::class);
+        $rideRepository
+            ->expects($this->once())
+            ->method('findByCityAndMonth')
+            ->with($this->equalTo($city), $this->equalTo(new \DateTime('2011-06-01 00:00:00')))
+            ->will($this->returnValue([$ride]));
+
+        $registry = $this->createMock(RegistryInterface::class);
+
+        $registry
+            ->expects($this->any())
+            ->method('getRepository')
+            ->withConsecutive($this->equalTo(CitySlug::class), $this->equalTo(Ride::class))
+            ->willReturnOnConsecutiveCalls($citySlugRepository, $rideRepository);
+
+        $paramConverter = new RideParamConverter($registry);
+
+        $config = new ParamConverterConfig([
+            'class' => 'App:Ride',
+            'name' => 'ride',
+        ]);
+
+        $request = new Request([
+            'citySlug' => 'test-city-slug',
+            'rideIdentifier' => '2011-06'
+        ]);
+
+        $paramConverter->apply($request, $config);
+
+        $this->assertCount(1, $request->attributes);
+        $this->assertEquals($ride, $request->attributes->get('ride'));
+    }
 }
