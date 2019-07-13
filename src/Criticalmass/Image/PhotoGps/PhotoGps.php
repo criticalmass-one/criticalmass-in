29093adced64df864db21733d80453b5035e07fd
Malte Hübner <maltehuebner@gmx.org>
2019-07-13 19:19:08 +0200

    Next try.

--- src/Criticalmass/Image/PhotoGps/PhotoGps.php
+++ src/Criticalmass/Image/PhotoGps/PhotoGps.php
@@ -14,7 +14,6 @@ class PhotoGps extends AbstractPhotoGps
             $this->photo
                 ->setLatitude($gps['lat'])
                 ->setLongitude($gps['lon']);
-
         } elseif ($this->track) {
             $this->approximateCoordinates();
         }
@@ -31,6 +30,7 @@ class PhotoGps extends AbstractPhotoGps
             $positionList = $converter->convert($this->track);
 
             $position = $this->loop
+                ->setDateTimeZone($this->dateTimeZone)
                 ->setPositionList($positionList)
                 ->searchPositionForDateTime($dateTime);
 
@@ -49,8 +49,10 @@ class PhotoGps extends AbstractPhotoGps
         $exif = $this->readExifData();
 
         if ($exif && $dateTime = $exif->getCreationDate()) {
+            $dateTime = new \DateTime(sprintf($dateTime->format('Y-m-d H:i:s')), new \DateTimeZone('Europe/Berlin'));
 
-            $dateTime = new \DateTime(sprintf('@%d', $dateTime->format('U')), new \DateTimeZone('Europe/Berlin'));
+            $dateTime->setTimezone(new \DateTimeZone('UTC'))->getTimezone();
+
             return $dateTime;
         }
 
