90360ad59aa3e0ebf5a6bf0ba89026556e4fea8b
Malte Hübner <maltehuebner@gmx.org>
2019-07-15 19:08:42 +0200

    Adjust namespaces.

--- src/Criticalmass/Wikidata/CityPopulationFetcher/WikidataCityPopulationFetcher.php
+++ src/Criticalmass/Wikidata/CityPopulationFetcher/WikidataCityPopulationFetcher.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Criticalmass\CityPopulationFetcher;
+namespace App\Criticalmass\Wikidata\CityPopulationFetcher;
 
 use App\Criticalmass\CityPopulationFetcher\Exception\CityNotFoundException;
 use App\Criticalmass\CityPopulationFetcher\Exception\ValueNotFoundException;
