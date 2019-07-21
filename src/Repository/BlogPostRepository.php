4cda34c77484bca5fa11c79dfd0400679ee216f9
Malte Hübner <maltehuebner@gmx.org>
2019-07-21 18:35:17 +0200

    Fix repositories.

--- src/Repository/BlogPostRepository.php
+++ src/Repository/BlogPostRepository.php
@@ -16,4 +16,37 @@ class BlogPostRepository extends EntityRepository
 
         return $qb->getQuery()->getResult();
     }
+
+    public function findForTimelineBlogPostCollector(\DateTime $startDateTime = null, \DateTime $endDateTime = null, $limit = null): array
+    {
+        $builder = $this->createQueryBuilder('bp');
+
+        $builder
+            ->select('bp')
+            ->where($builder->expr()->eq('bp.enabled', ':enabled'))
+            ->setParameter('enabled', true)
+            ->addOrderBy('bp.createdAt', 'DESC');
+
+        if ($startDateTime) {
+            $builder
+                ->andWhere($builder->expr()->gte('bp.createdAt',':startDateTime'))
+                ->setParameter('startDateTime', $startDateTime);
+        }
+
+        if ($endDateTime) {
+            $builder
+                ->andWhere($builder->expr()->lte('bp.createdAt', ':endDateTime'))
+                ->setParameter('endDateTime', $endDateTime);
+        }
+
+        if ($limit) {
+            $builder->setMaxResults($limit);
+        }
+
+        $query = $builder->getQuery();
+
+        $result = $query->getResult();
+
+        return $result;
+    }
 }
