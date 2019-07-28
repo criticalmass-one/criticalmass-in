213647f4adccd1cfceeeb969dce5d4d6a872e686
Malte Hübner <maltehuebner@gmx.org>
2019-07-28 17:32:29 +0200

    Add FilenameGenerator and FilenameGeneratorTest.

---
+++ src/Criticalmass/Heatmap/FilenameGenerator/FilenameGenerator.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\Heatmap\FilenameGenerator;
+
+use App\Criticalmass\Heatmap\HeatmapInterface;
+use App\Criticalmass\Heatmap\Tile;
+
+class FilenameGenerator
+{
+    public static function generate(HeatmapInterface $heatmap, Tile $tile): string
+    {
+        return sprintf('%s/%d/%d/%d.png',
+            $heatmap->getIdentifier(),
+            $tile->getZoomLevel(),
+            $tile->getTileX(),
+            $tile->getTileY(),
+        );
+    }
+}
