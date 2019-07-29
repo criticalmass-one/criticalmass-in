aa45218d915d82b23f610c022411fb5a795748d8
Malte Hübner <maltehuebner@gmx.org>
2019-07-29 08:05:31 +0200

    Add CoordCalculator.

---
+++ tests/Heatmap/CoordCalculator/CoordCalculatorTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Heatmap\CoordCalculator;
+
+use PHPUnit\Framework\TestCase;
+
+class CoordCalculatorTest extends TestCase
+{
+    public function testFoo(): void
+    {
+
+    }
+}
