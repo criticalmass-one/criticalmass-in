eb329a072660781727ef7543355ec58dfafadfe8
Malte Hübner <maltehuebner@gmx.org>
2019-07-30 20:01:33 +0200

    Make DimensionCalculator static.

--- src/Criticalmass/Heatmap/DimensionCalculator/DimensionCalculator.php
+++ src/Criticalmass/Heatmap/DimensionCalculator/DimensionCalculator.php
@@ -3,56 +3,56 @@
 namespace App\Criticalmass\Heatmap\DimensionCalculator;
 
 use App\Criticalmass\Heatmap\CoordCalculator\CoordCalculator;
+use App\Criticalmass\Heatmap\Path\Path;
+use App\Criticalmass\Heatmap\Path\PathList;
 
 class DimensionCalculator
 {
-    protected $pathArray;
-    protected $zoom;
-
-    /** @var HeatmapDimension $heatmapDimension */
-    protected $heatmapDimension;
-
-    public function __construct($pathArray, int $zoomLevel)
+    private function __construct()
     {
-        $this->heatmapDimension = new HeatmapDimension($zoomLevel);
-        $this->pathArray = $pathArray;
 
-        $path = array_shift($this->pathArray);
-
-        $minLat = $path->getStartPosition()->getLatitude();
-        $minLon = $path->getStartPosition()->getLongitude();
-        $maxLat = $path->getStartPosition()->getLatitude();
-        $maxLon = $path->getStartPosition()->getLongitude();
-
-        while ($path != null) {
-            if ($path->getEndPosition()->getLatitude() < $minLat) {
-                $minLat = $path->getEndPosition()->getLatitude();
-            }
-
-            if ($path->getEndPosition()->getLongitude() < $minLon) {
-                $minLon = $path->getEndPosition()->getLongitude();
-            }
-
-            if ($path->getEndPosition()->getLatitude() > $maxLat) {
-                $maxLat = $path->getEndPosition()->getLatitude();
-            }
-
-            if ($path->getEndPosition()->getLongitude() > $maxLon) {
-                $maxLon = $path->getEndPosition()->getLongitude();
-            }
-
-            $path = array_shift($this->pathArray);
-        }
-
-        $this->heatmapDimension
-            ->setTopTile(CoordCalculator::latitudeToYTile($minLat, $this->zoom))
-            ->setBottomTile(CoordCalculator::latitudeToYTile($maxLat, $this->zoom))
-            ->setLeftTile(CoordCalculator::longitudeToXTile($minLon, $this->zoom))
-            ->setRightTile(CoordCalculator::longitudeToXTile($maxLon, $this->zoom));
     }
 
-    public function getHeatmapDimension(): HeatmapDimension
+    public static function calculate(PathList $pathList, int $zoomLevel): HeatmapDimension
     {
-        return $this->heatmapDimension;
+        $heatmapDimension = new HeatmapDimension($zoomLevel);
+
+        /** @var Path $path */
+        $path = $pathList->get();
+
+        $minLat = $path->getStartCoord()->getLatitude();
+        $minLon = $path->getStartCoord()->getLongitude();
+        $maxLat = $path->getStartCoord()->getLatitude();
+        $maxLon = $path->getStartCoord()->getLongitude();
+
+        while ($path !== null) {
+            $endCoord = $path->getEndCoord();
+
+            if ($endCoord->getLatitude() < $minLat) {
+                $minLat = $endCoord->getLatitude();
+            }
+
+            if ($endCoord->getLongitude() < $minLon) {
+                $minLon = $endCoord->getLongitude();
+            }
+
+            if ($endCoord->getLatitude() > $maxLat) {
+                $maxLat = $endCoord->getLatitude();
+            }
+
+            if ($endCoord->getLongitude() > $maxLon) {
+                $maxLon = $endCoord->getLongitude();
+            }
+
+            $path = $pathList->get();
+        }
+
+        $heatmapDimension
+            ->setTopTile(CoordCalculator::latitudeToYTile($minLat, $zoomLevel))
+            ->setBottomTile(CoordCalculator::latitudeToYTile($maxLat, $zoomLevel))
+            ->setLeftTile(CoordCalculator::longitudeToXTile($minLon, $zoomLevel))
+            ->setRightTile(CoordCalculator::longitudeToXTile($maxLon, $zoomLevel));
+
+        return $heatmapDimension;
     }
 }
