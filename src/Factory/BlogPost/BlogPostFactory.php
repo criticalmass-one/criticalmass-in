d26742f2b0e10e00d0f01a649c55cce26d4084b6
Malte Hübner <maltehuebner@gmx.org>
2019-08-08 14:54:47 +0200

    Introduce BogPostFactory.

---
+++ src/Factory/BlogPost/BlogPostFactory.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Factory\BlogPost;
+
+use App\Entity\BlogPost;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BlogPostFactory implements BlogPostFactoryInterface
+{
+    /**
+     * @var BlogPost $blogPost
+     */
+    protected $blogPost;
+
+    public function __construct()
+    {
+        $this->blogPost = new BlogPost();
+    }
+
+    public function withUser(UserInterface $user): BlogPostFactoryInterface
+    {
+        $this->blogPost->setUser($user);
+
+        return $this;
+    }
+
+    public function build(): BlogPost
+    {
+        return $this->blogPost;
+    }
+}
