75fddde8acce4b07b2a43ea69ec30e414a3573cc
Malte Hübner <maltehuebner@gmx.org>
2019-08-11 09:57:35 +0200

    Cleanup.

--- src/Admin/BlogPostAdmin.php
+++ src/Admin/BlogPostAdmin.php
@@ -13,7 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 final class BlogPostAdmin extends AbstractAdmin
