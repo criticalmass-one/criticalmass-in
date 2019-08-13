0ce419a517d7683680c1b6e2acadea85d2e61865
Malte Hübner <maltehuebner@gmx.org>
2019-08-13 06:05:39 +0200

    Reset TileLoader.

--- src/Criticalmass/Heatmap/Tile/TileLoader.php
+++ src/Criticalmass/Heatmap/Tile/TileLoader.php
@@ -4,7 +4,6 @@ namespace App\Criticalmass\Heatmap\Tile;
 
 use App\Criticalmass\Heatmap\FilenameGenerator\FilenameGenerator;
 use App\Criticalmass\Heatmap\HeatmapInterface;
-use Imagine\Gd\Image;
 use Imagine\Gd\Imagine;
 use Imagine\Image\Box;
 use League\Flysystem\FilesystemInterface;
@@ -19,7 +18,6 @@ class TileLoader
         $this->filesystem = $filesystem;
     }
 
-    /*
     public function load(HeatmapInterface $heatmap, int $tileX, int $tileY, int $zoomLevel): Tile
     {
         $tile = new Tile($tileX, $tileY, $zoomLevel);
@@ -27,24 +25,11 @@ class TileLoader
         $filename = FilenameGenerator::generate($heatmap, $tile);
 
         if (!$this->filesystem->has($filename)) {
-            $box = new Box(256, 256);
+            $box = new Box(Tile::SIZE, Tile::SIZE);
             $image = (new Imagine())->create($box);
         } else {
             $image = (new Imagine())->read($this->filesystem->read($filename));
         }
-
-        $tile->setImage($image);
-
-        return $tile;
-    }*/
-
-    public function load(HeatmapInterface $heatmap, int $tileX, int $tileY, int $zoomLevel): Tile
-    {
-        $tile = new Tile($tileX, $tileY, $zoomLevel);
-
-        $filename = sprintf('https://tiles.caldera.cc/wikimedia-intl/%d/%d/%d.png', $zoomLevel, $tileX, $tileY);
-
-        $image = (new Imagine())->open($filename);
 
         $tile->setImage($image);
 
