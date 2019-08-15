6bb92cd90cbe4c4ac4ff8c196aad9b5e67f15d73
Malte Hübner <maltehuebner@gmx.org>
2019-08-15 19:05:12 +0200

    Fix test.

--- tests/Heatmap/Pipette/AveragePipetteTest.php
+++ tests/Heatmap/Pipette/AveragePipetteTest.php
@@ -12,39 +12,66 @@ use PHPUnit\Framework\TestCase;
 
 class AveragePipetteTest extends TestCase
 {
-    public function testFoo(): void
+    public function testSameColor(): void
     {
-        $tile = $this->createMock(Tile::class);
-
         $image = $this->createMock(ImageInterface::class);
+        $tile = new Tile(5, 5, 10);
+        $tile->setOldImage($image);
 
-        $tile
-            ->method('oldImage')
-            ->will($this->returnValue($image));
-
-        $color = $this->createMock(ColorInterface::class);
+        $color = $this->createColor(255, 127, 0);
 
         $image
-            ->method('getColorAt')
+            ->expects($this->exactly(9))
+            ->method($this->equalTo('getColorAt'))
             ->will($this->returnValue($color));
 
-        $color
-            ->method('getValue')
-            ->willReturnOnConsecutiveCalls(
-                $this->createColor(255, 0, 0),
-                $this->createColor(0, 255, 0),
-                $this->createColor(0, 0, 255)
-            );
-        
         $point = new Point(5, 5);
 
         $actualColor = AveragePipette::getColor($tile, $point);
 
-        $this->assertEquals($this->createColor(200, 200, 0), $actualColor);
+        $this->assertEquals($this->createColor(255, 127, 0), $actualColor);
     }
 
-    protected function createColor(int $red, int $green, int $blue): ColorInterface
+    public function testDifferentColors(): void
     {
-        return (new RGB())->color([$red, $green, $blue]);
+        $color1 = $this->createColor(11, 12, 13);
+        $color2 = $this->createColor(21, 22, 23);
+        $color3 = $this->createColor(31, 32, 33);
+        $color4 = $this->createColor(41, 42, 43);
+        $color5 = $this->createColor(51, 52, 53);
+        $color6 = $this->createColor(61, 62, 63);
+        $color7 = $this->createColor(71, 72, 73);
+        $color8 = $this->createColor(81, 82, 83);
+        $color9 = $this->createColor(91, 92, 93);
+
+        $image = $this->createMock(ImageInterface::class);
+        $tile = new Tile(5, 5, 10);
+        $tile->setOldImage($image);
+
+        $image
+            ->expects($this->exactly(9))
+            ->method($this->equalTo('getColorAt'))
+            ->will($this->onConsecutiveCalls(
+                $color1,
+                $color2,
+                $color3,
+                $color4,
+                $color5,
+                $color6,
+                $color7,
+                $color8,
+                $color9
+            ));
+
+        $point = new Point(5, 5);
+
+        $actualColor = AveragePipette::getColor($tile, $point);
+
+        $this->assertEquals($this->createColor(51, 52, 53), $actualColor);
+    }
+
+    protected function createColor(int $red, int $green, int $blue, int $alpha = 100): ColorInterface
+    {
+        return (new RGB())->color([$red, $green, $blue], $alpha);
     }
 }
