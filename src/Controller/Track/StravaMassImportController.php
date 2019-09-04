505df5587278ac48fd139bf9cab25bc2e77fba9f
Malte Hübner <maltehuebner@gmx.org>
2019-09-04 19:11:56 +0200

    Introduce routes, controller and templates.

---
+++ src/Controller/Track/StravaMassImportController.php
@@ -0,0 +1,114 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Track;
+
+use App\Controller\AbstractController;
+use App\Criticalmass\MassTrackImport\MassTrackImporterInterface;
+use App\Criticalmass\Router\ObjectRouterInterface;
+use App\Criticalmass\Strava\Importer\TrackImporterInterface;
+use App\Entity\Ride;
+use App\Event\Track\TrackUploadedEvent;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Strava\API\OAuth;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class StravaMassImportController extends AbstractController
+{
+    /**
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function authAction(Request $request, RouterInterface $router): Response
+    {
+        $oauth = $this->initOauthForRide($request, $router);
+
+        $authorizationOptions = [
+            'state' => '',
+            'approval_prompt' => 'force',
+            'scope' => 'public',
+        ];
+
+        $authorizationUrl = $oauth->getAuthorizationUrl($authorizationOptions);
+
+        return $this->render('TrackMassImport/auth.html.twig', [
+            'authorizationUrl' => $authorizationUrl,
+        ]);
+    }
+
+    /**
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function tokenAction(Request $request, SessionInterface $session, RouterInterface $router): Response
+    {
+        $error = $request->get('error');
+
+        if ($error) {
+            return $this->redirect($router->generate('caldera_criticalmass_trackmassimport_auth'));
+        }
+
+        $oauth = $this->initOauthForRide($request, $router);
+
+        try {
+            $token = $oauth->getAccessToken('authorization_code', [
+                'code' => $request->get('code')
+            ]);
+
+            $session->set('strava_token', $token);
+
+            return $this->redirect($router->generate('caldera_criticalmass_trackmassimport_list'));
+        } catch (\Exception $e) {
+            return $this->redirect($router->generate('caldera_criticalmass_trackmassimport_auth'));
+        }
+    }
+
+    /**
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function listridesAction(MassTrackImporterInterface $massTrackImporter): Response
+    {
+        $massTrackImporter
+            ->setStartDateTime(new \DateTime('2019-09-01 00:00:00'))
+            ->setEndDateTime(new \DateTime('2019-09-03 23:59:59'))
+            ->load();
+
+        return new Response();
+    }
+
+    /**
+     * @Security("has_role('ROLE_USER')")
+     * @ParamConverter("ride", class="App:Ride")
+     */
+    public function importAction(Request $request, UserInterface $user, EventDispatcherInterface $eventDispatcher, ObjectRouterInterface $objectRouter, Ride $ride, TrackImporterInterface $trackImporter): Response
+    {
+        $activityId = (int)$request->get('activityId');
+
+        $track = $trackImporter
+            ->setStravaActivityId($activityId)
+            ->setRide($ride)
+            ->setUser($user)
+            ->importTrack();
+
+        $eventDispatcher->dispatch(TrackUploadedEvent::NAME, new TrackUploadedEvent($track));
+
+        return $this->redirect($objectRouter->generate($track));
+    }
+
+    protected function initOauthForRide(Request $request, RouterInterface $router): OAuth
+    {
+        $redirectUri = $request->getUriForPath($router->generate('caldera_criticalmass_trackmassimport_token'));
+
+        $oauthOptions = [
+            'clientId' => $this->getParameter('strava.client_id'),
+            'clientSecret' => $this->getParameter('strava.secret'),
+            'redirectUri' => $redirectUri,
+            'scopes' => ['view_private'],
+        ];
+
+        return new OAuth($oauthOptions);
+    }
+}
