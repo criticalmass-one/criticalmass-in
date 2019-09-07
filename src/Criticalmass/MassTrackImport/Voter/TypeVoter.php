9ae8fe0e6f1b872c3dfcdf66f9c8db10e72e9e5a
Malte Hübner <maltehuebner@gmx.org>
2019-09-07 15:53:59 +0200

    Add TypeVoter and Test.

---
+++ src/Criticalmass/MassTrackImport/Voter/TypeVoter.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\MassTrackImport\Voter;
+
+use App\Criticalmass\MassTrackImport\Model\StravaActivityModel;
+use App\Entity\Ride;
+
+class TypeVoter implements VoterInterface
+{
+    public function vote(Ride $ride, StravaActivityModel $model): float
+    {
+        if ($model->getType() !== 'Ride') {
+            return -1;
+        }
+
+        return 0;
+    }
+}
