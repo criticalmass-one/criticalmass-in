798c453f3f182b11458d8921f934483f6275424c
Malte Hübner <maltehuebner@gmx.org>
2019-09-07 16:55:51 +0200

    Introduce ActivityLoader.

--- src/Criticalmass/MassTrackImport/MassTrackImporter.php
+++ src/Criticalmass/MassTrackImport/MassTrackImporter.php
@@ -2,58 +2,47 @@
 
 namespace App\Criticalmass\MassTrackImport;
 
+use App\Criticalmass\MassTrackImport\ActivityLoader\ActivityLoaderInterface;
 use App\Criticalmass\MassTrackImport\Converter\StravaActivityConverter;
 use App\Criticalmass\MassTrackImport\TrackDecider\TrackDeciderInterface;
 use JMS\Serializer\SerializerInterface;
-use Strava\API\Client;
-use Strava\API\Service\REST;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class MassTrackImporter implements MassTrackImporterInterface
 {
-    /** @var Client $client */
-    protected $client;
+    /** @var ActivityLoaderInterface $activityLoader */
+    protected $activityLoader;
 
     /** @var SerializerInterface $serializer */
     protected $serializer;
 
-    /** @var \DateTime $startDateTime */
-    protected $startDateTime;
-
-    /** @var \DateTime $endDateTime */
-    protected $endDateTime;
-
     /** @var TrackDeciderInterface $trackDecider */
     protected $trackDecider;
 
-    public function __construct(SessionInterface $session, SerializerInterface $serializer, TrackDeciderInterface $trackDecider)
+    public function __construct(SessionInterface $session, SerializerInterface $serializer, TrackDeciderInterface $trackDecider, ActivityLoaderInterface $activityLoader)
     {
         $this->serializer = $serializer;
         $this->trackDecider = $trackDecider;
-
-        $token = $session->get('strava_token');
-        $adapter = new \GuzzleHttp\Client(['base_uri' => 'https://www.strava.com/api/v3/']);
-        $service = new REST($token, $adapter);
-        $this->client = new Client($service);
+        $this->activityLoader = $activityLoader;
     }
 
     public function setStartDateTime(\DateTime $startDateTime): MassTrackImporterInterface
     {
-        $this->startDateTime = $startDateTime;
+        $this->activityLoader->setStartDateTime($startDateTime);
 
         return $this;
     }
 
     public function setEndDateTime(\DateTime $endDateTime): MassTrackImporterInterface
     {
-        $this->endDateTime = $endDateTime;
+        $this->activityLoader->setEndDateTime($endDateTime);
 
         return $this;
     }
 
     public function execute(): array
     {
-        $modelList = $this->client->getAthleteActivities($this->endDateTime->getTimestamp(), $this->startDateTime->getTimestamp());
+        $modelList = $this->activityLoader->load();
 
         foreach ($modelList as $key => $activityData) {
             $activity = StravaActivityConverter::convert($activityData);
