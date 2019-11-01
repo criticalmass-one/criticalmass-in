e991f111c1d568548b5e67c507b7d62433ed70ba
Malte Hübner <maltehuebner@gmx.org>
2019-11-01 08:04:15 +0100

    Implement RatingCalculator.

---
+++ src/Criticalmass/Rating/Calculator/RatingCalculator.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\Rating\Calculator;
+
+use App\Entity\Rating;
+use App\Entity\Ride;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class RatingCalculator implements RatingCalculatorInterface
+{
+    /** @var RegistryInterface $registry */
+    protected $registry;
+
+    public function __construct(RegistryInterface $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function calculateRide(Ride $ride): float
+    {
+        $ratings = $this->registry->getRepository(Rating::class)->findByRide($ride);
+
+        $ratingSum = 0;
+
+        /** @var Rating $rating */
+        foreach ($ratings as $rating) {
+            $ratingSum += $rating->getRating();
+        }
+
+        return round($ratingSum / count($ratings), 2);
+    }
+}
---
+++ src/Criticalmass/Rating/Calculator/RatingCalculatorInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\Rating\Calculator;
+
+use App\Entity\Ride;
+
+interface RatingCalculatorInterface
+{
+    public function calculateRide(Ride $ride): float;
+}
