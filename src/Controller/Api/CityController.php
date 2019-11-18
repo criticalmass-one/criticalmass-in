7ad4af7de7224ec0f00d743891f9c1be67cb651e
Malte Hübner <maltehuebner@gmx.org>
2019-11-18 09:16:13 +0100

    Short wip for city api.

--- src/Controller/Api/CityController.php
+++ src/Controller/Api/CityController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Criticalmass\DataQuery\DataQueryManager\DataQueryManagerInterface;
 use App\Entity\City;
 use App\Traits\RepositoryTrait;
 use App\Traits\UtilTrait;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CityController extends BaseController
@@ -21,12 +23,22 @@ class CityController extends BaseController
      * @ApiDoc(
      *  resource=true,
      *  description="Returns a list of critical mass cities",
-     *  section="City"
+     *  section="City",
+     *  parameters={
+     *     {"name"="region", "dataType"="string", "required"=false, "description"="Provide a region slug"},
+     *     {"name"="centerLatitude", "dataType"="float", "required"=false, "description"="Latitude of a coordinate to search rides around in a given radius."},
+     *     {"name"="centerLongitude", "dataType"="float", "required"=false, "description"="Longitude of a coordinate to search rides around in a given radius."},
+     *     {"name"="radius", "dataType"="float", "required"=false, "description"="Radius to look around for rides."},
+     *     {"name"="bbEastLongitude", "dataType"="float", "required"=false, "description"="East longitude of a bounding box to look for rides."},
+     *     {"name"="bbWestLongitude", "dataType"="float", "required"=false, "description"="West longitude of a bounding box to look for rides."},
+     *     {"name"="bbNorthLatitude", "dataType"="float", "required"=false, "description"="North latitude of a bounding box to look for rides."},
+     *     {"name"="bbSouthLatitude", "dataType"="float", "required"=false, "description"="South latitude of a bounding box to look for rides."}
+     *   }
      * )
      */
-    public function listAction(): Response
+    public function listAction(Request $request, DataQueryManagerInterface $dataQueryManager): Response
     {
-        $cityList = $this->getCityRepository()->findEnabledCities();
+        $cityList = $dataQueryManager->queryForRequest($request, City::class);
 
         $view = View::create();
         $view
