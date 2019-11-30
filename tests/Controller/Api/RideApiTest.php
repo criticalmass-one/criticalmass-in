ec31d15f0fe6087414f9a8e9c4e873b0040e9178
Malte Hübner <maltehuebner@gmx.org>
2019-11-30 12:00:35 +0100

    WIP for ride list.

--- tests/Controller/Api/RideApiTest.php
+++ tests/Controller/Api/RideApiTest.php
@@ -67,13 +67,61 @@ class RideApiTest extends AbstractApiControllerTest
         $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
     }
 
-    public function testRideList(): void
+    public function testRideListWithoutParameters(): void
     {
         $client = static::createClient();
 
         $client->request('GET', '/api/ride');
 
-        $expectedContent = '[{"id":8,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 24.09.2050","description":null,"dateTime":2547658800,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":9,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":"kidical-mass-hamburg-2035","title":"Critical Mass 24.06.2035","description":null,"dateTime":2066324400,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":7,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 30.06.2011","description":null,"dateTime":1309460400,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":6,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 29.06.2011","description":null,"dateTime":1309374000,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":5,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 28.06.2011","description":null,"dateTime":1309287600,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":4,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 27.06.2011","description":null,"dateTime":1309201200,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":3,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 26.06.2011","description":null,"dateTime":1309114800,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":2,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 25.06.2011","description":null,"dateTime":1309028400,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null},{"id":1,"cycle":null,"city":{"id":7,"mainSlug":{"id":7,"slug":"hamburg"},"name":"Hamburg","title":"Critical Mass Hamburg","description":null,"url":null,"facebook":null,"twitter":null,"latitude":0,"longitude":0,"slugs":[{"id":7,"slug":"hamburg"}],"timezone":"Europe\/Berlin"},"slug":null,"title":"Critical Mass 24.06.2011","description":null,"dateTime":1308942000,"location":null,"latitude":null,"longitude":null,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null,"facebook":null,"twitter":null,"url":null}]';
+        $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 01.02.2015","description":null,"dateTime":1422817200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.12.2015","description":null,"dateTime":1448996400,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.04.2016","description":null,"dateTime":1459537200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.05.2016","description":null,"dateTime":1462129200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.08.2016","description":null,"dateTime":1470078000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.01.2017","description":null,"dateTime":1483297200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.10.2017","description":null,"dateTime":1506884400,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.12.2017","description":null,"dateTime":1512154800,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.01.2018","description":null,"dateTime":1514833200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.03.2018","description":null,"dateTime":1519930800,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+    }
+
+    public function testRideListWithYear2050Parameter(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?year=2050');
+
+        $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 24.09.2050","description":null,"dateTime":2547658800,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+    }
+
+    public function testRideListWithYear2016Month2Parameter(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?year=2016&month=2');
+
+        $expectedContent = '[{"slug":null,"title":"Critical Mass London 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Berlin 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Mainz 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Halle 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+    }
+
+    public function testRideListWithYear2015Month6Day1Parameter(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?year=2015&month=6&day=1');
+
+        $expectedContent = '[{"slug":null,"title":"Critical Mass London 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Mainz 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Halle 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Berlin 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+    }
+
+    public function testRideListWithYear2011Month6Day24Parameter(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?year=2011&month=6&day=24');
+
+        $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 24.06.2011","description":null,"dateTime":1308942000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
