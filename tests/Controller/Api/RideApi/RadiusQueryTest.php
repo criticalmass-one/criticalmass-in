ad5658bec0269f9b7c7628227974f38247cfb27e
Malte Hübner <maltehuebner@gmx.org>
2019-12-01 23:25:28 +0100

    Fix all the radius query.

--- tests/Controller/Api/RideApi/RadiusQueryTest.php
+++ tests/Controller/Api/RideApi/RadiusQueryTest.php
@@ -2,21 +2,50 @@
 
 namespace Tests\Controller\Api\RideApi;
 
+use App\Entity\Ride;
 use Tests\Controller\Api\AbstractApiControllerTest;
 
 class RadiusQueryTest extends AbstractApiControllerTest
 {
-    public function testRideListWithRadiusQuery(): void
+    public function testRideListWithRadiusQueryAroundKielWithin10Kilometers(): void
     {
-        $this->markTestSkipped('This is still broken.');
-
         $client = static::createClient();
 
         $client->request('GET', '/api/ride?centerLatitude=54.343024&centerLongitude=10.129730&radius=10');
 
-        $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 24.09.2050","description":null,"dateTime":2547658800,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":"kidical-mass-hamburg-2035","title":"Critical Mass Hamburg 24.06.2035","description":null,"dateTime":2066324400,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Mainz 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Berlin 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Halle 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass London 01.12.2029","description":null,"dateTime":1890846000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass London 01.11.2029","description":null,"dateTime":1888254000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.11.2029","description":null,"dateTime":1888254000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+        $actualRideList = $this->deserializeEntityList($client->getResponse()->getContent(), Ride::class);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+        $this->assertCount(0, $actualRideList);
+    }
+
+    public function testRideListWithRadiusQueryAroundKielWithin200Kilometers(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?centerLatitude=54.343024&centerLongitude=10.129730&radius=200');
+
+        $actualRideList = $this->deserializeEntityList($client->getResponse()->getContent(), Ride::class);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(10, $actualRideList);
+    }
+
+    public function testRideListWithRadiusQueryAroundHamburg(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/ride?centerLatitude=53.550823&centerLongitude=9.993163&radius=10');
+
+        $actualRideList = $this->deserializeEntityList($client->getResponse()->getContent(), Ride::class);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(10, $actualRideList);
+
+        /** @var Ride $actualRide */
+        foreach ($actualRideList as $actualRide) {
+            $this->assertEquals(53.566676, $actualRide->getLatitude());
+            $this->assertEquals(9.984711, $actualRide->getLongitude());
+        }
     }
 }
