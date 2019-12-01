86df2f98033bb5ec3b9a349e15b134ca6cd4ab96
Malte Hübner <maltehuebner@gmx.org>
2019-12-01 22:32:59 +0100

    Simplify DateTimeTest.

--- tests/Controller/Api/RideApi/DateTimeTest.php
+++ tests/Controller/Api/RideApi/DateTimeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Controller\Api\RideApi;
 
+use App\Entity\Ride;
 use Tests\Controller\Api\AbstractApiControllerTest;
 
 class DateTimeTest extends AbstractApiControllerTest
@@ -14,8 +15,14 @@ class DateTimeTest extends AbstractApiControllerTest
 
         $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 24.09.2050","description":null,"dateTime":2547658800,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
 
+        $actualRideList = ($this->deserializeEntityList($client->getResponse()->getContent(), Ride::class));
+
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+
+        /** @var Ride $ride */
+        foreach ($actualRideList as $ride) {
+            $this->assertEquals(2050, $ride->getDateTime()->format('Y'));
+        }
     }
 
     public function testRideListWithYear2016Month2Parameter(): void
@@ -24,10 +31,14 @@ class DateTimeTest extends AbstractApiControllerTest
 
         $client->request('GET', '/api/ride?year=2016&month=2');
 
-        $expectedContent = '[{"slug":null,"title":"Critical Mass London 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Berlin 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Mainz 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Halle 01.02.2016","description":null,"dateTime":1454353200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+        $actualRideList = ($this->deserializeEntityList($client->getResponse()->getContent(), Ride::class));
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+
+        /** @var Ride $ride */
+        foreach ($actualRideList as $ride) {
+            $this->assertEquals('2016-02', $ride->getDateTime()->format('Y-m'));
+        }
     }
 
     public function testRideListWithYear2015Month6Day1Parameter(): void
@@ -36,10 +47,14 @@ class DateTimeTest extends AbstractApiControllerTest
 
         $client->request('GET', '/api/ride?year=2015&month=6&day=1');
 
-        $expectedContent = '[{"slug":null,"title":"Critical Mass London 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Mainz 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Hamburg 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Halle 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Berlin 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null},{"slug":null,"title":"Critical Mass Esslingen 01.06.2015","description":null,"dateTime":1433185200,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+        $actualRideList = ($this->deserializeEntityList($client->getResponse()->getContent(), Ride::class));
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+
+        /** @var Ride $ride */
+        foreach ($actualRideList as $ride) {
+            $this->assertEquals('2015-06-01', $ride->getDateTime()->format('Y-m-d'));
+        }
     }
 
     public function testRideListWithYear2011Month6Day24Parameter(): void
@@ -48,9 +63,13 @@ class DateTimeTest extends AbstractApiControllerTest
 
         $client->request('GET', '/api/ride?year=2011&month=6&day=24');
 
-        $expectedContent = '[{"slug":null,"title":"Critical Mass Hamburg 24.06.2011","description":null,"dateTime":1308942000,"location":null,"latitude":53.5,"longitude":10.5,"estimatedParticipants":null,"estimatedDistance":null,"estimatedDuration":null}]';
+        $actualRideList = ($this->deserializeEntityList($client->getResponse()->getContent(), Ride::class));
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertIdLessJsonEquals($expectedContent, $client->getResponse()->getContent());
+
+        /** @var Ride $ride */
+        foreach ($actualRideList as $ride) {
+            $this->assertEquals('2011-06-24', $ride->getDateTime()->format('Y-m-d'));
+        }
     }
 }
