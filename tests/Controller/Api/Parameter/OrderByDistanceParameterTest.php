2f3633ce1022f2f110761b698339677e91aa2520
Malte Hübner <maltehuebner@gmx.org>
2019-12-10 18:05:20 +0100

    Fix distance issues.

--- tests/Controller/Api/Parameter/OrderByDistanceParameterTest.php
+++ tests/Controller/Api/Parameter/OrderByDistanceParameterTest.php
@@ -7,6 +7,7 @@ use App\Entity\City;
 use App\Entity\Photo;
 use App\Entity\Ride;
 use App\EntityInterface\CoordinateInterface;
+use Caldera\GeoBasic\Coord\CoordInterface;
 use Tests\Controller\Api\AbstractApiControllerTest;
 use Tests\Coords;
 
@@ -15,13 +16,11 @@ class OrderByDistanceParameterTest extends AbstractApiControllerTest
     /**
      * @dataProvider apiClassProvider
      */
-    public function testResultListOrderByAscending(string $fqcn): void
+    public function testResultListOrderByAscending(string $fqcn, CoordInterface $centerCoord): void
     {
         $client = static::createClient();
 
-        $hamburg = Coords::hamburg();
-
-        $uri = sprintf('%s?centerLatitude=%f&centerLongitude=%f&distanceOrderDirection=ASC', $this->getApiEndpointForFqcn($fqcn), $hamburg->getLatitude(), $hamburg->getLongitude());
+        $uri = sprintf('%s?centerLatitude=%f&centerLongitude=%f&distanceOrderDirection=ASC', $this->getApiEndpointForFqcn($fqcn), $centerCoord->getLatitude(), $centerCoord->getLongitude());
 
         $client->request('GET', $uri);
 
@@ -34,7 +33,7 @@ class OrderByDistanceParameterTest extends AbstractApiControllerTest
 
         /** @var CoordinateInterface $result */
         foreach ($resultList as $result) {
-            $distance = DistanceCalculator::calculateDistance($hamburg, $result->toCoord());
+            $distance = DistanceCalculator::calculateDistance($centerCoord, $result->toCoord());
 
             if ($minDistance) {
                 $this->assertGreaterThanOrEqual($minDistance, $distance);
@@ -47,13 +46,11 @@ class OrderByDistanceParameterTest extends AbstractApiControllerTest
     /**
      * @dataProvider apiClassProvider
      */
-    public function testResultListOrderByDescending(string $fqcn): void
+    public function testResultListOrderByDescending(string $fqcn, CoordInterface $centerCoord): void
     {
         $client = static::createClient();
 
-        $hamburg = Coords::hamburg();
-
-        $uri = sprintf('%s?centerLatitude=%f&centerLongitude=%f&distanceOrderDirection=DESC', $this->getApiEndpointForFqcn($fqcn), $hamburg->getLatitude(), $hamburg->getLongitude());
+        $uri = sprintf('%s?centerLatitude=%f&centerLongitude=%f&distanceOrderDirection=DESC', $this->getApiEndpointForFqcn($fqcn), $centerCoord->getLatitude(), $centerCoord->getLongitude());
 
         $client->request('GET', $uri);
 
@@ -66,7 +63,7 @@ class OrderByDistanceParameterTest extends AbstractApiControllerTest
 
         /** @var CoordinateInterface $result */
         foreach ($resultList as $result) {
-            $distance = DistanceCalculator::calculateDistance($hamburg, $result->toCoord());
+            $distance = DistanceCalculator::calculateDistance($centerCoord, $result->toCoord());
 
             if ($maxDistance) {
                 $this->assertLessThanOrEqual($maxDistance, $distance);
@@ -79,9 +76,9 @@ class OrderByDistanceParameterTest extends AbstractApiControllerTest
     public function apiClassProvider(): array
     {
         return [
-            [City::class,],
-            [Ride::class,],
-            [Photo::class,],
+            [City::class, Coords::esslingen()],
+            [Ride::class, Coords::hamburg()],
+            [Photo::class, Coords::berlin()],
         ];
     }
 }
