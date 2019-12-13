7c72db92d3b5bf5a00c0a8bef19475a1bd90aac2
Malte Hübner <maltehuebner@gmx.org>
2019-12-14 00:19:40 +0100

    Much WIP for map.

--- assets/js/modules/map/AutoMap.js
+++ assets/js/modules/map/AutoMap.js
@@ -1,142 +1,145 @@
-define(['Polyline.encoded', 'leaflet.extra-markers', 'Container', 'jquery', 'Map'], function () {
-    AutoMap = function (mapId, settings) {
-        this._mapId = mapId;
+import Map from './Map';
+import PolylineEncoded from 'polyline-encoded';
+import LeafletExtraMarkers from 'leaflet-extra-markers';
 
-        this.settings = $.extend(this._defaults, settings);
+let AutoMap = function (mapId, settings) {
+    alert('fooo');
+    this._mapId = mapId;
 
-        this._init();
-    };
+    this.settings = $.extend(this._defaults, settings);
 
-    AutoMap.prototype._defaults = {
-        tileLayerUrl: 'https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png',
-        mapAttribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-        detectRetina: true,
-        defaultLatitude: 51.0851708,
-        defaultLongitude: 5.9692092,
-        defaultZoom: 5,
-        showZoomControl: false,
-        zoomControlPosition: 'bottomright'
-    };
+    this._init();
+};
 
-    // do not call Map constructor directly as this will execute the map
-    AutoMap.prototype = Object.create(Map.prototype);
-    AutoMap.prototype.constructor = AutoMap;
+AutoMap.prototype._defaults = {
+    tileLayerUrl: 'https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png',
+    mapAttribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    detectRetina: true,
+    defaultLatitude: 51.0851708,
+    defaultLongitude: 5.9692092,
+    defaultZoom: 5,
+    showZoomControl: false,
+    zoomControlPosition: 'bottomright'
+};
 
-    AutoMap.prototype._mapViewSet = false;
+// do not call Map constructor directly as this will execute the map
+AutoMap.prototype = Object.create(Map.prototype);
+AutoMap.prototype.constructor = AutoMap;
 
-    AutoMap.prototype._init = function () {
-        this._initMap();
-        this._addTileLayer();
-        this._autoSettings();
-    };
+AutoMap.prototype._mapViewSet = false;
 
-    AutoMap.prototype._autoSettings = function () {
-        this._autoSetView();
-        this._autoSetPolyline();
-        this._autoSetMarker();
-        this._autoSetPolylineMarker();
-        this._setLockMap();
-    };
+AutoMap.prototype._init = function () {
+    this._initMap();
+    this._addTileLayer();
+    this._autoSettings();
+};
 
-    AutoMap.prototype._autoSetView = function () {
-        var latitude = this._$mapContainer.data('map-center-latitude');
-        var longitude = this._$mapContainer.data('map-center-longitude');
-        var zoomLevel = this._$mapContainer.data('map-zoomlevel');
+AutoMap.prototype._autoSettings = function () {
+    this._autoSetView();
+    this._autoSetPolyline();
+    this._autoSetMarker();
+    this._autoSetPolylineMarker();
+    this._setLockMap();
+};
 
-        if (latitude && longitude && zoomLevel) {
-            this.map.setView([latitude, longitude], zoomLevel);
-            this._mapViewSet = true;
+AutoMap.prototype._autoSetView = function () {
+    var latitude = this._$mapContainer.data('map-center-latitude');
+    var longitude = this._$mapContainer.data('map-center-longitude');
+    var zoomLevel = this._$mapContainer.data('map-zoomlevel');
+
+    if (latitude && longitude && zoomLevel) {
+        this.map.setView([latitude, longitude], zoomLevel);
+        this._mapViewSet = true;
+    }
+};
+
+AutoMap.prototype._autoSetPolyline = function () {
+    var polylineString = this._$mapContainer.data('polyline');
+    var polylineColorString = this._$mapContainer.data('polyline-color');
+
+    if (polylineString && polylineColorString) {
+        var polyline = L.Polyline.fromEncoded(polylineString, {color: polylineColorString});
+
+        polyline.addTo(this.map);
+
+        console.log(polyline);
+
+        if (!this._mapViewSet) {
+            this.map.fitBounds(polyline.getBounds());
         }
-    };
+    }
+};
 
-    AutoMap.prototype._autoSetPolyline = function () {
-        var polylineString = this._$mapContainer.data('polyline');
-        var polylineColorString = this._$mapContainer.data('polyline-color');
+AutoMap.prototype._autoSetMarker = function () {
+    var latitude = this._$mapContainer.data('map-marker-latitude');
+    var longitude = this._$mapContainer.data('map-marker-longitude');
 
-        if (polylineString && polylineColorString) {
-            var polyline = L.Polyline.fromEncoded(polylineString, {color: polylineColorString});
+    var markerColor = this._$mapContainer.data('map-marker-color') || 'yellow';
+    var markerShape = this._$mapContainer.data('map-marker-shape') || 'square';
+    var markerIcon = this._$mapContainer.data('map-marker-icon') || 'fa-bicycle';
+    var markerClickable = this._$mapContainer.data('map-marker-clickable') || false;
 
-            polyline.addTo(this.map);
+    if (latitude && longitude && markerColor && markerShape && markerIcon) {
+        var extraMarkerIcon = L.ExtraMarkers.icon({
+            icon: markerIcon,
+            markerColor: markerColor,
+            shape: markerShape,
+            prefix: 'far'
+        });
 
-            console.log(polyline);
+        var marker = L.marker([latitude, longitude], {icon: extraMarkerIcon, clickable: markerClickable});
+        marker.addTo(this.map);
+    }
+};
 
-            if (!this._mapViewSet) {
-                this.map.fitBounds(polyline.getBounds());
-            }
-        }
-    };
+AutoMap.prototype._autoSetPolylineMarker = function () {
+    var polyline = this._$mapContainer.data('map-markers-polyline-list');
 
-    AutoMap.prototype._autoSetMarker = function () {
-        var latitude = this._$mapContainer.data('map-marker-latitude');
-        var longitude = this._$mapContainer.data('map-marker-longitude');
+    var markerColor = this._$mapContainer.data('map-marker-color') || 'yellow';
+    var markerShape = this._$mapContainer.data('map-marker-shape') || 'square';
+    var markerIcon = this._$mapContainer.data('map-marker-icon') || 'fa-bicycle';
+    var markerClickable = this._$mapContainer.data('map-marker-clickable') || false;
 
-        var markerColor = this._$mapContainer.data('map-marker-color') || 'yellow';
-        var markerShape = this._$mapContainer.data('map-marker-shape') || 'square';
-        var markerIcon = this._$mapContainer.data('map-marker-icon') || 'fa-bicycle';
-        var markerClickable = this._$mapContainer.data('map-marker-clickable') || false;
+    if (polyline && markerColor && markerShape && markerIcon) {
+        var markerLatLngs = L.PolylineUtil.decode(polyline);
 
-        if (latitude && longitude && markerColor && markerShape && markerIcon) {
+        var markerList = [];
+
+        for (var index in markerLatLngs) {
+            var latLng = markerLatLngs[index];
+
             var extraMarkerIcon = L.ExtraMarkers.icon({
-                icon: markerIcon,
-                markerColor: markerColor,
-                shape: markerShape,
+                icon: 'fa-bicycle',
+                markerColor: 'yellow',
+                shape: 'square',
                 prefix: 'far'
             });
 
-            var marker = L.marker([latitude, longitude], {icon: extraMarkerIcon, clickable: markerClickable});
-            marker.addTo(this.map);
+            var marker = L.marker(latLng, {icon: extraMarkerIcon, clickable: markerClickable});
+
+            markerList.push(marker);
         }
-    };
 
-    AutoMap.prototype._autoSetPolylineMarker = function () {
-        var polyline = this._$mapContainer.data('map-markers-polyline-list');
+        var markerGroup = new L.featureGroup(markerList);
 
-        var markerColor = this._$mapContainer.data('map-marker-color') || 'yellow';
-        var markerShape = this._$mapContainer.data('map-marker-shape') || 'square';
-        var markerIcon = this._$mapContainer.data('map-marker-icon') || 'fa-bicycle';
-        var markerClickable = this._$mapContainer.data('map-marker-clickable') || false;
+        markerGroup.addTo(this.map);
 
-        if (polyline && markerColor && markerShape && markerIcon) {
-            var markerLatLngs = L.PolylineUtil.decode(polyline);
+        if (!this._mapViewSet && markerLatLngs.length > 1) {
+            this.map.fitBounds(markerGroup.getBounds());
+        } else if (!this._mapViewSet) {
+            var latLng = markerLatLngs.pop();
 
-            var markerList = [];
-
-            for (var index in markerLatLngs) {
-                var latLng = markerLatLngs[index];
-
-                var extraMarkerIcon = L.ExtraMarkers.icon({
-                    icon: 'fa-bicycle',
-                    markerColor: 'yellow',
-                    shape: 'square',
-                    prefix: 'far'
-                });
-
-                var marker = L.marker(latLng, {icon: extraMarkerIcon, clickable: markerClickable});
-
-                markerList.push(marker);
-            }
-
-            var markerGroup = new L.featureGroup(markerList);
-
-            markerGroup.addTo(this.map);
-
-            if (!this._mapViewSet && markerLatLngs.length > 1) {
-                this.map.fitBounds(markerGroup.getBounds());
-            } else if (!this._mapViewSet) {
-                var latLng = markerLatLngs.pop();
-
-                this.setView(latLng, 13);
-            }
+            this.setView(latLng, 13);
         }
-    };
+    }
+};
 
-    AutoMap.prototype._setLockMap = function () {
-        var lockMap = this._$mapContainer.data('lock-map');
+AutoMap.prototype._setLockMap = function () {
+    var lockMap = this._$mapContainer.data('lock-map');
 
-        if (lockMap == true) {
-            this.disableInteraction();
-        }
-    };
+    if (lockMap == true) {
+        this.disableInteraction();
+    }
+};
 
-    return AutoMap;
-});
+export default AutoMap;
--- assets/js/modules/map/Map.js
+++ assets/js/modules/map/Map.js
@@ -1,125 +1,124 @@
-//define(['leaflet', 'CityMarker', 'LocationMarker', 'L.Control.Locate', 'Leaflet.Sleep'], function () {
-define(['leaflet', 'CityMarker', 'LocationMarker'], function () {
-    Map = function (mapId, settings) {
-        this._mapId = mapId;
+import L from 'leaflet';
 
-        this.settings = $.extend(this._defaults, settings);
+Map = function (mapId, settings) {
+    this._mapId = mapId;
 
-        this._init();
-    };
+    this.settings = $.extend(this._defaults, settings);
 
-    Map.prototype._defaults = {
-        tileLayerUrl: 'https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png',
-        mapAttribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-        detectRetina: true,
-        defaultLatitude: 51.0851708,
-        defaultLongitude: 5.9692092,
-        defaultZoom: 5,
-        showZoomControl: true,
-        zoomControlPosition: 'bottomright'
-    };
+    this._init();
+};
 
-    Map.prototype._$mapContainer = null;
+Map.prototype._defaults = {
+    tileLayerUrl: 'https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png',
+    mapAttribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    detectRetina: true,
+    defaultLatitude: 51.0851708,
+    defaultLongitude: 5.9692092,
+    defaultZoom: 5,
+    showZoomControl: true,
+    zoomControlPosition: 'bottomright'
+};
 
-    Map.prototype._init = function () {
-        this._initMap();
-        this._addTileLayer();
-    };
+Map.prototype._$mapContainer = null;
 
-    Map.prototype._initMap = function () {
-        this._$mapContainer = $('#' + this._mapId);
+Map.prototype._init = function () {
+    this._initMap();
+    this._addTileLayer();
+};
 
-        this.map = L.map(this._mapId,
-            {
-                zoomControl: false,
-                sleepNote: false
-            }
-        );
+Map.prototype._initMap = function () {
+    this._$mapContainer = $('#' + this._mapId);
 
-        if (this.settings.showZoomControl) {
-            this.addZoomControl(this.settings.zoomControlPosition);
+    this.map = L.map(this._mapId,
+        {
+            zoomControl: false,
+            sleepNote: false
         }
-    };
+    );
 
-    Map.prototype.addZoomControl = function (zoomControlPosition) {
-        var zoomControl = new L.Control.Zoom(
-            {
-                position: zoomControlPosition
-            }
-        );
+    if (this.settings.showZoomControl) {
+        this.addZoomControl(this.settings.zoomControlPosition);
+    }
+};
 
-        zoomControl.addTo(this.map);
-    };
-
-    Map.prototype._addTileLayer = function () {
-
-        L.tileLayer(this.settings.tileLayerUrl, {
-            attribution: this.settings.mapAttribution,
-            detectRetina: this.settings.detectRetina
-        }).addTo(this.map);
-    };
-
-    Map.prototype.addLayer = function (layer) {
-        this.map.addLayer(layer);
-    };
-
-    Map.prototype.setView = function (latLng, zoom) {
-        this.map.setView(latLng, zoom);
-    };
-
-    Map.prototype.getCenter = function () {
-        return this.map.getCenter();
-    };
-
-    Map.prototype.fitBounds = function (bounds, options) {
-        this.map.fitBounds(bounds, options);
-    };
-
-    Map.prototype.getBounds = function () {
-        return this.map.getBounds();
-    };
-
-    Map.prototype.on = function (event, callback) {
-        this.map.on(event, callback);
-    };
-
-    Map.prototype.disableInteraction = function () {
-        this._$mapContainer.find('.leaflet-control-zoom').hide();
-        this._$mapContainer.css('cursor', 'default');
-        this.map.dragging.disable();
-        this.map.touchZoom.disable();
-        this.map.doubleClickZoom.disable();
-        this.map.scrollWheelZoom.disable();
-        this.map.boxZoom.disable();
-        this.map.keyboard.disable();
-
-        if (this.map.tap) {
-            this.map.tap.disable();
+Map.prototype.addZoomControl = function (zoomControlPosition) {
+    var zoomControl = new L.Control.Zoom(
+        {
+            position: zoomControlPosition
         }
-    };
+    );
 
-    Map.prototype.enableInteraction = function () {
-        this._$mapContainer.find('.leaflet-control-zoom').show();
-        this._$mapContainer.css('cursor', 'move');
-        this.map.dragging.enable();
-        this.map.touchZoom.enable();
-        this.map.doubleClickZoom.enable();
-        this.map.scrollWheelZoom.enable();
-        this.map.boxZoom.enable();
-        this.map.keyboard.enable();
+    zoomControl.addTo(this.map);
+};
 
-        if (this.map.tap) {
-            this.map.tap.enable();
-        }
-    };
+Map.prototype._addTileLayer = function () {
 
-    Map.prototype.setMaxZoom = function (maxZoom) {
-        this.map.setMaxZoom(maxZoom);
-    };
+    L.tileLayer(this.settings.tileLayerUrl, {
+        attribution: this.settings.mapAttribution,
+        detectRetina: this.settings.detectRetina
+    }).addTo(this.map);
+};
 
-    Map.prototype.setMinZoom = function (minZoom) {
-        this.map.setMinZoom(minZoom);
-    };
+Map.prototype.addLayer = function (layer) {
+    this.map.addLayer(layer);
+};
 
-    return Map;
-});
+Map.prototype.setView = function (latLng, zoom) {
+    this.map.setView(latLng, zoom);
+};
+
+Map.prototype.getCenter = function () {
+    return this.map.getCenter();
+};
+
+Map.prototype.fitBounds = function (bounds, options) {
+    this.map.fitBounds(bounds, options);
+};
+
+Map.prototype.getBounds = function () {
+    return this.map.getBounds();
+};
+
+Map.prototype.on = function (event, callback) {
+    this.map.on(event, callback);
+};
+
+Map.prototype.disableInteraction = function () {
+    this._$mapContainer.find('.leaflet-control-zoom').hide();
+    this._$mapContainer.css('cursor', 'default');
+    this.map.dragging.disable();
+    this.map.touchZoom.disable();
+    this.map.doubleClickZoom.disable();
+    this.map.scrollWheelZoom.disable();
+    this.map.boxZoom.disable();
+    this.map.keyboard.disable();
+
+    if (this.map.tap) {
+        this.map.tap.disable();
+    }
+};
+
+Map.prototype.enableInteraction = function () {
+    this._$mapContainer.find('.leaflet-control-zoom').show();
+    this._$mapContainer.css('cursor', 'move');
+    this.map.dragging.enable();
+    this.map.touchZoom.enable();
+    this.map.doubleClickZoom.enable();
+    this.map.scrollWheelZoom.enable();
+    this.map.boxZoom.enable();
+    this.map.keyboard.enable();
+
+    if (this.map.tap) {
+        this.map.tap.enable();
+    }
+};
+
+Map.prototype.setMaxZoom = function (maxZoom) {
+    this.map.setMaxZoom(maxZoom);
+};
+
+Map.prototype.setMinZoom = function (minZoom) {
+    this.map.setMinZoom(minZoom);
+};
+
+export default Map;
