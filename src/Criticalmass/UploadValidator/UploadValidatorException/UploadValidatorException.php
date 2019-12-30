a54baceb8485b59345dd20544673030758aaa390
Malte Hübner <maltehuebner@gmx.org>
2019-12-30 21:10:06 +0100

    And fix also this occurence as well foo bar baz lalala.

--- src/Criticalmass/UploadValidator/UploadValidatorException/UploadValidatorException.php
+++ src/Criticalmass/UploadValidator/UploadValidatorException/UploadValidatorException.php
@@ -1,6 +1,6 @@
-<?php
+<?php declare(strict_types=1);
 
-namespace App\Criticalmass\UploadValidator\UploadValidatorException\TrackValidatorException;
+namespace App\Criticalmass\UploadValidator\UploadValidatorException;
 
 use Exception;
 
