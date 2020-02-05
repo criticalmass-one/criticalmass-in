10d10177ae3ffd13b2b9720b7c868291a8978736
Malte Hübner <maltehuebner@gmx.org>
2020-02-05 07:33:17 +0100

    Add test.

--- tests/CityCycle/Analyzer/AnalyzerTest.php
+++ tests/CityCycle/Analyzer/AnalyzerTest.php
@@ -63,4 +63,50 @@ class AnalyzerTest extends TestCase
 
         $this->assertEmpty($resultList);
     }
+
+    public function testAnalyzerWithRides(): void
+    {
+        $repositoryList = [];
+
+        $cityCycleRepository = $this->createMock(CityCycleRepository::class);
+        $cityCycleRepository
+            ->method($this->equalTo('findByCity'))
+            ->will($this->returnValue([]));
+
+        $repositoryList[CityCycle::class] = $cityCycleRepository;
+
+        $rideRepository = $this->createMock(RideRepository::class);
+        $rideRepository
+            ->method($this->equalTo('findRides'))
+            ->will($this->returnValue([]));
+
+        $repositoryList[Ride::class] = $rideRepository;
+
+        $registry = $this->createMock(RegistryInterface::class);
+
+        $registry
+            ->method('getRepository')
+            ->will($this->returnCallback(function (string $entityFqcn) use ($repositoryList) {
+                return $repositoryList[$entityFqcn];
+            }));
+
+        $rideGenerator = $this->createMock(CityRideGeneratorInterface::class);
+        $rideNamerList = new RideNamerList();
+        $rideNamerList->addRideNamer(new GermanCityDateRideNamer());
+        $rideCalculator = new RideCalculator($rideNamerList);
+        $analyzerModelFactory = new CycleAnalyzerModelFactory();
+
+        $cycleAnalyzer = new CycleAnalyzer($registry, $rideGenerator, $rideCalculator, $analyzerModelFactory);
+
+        $hamburg = new City();
+        $hamburg->setCity('Hamburg');
+
+        $resultList = $cycleAnalyzer->setCity($hamburg)
+            ->setStartDateTime(new \DateTime('2019-01-01'))
+            ->setEndDateTime(new \DateTime('2019-01-31'))
+            ->analyze()
+            ->getResultList();
+
+        $this->assertEmpty($resultList);
+    }
 }
