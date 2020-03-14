49ef6408b92c24c66e49cc157d9b4fad3502d95a
Malte Hübner <maltehuebner@gmx.org>
2020-03-14 11:22:07 +0100

    Add admin class and config.

---
+++ src/Admin/AlertAdmin.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AlertAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $formMapper
+            ->with('Inhalt', ['class' => 'col-md-6'])
+            ->add('title', TextType::class, ['required' => true])
+            ->add('message', TextareaType::class, ['required' => true])
+            ->end()
+            ->with('Zeitraum', ['class' => 'col-md-6'])
+            ->add('fromDateTime', DateTimeType::class, ['widget' => 'single_text', 'required' => false])
+            ->add('untilDateTime', DateTimeType::class, ['widget' => 'single_text', 'required' => false])
+            ->end();
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('message')
+            ->add('fromDateTime')
+            ->add('untilDateTime');
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('message')
+            ->add('fromDateTime')
+            ->add('untilDateTime');
+    }
+}
