19e1dd8946aeb0b93324b7eee8d14a5198cfabf6
Malte Hübner <maltehuebner@gmx.org>
2020-03-15 15:48:34 +0100

    More fixes for Ride Admin.

--- src/Admin/RideAdmin.php
+++ src/Admin/RideAdmin.php
@@ -7,6 +7,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -24,13 +25,13 @@ class RideAdmin extends AbstractAdmin
         $formMapper
             ->with('Details', ['class' => 'col-md-6'])
             ->add('city')
-            ->add('slug')
-            ->add('title')
-            ->add('description')
+            ->add('slug', TextType::class, ['required' => false])
+            ->add('title', TextType::class, ['required' => true])
+            ->add('description', TextareaType::class, ['required' => false, 'attr' => ['rows' => 3]])
             ->end()
             ->with('Einstellungen', ['class' => 'col-md-6'])
-            ->add('restrictedPhotoAccess')
-            ->add('enabled')
+            ->add('restrictedPhotoAccess', CheckboxType::class, ['required' => false])
+            ->add('enabled', CheckboxType::class, ['required' => false])
             ->add('disabledReason', ChoiceType::class, [
                 'choices' => $disabledReasonList,
                 'required' => 'false',
