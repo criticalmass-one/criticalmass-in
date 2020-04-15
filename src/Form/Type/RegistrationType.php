fc0078d5ba4409ca52e227b23459915b5bc48af6
Malte Hübner <maltehuebner@gmx.org>
2020-04-16 01:01:04 +0200

    Show captcha in registration form.

--- src/Form/Type/RegistrationType.php
+++ src/Form/Type/RegistrationType.php
@@ -3,8 +3,15 @@
 namespace App\Form\Type;
 
 use FOS\UserBundle\Form\Type\RegistrationFormType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends RegistrationFormType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
 
+        $builder->add('captcha', CaptchaType::class);
+    }
 }
