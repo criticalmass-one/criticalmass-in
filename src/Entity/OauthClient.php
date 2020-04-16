9a2db5e72a372f528cfde6c99004401ca4a37224
Malte Hübner <maltehuebner@gmx.org>
2020-04-16 21:27:53 +0200

    Add oauth entities.

---
+++ src/Entity/OauthClient.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\OAuthServerBundle\Entity\Client as BaseClient;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="oauth_client")
+ */
+class OauthClient extends BaseClient
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
