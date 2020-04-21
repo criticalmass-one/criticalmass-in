6fab3bea0d546ba53bbc841850c09625fc0134f5
Malte Hübner <maltehuebner@gmx.org>
2020-04-21 02:25:17 +0200

    More template tweaks.

--- src/Entity/OauthClient.php
+++ src/Entity/OauthClient.php
@@ -18,8 +18,59 @@ class OauthClient extends BaseClient
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $url;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
     }
 }
