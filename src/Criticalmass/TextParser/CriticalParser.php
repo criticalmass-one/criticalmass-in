d0b54c98320d4f8ec3f588252c49448519bb98c7
Malte Hübner <maltehuebner@gmx.org>
2020-04-22 22:47:42 +0200

    Add autolink.

--- src/Criticalmass/TextParser/CriticalParser.php
+++ src/Criticalmass/TextParser/CriticalParser.php
@@ -5,26 +5,41 @@ namespace App\Criticalmass\TextParser;
 use App\Criticalmass\Embed\Embedder\EmbedderInterface;
 use Flagception\Manager\FeatureManagerInterface;
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\ConverterInterface;
+use League\CommonMark\Environment;
+use League\CommonMark\Extension\Autolink\AutolinkExtension;
 
 class CriticalParser implements TextParserInterface
 {
     protected EmbedderInterface $embedder;
     protected FeatureManagerInterface $featureManager;
+    protected ConverterInterface $converter;
 
     public function __construct(EmbedderInterface $embedder, FeatureManagerInterface $featureManager)
     {
         $this->embedder = $embedder;
         $this->featureManager = $featureManager;
+
+        $this->configure();
+    }
+
+    protected function configure(): void
+    {
+        $environment = Environment::createCommonMarkEnvironment();
+
+        $environment->addExtension(new AutolinkExtension());
+
+        $config = [
+            'html_input' => 'strip',
+            'allow_unsafe_links' => false,
+        ];
+
+        $this->converter = new CommonMarkConverter($config, $environment);
     }
 
     public function parse(string $text): string
     {
-        $converter = new CommonMarkConverter([
-            'html_input' => 'strip',
-            'allow_unsafe_links' => true,
-        ]);
-
-        $text = $converter->convertToHtml($text);
+        $text = $this->converter->convertToHtml($text);
 
         if ($this->featureManager->isActive('oembed')) {
             $text = $this->embedder->processEmbedsInText($text);
