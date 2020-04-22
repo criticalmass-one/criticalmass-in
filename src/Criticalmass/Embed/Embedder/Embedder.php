5e7126c5cca3440161731428bfa43778ad4e745f
Malte Hübner <maltehuebner@gmx.org>
2020-04-22 02:42:14 +0200

    Add LinkCache.

--- src/Criticalmass/Embed/Embedder/Embedder.php
+++ src/Criticalmass/Embed/Embedder/Embedder.php
@@ -2,6 +2,7 @@
 
 namespace App\Criticalmass\Embed\Embedder;
 
+use App\Criticalmass\Embed\LinkCache\LinkCacheInterface;
 use App\Criticalmass\Embed\LinkFinder\LinkFinderInterface;
 use Embed\Embed;
 use Embed\EmbedCode;
@@ -10,11 +11,13 @@ class Embedder implements EmbedderInterface
 {
     protected Embed $embed;
     protected LinkFinderInterface $linkFinder;
+    protected LinkCacheInterface $linkCache;
 
-    public function __construct(LinkFinderInterface $linkFinder)
+    public function __construct(LinkFinderInterface $linkFinder, LinkCacheInterface $linkCache)
     {
         $this->embed = new Embed();
         $this->linkFinder = $linkFinder;
+        $this->linkCache = $linkCache;
     }
 
     public function processEmbedsInText(string $text): string
@@ -22,12 +25,20 @@ class Embedder implements EmbedderInterface
         $links = $this->linkFinder->findInText($text);
 
         foreach ($links as $link) {
+            if ($this->linkCache->has($link)) {
+                return $this->linkCache->get($link);
+            }
+
             $info = $this->embed->get(trim($link));
 
             $code = $info->code;
 
             if ($code instanceof EmbedCode) {
-                $text = str_replace($link, $info->code->html, $text);
+                $embedCode = $info->code->html;
+
+                $this->linkCache->set($link, $embedCode);
+
+                $text = str_replace($link, $embedCode, $text);
             }
         }
 
