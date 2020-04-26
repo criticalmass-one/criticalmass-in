4e405003aa6f059a525d9d7992267e3821f0ac75
Malte Hübner <maltehuebner@gmx.org>
2020-04-26 11:44:27 +0200

    Adjust namespaces.

--- tests/SocialNetworkDetector/FacebookPageDetectorTest.php
+++ tests/SocialNetworkDetector/FacebookPageDetectorTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Criticalmass\SocialNetwork\Tests\SocialNetworkDetector;
+use App\Criticalmass\SocialNetwork\Tests\SocialNetworkDetector\AbstractNetworkDetectorTest;
 
 class FacebookPageDetectorTest extends AbstractNetworkDetectorTest
 {
