54b919381b137ee76da8e709bab2b7623995414e
Malte Hübner <maltehuebner@gmx.org>
2020-07-06 17:18:09 +0200

    Transform location routing.

--- src/Controller/Api/LocationController.php
+++ src/Controller/Api/LocationController.php
@@ -4,12 +4,13 @@ namespace App\Controller\Api;
 
 use App\Entity\City;
 use App\Entity\Location;
+use Doctrine\Persistence\ManagerRegistry;
 use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LocationController extends BaseController
 {
@@ -23,6 +24,7 @@ class LocationController extends BaseController
      *  }
      * )
      * @ParamConverter("city", class="App:City")
+     * @Route("/{citySlug}/location", name="caldera_criticalmass_rest_location_list", methods={"GET"}, options={"expose"=true})
      */
     public function listLocationAction(ManagerRegistry $registry, City $city): Response
     {
@@ -50,6 +52,7 @@ class LocationController extends BaseController
      *  }
      * )
      * @ParamConverter("location", class="App:Location")
+     * @Route("/{citySlug}/location/{locationSlug}", name="caldera_criticalmass_rest_location_show", methods={"GET"}, options={"expose"=true})
      */
     public function showLocationAction(Location $location): Response
     {
