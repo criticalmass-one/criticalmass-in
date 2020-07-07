bf0fdd1588484f44b7692ece2a908903e37ed712
Malte Hübner <maltehuebner@gmx.org>
2020-07-07 19:43:40 +0200

    Introduce EntityMerger.

---
+++ src/Criticalmass/EntityMerger/EntityMerger.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+namespace App\Criticalmass\EntityMerger;
+
+use Doctrine\Common\Annotations\Reader;
+use JMS\Serializer\Annotation\Expose;
+
+class EntityMerger implements EntityMergerInterface
+{
+    protected Reader $annotationReader;
+
+    public function __construct(Reader $annotationReader)
+    {
+        $this->annotationReader = $annotationReader;
+    }
+
+    public function merge(object $source, object $destination): object
+    {
+        $reflectionClass = new \ReflectionClass($source);
+
+        /** @var \ReflectionProperty $reflectionClass */
+        foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+            if ($this->isPropertyExposed($reflectionProperty)) {
+                $setMethodName = $this->generateSetMethodName($reflectionProperty);
+                $getMethodName = $this->generateGetMethodName($reflectionProperty, $reflectionClass);
+
+                $newValue = $source->$getMethodName();
+
+                if ($newValue) {
+                    $destination->$setMethodName($newValue);
+                }
+            }
+        }
+
+        return $destination;
+    }
+
+    protected function isPropertyExposed(\ReflectionProperty $reflectionProperty): bool
+    {
+        $propertyAnnotations = $this->annotationReader->getPropertyAnnotations($reflectionProperty);
+
+        foreach ($propertyAnnotations as $propertyAnnotation) {
+            if ($propertyAnnotation instanceof Expose) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function generateSetMethodName(\ReflectionProperty $reflectionProperty): string
+    {
+        return sprintf('set%s', ucfirst($reflectionProperty->getName()));
+    }
+
+    protected function generateGetMethodName(\ReflectionProperty $reflectionProperty, \ReflectionClass $reflectionClass): ?string
+    {
+        $getMethodPrefixes = ['get', 'has', 'is'];
+
+        foreach ($getMethodPrefixes as $getMethodPrefix) {
+            $getMethodName = sprintf('%s%s', $getMethodPrefix, ucfirst($reflectionProperty->getName()));
+
+            if ($reflectionClass->hasMethod($getMethodName)) {
+                return $getMethodName;
+            }
+        }
+
+        return null;
+    }
+}
