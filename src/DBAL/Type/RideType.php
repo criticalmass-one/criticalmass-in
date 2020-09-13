fe92c1681456e1b317548111c800df9ed4f78891
Malte Hübner <maltehuebner@gmx.org>
2020-09-13 13:28:38 +0200

    Add more properties to ride type.

--- src/DBAL/Type/RideType.php
+++ src/DBAL/Type/RideType.php
@@ -8,6 +8,10 @@ final class RideType extends AbstractEnumType
 {
     public const CRITICAL_MASS = 'CRITICAL_MASS';
     public const KIDICAL_MASS = 'KIDICAL_MASS';
+    public const NIGHT_RIDE = 'NIGHT_RIDE';
+    public const LUNCH_RIDE = 'LUNCH_RIDE';
+    public const DAWN_RIDE = 'DAWN_RIDE';
+    public const DUSK_RIDE = 'DUSK_RIDE';
     public const DEMONSTRATION = 'DEMONSTRATION';
     public const ALLEYCAT = 'ALLEYCAT';
     public const TOUR = 'TOUR';
@@ -16,6 +20,10 @@ final class RideType extends AbstractEnumType
     public static $choices = [
         self::CRITICAL_MASS => 'Critical Mass',
         self::KIDICAL_MASS => 'Kidical Mass',
+        self::NIGHT_RIDE => 'Nightride',
+        self::LUNCH_RIDE => 'Lunchride',
+        self::DAWN_RIDE => 'Dawn Ride',
+        self::DUSK_RIDE => 'Dusk Ride',
         self::DEMONSTRATION => 'Demonstration',
         self::ALLEYCAT => 'Alleycat',
         self::TOUR => 'Rund- oder Sternfahrt',
