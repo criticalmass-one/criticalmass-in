76d4e50e658cb36bcebdd1a0446e34f8effa6035
Malte Hübner <maltehuebner@gmx.org>
2020-09-17 22:08:24 +0200

    WIP for displaying maps.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,42 @@
 import '../scss/criticalmass.scss';
-import CookieNotice from '../js/modules/CookieNotice.js';
-import AutoMap from '../js/modules/map/AutoMap.js';
-import CalendarPage from '../js/modules/page/CalendarPage.js';
+import L from 'leaflet';
+import polylineEncoded from 'polyline-encoded';
 
 //window.bootstrap = bootstrap;
 require('bootstrap');
 
-window.CookieNotice = CookieNotice;
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelectorAll('.map').forEach(function (mapContainer) {
+        const map = new L.map(mapContainer.id);
+        const basemap = L.tileLayer('https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png', {
+            attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        });
+        basemap.addTo(map);
 
-window.AutoMap = AutoMap;
-window.CalendarPage = CalendarPage;
+        const mapCenterLatitude = mapContainer.dataset.mapCenterLatitude;
+        const mapCenterLongitude = mapContainer.dataset.mapCenterLongitude;
+        const mapZoomLevel = mapContainer.dataset.mapZoomlevel;
+
+        if (mapCenterLatitude && mapCenterLongitude && mapZoomLevel) {
+            const mapCenter = L.latLng(mapCenterLatitude, mapCenterLongitude);
+
+            const marker = L.marker(mapCenter);
+
+            map.setView(mapCenter, mapZoomLevel);
+
+            marker.addTo(map);
+        }
+
+        var polylineString = mapContainer.dataset.polyline;
+        var polylineColorString = mapContainer.dataset.polylineColor;
+
+        if (polylineString && polylineColorString) {
+            var polyline = L.Polyline.fromEncoded(polylineString, {color: polylineColorString});
+
+            polyline.addTo(map);
+
+            map.fitBounds(polyline.getBounds());
+        }
+    });
+});
 
