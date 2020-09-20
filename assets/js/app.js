bedcb0a00c412e38ad2f039bc0ac1d637f349103
Malte Hübner <maltehuebner@gmx.org>
2020-09-20 14:35:18 +0200

    Replace CalenderPage js.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,19 @@ import extraMarkers from 'leaflet-extra-markers';
 //window.bootstrap = bootstrap;
 require('bootstrap');
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
+    const calendarDayList = document.querySelectorAll('#calendar .day ul.ride-list');
+
+    calendarDayList.forEach(function(rideList) {
+        const outerHeight = getOuterHeight(rideList);
+        const scrollHeight = rideList.scrollHeight;
+
+        console.log(outerHeight, scrollHeight);
+        if (scrollHeight > outerHeight) {
+            rideList.classList.add('shadow');
+        }
+    });
+
     document.querySelectorAll('.map').forEach(function (mapContainer) {
         const map = new L.map(mapContainer.id);
         const basemap = L.tileLayer('https://tiles.caldera.cc/wikimedia-intl/{z}/{x}/{y}.png', {
@@ -131,3 +143,31 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
+/**
+ * @see https://blog.jiniworld.me/80#a03-2
+ */
+function getInnerHeight(el) {
+    var c = window.getComputedStyle(el);
+    var border = parseFloat(c.borderTopWidth) + parseFloat(c.borderBottomWidth);
+
+    return el.offsetHeight - border;
+}
+
+/**
+ * @see https://blog.jiniworld.me/80#a03-2
+ */
+function getOuterHeight(el, includeMargin) {
+    includeMargin = includeMargin || false;
+    var c = window.getComputedStyle(el);
+    var margin = parseFloat(c.marginTop) + parseFloat(c.marginBottom),
+        border = parseFloat(c.borderTopWidth) + parseFloat(c.borderBottomWidth);
+    var scrollBar = el.offsetHeight - el.clientHeight - border;
+    if(includeMargin) {
+        if(c.boxSizing == "border-box") {
+            return el.offsetHeight + margin;
+        } else {
+            return el.offsetHeight + margin - scrollBar;
+        }
+    }
+    return el.offsetHeight;
+}
