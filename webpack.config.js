846782fd3c8c6515a5ca349234ff8f1fb6735054
Malte Hübner <maltehuebner@gmx.org>
2021-06-06 17:28:12 +0200

    Autoprovide jQuery.

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
