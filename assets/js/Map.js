e4794d04b6f6b3ba313766df9acd5592c5c414f5
Malte Hübner <maltehuebner@gmx.org>
2021-06-07 01:01:32 +0200

    Use fetch.

--- assets/js/Map.js
+++ assets/js/Map.js
@@ -76,31 +76,25 @@ export default class Map {
         const that = this;
 
         if (rideApiQuery) {
-            const apiRequest = new XMLHttpRequest();
+            fetch(rideApiQuery).then((response) => {
+                return response.json();
+            }).then((rideList) => {
+                const layer = L.featureGroup();
 
-            apiRequest.onreadystatechange = function () {
-                if (apiRequest.readyState === 4) {
-                    if (apiRequest.status === 200) {
-                        const rideList = JSON.parse(apiRequest.responseText);
-                        const layer = L.featureGroup();
+                for (var i in rideList) {
+                    const ride = rideList[i];
+                    const rideLatLng = L.latLng(ride.latitude, ride.longitude);
 
-                        for (var i in rideList) {
-                            const ride = rideList[i];
-                            const rideLatLng = L.latLng(ride.latitude, ride.longitude);
+                    const marker = L.marker(rideLatLng, {icon: that.rideIcon});
 
-                            const marker = L.marker(rideLatLng, {icon: that.rideIcon});
-
-                            marker.addTo(layer);
-                        }
-
-                        layer.addTo(that.map);
-                        that.map.fitBounds(layer.getBounds());
-                    }
+                    marker.addTo(layer);
                 }
-            }
 
-            apiRequest.open('Get', rideApiQuery);
-            apiRequest.send();
+                layer.addTo(that.map);
+                that.map.fitBounds(layer.getBounds());
+            }).catch(function (err) {
+                console.warn(err);
+            });
         }
     }
 
@@ -110,30 +104,23 @@ export default class Map {
         const rideIdentifier = this.mapContainer.dataset.rideIdentifier;
 
         if (citySlug && rideIdentifier) {
-            const rideRequest = new XMLHttpRequest();
-
-            rideRequest.onreadystatechange = function () {
-                if (rideRequest.readyState === 4) {
-                    if (rideRequest.status === 200) {
-                        const ride = JSON.parse(rideRequest.responseText);
-
-                        const rideLatLng = L.latLng(ride.latitude, ride.longitude);
-
-                        const marker = L.marker(rideLatLng, {icon: that.rideIcon});
-
-                        that.map.setView(rideLatLng, 10);
-                        marker.addTo(that.map);
-                    }
-                }
-            }
-
             const rideUrl = Routing.generate('caldera_criticalmass_rest_ride_show', {
                 citySlug: citySlug,
                 rideIdentifier: rideIdentifier
             });
 
-            rideRequest.open('Get', rideUrl);
-            rideRequest.send();
+            fetch(rideUrl).then((response) => {
+                return response.json();
+            }).then((ride) => {
+                const rideLatLng = L.latLng(ride.latitude, ride.longitude);
+
+                const marker = L.marker(rideLatLng, {icon: that.rideIcon});
+
+                that.map.setView(rideLatLng, 10);
+                marker.addTo(that.map);
+            }).catch(function (err) {
+                console.warn(err);
+            });
         }
     }
 
@@ -143,42 +130,36 @@ export default class Map {
         const rideIdentifier = this.mapContainer.dataset.rideIdentifier;
 
         if (citySlug && rideIdentifier) {
-            const photoRequest = new XMLHttpRequest();
-
-            photoRequest.onreadystatechange = function () {
-                if (photoRequest.readyState === 4) {
-                    if (photoRequest.status === 200) {
-                        const photoList = JSON.parse(photoRequest.responseText);
-                        const photoLayer = L.markerClusterGroup({
-                            showCoverageOnHover: false,
-                            iconCreateFunction: function (cluster) {
-                                return that.photoIcon;
-                            }
-                        });
-
-                        for (const i in photoList) {
-                            const photo = photoList[i];
-
-                            if (photo.latitude && photo.longitude) {
-                                const photoLatLng = L.latLng(photo.latitude, photo.longitude);
-
-                                const marker = L.marker(photoLatLng);
-                                marker.addTo(photoLayer);
-                            }
-                        }
-
-                        photoLayer.addTo(that.map);
-                    }
-                }
-            }
-
             const photoUrl = Routing.generate('caldera_criticalmass_rest_photo_ridelist', {
                 citySlug: citySlug,
                 rideIdentifier: rideIdentifier
             });
 
-            photoRequest.open('Get', photoUrl);
-            photoRequest.send();
+            fetch(photoUrl).then((response) => {
+                return response.json();
+            }).then((photoList) => {
+                const photoLayer = L.markerClusterGroup({
+                    showCoverageOnHover: false,
+                    iconCreateFunction: function (cluster) {
+                        return that.photoIcon;
+                    }
+                });
+
+                for (const i in photoList) {
+                    const photo = photoList[i];
+
+                    if (photo.latitude && photo.longitude) {
+                        const photoLatLng = L.latLng(photo.latitude, photo.longitude);
+
+                        const marker = L.marker(photoLatLng);
+                        marker.addTo(photoLayer);
+                    }
+                }
+
+                photoLayer.addTo(that.map);
+            }).catch(function (err) {
+                console.warn(err);
+            });
         }
     }
 
@@ -188,37 +169,30 @@ export default class Map {
         const rideIdentifier = this.mapContainer.dataset.rideIdentifier;
 
         if (citySlug && rideIdentifier) {
-            const trackRequest = new XMLHttpRequest();
-
-            trackRequest.onreadystatechange = function () {
-                if (trackRequest.readyState === 4) {
-                    if (trackRequest.status === 200) {
-                        const trackList = JSON.parse(trackRequest.responseText);
-
-                        const trackLayer = L.featureGroup();
-
-                        for (const i in trackList) {
-                            const track = trackList[i];
-                            const polylineString = track.polylineString;
-                            const polylineColor = 'red';
-                            const polyline = L.Polyline.fromEncoded(polylineString, {color: polylineColor});
-
-                            polyline.addTo(trackLayer);
-                        }
-
-                        that.map.fitBounds(trackLayer.getBounds());
-                        trackLayer.addTo(that.map);
-                    }
-                }
-            }
-
             const trackUrl = Routing.generate('caldera_criticalmass_rest_track_ridelist', {
                 citySlug: citySlug,
                 rideIdentifier: rideIdentifier
             });
 
-            trackRequest.open('Get', trackUrl);
-            trackRequest.send();
+            fetch(trackUrl).then((response) => {
+                return response.json();
+            }).then((trackList) => {
+                const trackLayer = L.featureGroup();
+
+                for (const i in trackList) {
+                    const track = trackList[i];
+                    const polylineString = track.polylineString;
+                    const polylineColor = 'red';
+                    const polyline = L.Polyline.fromEncoded(polylineString, {color: polylineColor});
+
+                    polyline.addTo(trackLayer);
+                }
+
+                that.map.fitBounds(trackLayer.getBounds());
+                trackLayer.addTo(that.map);
+            }).catch(function (err) {
+                console.warn(err);
+            });
         }
     }
 
