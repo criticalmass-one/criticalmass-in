90c89d2fe8df44cfee13dc5ff3fac9b7b74b63a4
Malte Hübner <maltehuebner@gmx.org>
2021-06-08 16:40:38 +0200

    Remove old datetime picker js.

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,6 @@ import './scss/criticalmass.scss';
 //window.bootstrap = bootstrap;
 require('bootstrap');
 
-import DateTimePicker from './js/DateTimePicker';
 import GeocodingButton from './js/GeocodingButton';
 import Map from './js/Map';
 import DataTable from './js/DataTable';
@@ -15,4 +14,4 @@ import '@fortawesome/fontawesome-pro/js/fontawesome';
 import '@fortawesome/fontawesome-pro/js/solid';
 import '@fortawesome/fontawesome-pro/js/regular';
 
-export {DateTimePicker, GeocodingButton, Map, DataTable, Search};export {GeocodingButton, Map, DataTable, Search, HintModal};
+export {GeocodingButton, Map, DataTable, Search, HintModal};
