f3450cfe713b4e230dc94b5a6fafdbe239e65b61
Malte Hübner <maltehuebner@gmx.org>
2021-06-08 16:40:21 +0200

    Convert HintModal.

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,10 @@ import GeocodingButton from './js/GeocodingButton';
 import Map from './js/Map';
 import DataTable from './js/DataTable';
 import Search from './js/Search';
+import HintModal from './js/HintModal';
 
 import '@fortawesome/fontawesome-pro/js/fontawesome';
 import '@fortawesome/fontawesome-pro/js/solid';
 import '@fortawesome/fontawesome-pro/js/regular';
 
-export {DateTimePicker, GeocodingButton, Map, DataTable, Search};
+export {DateTimePicker, GeocodingButton, Map, DataTable, Search};export {GeocodingButton, Map, DataTable, Search, HintModal};
