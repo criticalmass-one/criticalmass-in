c7562bf52971c2634277a6a70bdcb37e64161bdb
Malte Hübner <maltehuebner@gmx.org>
2021-06-22 00:51:16 +0200

    More cool features for geocoding.

--- assets/js/GeocodingButton.js
+++ assets/js/GeocodingButton.js
@@ -7,15 +7,67 @@ export default class GeocodingButton {
         this.settings = {...defaults, ...options};
 
         geocodingButton.addEventListener('click', (event) => {
-            const country = 'Deutschland';//geocodingButton.dataset.geocodingCountry;
-            const state = 'Hamburg';//geocodingButton.dataset.geocodingState;
-            const city = 'Hamburg';//geocodingButton.dataset.geocodingCity;
+            const queryParts = [];
 
-            const inputSelector = geocodingButton.dataset.geocodingInputSelector;
+            let query = geocodingButton.dataset.geocodingQuery;
+            const geocodingQuerySelector = geocodingButton.dataset.geocodingQuerySelector;
 
-            const street = document.querySelector(inputSelector).value;
+            if (!query && geocodingQuerySelector) {
+                const geocodingQueryElement = document.querySelector(geocodingQuerySelector);
 
-            const nominatimUrl = 'https://nominatim.openstreetmap.org/search?limit=1&polygon_geojson=1&format=jsonv2&q=' + street + ',' + city + ',' + state + ',' + country;
+                if ('INPUT' === geocodingQueryElement.tagName) {
+                    query = geocodingQueryElement.value;
+                }
+
+                if ('SELECT' === geocodingQueryElement.tagName) {
+                    query = geocodingQueryElement.selectedOptions[0].text;
+                }
+            }
+
+            if (query) {
+                queryParts.push(query);
+            }
+
+            let querySuffix = geocodingButton.dataset.geocodingQuerySuffix;
+            const geocodingQuerySuffixSelector = geocodingButton.dataset.geocodingQuerySuffixSelector;
+
+            if (!querySuffix && geocodingQuerySuffixSelector) {
+                const geocodingQuerySuffixElement = document.querySelector(geocodingQuerySuffixSelector);
+
+                if ('INPUT' === geocodingQuerySuffixElement.tagName) {
+                    querySuffix = geocodingQuerySuffixElement.value;
+                }
+
+                if ('SELECT' === geocodingQuerySuffixElement.tagName) {
+                    querySuffix = geocodingQuerySuffixElement.selectedOptions[0].text;
+                }
+            }
+
+            if (querySuffix) {
+                queryParts.push(querySuffix);
+            }
+
+            let queryPart = geocodingButton.dataset.geocodingQueryPart;
+            const geocodingQueryPartSelector = geocodingButton.dataset.geocodingQueryPartSelector;
+
+            if (!queryPart && geocodingQueryPartSelector) {
+                const geocodingQueryPartElement = document.querySelector(geocodingQueryPartSelector);
+
+                if ('INPUT' === geocodingQueryPartElement.tagName) {
+                    queryPart = geocodingQueryPartElement.value;
+                }
+
+                if ('SELECT' === geocodingQueryPartElement.tagName) {
+                    queryPart = geocodingQueryPartElement.selectedOptions[0].text;
+                }
+            }
+
+            if (queryPart) {
+                queryParts.push(queryPart);
+            }
+
+            let nominatimUrl = 'https://nominatim.openstreetmap.org/search?limit=1&polygon_geojson=1&format=jsonv2&q=' + queryParts.join(',');
+
             console.log(nominatimUrl);
 
             fetch(nominatimUrl).then((response) => {
