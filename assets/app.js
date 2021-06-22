660bac8dddab002e831ed191866ac157b543dd5c
Malte Hübner <maltehuebner@gmx.org>
2021-06-22 10:39:01 +0200

    Convert file upload.

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 //import './styles/app.css';
 import './scss/criticalmass.scss';
+import 'dropzone/dist/dropzone.css';
 
 //window.bootstrap = bootstrap;
 require('bootstrap');
@@ -10,9 +11,10 @@ import DataTable from './js/DataTable';
 import Search from './js/Search';
 import HintModal from './js/HintModal';
 import RideDateChecker from './js/RideDateChecker';
+import PhotoUpload from './js/PhotoUpload';
 
 import '@fortawesome/fontawesome-pro/js/fontawesome';
 import '@fortawesome/fontawesome-pro/js/solid';
 import '@fortawesome/fontawesome-pro/js/regular';
 
-export {RideDateChecker, GeocodingButton, Map, DataTable, Search, HintModal};
+export {PhotoUpload, RideDateChecker, GeocodingButton, Map, DataTable, Search, HintModal};
