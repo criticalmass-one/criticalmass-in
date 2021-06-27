584c60995871c5efe962f9fb4b51621e253f5207
Malte Hübner <maltehuebner@gmx.org>
2021-06-27 02:18:50 +0200

    Fake UTC here.

--- src/Controller/Search/PrefetchController.php
+++ src/Controller/Search/PrefetchController.php
@@ -25,7 +25,7 @@ class PrefetchController extends AbstractController
                 'url' => $objectRouter->generate($ride),
                 'value' => $ride->getTitle(),
                 'meta' => [
-                    'dateTime' => $ride->getDateTime()->setTimezone($cityTimezone)->format('Y-m-d H:i:s'), // @todo fix timezone here
+                    'dateTime' => $ride->getDateTime()->setTimezone($cityTimezone)->format('Y-m-dTH:i:s'), // @todo fix timezone here
                     'location' => $ride->getLocation() ?? '',
                 ]
             ];
