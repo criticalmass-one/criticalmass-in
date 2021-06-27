74800a1fb532beaec1c1c2bed34d376dff212e81
Malte Hübner <maltehuebner@gmx.org>
2021-06-27 02:16:02 +0200

    Try to fix timezones in search prefetch.

--- src/Controller/Search/PrefetchController.php
+++ src/Controller/Search/PrefetchController.php
@@ -18,12 +18,14 @@ class PrefetchController extends AbstractController
 
         /** @var Ride $ride */
         foreach ($rides as $ride) {
+            $cityTimezone = new \DateTimeZone($ride->getCity()->getTimezone());
+
             $result[] = [
                 'type' => 'ride',
                 'url' => $objectRouter->generate($ride),
                 'value' => $ride->getTitle(),
                 'meta' => [
-                    'dateTime' => $ride->getDateTime()->format('Y-m-d\TH:i:s'),
+                    'dateTime' => $ride->getDateTime()->setTimezone($cityTimezone)->format('Y-m-d H:i:s'), // @todo fix timezone here
                     'location' => $ride->getLocation() ?? '',
                 ]
             ];
