ce82d6a2ebc8055d241c7bce88894eeb79ca8250
Malte Hübner <maltehuebner@gmx.org>
2024-08-06 11:59:24 +0200

    Remove UtilTrait.

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Traits\RepositoryTrait;
-use App\Traits\UtilTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbstractFrameworkController;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 abstract class AbstractController extends AbstractFrameworkController
 {
     use RepositoryTrait;
-    use UtilTrait;
 
     public function __construct(private readonly AuthorizationCheckerInterface $authorizationChecker)
     {
