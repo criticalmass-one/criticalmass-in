1ac2dda8be7f329d908025e94f0a2836a65c119c
Malte Hübner <maltehuebner@gmx.org>
2024-08-07 23:24:53 +0200

    Improve syntax.

--- src/Criticalmass/SeoPage/AbstractSeoPage.php
+++ src/Criticalmass/SeoPage/AbstractSeoPage.php
@@ -3,34 +3,18 @@
 namespace App\Criticalmass\SeoPage;
 
 use App\Criticalmass\Router\ObjectRouterInterface;
-use App\Criticalmass\Router\ObjectRouter;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Sonata\SeoBundle\Seo\SeoPageInterface as SonataSeoPageInterface;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 abstract class AbstractSeoPage implements SeoPageInterface
 {
-    /** @var SonataSeoPageInterface $sonataSeoPage*/
-    protected $sonataSeoPage;
-
-    /** @var UploaderHelper $uploaderHelper */
-    protected $uploaderHelper;
-
-    /** @var CacheManager $cacheManager */
-    protected $cacheManager;
-
-    /** @var ObjectRouter $objectRouter */
-    protected $objectRouter;
-
     public function __construct(
-        SonataSeoPageInterface $sonataSeoPage,
-        UploaderHelper $uploaderHelper,
-        CacheManager $cacheManager,
-        ObjectRouterInterface $objectRouter,
+        protected readonly SonataSeoPageInterface $sonataSeoPage,
+        protected readonly UploaderHelper $uploaderHelper,
+        protected readonly CacheManager $cacheManager,
+        protected readonly ObjectRouterInterface $objectRouter,
     ) {
-        $this->sonataSeoPage = $sonataSeoPage;
-        $this->uploaderHelper = $uploaderHelper;
-        $this->cacheManager = $cacheManager;
-        $this->objectRouter = $objectRouter;
+
     }
 }
