0778b068a29cd9473adc4077562732cfb1467e7d
Malte Hübner <maltehuebner@gmx.org>
2024-08-07 23:23:54 +0200

    Remove static map stuff from SeoPage.

--- src/Criticalmass/SeoPage/AbstractSeoPage.php
+++ src/Criticalmass/SeoPage/AbstractSeoPage.php
@@ -3,7 +3,6 @@
 namespace App\Criticalmass\SeoPage;
 
 use App\Criticalmass\Router\ObjectRouterInterface;
-use App\Criticalmass\StaticMap\UrlGenerator\UrlGeneratorInterface;
 use App\Criticalmass\Router\ObjectRouter;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Sonata\SeoBundle\Seo\SeoPageInterface as SonataSeoPageInterface;
@@ -23,20 +22,15 @@ abstract class AbstractSeoPage implements SeoPageInterface
     /** @var ObjectRouter $objectRouter */
     protected $objectRouter;
 
-    /** @var UrlGeneratorInterface $urlGenerator */
-    protected $urlGenerator;
-
     public function __construct(
         SonataSeoPageInterface $sonataSeoPage,
         UploaderHelper $uploaderHelper,
         CacheManager $cacheManager,
         ObjectRouterInterface $objectRouter,
-        UrlGeneratorInterface $urlGenerator
     ) {
         $this->sonataSeoPage = $sonataSeoPage;
         $this->uploaderHelper = $uploaderHelper;
         $this->cacheManager = $cacheManager;
         $this->objectRouter = $objectRouter;
-        $this->urlGenerator = $urlGenerator;
     }
 }
