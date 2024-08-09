46afac441e3824dd6560b98a759063fe232e8d17
Malte Hübner <maltehuebner@gmx.org>
2024-08-09 13:07:35 +0200

    Remove AuthorizationCheckerInterface.

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -5,13 +5,9 @@ namespace App\Controller;
 use App\Traits\RepositoryTrait;
 use App\Traits\UtilTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbstractFrameworkController;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 abstract class AbstractController extends AbstractFrameworkController
 {
     use RepositoryTrait;
     use UtilTrait;
-    public function __construct(private readonly AuthorizationCheckerInterface $authorizationChecker)
-    {
-    }
 }
