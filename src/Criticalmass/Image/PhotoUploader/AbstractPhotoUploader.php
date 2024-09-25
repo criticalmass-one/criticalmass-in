c0448551777ce97f2bbae6411ee7cdcf55b7fde9
Malte Hübner <maltehuebner@gmx.org>
2024-09-25 22:16:12 +0200

    Change flysystem.

--- src/Criticalmass/Image/PhotoUploader/AbstractPhotoUploader.php
+++ src/Criticalmass/Image/PhotoUploader/AbstractPhotoUploader.php
@@ -6,39 +6,30 @@ use App\Criticalmass\UploadFaker\UploadFakerInterface;
 use App\Entity\Ride;
 use App\Entity\Track;
 use App\Entity\User;
-use League\Flysystem\FilesystemInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use League\Flysystem\Filesystem;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 abstract class AbstractPhotoUploader implements PhotoUploaderInterface
 {
-    /** @var ManagerRegistry $doctrine */
-    protected $doctrine;
-
-    /** @var FilesystemInterface $filesystem */
-    protected $filesystem;
-
     /** @var User $user */
     protected $user;
 
     /** @var Ride $ride */
     protected $ride;
 
-    /** @var EventDispatcherInterface $eventDispatcher */
-    protected $eventDispatcher;
-
     /** @var array $addedPhotoList */
     protected $addedPhotoList = [];
 
-    /** @var UploadFakerInterface $uploadFaker */
-    protected $uploadFaker;
 
-    public function __construct(ManagerRegistry $doctrine, EventDispatcherInterface $eventDispatcher, FilesystemInterface $filesystem, UploadFakerInterface $uploadFaker)
+    public function __construct(
+        protected readonly ManagerRegistry $doctrine,
+        protected readonly EventDispatcherInterface $eventDispatcher,
+        protected readonly Filesystem $filesystem,
+        protected readonly UploadFakerInterface $uploadFaker
+    )
     {
-        $this->doctrine = $doctrine;
-        $this->filesystem = $filesystem;
-        $this->eventDispatcher = $eventDispatcher;
-        $this->uploadFaker = $uploadFaker;
+
     }
 
     public function setUser(User $user): PhotoUploaderInterface
