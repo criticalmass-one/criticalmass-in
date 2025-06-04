bcf16fa0a7b4efb67469ebb77d477d0894b85c5c
Malte Hübner <maltehuebner@gmx.org>
2025-06-04 19:12:22 +0200

    Remove gregwar captcha bundle as we use friendlycaptcha.

--- config/bundles.php
+++ config/bundles.php
@@ -28,7 +28,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     MalteHuebner\OrderedEntitiesBundle\MalteHuebnerOrderedEntitiesBundle::class => ['all' => true],
     MalteHuebner\ImpressBundle\MalteHuebnerImpressBundle::class => ['all' => true],
