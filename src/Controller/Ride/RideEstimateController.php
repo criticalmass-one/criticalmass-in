7bf3dba57f0249bca8eba0d1431e89409d9ece57
Malte Hübner <maltehuebner@gmx.org>
2025-11-04 00:03:08 +0100

    Convert RideEstimate.

--- src/Controller/Ride/RideEstimateController.php
+++ src/Controller/Ride/RideEstimateController.php
@@ -11,12 +11,18 @@ use App\Form\Type\RideEstimateType;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class RideEstimateController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
+    #[Route(
+        '/{citySlug}/{rideIdentifier}/addestimate',
+        name: 'caldera_criticalmass_ride_addestimate',
+        priority: 60
+    )]
     public function addestimateAction(
         Request $request,
         UserInterface $user = null,
@@ -46,6 +52,11 @@ class RideEstimateController extends AbstractController
         return $this->redirect($objectRouter->generate($ride));
     }
 
+    #[Route(
+        '/{citySlug}/{rideIdentifier}/anonymousestimate',
+        name: 'caldera_criticalmass_ride_addestimate_anonymous',
+        priority: 60
+    )]
     public function anonymousestimateAction(
         Request $request,
         UserInterface $user = null,
