713e720c079f2a02bba7546306fdd99bb259a646
Malte Hübner <maltehuebner@gmx.org>
2025-11-05 18:27:56 +0100

    Convert Track routing.

--- src/Controller/Track/TrackController.php
+++ src/Controller/Track/TrackController.php
@@ -3,30 +3,48 @@
 namespace App\Controller\Track;
 
 use App\Controller\AbstractController;
-use App\Entity\Track;
+use App\Criticalmass\Router\ObjectRouterInterface;
+use App\Criticalmass\Strava\Importer\TrackImporterInterface;
+use App\Criticalmass\Strava\Token\StravaTokenStorage;
+use App\Criticalmass\Util\DateTimeUtil;
+use App\Entity\Ride;
+use App\Event\Track\TrackUploadedEvent;
 use Doctrine\Persistence\ManagerRegistry;
+use Iamstuartwilson\StravaApi;
+use Strava\API\OAuth;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+// ↓ neu
+use Symfony\Component\Routing\Attribute\Route;
 
-class TrackController extends AbstractController
+class StravaController extends AbstractController
 {
-    #[IsGranted('view', 'track')]
-    public function viewAction(Track $track): Response
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        private readonly string $stravaClientId,
+        private readonly string $stravaSecret
+    )
     {
-        return $this->render('Track/view.html.twig', [
-            'track' => $track,
-        ]);
+        parent::__construct($managerRegistry);
     }
 
-    #[IsGranted('approve', 'track')]
-    public function approveAction(Track $track, ManagerRegistry $registry): Response
-    {
-        $track->setReviewed(true);
+    #[IsGranted('ROLE_USER')]
+    public function authAction(Request $request, Ride $ride, ObjectRouterInterface $objectRouter): Response { /* unverändert */ }
 
-        $registry->getManager()->flush();
+    #[IsGranted('ROLE_USER')]
+    public function tokenAction(Request $request, Ride $ride, ObjectRouterInterface $objectRouter, SessionInterface $session): Response { /* unverändert */ }
 
-        return $this->redirectToRoute('caldera_criticalmass_track_view', [
-            'id' => $track->getId(),
-        ]);
-    }
+    #[IsGranted('ROLE_USER')]
+    public function listridesAction(Ride $ride, SessionInterface $session): Response { /* unverändert */ }
+
+    #[IsGranted('ROLE_USER')]
+    public function importAction(Request $request, UserInterface $user, EventDispatcherInterface $eventDispatcher, ObjectRouterInterface $objectRouter, Ride $ride, TrackImporterInterface $trackImporter): Response { /* unverändert */ }
+
+    protected function initOauthForRide(Request $request, Ride $ride, ObjectRouterInterface $objectRouter): OAuth { /* unverändert */ }
+
+    protected function createApi(): StravaApi { /* unverändert */ }
 }
--- src/Controller/Track/TrackManagementController.php
+++ src/Controller/Track/TrackManagementController.php
@@ -1,66 +1,244 @@
 <?php declare(strict_types=1);
 
-namespace App\Controller\Track;
+namespace App\Controller\Ride;
 
-use App\Event\Track\TrackHiddenEvent;
-use App\Event\Track\TrackShownEvent;
-use App\Repository\TrackRepository;
-use Knp\Component\Pager\PaginatorInterface;
 use App\Controller\AbstractController;
-use App\Entity\Track;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use App\Criticalmass\Router\ObjectRouterInterface;
+use App\Entity\City;
+use App\Entity\Ride;
+use App\Form\Type\RideDisableType;
+use App\Form\Type\RideSocialPreviewType;
+use App\Form\Type\RideType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-class TrackManagementController extends AbstractController
+class RideManagementController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
-    public function listAction(
-        Request $request,
-        TrackRepository $trackRepository,
-        PaginatorInterface $paginator,
-        UserInterface $user = null
-    ): Response {
-        $query = $trackRepository->findByUserQuery($user, null, false);
+    #[Route('/{citySlug}/add-ride', name: 'caldera_criticalmass_ride_add', priority: 70)]
+    public function addAction(Request $request, UserInterface $user = null, EntityManagerInterface $entityManager, City $city, ObjectRouterInterface $objectRouter): Response
+    {
+        $ride = new Ride();
+        $ride
+            ->setCity($city)
+            ->setUser($user)
+            ->setLatitude($city->getLatitude())
+            ->setLongitude($city->getLongitude())
+        ;
 
-        $pagination = $paginator->paginate(
-            $query,
-            $request->query->getInt('page', 1),
-            12
-        );
+        $form = $this->createForm(RideType::class, $ride, [
+            'action' => $objectRouter->generate($city,'caldera_criticalmass_ride_add'),
+        ]);
 
-        return $this->render('Track/list.html.twig', [
-            'pagination' => $pagination,
+        if ($request->isMethod(Request::METHOD_POST)) {
+            return $this->addPostAction($request, $user, $ride, $entityManager, $objectRouter, $city, $form);
+        } else {
+            return $this->addGetAction($request, $user, $ride, $entityManager, $objectRouter, $city, $form);
+        }
+    }
+
+    protected function addGetAction(Request $request, UserInterface $user = null, Ride $ride, EntityManagerInterface $entityManager, ObjectRouterInterface $objectRouter, City $city, FormInterface $form): Response
+    {
+        return $this->render('RideManagement/edit.html.twig', [
+            'ride' => $ride,
+            'form' => $form->createView(),
+            'city' => $city,
+            'dateTime' => new \DateTime(),
         ]);
     }
 
-    #[IsGranted('edit', 'track')]
-    public function toggleAction(EventDispatcherInterface $eventDispatcher, Track $track): Response
-    {
-        $track->setEnabled(!$track->getEnabled());
+    protected function addPostAction(
+        Request $request,
+        UserInterface $user = null,
+        Ride $ride,
+        EntityManagerInterface $entityManager,
+        ObjectRouterInterface $objectRouter,
+        City $city,
+        FormInterface $form
+    ): Response {
+        $form->handleRequest($request);
 
-        $this->managerRegistry->getManager()->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ride = $form->getData();
 
-        if ($track->getEnabled()) {
-            $eventDispatcher->dispatch(new TrackShownEvent($track), TrackShownEvent::NAME);
-        } else {
-            $eventDispatcher->dispatch(new TrackHiddenEvent($track), TrackHiddenEvent::NAME);
+            $entityManager->persist($ride);
+            $entityManager->flush();
+
+            /* As we have created our new ride, we serve the user the new "edit ride form". Normally it would be enough
+            just to change the action url of the form, but we are far to stupid for this hack. */
+            $form = $this->createForm(RideType::class, $ride, [
+                'action' => $objectRouter->generate($ride, 'caldera_criticalmass_ride_edit'),
+            ]);
+
+            $request->getSession()->getFlashBag()->add('success', 'Deine Änderungen wurden gespeichert.');
+
+            return $this->redirect($objectRouter->generate($ride));
         }
 
-        return $this->redirectToRoute('caldera_criticalmass_track_list');
+        return $this->render('RideManagement/edit.html.twig', [
+            'ride' => $ride,
+            'form' => $form->createView(),
+            'city' => $city,
+            'dateTime' => new \DateTime(),
+        ]);
     }
 
-    #[IsGranted('edit', 'track')]
-    public function deleteAction(Track $track, EventDispatcherInterface $eventDispatcher): Response
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{citySlug}/{rideIdentifier}/edit', name: 'caldera_criticalmass_ride_edit', priority: 70)]
+    public function editAction(Request $request, UserInterface $user = null, Ride $ride, ObjectRouterInterface $objectRouter): Response
     {
-        $track->setDeleted(true);
+        $form = $this->createForm(RideType::class, $ride, [
+            'action' => $objectRouter->generate($ride, 'caldera_criticalmass_ride_edit'),
+        ]);
 
-        $this->managerRegistry->getManager()->flush();
+        if (Request::METHOD_POST == $request->getMethod()) {
+            return $this->editPostAction($request, $user, $ride, $ride->getCity(), $form, $objectRouter);
+        } else {
+            return $this->editGetAction($request, $user, $ride, $ride->getCity(), $form, $objectRouter);
+        }
+    }
 
-        $eventDispatcher->dispatch(new TrackDeletedEvent($track), TrackDeletedEvent::NAME);
+    protected function editGetAction(
+        Request $request,
+        UserInterface $user = null,
+        Ride $ride,
+        City $city,
+        FormInterface $form,
+        ObjectRouterInterface $objectRouter
+    ): Response {
+        return $this->render('RideManagement/edit.html.twig', [
+            'ride' => $ride,
+            'city' => $city,
+            'form' => $form->createView(),
+            'dateTime' => new \DateTime(),
+        ]);
+    }
 
-        return $this->redirectToRoute('caldera_criticalmass_track_list');
+    protected function editPostAction(
+        Request $request,
+        UserInterface $user = null,
+        Ride $ride,
+        City $city,
+        FormInterface $form,
+        ObjectRouterInterface $objectRouter
+    ): Response {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ride
+                ->setUpdatedAt(new \DateTime())
+                ->setUser($user);
+
+            if ($ride->isEnabled()) {
+                $ride->setDisabledReason(null);
+            }
+
+            $this->managerRegistry->getManager()->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'Deine Änderungen wurden gespeichert.');
+
+            return $this->redirect($objectRouter->generate($ride));
+        }
+
+        return $this->render('RideManagement/edit.html.twig', [
+            'ride' => $ride,
+            'city' => $city,
+            'form' => $form->createView(),
+            'dateTime' => new \DateTime(),
+        ]);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{citySlug}/{rideIdentifier}/socialpreview', name: 'caldera_criticalmass_ride_socialpreview', priority: 70)]
+    public function socialPreviewAction(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserInterface $user = null,
+        ObjectRouterInterface $objectRouter,
+        Ride $ride
+    ): Response {
+        $form = $this->createForm(RideSocialPreviewType::class, $ride, [
+            'action' => $objectRouter->generate($ride, 'caldera_criticalmass_ride_socialpreview'),
+        ]);
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            return $this->socialPreviewPostAction($entityManager, $request, $user, $ride, $form);
+        } else {
+            return $this->socialPreviewGetAction($entityManager, $request, $user, $ride, $form);
+        }
+    }
+
+    protected function socialPreviewGetAction(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserInterface $user = null,
+        Ride $ride,
+        Form $form
+    ): Response {
+        return $this->render('RideManagement/social_preview.html.twig', [
+            'ride' => $ride,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    protected function socialPreviewPostAction(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserInterface $user = null,
+        Ride $ride,
+        Form $form
+    ): Response {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ride
+                ->setUpdatedAt(new \DateTime())
+                ->setUser($user);
+
+            $entityManager->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'Änderungen gespeichert!');
+        }
+
+        return $this->socialPreviewGetAction($entityManager, $request, $user, $ride, $form);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{citySlug}/{rideIdentifier}/disable', name: 'caldera_criticalmass_ride_disable', priority: 70)]
+    public function disableAction(Request $request, ManagerRegistry $registry, UserInterface $user = null, Ride $ride, ObjectRouterInterface $objectRouter): RedirectResponse
+    {
+        if (Request::METHOD_POST === $request->getMethod()) {
+            $disableForm = $this->createForm(RideDisableType::class, $ride);
+            $disableForm->handleRequest($request);
+
+            if ($disableForm->isSubmitted() && $disableForm->isValid()) {
+                $ride->setEnabled(false);
+
+                $registry->getManager()->flush();
+            }
+        }
+
+        return $this->redirect($objectRouter->generate($ride));
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{citySlug}/{rideIdentifier}/enable', name: 'caldera_criticalmass_ride_enable', priority: 70)]
+    public function enableAction(ManagerRegistry $registry, UserInterface $user = null, Ride $ride, ObjectRouterInterface $objectRouter): RedirectResponse
+    {
+        $ride->setEnabled(true)
+            ->setDisabledReason(null)
+            ->setDisabledReasonMessage(null);
+
+        $registry->getManager()->flush();
+
+        return $this->redirect($objectRouter->generate($ride));
     }
 }
