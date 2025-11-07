5ec651cde4437a9108a9c5da270cf2f6a326ec47
Malte Hübner <maltehuebner@gmx.org>
2025-11-07 09:27:22 +0100

    Convert statistic routing.

--- src/Controller/Statistic/MonthlyStatsController.php
+++ src/Controller/Statistic/MonthlyStatsController.php
@@ -5,9 +5,15 @@ namespace App\Controller\Statistic;
 use App\Controller\AbstractController;
 use App\Repository\RideRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MonthlyStatsController extends AbstractController
 {
+    #[Route(
+        '/statistic/{year}/{month}',
+        name: 'caldera_criticalmass_statistic_ride_month',
+        priority: 140
+    )]
     public function listRidesAction(
         RideRepository $rideRepository,
         int $year = null,
