f4d9e6004cc3b72f6fba45dec492b67d94ea595c
Malte Hübner <maltehuebner@gmx.org>
2025-11-11 09:58:39 +0100

    Convert more xml.

--- src/DependencyInjection/AppExtension.php
+++ src/DependencyInjection/AppExtension.php
@@ -21,8 +21,5 @@ class AppExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $xmlLoader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $xmlLoader->load('services.xml');
     }
 }
