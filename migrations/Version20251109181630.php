c79bffd8f2005f355e0d40bb36b15bafcc0b4765
Malte Hübner <maltehuebner@gmx.org>
2025-12-02 15:05:59 +0100

    Fix migration.

--- migrations/Version20251109181630.php
+++ migrations/Version20251109181630.php
@@ -21,7 +21,7 @@ final class Version20251109181630 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE track ADD app VARCHAR(255) DEFAULT NULL, DROP previewPolyline, CHANGE username username VARCHAR(255) DEFAULT NULL, CHANGE creationDateTime creationDateTime DATETIME DEFAULT NULL, CHANGE enabled enabled TINYINT(1) DEFAULT NULL, CHANGE trackFilename trackFilename VARCHAR(255) DEFAULT NULL, CHANGE deleted deleted TINYINT(1) DEFAULT NULL
+            ALTER TABLE track ADD app VARCHAR(255) DEFAULT NULL
         SQL);
     }
 
@@ -29,7 +29,7 @@ final class Version20251109181630 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE track ADD previewPolyline LONGTEXT DEFAULT NULL, DROP app, CHANGE username username VARCHAR(255) NOT NULL, CHANGE creationDateTime creationDateTime DATETIME NOT NULL, CHANGE enabled enabled TINYINT(1) NOT NULL, CHANGE deleted deleted TINYINT(1) NOT NULL, CHANGE trackFilename trackFilename VARCHAR(255) NOT NULL
+            ALTER TABLE track ADD previewPolyline LONGTEXT DEFAULT NULL
         SQL);
     }
 }
