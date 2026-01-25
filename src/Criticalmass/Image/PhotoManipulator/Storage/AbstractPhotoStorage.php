be2bcd3eb0e7b26d1b96b97107b23439099b2483
Malte Hübner <maltehuebner@gmx.org>
2026-01-25 23:29:17 +0100

    More improvements for different classes.

--- src/Criticalmass/Image/PhotoManipulator/Storage/AbstractPhotoStorage.php
+++ src/Criticalmass/Image/PhotoManipulator/Storage/AbstractPhotoStorage.php
@@ -3,29 +3,17 @@
 namespace App\Criticalmass\Image\PhotoManipulator\Storage;
 
 use App\Criticalmass\Image\PhotoManipulator\Cache\PhotoCacheInterface;
-use League\Flysystem\FilesystemInterface;
+use League\Flysystem\FilesystemOperator;
 use Doctrine\Persistence\ManagerRegistry;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 abstract class AbstractPhotoStorage implements PhotoStorageInterface
 {
-    /** @var UploaderHelper $uploaderHelper */
-    protected $uploaderHelper;
-
-    /** @var PhotoCacheInterface $photoCache */
-    protected $photoCache;
-
-    /** @var FilesystemInterface $filesystem */
-    protected $filesystem;
-
-    /** @var ManagerRegistry $registry */
-    protected $registry;
-
-    public function __construct(UploaderHelper $uploaderHelper, PhotoCacheInterface $photoCache, ManagerRegistry $registry, FilesystemInterface $filesystem)
-    {
-        $this->uploaderHelper = $uploaderHelper;
-        $this->filesystem = $filesystem;
-        $this->photoCache = $photoCache;
-        $this->registry = $registry;
+    public function __construct(
+        protected UploaderHelper $uploaderHelper,
+        protected PhotoCacheInterface $photoCache,
+        protected ManagerRegistry $registry,
+        protected FilesystemOperator $filesystem
+    ) {
     }
 }
